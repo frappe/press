603d392ed2b23874ad85a0f0cea373b4cb10feba
Aditya Hase <aditya@adityahase.com>
2019-11-22 14:54:39 +0530

    chore: Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in press/__init__.py
+from press import __version__ as version
+
+setup(
+	name='press',
+	version=version,
+	description='Managed Frappe Hosting',
+	author='Frappe',
+	author_email='aditya@erpnext.com',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
