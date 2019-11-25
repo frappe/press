6a8b0625a0a0dbb3fea4ea62fd72185baed09861
Aditya Hase <aditya@adityahase.com>
2019-11-25 17:59:03 +0530

    feat: Add dashboard page

---
+++ press/public/js/DashboardRoot.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>Hello World!</h1>
+</template>
+
+<script>
+export default {
+	name: "DashboardRoot",
+};
+</script>
---
+++ press/www/dashboard.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+
+
+def get_context(context):
+	pass
