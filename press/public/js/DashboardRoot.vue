9d7ddb2ddde053ece1e91c535e45be388149fdf6
Aditya Hase <aditya@adityahase.com>
2019-12-11 16:01:52 +0530

    fix: Remove links to detail view

--- press/public/js/DashboardRoot.vue
+++ press/public/js/DashboardRoot.vue
@@ -2,7 +2,6 @@
 	<div>
 		<div>
 			<router-link :to="{ name: 'site-list' }">Sites</router-link>
-			<router-link :to="{ name: 'site-detail' }">Site</router-link>
 			<router-link :to="{ name: 'site-new' }">New Site</router-link>
 			<router-link :to="{ name: 'payment-list' }">Payments</router-link>
 		</div>
