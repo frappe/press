b4d3880b5f15743dff59ca9cd96a241d553eb622
Aditya Hase <aditya@adityahase.com>
2019-12-11 16:02:25 +0530

    fix: Add DocTypes for Payment Management

---
+++ press/api/payment.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+@frappe.whitelist()
+def all():
+	payments = frappe.get_all(
+		"Payment", fields=["name"], filters={"user": frappe.session.user}
+	)
+	return payments
