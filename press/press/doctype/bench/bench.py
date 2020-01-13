f0e3a8a6a38bc55b2c857bfeca2f940a45568f58
Aditya Hase <aditya@adityahase.com>
2020-01-13 17:09:08 +0530

    feat: Create Bench from Release Group and Deploy Candidate

--- press/press/doctype/bench/bench.py
+++ press/press/doctype/bench/bench.py
@@ -4,8 +4,16 @@
 
 from __future__ import unicode_literals
 
+import frappe
 from frappe.model.document import Document
 
 
 class Bench(Document):
-	pass
+	def validate(self):
+		if not self.candidate:
+			candidate = frappe.get_all("Deploy Candidate", filters={"group": self.group})[0]
+			self.candidate = candidate.name
+		candidate = frappe.get_doc("Deploy Candidate", self.candidate)
+		for release in candidate.apps:
+			scrubbed = frappe.get_value("Frappe App", release.app, "scrubbed")
+			self.append("apps", {"app": release.app, "scrubbed": scrubbed, "hash": release.hash})
