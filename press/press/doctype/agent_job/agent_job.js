4f4c43cc614d2a82cd11adbbf26ff51756f89a63
Aditya Hase <aditya@adityahase.com>
2020-01-24 13:26:35 +0530

    feat: Allow navigating to steps from Agent Job

--- press/press/doctype/agent_job/agent_job.js
+++ press/press/doctype/agent_job/agent_job.js
@@ -2,7 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Agent Job', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button(__('Steps'), () => {
+			const filters = {agent_job: frm.doc.name};
+			frappe.set_route("List", "Agent Job Step", filters);
+		});
+	}
 });
