2005796cc75aae7f39a0b5666df05ea21f87e8c4
Aditya Hase <aditya@adityahase.com>
2020-02-25 15:28:38 +0530

    fix: Add links to Uptime, Job and Request Logs

--- press/press/doctype/site/site.js
+++ press/press/doctype/site/site.js
@@ -13,14 +13,26 @@ frappe.ui.form.on('Site', {
 	},
 	refresh: function(frm) {
 		frm.add_web_link(`https://${frm.doc.name}`, __('Visit Site'));
-		frm.add_custom_button(__('Jobs'), () => {
+		frm.add_custom_button(__('Agent Jobs'), () => {
 			const filters = {site: frm.doc.name};
 			frappe.set_route("List", "Agent Job", filters);
-		});
+		}, __('Logs'));
 		frm.add_custom_button(__('Backups'), () => {
 			const filters = {site: frm.doc.name};
 			frappe.set_route("List", "Site Backup", filters);
-		});
+		}, __('Logs'));
+		frm.add_custom_button(__('Site Uptime'), () => {
+			const filters = {site: frm.doc.name};
+			frappe.set_route("List", "Site Uptime Log", filters);
+		}, __('Logs'));
+		frm.add_custom_button(__('Web Requests'), () => {
+			const filters = {site: frm.doc.name};
+			frappe.set_route("List", "Site Request Log", filters);
+		}, __('Logs'));
+		frm.add_custom_button(__('Background Jobs'), () => {
+			const filters = {site: frm.doc.name};
+			frappe.set_route("List", "Site Job Log", filters);
+		}, __('Logs'));
 		frm.add_custom_button(__('Backup'), () => {
 			frm.call({method: "perform_backup", doc: frm.doc, callback: result => frappe.msgprint(result.message)});
 		}, __('Actions'));
