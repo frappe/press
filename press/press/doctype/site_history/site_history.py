6b2ff3802c08acd1c2fc216bf2da25c1ecabc9df
Aditya Hase <aditya@adityahase.com>
2020-02-27 15:29:44 +0530

    style: Black

--- press/press/doctype/site_history/site_history.py
+++ press/press/doctype/site_history/site_history.py
@@ -6,9 +6,12 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class SiteHistory(Document):
 	pass
 
-def log_site_history(site, action):
-	return frappe.get_doc({"doctype": "Site History", "site": site, "action": action}).insert()
 
+def log_site_history(site, action):
+	return frappe.get_doc(
+		{"doctype": "Site History", "site": site, "action": action}
+	).insert()
