86859e62bb96d51118ddb53f2255d15b49298678
Aditya Hase <aditya@adityahase.com>
2020-02-27 15:07:43 +0530

    perf: Add indexes on analytics tables

--- press/press/doctype/site_job_log/site_job_log.py
+++ press/press/doctype/site_job_log/site_job_log.py
@@ -3,8 +3,14 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+
+import frappe
 from frappe.model.document import Document
+
 
 class SiteJobLog(Document):
 	pass
+
+
+def on_doctype_update():
+	frappe.db.add_index("Site Job Log", ["site", "timestamp"])
--- press/press/doctype/site_uptime_log/site_uptime_log.py
+++ press/press/doctype/site_uptime_log/site_uptime_log.py
@@ -3,8 +3,14 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+
+import frappe
 from frappe.model.document import Document
+
 
 class SiteUptimeLog(Document):
 	pass
+
+
+def on_doctype_update():
+	frappe.db.add_index("Site Uptime Log", ["site", "timestamp"])
