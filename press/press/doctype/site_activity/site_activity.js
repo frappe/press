f144be1b602576ee5b06cca2c0213e5ee5c89e98
Faris Ansari <netchamp.faris@gmail.com>
2020-02-28 19:17:12 +0530

    fix: Rename Site History to Site Activity

--- press/press/doctype/site_activity/site_activity.js
+++ press/press/doctype/site_activity/site_activity.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Site History', {
+frappe.ui.form.on('Site Activity', {
 	// refresh: function(frm) {
 
 	// }
