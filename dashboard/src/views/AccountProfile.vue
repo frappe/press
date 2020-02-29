7e62d7c8b9975c3ff6a17bd854270453c84e73b2
Faris Ansari <netchamp.faris@gmail.com>
2020-03-01 00:34:10 +0530

    feat: Change profile photo in Account

--- dashboard/src/views/AccountProfile.vue
+++ dashboard/src/views/AccountProfile.vue
@@ -4,7 +4,34 @@
 			<h2 class="font-medium text-lg">Profile</h2>
 			<p class="text-gray-600">Your profile information</p>
 			<div class="w-full sm:w-1/2 mt-6 text-sm">
-				<div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+				<div>
+					<span class="text-gray-800">Photo</span>
+					<div class="mt-2 flex items-center">
+						<Avatar
+							size="lg"
+							:label="account.user.first_name"
+							:imageURL="account.user.image"
+						/>
+						<input
+							ref="userImage"
+							type="file"
+							accept="image/*"
+							class="hidden"
+							@change="onUserImage"
+						/>
+						<Button
+							class="ml-4 border hover:bg-gray-100"
+							:class="uploading ? 'pointer-events-none' : ''"
+							@click="$refs.userImage.click()"
+						>
+							<span v-if="uploading">
+								Uploading {{ Math.floor((uploaded / total) * 100) }}%
+							</span>
+							<span v-else>Change</span>
+						</Button>
+					</div>
+				</div>
+				<div class="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
 					<label class="block">
 						<span class="text-gray-800">First Name</span>
 						<input
@@ -76,8 +103,51 @@
 </template>
 
 <script>
+import FileUploader from '@/store/fileUploader';
+
 export default {
 	name: 'AccountProfile',
-	props: ['account']
+	props: ['account'],
+	data() {
+		return {
+			uploader: null,
+			userImage: null,
+			uploading: false,
+			uploaded: 0,
+			total: 0
+		};
+	},
+	methods: {
+		onUserImage(e) {
+			let file = e.target.files[0];
+			this.uploadFile(file);
+		},
+		async uploadFile(file) {
+			this.uploader = new FileUploader();
+			this.uploader.on('start', () => {
+				this.uploading = true;
+			});
+			this.uploader.on('progress', data => {
+				this.uploaded = data.uploaded;
+				this.total = data.total;
+			});
+			this.uploader.on('error', () => {
+				this.uploading = false;
+			});
+			this.uploader.on('finish', () => {
+				this.uploading = false;
+			});
+			let fileDoc = await this.uploader.upload(file, {
+				doctype: 'User',
+				docname: this.$store.account.user.name
+			});
+			if (fileDoc.file_url) {
+				await this.$call('press.api.account.update_profile_picture', {
+					image_url: fileDoc.file_url
+				});
+				await this.$store.account.fetchAccount();
+			}
+		}
+	}
 };
 </script>
