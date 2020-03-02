6ed4f1474acef4750a932ec5eab675d5291d9e83
Aditya Hase <aditya@adityahase.com>
2020-03-02 11:52:29 +0530

    fix: Don't cache dashboard.py

--- press/www/dashboard.py
+++ press/www/dashboard.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import frappe
 
 base_template_path = "templates/www/dashboard.html"
-
+no_cache = 1
 
 def get_context(context):
 	csrf_token = frappe.sessions.get_csrf_token()
