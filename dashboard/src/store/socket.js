f30cb1199e9d13db68ec5e28d456723a686f3ac1
Aditya Hase <aditya@adityahase.com>
2020-03-02 11:51:54 +0530

    fix: socketio on https

--- dashboard/src/store/socket.js
+++ dashboard/src/store/socket.js
@@ -2,7 +2,8 @@ import io from 'socket.io-client';
 
 let host = window.location.hostname;
 let port = window.location.port ? ':9000' : '';
-let url = `http://${host}${port}`;
+let protocol = port ? 'https' : 'http';
+let url = `${protocol}://${host}${port}`;
 let socket = io(url);
 
 export default socket;
