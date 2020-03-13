968b32a575db1ca35ff9f89d49b3e657b776b41a
Aditya Hase <aditya@adityahase.com>
2020-03-13 11:52:45 +0530

    style: Black

--- press/www/index.py
+++ press/www/index.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 
+
 def get_context(context):
-	frappe.local.flags.redirect_location = '/dashboard/#/login'
+	frappe.local.flags.redirect_location = "/dashboard/#/login"
 	raise frappe.Redirect
