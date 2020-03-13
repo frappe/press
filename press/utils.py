c850b715da2edfe6c5f75a8ffe52378dee9f5693
Aditya Hase <aditya@adityahase.com>
2020-03-13 12:14:51 +0530

    fix: Use custom error logging logic

---
+++ press/utils.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+import json
+
+
+def log_error(title, **kwargs):
+	traceback = frappe.get_traceback()
+	serialized = json.dumps(kwargs, indent=4, sort_keys=True)
+	message = f"Data:\n{serialized}\nException:\n{traceback}"
+	frappe.log_error(title=title, message=message)
+	
