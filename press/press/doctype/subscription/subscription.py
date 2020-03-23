1b889b89cdaa7db5c66b43ebeab8008aa8508877
Saurabh <saurabh6790@gmail.com>
2020-03-23 15:29:12 +0530

    feat: setup subscription against team

--- press/press/doctype/subscription/subscription.py
+++ press/press/doctype/subscription/subscription.py
@@ -5,16 +5,74 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from datetime import datetime
 
 class Subscription(Document):
 	def validate(self):
-		if self.status == "Active":
+		if self.status == "Active" and self.is_new():
 			for subscription in frappe.get_all("Subscription",
 					{
 						"status": "Active",
-						"user_account": self.user_account,
+						"team": self.user_account,
 						"name": ["!=", self.name]
 					}
 				):
 
 				frappe.db.set_value("Subscription", subscription.name, 'status', 'Completed')
+
+class SubscriptionController(object):
+	def __init__(self, payer_name, email_id):
+		self.payer_name = payer_name
+		self.email_id = email_id
+		self.team = None
+		self.transaction_currency = 'USD'
+		self.subscription_item_id = None
+
+		self.subscription_details = {}
+
+	def setup(self):
+		self.set_team()
+		self.set_subscription_item_id()
+
+	def set_team(self):
+		self.team = frappe.db.get_value('Team Member', {'user': self.email_id}, 'parent')
+
+	def set_transaction_currency(self):
+		if self.team:
+			self.transaction_currency = frappe.db.get_value("Team", self.team, 'transaction_currency')
+
+	def get_subscription_item(self):
+		account_plan = frappe.db.get_value("Plan", {'is_default': 1, 'for_account': 1})
+		account_plan_doc = frappe.get_cached_doc("Plan", account_plan)
+		plan_id = account_plan_doc.get_plan_id(currency=self.transaction_currency)
+
+		return [
+			{
+				"plan": plan_id,
+			}
+		]
+
+	def set_subscription_item_id(self):
+		self.subscription_item_id = frappe.db.get_value("Subscription",
+			{
+				'user_account': self.email_id,
+				'status': "Active",
+			},
+			'subscription_item_id'
+		)
+
+	def setup_press_subscription_record(self):
+		current_period_end = self.subscription_details['current_period_end']
+		current_period_start = self.subscription_details['current_period_start']
+
+		subscription_doc = frappe.get_doc({
+			"doctype": "Subscription",
+			"team": self.team,
+			"subscription_id": self.subscription_details['id'],
+			"subscription_item_id": self.subscription_details['items']['data'][0]['id'], # charge usage against this id
+			"status": "Active",
+			"start_date": datetime.fromtimestamp(current_period_start).strftime('%Y-%m-%d'),
+			"end_date": datetime.fromtimestamp(current_period_end).strftime('%Y-%m-%d')
+		})
+
+		subscription_doc.save(ignore_permissions=True)
