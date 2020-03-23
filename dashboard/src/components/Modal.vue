5c4a72f47b4ab71683815e625f0e539e3e5e8d06
Faris Ansari <netchamp.faris@gmail.com>
2020-03-23 12:41:41 +0530

    fix: Hide Modal on escape

--- dashboard/src/components/Modal.vue
+++ dashboard/src/components/Modal.vue
@@ -4,7 +4,11 @@
 			v-show="open"
 			class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center"
 		>
-			<div v-show="open" class="fixed inset-0 transition-opacity" @click="open = false">
+			<div
+				v-show="open"
+				class="fixed inset-0 transition-opacity"
+				@click="open = false"
+			>
 				<div class="absolute inset-0 bg-gray-900 opacity-75"></div>
 			</div>
 
@@ -39,6 +43,17 @@ export default {
 				this.$emit('hide');
 			}
 		}
+	},
+	created() {
+		this.escapeListener = e => {
+			if (e.key === 'Escape') {
+				this.open = false;
+			}
+		};
+		document.addEventListener('keydown', this.escapeListener);
+	},
+	destroyed() {
+		document.removeEventListener('keydown', this.escapeListener);
 	},
 	data() {
 		return {
