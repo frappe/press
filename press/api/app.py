560be369f097ff5d76ca352b2ee3a8f74df3c8fe
Aditya Hase <aditya@adityahase.com>
2020-03-31 08:13:01 +0530

    feat: API for adding a new App

---
+++ press/api/app.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.utils.password import get_decrypted_password
+from press.press.doctype.agent_job.agent_job import job_detail
+
+
+@frappe.whitelist()
+def new(name):
+	app = frappe.get_doc({"doctype": "Frappe App", "name": name})
+	app.insert()
+	return app
+
+
+@frappe.whitelist()
+def all():
+	groups = frappe.get_all("Release Group", filters={"owner": frappe.session.user})
+	if groups:
+		group = groups[0]
+		group = frappe.get_doc("Release Group", group.name)
+		return group
+	else:
+		return []
