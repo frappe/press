135c4f78bd06bcbcd650e0f69992e2917577c839
Faris Ansari <netchamp.faris@gmail.com>
2020-03-31 16:31:10 +0530

    feat: Setup payment method in Billing section
    - Use Stripe elements for card input
    - Payment method is added to the Customer on Stripe

--- dashboard/src/views/AccountBilling.vue
+++ dashboard/src/views/AccountBilling.vue
@@ -1,5 +1,86 @@
 <template>
 	<div>
-		Billing
+		<section v-if="state.startsWith('ShowSetup')">
+			<h2 class="text-lg font-medium">Setup Payment Method</h2>
+			<p class="text-gray-600">
+				Add your card details to start your subscription
+			</p>
+			<div
+				class="w-full py-4 mt-6 border border-gray-100 rounded-md shadow sm:w-1/2"
+			>
+				<div class="px-6 py-4">
+					<Button
+						type="primary"
+						@click="state = 'ShowSetup.ShowStripeCard'"
+						v-if="state != 'ShowSetup.ShowStripeCard'"
+					>
+						Setup Payment Method
+					</Button>
+					<StripeCard
+						v-if="state === 'ShowSetup.ShowStripeCard'"
+						@complete="onCardAdd"
+					/>
+				</div>
+			</div>
+		</section>
+		<section v-if="state === 'ShowPaymentMethods'">
+			<h2 class="text-lg font-medium">Payment Methods</h2>
+			<p class="text-gray-600">
+				Cards you have added for automatic billing
+			</p>
+			<div
+				class="w-full py-4 mt-6 border border-gray-100 rounded-md shadow sm:w-1/2"
+			>
+				<div
+					class="grid items-center grid-cols-3 px-6 py-4 hover:bg-gray-50"
+					v-for="paymentMethod in paymentMethods"
+					:key="paymentMethod.id"
+				>
+					<div class="font-semibold">•••• {{ paymentMethod.card.last4 }}</div>
+					<div>
+						{{ paymentMethod.billing_details.name }}
+					</div>
+					<div>
+						{{ paymentMethod.card.exp_month }} /
+						{{ paymentMethod.card.exp_year }}
+					</div>
+				</div>
+			</div>
+		</section>
 	</div>
 </template>
+
+<script>
+import StripeCard from '@/components/StripeCard';
+export default {
+	name: 'AccountBilling',
+	components: {
+		StripeCard
+	},
+	data() {
+		return {
+			state: 'Idle',
+			paymentMethods: null
+		};
+	},
+	mounted() {
+		this.fetchPaymentMethods();
+	},
+	methods: {
+		async fetchPaymentMethods() {
+			this.paymentMethods = await this.$call(
+				'press.api.billing.get_payment_methods'
+			);
+			if (this.paymentMethods.length > 0) {
+				this.state = 'ShowPaymentMethods';
+			} else {
+				this.state = 'ShowSetup';
+			}
+		},
+		onCardAdd() {
+			this.state = 'Idle';
+			this.fetchPaymentMethods();
+		}
+	}
+};
+</script>
--- press/api/billing.py
+++ press/api/billing.py
@@ -6,9 +6,48 @@ import frappe
 import stripe
 
 
+@frappe.whitelist()
+def get_publishable_key_and_setup_intent():
+	team = frappe.get_request_header("X-Press-Team")
+	if not team:
+		frappe.throw("Invalid Team")
+
+	return {
+		"publishable_key": get_publishable_key(),
+		"setup_intent": get_setup_intent(team),
+	}
+
+
+@frappe.whitelist()
+def get_payment_methods():
+	team = frappe.get_request_header("X-Press-Team")
+	customer_id = frappe.db.get_value("Team", team, "stripe_customer_id")
+	stripe = get_stripe()
+	res = stripe.PaymentMethod.list(customer=customer_id, type="card")
+	return res["data"] or []
+
+
+@frappe.whitelist()
+def clear_setup_intent():
+	team = frappe.get_request_header("X-Press-Team")
+	frappe.cache().hdel("setup_intent", team)
+
+
 def get_publishable_key():
 	strip_account = frappe.db.get_single_value("Press Settings", "stripe_account")
 	return frappe.db.get_value("Stripe Settings", strip_account, "publishable_key")
+
+
+def get_setup_intent(team):
+	intent = frappe.cache().hget("setup_intent", team)
+	if not intent:
+		customer_id = frappe.db.get_value("Team", team, "stripe_customer_id")
+		stripe = get_stripe()
+		intent = stripe.SetupIntent.create(
+			customer=customer_id, payment_method_types=["card"]
+		)
+		frappe.cache().hset("setup_intent", team, intent)
+	return intent
 
 
 def get_stripe():
