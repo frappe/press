78ad924aec0a131aa4989efeb94f4dd2ef4adf5c
Aditya Hase <aditya@adityahase.com>
2020-03-31 11:46:47 +0530

    feat: Listen to GItHub Webhooks for repository updates

--- press/api/app.py
+++ press/api/app.py
@@ -3,7 +3,9 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import json
 import frappe
+from press.utils import log_error
 
 
 @frappe.whitelist()
@@ -22,3 +24,29 @@ def all():
 		return group
 	else:
 		return []
+
+
+@frappe.whitelist(allow_guest=True)
+def hook(*args, **kwargs):
+	event = frappe.request.headers.get("X-Github-Event")
+
+	if event != "push":
+		return
+
+	payload = frappe.request.get_data()
+	try:
+		payload = json.loads(payload)
+
+		url = payload["repository"]["html_url"]
+		branch = payload["ref"].rsplit("refs/heads/")[1]
+
+		app = frappe.get_all("Frappe App", {"url": url, "branch": branch})[0].name
+
+		hash = payload["after"]
+		if not frappe.db.exists("App Release", {"app": app, "hash": hash}):
+			frappe.get_doc({"doctype": "App Release", "app": app, "hash": hash}).insert(
+				ignore_permissions=True
+			)
+
+	except Exception:
+		log_error("GitHub Webhook Error", payload=payload)
