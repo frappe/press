2a9c26b8e3eb5f67208eaea7a9f112dd72fbc4df
Aditya Hase <aditya@adityahase.com>
2020-03-31 19:43:41 +0530

    fix: Syntax Error

--- press/press/doctype/tls_certificate/tls_certificate.js
+++ press/press/doctype/tls_certificate/tls_certificate.js
@@ -5,7 +5,7 @@ frappe.ui.form.on('TLS Certificate', {
 	refresh: function(frm) {
 		if (!frm.doc.fullchain) {
 			frm.add_custom_button(__('Obtain Certificate'), () => {
-				frm.call({method: "obtain_certificate", doc: frm.doc, callback: });
+				frm.call({method: "obtain_certificate", doc: frm.doc, callback: result => frm.refresh()});
 			});
 		}
 	}
