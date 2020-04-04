38f7ff7a943be8f8101f2573b8a1734569b4b96e
Faris Ansari <netchamp.faris@gmail.com>
2020-04-04 16:57:22 +0530

    fix: Remove unused files

--- press/www/subscription/index.py
+++
@@ -1,9 +0,0 @@
-import frappe
-from press.utils.stripe_controller import StripeController
-
-@frappe.whitelist(allow_guest=True)
-def setup_subscription(token):
-    sc = StripeController(payer_name='Test', email_id='saurabh6790@gmail.com', token=token)
-    sc.set_transaction_currency()
-    sc.create_customer()
-    sc.create_subscription()
