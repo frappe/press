e1b9a717d9523938146be1babec9fd7490a93a1e
Faris Ansari <netchamp.faris@gmail.com>
2020-04-05 23:38:22 +0530

    fix: Convert Cards to List

--- dashboard/src/views/Sites.vue
+++ dashboard/src/views/Sites.vue
@@ -3,51 +3,53 @@
 		<PageHeader>
 			<h1 slot="title">Sites</h1>
 			<div class="flex items-center" slot="actions">
-				<Button
-					v-if="$store.sites.all.length"
-					route="/sites/new"
-					type="primary"
-				>
-					<FeatherIcon name="plus" class="w-4 h-4" />
-					<span class="ml-1">
-						New Site
-					</span>
+				<Button route="/sites/new" type="primary" iconLeft="plus">
+					New Site
 				</Button>
-				<SearchBar class="hidden ml-4 sm:block" />
 			</div>
 		</PageHeader>
 		<div class="px-4 sm:px-8">
-			<div class="mb-5 border-t"></div>
-			<div
-				v-if="$store.sites.all.length"
-				class="grid grid-cols-1 gap-4 mb-16 sm:grid-cols-3"
-			>
-				<router-link
-					:to="`/sites/${site.name}`"
+			<div v-if="$store.sites.all.length">
+				<div
+					class="grid items-center grid-cols-4 gap-12 py-4 text-sm text-gray-700 border-b"
+				>
+					<span>Site Name</span>
+					<span class="text-center">Status</span>
+					<span class="text-right">Last Updated</span>
+					<span></span>
+				</div>
+				<a
+					class="grid items-center grid-cols-4 gap-12 py-4 text-sm border-b hover:bg-gray-50 focus:outline-none focus:shadow-outline"
 					v-for="site in $store.sites.all"
 					:key="site.name"
+					:href="'#/sites/' + site.name"
 				>
-					<div class="p-4 border rounded-md shadow hover:shadow-md">
-						<div class="flex items-baseline justify-between">
-							<span>
-								{{ site.name }}
-							</span>
-							<Badge :status="site.status">
-								{{ site.status }}
-							</Badge>
-						</div>
-						<div class="mt-2 text-sm text-gray-600">
-							Last updated
-							<FormatDate type="relative">{{ site.modified }}</FormatDate>
-						</div>
-					</div>
-				</router-link>
+					<span class="font-medium">{{ site.name }}</span>
+					<span class="text-center">
+						<Badge :status="site.status" />
+					</span>
+					<FormatDate class="text-right" type="relative">
+						{{ site.modified }}
+					</FormatDate>
+					<span class="text-right">
+						<a
+							v-if="site.status === 'Active'"
+							:href="`https://${site.name}`"
+							target="_blank"
+							class="inline-flex items-baseline text-sm text-blue-500 hover:underline"
+							>Visit Site<FeatherIcon
+								name="external-link"
+								class="w-3 h-3 ml-1"
+							/>
+						</a>
+					</span>
+				</a>
 			</div>
 			<div
 				class="p-24 text-center"
 				v-if="$store.sites.fetched && $store.sites.all.length === 0"
 			>
-				<div class="text-gray-800">
+				<div class="text-xl text-gray-800">
 					You haven't created any sites yet.
 				</div>
 				<Button route="/sites/new" class="mt-10" type="primary">
