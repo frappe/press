7a7e703f821985ab09f5d256559506d346e3c4a3
Faris Ansari <netchamp.faris@gmail.com>
2020-04-05 23:35:19 +0530

    feat: Notification component
    - Use with this.$notify

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,24 +1,64 @@
 <template>
-    <div class="font-sans text-gray-900 antialiased">
-        <div class="flex h-screen overflow-hidden">
-            <Sidebar class="w-64 flex-shrink-0 hidden sm:block" v-if="$store.auth.isLoggedIn" />
-            <div class="flex flex-1 overflow-y-auto" :class="{'sm:bg-gray-100': $route.meta.isLoginPage}">
-                <div class="flex-1 container mx-auto">
-                    <router-view />
-                </div>
-            </div>
-        </div>
-        <portal-target name="popovers" multiple></portal-target>
-        <portal-target name="modals" multiple></portal-target>
-    </div>
+	<div class="font-sans antialiased text-gray-900">
+		<div class="flex h-screen overflow-hidden">
+			<Sidebar
+				class="flex-shrink-0 hidden w-64 sm:block"
+				v-if="$store.auth.isLoggedIn"
+			/>
+			<div
+				class="flex flex-1 overflow-y-auto"
+				:class="{ 'sm:bg-gray-100': $route.meta.isLoginPage }"
+			>
+				<div class="container flex-1 mx-auto">
+					<router-view />
+				</div>
+			</div>
+		</div>
+		<portal-target name="popovers" multiple></portal-target>
+		<portal-target name="modals" multiple></portal-target>
+		<div
+			class="fixed inset-0 flex items-start justify-end px-4 py-6 pointer-events-none sm:p-6"
+		>
+			<div>
+				<Notification
+					class="mb-4"
+					:key="i"
+					v-for="(props, i) in notifications"
+					v-bind="props"
+					@dismiss="hideNotification"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
-import Sidebar from "@/components/Sidebar";
+import Vue from 'vue';
+import Sidebar from '@/components/Sidebar';
+import Notification from '@/components/Notification';
 export default {
-    name: 'App',
-    components: {
-        Sidebar
-    }
+	name: 'App',
+	components: {
+		Sidebar,
+		Notification
+	},
+	data() {
+		return {
+			notifications: []
+		};
+	},
+	created() {
+		Vue.prototype.$notify = this.notify;
+	},
+	methods: {
+		notify(props) {
+			props.id = Math.floor(Math.random() * 1000 + Date.now());
+			this.notifications.push(props);
+			setTimeout(() => this.hideNotification(props.id), props.timeout || 5000);
+		},
+		hideNotification(id) {
+			this.notifications = this.notifications.filter(props => props.id !== id);
+		}
+	}
 };
 </script>
-<style src="./assets/style.css">
+<style src="./assets/style.css"></style>
---
+++ dashboard/src/components/Notification.vue
@@ -0,0 +1,48 @@
+<template>
+	<div
+		class="w-full max-w-sm bg-white rounded-lg shadow-lg pointer-events-auto"
+	>
+		<div class="overflow-hidden rounded-lg shadow-xs">
+			<div class="p-4">
+				<div class="flex items-start">
+					<div class="flex-shrink-0" v-if="icon">
+						<div class="w-6 h-6 p-1 mr-3 bg-green-100 rounded-full">
+							<FeatherIcon :name="icon" :class="iconClass" class="w-4 h-4" />
+						</div>
+					</div>
+					<div class="flex-1">
+						<p class="text-sm font-medium leading-5 text-gray-900">
+							{{ title }}
+						</p>
+						<p class="mt-1 text-sm leading-5 text-gray-700">
+							{{ message }}
+						</p>
+					</div>
+					<div class="flex-shrink-0 ml-3">
+						<button @click="$emit('dismiss', id)" class="focus:outline-none">
+							<FeatherIcon name="x" class="w-5 h-5 text-gray-800" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Notification',
+	props: ['id', 'title', 'message', 'color', 'icon'],
+	computed: {
+		iconClass() {
+			return {
+				red: 'text-red-500',
+				green: 'text-green-500',
+				orange: 'text-orange-500',
+				blue: 'text-blue-500',
+				gray: 'text-gray-500'
+			}[this.color || 'gray'];
+		}
+	}
+};
+</script>
