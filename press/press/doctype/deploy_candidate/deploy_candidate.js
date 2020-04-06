71e7aaa62793f1df706acb82baca3885d6386942
Aditya Hase <aditya@adityahase.com>
2020-04-06 15:40:09 +0530

    feat: Add links

--- press/press/doctype/deploy_candidate/deploy_candidate.js
+++ press/press/doctype/deploy_candidate/deploy_candidate.js
@@ -2,7 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Deploy Candidate', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button(__('Benches'), () => {
+			const filters = {candidate: frm.doc.name};
+			frappe.set_route("List", "Bench", filters);
+		});
+	}
 });
