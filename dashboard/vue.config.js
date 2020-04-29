5cec1b4a245ec54aa1d91f7daabf5ce3509e8d5d
Aditya Hase <aditya@adityahase.com>
2020-04-29 12:03:34 +0530

    fix: Remove vue.js favicon

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -40,5 +40,14 @@ module.exports = {
 				}
 			}
 		}
+	},
+	pwa: {
+		iconPaths: {
+			favicon32: 'img/icons/favicon.png',
+			favicon16: 'img/icons/favicon.png',
+			appleTouchIcon: 'img/icons/favicon.png',
+			maskIcon: 'img/icons/favicon.png',
+			msTileImage: 'img/icons/favicon.png'
+		}
 	}
 };
