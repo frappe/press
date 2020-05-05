b943ab2f72701b6beae37367827b3cefe3479d5b
Faris Ansari <netchamp.faris@gmail.com>
2020-05-05 22:26:52 +0530

    fix: Reload doc in patch

--- press/patches/v0_0_1/create_site_plan_change_log.py
+++ press/patches/v0_0_1/create_site_plan_change_log.py
@@ -6,5 +6,6 @@ import frappe
 
 
 def execute():
+	frappe.reload_doc("press", "doctype", "site_plan_change")
 	for site in frappe.db.get_all("Site", {"status": "Active"}, ["name"]):
 		frappe.get_doc("Site", site.name)._create_initial_site_plan_change()
