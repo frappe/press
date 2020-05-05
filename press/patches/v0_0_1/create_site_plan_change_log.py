f88944002a47e3d338b759a0569f435340433557
Faris Ansari <netchamp.faris@gmail.com>
2020-05-05 21:59:01 +0530

    fix: Patch to create Site Plan Change for initial plan

---
+++ press/patches/v0_0_1/create_site_plan_change_log.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+	for site in frappe.db.get_all("Site", {"status": "Active"}, ["name"]):
+		frappe.get_doc("Site", site.name)._create_initial_site_plan_change()
