bd65219ffdd2976a2108ca26baa96863c2d90bf1
Faris Ansari <netchamp.faris@gmail.com>
2020-05-06 06:55:48 +0530

    chore: Use tailwind's purge option

--- dashboard/postcss.config.js
+++ dashboard/postcss.config.js
@@ -1,13 +1,3 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-	content: ['./public/index.html', './src/**/*.html', './src/**/*.vue'],
-    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
-    whitelistPatternsChildren: [/chart-container$/, /graph-svg-tip$/]
-});
-
 module.exports = {
-	plugins: [
-		require('tailwindcss'),
-		require('autoprefixer'),
-		process.env.NODE_ENV === 'production' ? purgecss : null
-	].filter(Boolean)
+	plugins: [require('tailwindcss'), require('autoprefixer')]
 };
