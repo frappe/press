d93430987999c5da80b0b16edff039f0e7b6d960
Faris Ansari <netchamp.faris@gmail.com>
2020-05-13 17:08:44 +0530

    fix: Button loading and loadingText

--- dashboard/src/components/global/Button.vue
+++ dashboard/src/components/global/Button.vue
@@ -2,7 +2,7 @@
 	<button
 		class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium leading-5 border rounded-md focus:outline-none "
 		:class="{
-			'opacity-50 cursor-not-allowed pointer-events-none': disabled,
+			'opacity-50 cursor-not-allowed pointer-events-none': isDisabled,
 			'bg-blue-500 border-transparent hover:bg-blue-400 text-white focus:shadow-outline-blue focus:border-blue-700':
 				type === 'primary',
 			'bg-white border-gray-400 hover:text-gray-800 text-gray-900 focus:shadow-outline-blue focus:border-blue-300':
@@ -13,10 +13,15 @@
 		@click="route && $router.push(route)"
 		v-bind="$attrs"
 		v-on="$listeners"
-		:disabled="disabled"
+		:disabled="isDisabled"
 	>
 		<FeatherIcon v-if="iconLeft" :name="iconLeft" class="w-4 h-4 mr-2" />
-		<slot></slot>
+		<template v-if="loading && loadingText">
+			{{ loadingText }}
+		</template>
+		<template v-else>
+			<slot></slot>
+		</template>
 		<FeatherIcon v-if="iconRight" :name="iconRight" class="w-4 h-4 ml-2" />
 	</button>
 </template>
@@ -45,6 +50,19 @@ export default {
 		iconRight: {
 			type: String,
 			default: null
+		},
+		loading: {
+			type: Boolean,
+			default: false
+		},
+		loadingText: {
+			type: String,
+			default: null
+		}
+	},
+	computed: {
+		isDisabled() {
+			return this.disabled || this.loading;
 		}
 	}
 };
