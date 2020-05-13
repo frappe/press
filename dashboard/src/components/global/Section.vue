c1186f94ed1186f4537ed369a194c41adf5c0376
Faris Ansari <netchamp.faris@gmail.com>
2020-05-13 07:19:19 +0530

    fix: Section and SectionCard components

---
+++ dashboard/src/components/global/Section.vue
@@ -0,0 +1,18 @@
+<template>
+	<section>
+		<h2 class="text-lg font-medium">{{ title }}</h2>
+		<p class="text-gray-600" v-if="description">
+			{{ description }}
+		</p>
+		<div class="mt-6">
+			<slot />
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'Section',
+	props: ['title', 'description']
+};
+</script>
---
+++ dashboard/src/components/global/SectionCard.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="w-full py-2 border border-gray-100 rounded shadow sm:w-1/2">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SectionCard'
+};
+</script>
