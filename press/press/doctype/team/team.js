97d1675b6ea132d7315f6e83c14cb419c80064db
Faris Ansari <netchamp.faris@gmail.com>
2020-05-14 20:35:07 +0530

    fix: Minor
    - Add fields in standard filter
    - Add navigation buttons in Team form

--- press/press/doctype/team/team.js
+++ press/press/doctype/team/team.js
@@ -2,7 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Team', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		frm.add_custom_button(
+			'Sites',
+			() => frappe.set_route('List', 'Site', { team: frm.doc.name }),
+			'View'
+		);
+		frm.add_custom_button(
+			'Payments',
+			() => frappe.set_route('List', 'Payment', { team: frm.doc.name }),
+			'View'
+		);
+		frm.add_custom_button(
+			'Payment Ledger Entries',
+			() => frappe.set_route('List', 'Payment Ledger Entry', { team: frm.doc.name }),
+			'View'
+		);
+	},
 });
