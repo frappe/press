449e12b96fcff016761b540127e2c97ca1eefa55
Faris Ansari <netchamp.faris@gmail.com>
2020-05-20 21:31:20 +0530

    fix: Get Address Details in Stripe Card
    - Create Stripe Customer on signup
    - Get address details in Billing Information
    - Create Address and link it to Card and Team
    - When card is added, create subscription and allocate free credits

--- press/press/doctype/stripe_payment_method/stripe_payment_method.py
+++ press/press/doctype/stripe_payment_method/stripe_payment_method.py
@@ -7,9 +7,30 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from press.api.billing import get_stripe
+from frappe.contacts.address_and_contact import load_address_and_contact
 
 
 class StripePaymentMethod(Document):
+	def onload(self):
+		load_address_and_contact(self)
+
+	def after_insert(self):
+		country = frappe.db.get_value("Country", {"code": self.address_country.lower()})
+		address = frappe.get_doc(
+			doctype="Address",
+			address_title=self.team,
+			address_line1=self.address_line1,
+			city=self.address_city,
+			state=self.address_state,
+			pincode=self.address_postal_code,
+			country=country,
+			links=[
+				{"link_doctype": self.doctype, "link_name": self.name, "link_title": self.team},
+				{"link_doctype": "Team", "link_name": self.team, "link_title": self.team},
+			],
+		)
+		address.insert()
+
 	def set_default(self):
 		stripe = get_stripe()
 		# set default payment method on stripe
