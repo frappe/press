594fb6d2b5bdc3f447d5c048eeb245879911ef30
Aditya Hase <aditya@adityahase.com>
2020-05-21 15:06:21 +0530

    fix: Patch for updating proxy status of inactive and suspended sites

---
+++ press/patches/v0_0_1/update_proxy_for_suspended_and_inactive_sites.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+	sites = frappe.get_all(
+		"Site",
+		fields=["name", "status"],
+		filters={"status": ("in", ("Suspended", "Inactive"))},
+	)
+	for site in sites:
+		site = frappe.get_doc("Site", site.name)
+		proxy_status = {"Suspended": "suspended", "Inactive": "deactivated"}
+		site.update_site_status_on_proxy(proxy_status[site.status])
