858a59855b8d8950d0924c5b07ac6ed9bf28bdeb
Aditya Hase <aditya@adityahase.com>
2020-05-29 18:16:36 +0530

    feat: Listen to GitHub webhooks

---
+++ press/press/doctype/github_webhook_log/github_webhook_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('GitHub Webhook Log', {
+	// refresh: function(frm) {
+
+	// }
+});
