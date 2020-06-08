af2a1f449463667d0b8f852893a935e69e1939b5
Aditya Hase <aditya@adityahase.com>
2020-06-08 11:29:28 +0530

    feat: App Release approval

--- press/press/doctype/app_release/app_release.js
+++ press/press/doctype/app_release/app_release.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('App Release', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		[
+			[__('Request Approal'), 'request_approval'],
+			[__('Approve'), 'approve'],
+		].forEach(([label, method]) => {
+			frm.add_custom_button(
+				label,
+				() => {
+					frm.call(method).then((r) => frappe.msgprint(r.message));
+				},
+				__('Actions')
+			);
+		});
+	},
 });
