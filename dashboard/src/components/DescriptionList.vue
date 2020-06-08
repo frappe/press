68ab4dc8b80adcde26494fe56873c4fea8fbab6c
Faris Ansari <netchamp.faris@gmail.com>
2020-06-08 07:53:20 +0530

    feat: DescriptionList component

---
+++ dashboard/src/components/DescriptionList.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="space-y-4">
+		<div v-for="item in items" :key="item.label">
+			<div class="text-sm text-gray-700">{{ item.label }}</div>
+			<div class="px-3 py-1 mt-1 text-base text-gray-900 bg-gray-100 rounded">
+				{{ item.value }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'DescriptionList',
+	props: ['items']
+};
+</script>
