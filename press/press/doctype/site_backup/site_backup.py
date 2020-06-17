a1a3cc4cf9dbf05202e0a6ab4b2f8810ee36b894
Gavin D'souza <gavin18d@gmail.com>
2020-06-17 18:20:48 +0530

    fix: Update Job statuses from agent

--- press/press/doctype/site_backup/site_backup.py
+++ press/press/doctype/site_backup/site_backup.py
@@ -13,7 +13,7 @@ class SiteBackup(Document):
 	def after_insert(self):
 		site = frappe.get_doc("Site", self.site)
 		agent = Agent(site.server)
-		job = agent.backup_site(site, self.with_files)
+		job = agent.backup_site(site, self.with_files, self.offsite)
 		frappe.db.set_value("Site Backup", self.name, "job", job.name)
 
 
@@ -24,26 +24,27 @@ def process_backup_site_job_update(job):
 	if job.status != backup.status:
 		frappe.db.set_value("Site Backup", backup.name, "status", job.status)
 		if job.status == "Success":
-			data = json.loads(job.data)
+			backup_data, offsite_backup_data = json.loads(job.data)
 			frappe.db.set_value(
 				"Site Backup",
 				backup.name,
 				{
-					"database_size": data["database"]["size"],
-					"database_url": data["database"]["url"],
-					"database_file": data["database"]["file"],
+					"database_size": backup_data["database"]["size"],
+					"database_url": backup_data["database"]["url"],
+					"database_file": backup_data["database"]["file"],
+					"offsite_backup": json.dumps(offsite_backup_data, indent=4)
 				},
 			)
-			if "private" in data and "public" in data:
+			if "private" in backup_data and "public" in backup_data:
 				frappe.db.set_value(
 					"Site Backup",
 					backup.name,
 					{
-						"private_size": data["private"]["size"],
-						"private_url": data["private"]["url"],
-						"private_file": data["private"]["file"],
-						"public_size": data["public"]["size"],
-						"public_url": data["public"]["url"],
-						"public_file": data["public"]["file"],
+						"private_size": backup_data["private"]["size"],
+						"private_url": backup_data["private"]["url"],
+						"private_file": backup_data["private"]["file"],
+						"public_size": backup_data["public"]["size"],
+						"public_url": backup_data["public"]["url"],
+						"public_file": backup_data["public"]["file"],
 					},
 				)
