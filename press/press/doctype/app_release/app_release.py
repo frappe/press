e4fcfd51a9ef51e03f6f3934ffa19fedaa26b23c
Aditya Hase <aditya@adityahase.com>
2020-06-22 14:45:18 +0530

    fix: Typo

--- press/press/doctype/app_release/app_release.py
+++ press/press/doctype/app_release/app_release.py
@@ -115,7 +115,7 @@ class AppRelease(Document):
 			token = get_access_token(app.installation)
 			url = f"https://x-access-token:{token}@github.com/{app.repo_owner}/{app.repo}"
 		else:
-			url = app.repo_url
+			url = app.url
 		self.output = ""
 		self.output += self.run("git init")
 		self.output += self.run(f"git remote add origin {url}",)
