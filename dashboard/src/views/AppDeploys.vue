11a3817f8b7463000c5f234f69fcab984cf3eb6a
Aditya Hase <aditya@adityahase.com>
2020-06-22 10:00:24 +0530

    fix: Show status of deploys in deploys tab

--- dashboard/src/views/AppDeploys.vue
+++ dashboard/src/views/AppDeploys.vue
@@ -2,38 +2,56 @@
 	<div>
 		<Section title="Deploys" description="Deploy log of your app">
 			<SectionCard class="md:w-2/3">
-				<div v-if="deploys.data && deploys.data.length">
-					<div
-						class="text-base items-center px-6 py-3 hover:bg-gray-50"
-						v-for="deploy in deploys.data"
-						:key="deploy.name"
-					>
-						<div class="flex justify-between items-center">
-							<div class="pr-2 font-mono">
-								{{ deploy.hash.slice(0, 6) }}
+				<div v-if="deploys.loading == false">
+					<div v-if="deploys.data.releases && deploys.data.releases.length">
+						<div
+							class="text-base items-center px-6 py-3 hover:bg-gray-50"
+							v-for="deploy in deploys.data.releases"
+							:key="deploy.name"
+						>
+							<div class="flex justify-between items-center">
+								<div class="pr-2 font-mono">
+									{{ deploy.hash.slice(0, 6) }}
+								</div>
+								<div class="flex-1 pr-2 truncate font-semibold">
+									{{ deploy.message }}
+								</div>
+								<div class="flex items-center" v-if="deploy.tags">
+									<Badge
+										v-for="(tag, index) in deploy.tags"
+										:key="index"
+										class="mr-2"
+									>
+										{{ tag }}
+									</Badge>
+								</div>
 							</div>
-							<div class="flex-1 pr-2 truncate font-semibold">
-								{{ deploy.message }}
-							</div>
-							<div class="flex items-center" v-if="deploy.tags">
-								<Badge
-									v-for="(tag, index) in deploy.tags"
-									:key="index"
-									class="mr-2"
+							<div class="items-center pl-6" v-if="deploy.groups">
+								<div
+									v-for="group in deploy.groups"
+									:key="group.group"
+									class="block py-1 flex justify-between items-center"
 								>
-									{{ tag }}
-								</Badge>
-							</div>
-							<div class="w-32">
-								<Badge :status="deploy.status">
-									{{ deploy.status }}
-								</Badge>
+									<div>
+										<p class="text-base font-medium text-brand">
+											{{ group.group }}
+										</p>
+										<p class="text-sm text-gray-800">
+											frappe/{{ deploys.data.groups[group.group].branch }}
+										</p>
+									</div>
+									<div class="w-32">
+										<Badge :status="group.status">
+											{{ group.status }}
+										</Badge>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="px-6 py-2 text-base text-gray-600" v-else>
-					This app isn't deployed yet.
+					<div class="px-6 py-2 text-base text-gray-600" v-else>
+						This app isn't deployed yet.
+					</div>
 				</div>
 			</SectionCard>
 		</Section>
