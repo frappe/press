f5fe94b5cf588f12b93bcb10290f3887deaeaca7
Faris Ansari <netchamp.faris@gmail.com>
2020-07-13 18:16:03 +0530

    fix: Patch to enable partner privileges

---
+++ press/patches/v0_0_1/enable_partner_privileges.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+	for d in frappe.db.get_all("Team"):
+		team = frappe.get_doc("Team", d.name)
+		if team.has_partner_account_on_erpnext_com():
+			team.enable_erpnext_partner_privileges()
