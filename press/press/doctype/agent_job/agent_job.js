e6063d1eb72d2039f9155d92570dfd67cdcd823e
Aditya Hase <aditya@adityahase.com>
2020-07-30 13:19:52 +0530

    fix: Form dashboard  in Agent Job

--- press/press/doctype/agent_job/agent_job.js
+++ press/press/doctype/agent_job/agent_job.js
@@ -2,11 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Agent Job', {
-	refresh: function(frm) {
-		frm.add_custom_button(__('Steps'), () => {
-			const filters = {agent_job: frm.doc.name};
-			frappe.set_route("List", "Agent Job Step", filters);
-		});
+	refresh: function (frm) {
 		frm.add_custom_button(__('Retry'), () => {
 			frm.call({method: "retry", doc: frm.doc, callback: result => frappe.msgprint(result.message.name)});
 		});
