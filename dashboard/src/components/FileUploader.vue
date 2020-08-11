30ffda2746daec292bccc932f6bfc9e5bdeaaf3e
Gavin D'souza <gavin18d@gmail.com>
2020-08-11 18:09:52 +0530

    fix: check if wasm is supported on browser before validating files

--- dashboard/src/components/FileUploader.vue
+++ dashboard/src/components/FileUploader.vue
@@ -70,11 +70,37 @@ export default {
 				return;
 			}
 
+			// Check if browser supports WASM
+			// ref: https://stackoverflow.com/a/47880734/10309266
+			const supported = (() => {
+				try {
+					if (
+						typeof WebAssembly === 'object' &&
+						typeof WebAssembly.instantiate === 'function'
+					) {
+						const module = new WebAssembly.Module(
+							Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
+						);
+						if (module instanceof WebAssembly.Module)
+							return (
+								new WebAssembly.Instance(module) instanceof WebAssembly.Instance
+							);
+					}
+				} catch (e) {}
+				return false;
+			})();
+
 			// Check for validity of files
-			this.message = 'Validating File';
-			const validationMessage = await this.validateFile();
-			const validationName = await this.validateFileName();
-			this.message = '';
+			let validationMessage, validationName;
+
+			if (supported) {
+				this.message = 'Validating File';
+				validationMessage = await this.validateFile();
+				validationName = await this.validateFileName();
+				this.message = '';
+			} else {
+				validationMessage = 'WASM not supported on this browser';
+			}
 
 			// Try uploading the files
 			if (validationMessage === true) {
