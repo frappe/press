d81eaa3586f9c93b972a3cf617f7462da27137da
Aditya Hase <aditya@adityahase.com>
2020-08-24 21:46:05 +0530

    feat: Backbone

---
+++ backbone/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Frappe and contributors
+# For license information, please see license.txt
--- setup.py
+++ setup.py
@@ -18,4 +18,5 @@ setup(
 	zip_safe=False,
 	include_package_data=True,
 	install_requires=install_requires,
+	entry_points={"console_scripts": ["backbone = backbone.cli:cli"]},
 )
