ddad64ee7f89108597fb56137658cd6bec83f635
Aditya Hase <aditya@adityahase.com>
2020-08-24 21:48:52 +0530

    feat(backbone): Setup KVM

--- backbone/cli.py
+++ backbone/cli.py
@@ -3,12 +3,25 @@
 # For license information, please see license.txt
 import click
 
+from backbone.hypervisor import Hypervisor, Shell
 from backbone.tests import run_tests
 
 
 @click.group()
 def cli():
 	pass
+
+
+@cli.group()
+def hypervisor():
+	pass
+
+
+@hypervisor.command()
+def setup():
+	shell = Shell()
+	hypervisor = Hypervisor(shell=shell)
+	hypervisor.setup()
 
 
 @cli.command()
