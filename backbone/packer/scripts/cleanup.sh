b6410e401592f3632009f321e97d62f01027136a
Aditya Hase <aditya@adityahase.com>
2020-08-28 13:08:57 +0530

    fix(backbone): Remove logs, cache contents, temporary files from base box

--- backbone/packer/scripts/cleanup.sh
+++ backbone/packer/scripts/cleanup.sh
@@ -83,3 +83,31 @@ apt-get -y purge deborphan dialog
 apt-get -y autoremove
 apt-get -y autoclean
 apt-get -y clean
+
+# Remove docs
+rm -rf /usr/share/doc/*
+
+# Remove man pages
+rm -rf /usr/share/man/*
+
+# Remove cache files
+find /var/cache -type f -exec rm -rf {} \;
+
+# Remove APT files"
+find /var/lib/apt -type f | xargs rm -f
+
+# truncate any logs that have built up during the install
+find /var/log -type f -exec truncate --size=0 {} \;
+
+# Blank netplan machine-id (DUID) so machines get unique ID generated on boot.
+truncate -s 0 /etc/machine-id
+
+# remove the contents of /tmp and /var/tmp
+rm -rf /tmp/* /var/tmp/*
+
+# clear the history so our install isn't there
+export HISTSIZE=0
+rm -f /root/.wget-hsts
+
+# Remove unused blocks
+/sbin/fstrim -v /
