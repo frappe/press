1a661b23e3ea391f41dda2da30e593b3ea893c59
Aditya Hase <aditya@adityahase.com>
2020-09-15 10:57:10 +0530

    chore: Remove obsolete DocTypes

---
+++ press/patches/v0_0_1/remove_obsolete_doctypes.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+	obsolete_doctypes = [
+		"Credit Ledger Entry",
+		"Custom Domain",
+		"Site Analytics",
+		"Site History",
+		"Site Usage",
+		"Site Usage Ledger Entry",
+		"Usage Report",
+		"User Account",
+	]
+	for doctype in obsolete_doctypes:
+		if frappe.db.exists('DocType', doctype):
+			frappe.delete_doc('DocType', doctype)
