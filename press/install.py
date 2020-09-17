c48501a96ce7e8dd8f4ea56fae4c33bfcaf094c9
Aditya Hase <aditya@adityahase.com>
2020-09-17 19:45:51 +0530

    chore: Create default CAs during app installation

--- press/install.py
+++ press/install.py
@@ -3,10 +3,46 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+
+import os
+
 import frappe
 
 
 def after_install():
+	create_administrator_team()
+	create_certificate_authorities()
+
+
+def create_certificate_authorities():
+	if not frappe.conf.developer_mode:
+		return
+
+	bench = frappe.utils.get_bench_path()
+	scratch = os.path.join(bench, f"backbone-ca-{frappe.generate_hash(length=6)}")
+
+	if not os.path.exists(scratch):
+		os.mkdir(scratch)
+
+	root_ca = frappe.get_doc(
+		{
+			"doctype": "Certificate Authority",
+			"common_name": "Backbone Root Certificate Authority",
+			"is_root_ca": True,
+			"directory": f"{scratch}/root",
+		}
+	).insert()
+	frappe.get_doc(
+		{
+			"doctype": "Certificate Authority",
+			"common_name": "Backbone Intermediate Certificate Authority",
+			"parent_authority": root_ca.name,
+			"directory": f"{scratch}/intermediate",
+		}
+	).insert()
+
+
+def create_administrator_team():
 	administrator_team = frappe.get_doc(
 		{
 			"doctype": "Team",
