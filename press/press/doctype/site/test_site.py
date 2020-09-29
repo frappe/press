c3284860842aa457d476692a0ca1095076b680c1
Balamurali Murali <balamurali@erpnext.com>
2020-09-30 04:08:17 +0530

    test(Site): Create dummy docs

--- press/press/doctype/site/test_site.py
+++ press/press/doctype/site/test_site.py
@@ -3,9 +3,110 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
 import unittest
+from press.agent import Agent
+from press.press.doctype.frappe_app.test_frappe_app import (
+	create_test_frappe_app
+)
+
+
+def create_test_site(subdomain):
+	"""Create test Site doc."""
+	frappe.set_user("Administrator")
+
+	proxy_server = frappe.get_doc({
+		"doctype": "Proxy Server",
+		"status": "Active",
+		"ip": "0.0.0.0.0",
+		"private_ip": "0.9.9.9.9",
+		"name": "n.frappe.cloudtest",
+	}).insert(ignore_if_duplicate=True)
+
+	server = frappe.get_doc({
+		"doctype": "Server",
+		"status": "Active",
+		"ip": "0.0.0.0",
+		"private_ip": "9.9.9.9",
+		"name": "f.frappe.cloudtest",
+		"mariadb_root_password": "admin",
+		"proxy_server": proxy_server.name
+	}).insert(ignore_if_duplicate=True)
+
+	frappe_app = create_test_frappe_app()
+
+	release_group = frappe.get_doc({
+		"doctype": "Release Group",
+		"name": "Test Release Group",
+		"apps": [{
+			"app": frappe_app.name,
+		}],
+		"enabled": True
+	}).insert(ignore_if_duplicate=True)
+
+	release_group.create_deploy_candidate()
+
+	bench = frappe.get_doc({
+		"name": "Test Bench",
+		"doctype": "Bench",
+		"status": "Active",
+		"workers": 1,
+		"gunicorn_workers": 2,
+		"group": release_group.name,
+		"server": server.name
+	}).insert(ignore_if_duplicate=True)
+
+	plan = frappe.get_doc({
+		"name": "Test 10 dollar plan",
+		"doctype": "Plan",
+		"price_inr": 750.0,
+		"price_usd": 10.0,
+		"period": 30
+	}).insert(ignore_if_duplicate=True)
+
+	return frappe.get_doc({
+		"doctype": "Site",
+		"status": "Active",
+		"subdomain": subdomain,
+		"server": server.name,
+		"bench": bench.name,
+		"plan": plan.name,
+		"apps": [{
+			"app": frappe_app.name
+		}],
+		"admin_password": "admin"
+	}).insert(ignore_if_duplicate=True)
+
+
+def fake_agent_job():
+	"""Monkey patch Agent Job doctype methods to work in isolated tests."""
+
+	def create_agent_job(
+		self,
+		job_type,
+		path,
+		data=None,
+		files=None,
+		method="POST",
+		bench=None,
+		site=None,
+		upstream=None,
+		host=None,
+	):
+		return {"job": 1}
+
+	Agent.create_agent_job = create_agent_job
 
 
 class TestSite(unittest.TestCase):
-	pass
+	"""Tests for Site Document methods."""
+
+	def setUp(self):
+		self.subdomain = "testsubdomain"
+		fake_agent_job()
+
+	def tearDown(self):
+		frappe.db.rollback()
+
+	def test_primary_domain_is_default_when_no_site_domain_is_primary(self):
+		site = create_test_site(self.subdomain)
--- press/press/doctype/site_domain/test_site_domain.py
+++ press/press/doctype/site_domain/test_site_domain.py
@@ -3,8 +3,21 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
 import unittest
+
+import frappe
+
+
+def create_test_site_domain(site):
+	"""Create test Site Domain doc."""
+	return frappe.get_doc({
+		"doctype": "Site Domain",
+		"site": site,
+		"domain": frappe.mock("url"),
+		"status": "Active",
+		"retry_count": 1,
+		"dns_type": "A"
+	}).insert(ignore_if_duplicate=True)
 
 
 class TestSiteDomain(unittest.TestCase):
