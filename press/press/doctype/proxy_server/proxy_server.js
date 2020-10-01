639ea91705ae3794a5b537614e04149409a7e69c
Aditya Hase <aditya@adityahase.com>
2020-10-01 19:37:30 +0530

    fix: Use form dashboard instead of Custom Buttons in Server and Proxy Server

--- press/press/doctype/proxy_server/proxy_server.js
+++ press/press/doctype/proxy_server/proxy_server.js
@@ -2,24 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Proxy Server', {
-	refresh: function(frm) {
-		frm.add_custom_button(__('Servers'), () => {
-			const filters = {proxy_server: frm.doc.name};
-			frappe.set_route("List", "Server", filters);
-		});
-		frm.add_custom_button(__('Jobs'), () => {
-			const filters = {server: frm.doc.name};
-			frappe.set_route("List", "Agent Job", filters);
-		});
+	refresh: function (frm) {
 		frm.add_custom_button(__('Ping'), () => {
-			frm.call({method: "ping", doc: frm.doc, callback: result => frappe.msgprint(result.message)});
+			frm.call({
+				method: "ping",
+				doc: frm.doc,
+				callback: result => frappe.msgprint(result.message)
+			});
 		}, __("Actions"));
 		frm.add_custom_button(__('Update Agent'), () => {
-			frm.call({method: "update_agent", doc: frm.doc, callback: result => frappe.msgprint(result.message)});
+			frm.call({
+				method: "update_agent",
+				doc: frm.doc,
+				callback: result => frappe.msgprint(result.message)
+			});
 		}, __("Actions"));
 		if (!frm.doc.is_server_setup) {
 			frm.add_custom_button(__('Setup Server'), () => {
-				frm.call({method: "setup_server", doc: frm.doc, callback: result => frappe.msgprint(result.message)});
+				frm.call({
+					method: "setup_server",
+					doc: frm.doc,
+					callback: result => frappe.msgprint(result.message)
+				});
 			}, __("Actions"));
 		}
 
