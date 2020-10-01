e99698e695af006b2d0b758cf19ac36c573eed73
Aditya Hase <aditya@adityahase.com>
2020-10-01 19:37:30 +0530

    fix: Use form dashboard instead of Custom Buttons in Server and Proxy Server

--- press/press/doctype/server/server.js
+++ press/press/doctype/server/server.js
@@ -2,30 +2,38 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Server', {
-	refresh: function(frm) {
-		frm.add_custom_button(__('Benches'), () => {
-			const filters = {bench: frm.doc.name};
-			frappe.set_route("List", "Bench", filters);
-		});
-		frm.add_custom_button(__('Jobs'), () => {
-			const filters = {server: frm.doc.name};
-			frappe.set_route("List", "Agent Job", filters);
-		});
+	refresh: function (frm) {
 		frm.add_custom_button(__('Ping'), () => {
-			frm.call({method: "ping", doc: frm.doc, callback: result => frappe.msgprint(result.message)});
+			frm.call({
+				method: "ping",
+				doc: frm.doc,
+				callback: result => frappe.msgprint(result.message)
+			});
 		}, __("Actions"));
 		frm.add_custom_button(__('Update Agent'), () => {
-			frm.call({method: "update_agent", doc: frm.doc, callback: result => frappe.msgprint(result.message)});
+			frm.call({
+				method: "update_agent",
+				doc: frm.doc,
+				callback: result => frappe.msgprint(result.message)
+			});
 		}, __("Actions"));
 		if (!frm.doc.is_server_setup) {
 			frm.add_custom_button(__('Setup Server'), () => {
-				frm.call({method: "setup_server", doc: frm.doc, callback: result => frappe.msgprint(result.message)});
+				frm.call({
+					method: "setup_server",
+					doc: frm.doc,
+					callback: result => frappe.msgprint(result.message)
+				});
 			}, __("Actions"));
 		}
 		if (!frm.doc.is_upstream_setup) {
 			frm.add_custom_button(__("Add to Proxy"), () => {
-				frm.call({method: "add_upstream_to_proxy", doc: frm.doc, callback: result => frappe.msgprint(result.message)});
+				frm.call({
+					method: "add_upstream_to_proxy",
+					doc: frm.doc,
+					callback: result => frappe.msgprint(result.message)
+				});
 			}, __("Actions"));
 		}
-}
+	}
 });
