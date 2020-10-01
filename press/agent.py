90283176820a18dad8d023a6601196d1e42a8d58
Gavin D'souza <gavin18d@gmail.com>
2020-10-01 20:01:33 +0530

    fix: Bulk collect site info bench wise

--- press/agent.py
+++ press/agent.py
@@ -425,6 +425,9 @@ class Agent:
 	def get_site_info(self, site):
 		return self.get(f"benches/{site.bench}/sites/{site.name}/info")["data"]
 
+	def get_sites_info(self, bench):
+		return self.get(f"benches/{bench.name}/info")
+
 	def get_jobs_status(self, ids):
 		status = self.get(f"jobs/{','.join(map(str, ids))}")
 		if len(ids) == 1:
