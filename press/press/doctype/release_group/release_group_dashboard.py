d11fd4b9a053f548fe6d75a54a2080c65c4ea504
Aditya Hase <aditya@adityahase.com>
2020-10-07 21:30:45 +0530

    fix: Form dashboard for Frappe App, Release Group

---
+++ press/press/doctype/release_group/release_group_dashboard.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and Contributors
+# Proprietary License. See license.txt
+
+from __future__ import unicode_literals
+
+
+def get_data():
+	return {
+		"fieldname": "group",
+		"transactions": [
+			{"items": ["Bench", "Deploy Candidate", "Deploy Candidate Difference"]},
+		],
+	}
