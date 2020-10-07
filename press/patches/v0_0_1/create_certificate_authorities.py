09ec12cf76c20b7bb78c544a832338f1f8a1ab37
Aditya Hase <aditya@adityahase.com>
2020-10-07 20:21:55 +0530

    fix(ca): Reload Certificate Authority DocType with low level api

--- press/patches/v0_0_1/create_certificate_authorities.py
+++ press/patches/v0_0_1/create_certificate_authorities.py
@@ -9,7 +9,7 @@ from press.install import create_certificate_authorities
 
 
 def execute():
-	frappe.reload_doctype("Certificate Authority")
+	frappe.reload_doc("press", "doctype", "certificate_authority")
 	if frappe.get_site_config().developer_mode:
 		frappe.conf.developer_mode = 1
 	create_certificate_authorities()
