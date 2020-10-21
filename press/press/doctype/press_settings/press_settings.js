b013a28be84c4d9369acd95cc95c075a6c918e0d
Aditya Hase <aditya@adityahase.com>
2020-10-21 11:06:50 +0530

    fix: Create GitHub App with one click
    Relies on https://docs.github.com/en/free-pro-team@latest/developers/apps/creating-a-github-app-from-a-manifest

--- press/press/doctype/press_settings/press_settings.js
+++ press/press/doctype/press_settings/press_settings.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Press Settings', {
-	refresh: function(frm) {
+	refresh: function (frm) {
 		frm.add_custom_button(
 			__('Obtain TLS Certificate'),
 			() => {
@@ -34,5 +34,24 @@ frappe.ui.form.on('Press Settings', {
 	},
 	create_stripe_webhook(frm) {
 		frm.call('create_stripe_webhook');
+	},
+	create_github_app(frm) {
+		frm.call({
+			method: 'get_github_app_manifest',
+			doc: frm.doc,
+			callback: response => {
+				window.location.href = response.message;
+				let $form = $('<form>', {
+					action: "https://github.com/settings/apps/new",
+					method: "post"
+				});
+				$('<input>').attr({
+					type: "hidden",
+					name: "manifest",
+					value: JSON.stringify(response.message),
+				}).appendTo($form);
+				$form.appendTo('body').submit();
+			}
+		});
 	}
 });
