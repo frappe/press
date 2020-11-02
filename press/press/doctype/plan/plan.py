ce177cfcc2272a8f463acdaff12afb874520f0b5
Gavin D'souza <gavin18d@gmail.com>
2020-11-02 17:00:40 +0530

    feat: Plans with storage limits
    * Track Storage, Database and Offsite backup availability via Plan
    * Remove offsite backups from USD 10
    * Add new plans: $300, $400

--- press/press/doctype/plan/plan.py
+++ press/press/doctype/plan/plan.py
@@ -7,11 +7,28 @@ import frappe
 from frappe.model.document import Document
 from frappe.utils import rounded
 
+def _h(num: int) -> str:
+	"""Assumes int data to describe size in MiB"""
+	for unit in ['Mi', 'Gi','Ti','Pi','Ei','Zi']:
+		if abs(num) < 1024:
+			return f"{num:3.1f}{unit}B"
+		num /= 1024
+	return f"{num:.1f}YiB"
 
 class Plan(Document):
 	def validate(self):
+		readable_mapped_fields = {
+			"max_database_usage": "_max_database_usage",
+			"max_storage_usage": "_max_storage_usage"
+		}
+
 		if not self.period:
 			frappe.throw("Period must be greater than 0")
+
+		for real, readable in readable_mapped_fields.items():
+			num = self.get(real)
+			if num:
+				setattr(self, readable, _h(num))
 
 	def get_price_per_day(self, currency):
 		price = self.price_inr if currency == "INR" else self.price_usd
