bc60cad5721bafc2167cfae7b99e2eb83c57d545
Balamurali M <balamurali@erpnext.com>
2020-11-10 09:55:21 +0530

    fix(Patches): Make default site domain for all sites that aren't archived

--- press/patches/v0_0_1/make_default_site_domain.py
+++ press/patches/v0_0_1/make_default_site_domain.py
@@ -7,5 +7,7 @@ import frappe
 
 def execute():
 	frappe.reload_doc("press", "doctype", "site")
-	for site in frappe.db.get_all("Site", {"status": "Active"}, ["name"]):
-		frappe.get_doc("Site", site.name)._create_default_site_domain()
+	for site in frappe.db.get_all(
+		"Site", {"status": ("!=", "Archived")}, pluck="name"
+	):
+		frappe.get_doc("Site", site)._create_default_site_domain()
