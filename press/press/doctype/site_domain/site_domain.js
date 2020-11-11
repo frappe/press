d34143bbe6c87d74334c31dec3d0998681554d99
Balamurali M <balamurali@erpnext.com>
2020-11-11 11:53:46 +0530

    refactor(SiteDomain): Remove need for client side agent update

--- press/press/doctype/site_domain/site_domain.js
+++ press/press/doctype/site_domain/site_domain.js
@@ -1,12 +1,5 @@
 // Copyright (c) 2020, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Site Domain', {
-	after_save: function (frm) {
-		if (frm.doc.redirect_to_primary) {
-			frm.call('setup_redirect');
-		} else {
-			frm.call('remove_redirect');
-		}
-	},
-});
+// frappe.ui.form.on('Site Domain', {
+// });
