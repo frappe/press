c1a51209a6517fba9eec9e74edbd8c8cb4d47c82
Faris Ansari <netchamp.faris@gmail.com>
2020-11-12 20:34:42 +0530

    fix: skip subscription if already created

--- press/patches/v0_0_1/create_site_subscriptions.py
+++ press/patches/v0_0_1/create_site_subscriptions.py
@@ -10,7 +10,6 @@ def execute():
 	frappe.db.sql("DROP TABLE IF EXISTS `tabSubscription`")
 
 	frappe.reload_doc("press", "doctype", "subscription", force=True)
-	frappe.reload_doc("press", "doctype", "plan")
 	frappe.reload_doc("press", "doctype", "site")
 
 	active_sites = frappe.db.get_all(
@@ -20,6 +19,13 @@ def execute():
 	)
 	for i, site in enumerate(active_sites):
 		update_progress_bar("Creating Subscriptions", i, len(active_sites))
+
+		# skip if already exists
+		if frappe.db.exists(
+			"Subscription",
+			{"team": site.team, "document_type": "Site", "document_name": site.name},
+		):
+			continue
 
 		try:
 			frappe.get_doc(
