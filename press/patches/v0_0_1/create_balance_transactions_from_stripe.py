b8891ee180f575a3e62877b149ae5e4197860667
Faris Ansari <netchamp.faris@gmail.com>
2020-11-18 19:35:26 +0530

    fix: Refactor patch for re-running and edge cases

--- press/patches/v0_0_1/create_balance_transactions_from_stripe.py
+++ press/patches/v0_0_1/create_balance_transactions_from_stripe.py
@@ -7,72 +7,102 @@ import frappe
 from press.api.billing import get_stripe
 from datetime import datetime
 
+migrated_cache_key = "migrated_teams"
+
 
 def execute():
 	frappe.reload_doc("press", "doctype", "balance_transaction")
 
-	teams = frappe.db.get_all("Team", pluck="name")
-	stripe = get_stripe()
-
+	skip_teams = list(frappe.cache().smembers(migrated_cache_key))
+	teams = frappe.db.get_all(
+		"Team",
+		filters={"name": ("not in", skip_teams), "stripe_customer_id": ("is", "set")},
+		pluck="name",
+	)
 	for name in teams:
-		team = frappe.get_doc("Team", name)
+		try:
+			create_balance_transactions_for_team(name)
+			frappe.cache().sadd(migrated_cache_key, name)
+		except Exception:
+			print(f"❗️ Failed for {name}")
+			print(frappe.get_traceback())
 
-		# skip if already done
-		if frappe.db.exists(
-			"Balance Transaction", {"source": "Free Credits", "team": team.name}
-		):
-			print(f"Skipping for {team.name}")
-			continue
 
-		print(f"Creating Balance Transactions for {team.name}")
+def create_balance_transactions_for_team(name):
+	team = frappe.get_doc("Team", name)
+	stripe = get_stripe()
+	# skip if already done
+	if frappe.db.exists(
+		"Balance Transaction", {"source": "Free Credits", "team": team.name}
+	):
+		print(f"Skipping for {team.name}")
+		return
 
-		response = stripe.Customer.list_balance_transactions(
-			team.stripe_customer_id, limit=100,
-		)
-		transactions = response.data
-		transactions.reverse()
+	print(f"Creating Balance Transactions for {team.name}")
 
+	response = stripe.Customer.list_balance_transactions(
+		team.stripe_customer_id, limit=100,
+	)
+	transactions = response.data
+	transactions.reverse()
+
+	if team.free_credits_allocated:
 		free_credits_left = 1800 if team.currency == "INR" else 25
-		balance_transactions = []
-		free_credit_balance_created = False
-		for transaction in transactions:
-			free_credits = 1800 if transaction.currency == "inr" else 25
-			amount = transaction.amount * -1 / 100
-			type = (
-				"Applied To Invoice" if transaction.type == "applied_to_invoice" else "Adjustment"
-			)
-			source = ""
-			invoice_name = ""
-			if type == "Adjustment":
-				if amount > 0:
-					source = (
-						"Free Credits"
-						if amount == free_credits and not free_credit_balance_created
-						else "Transferred Credits"
-					)
+	else:
+		free_credits_left = 0
 
-			if type == "Applied To Invoice":
-				invoice = frappe.get_doc("Invoice", {"stripe_invoice_id": transaction.invoice})
-				invoice_name = invoice.name
-				free_credits_applied = apply_to_invoice(
-					invoice, amount, free_credits_left, balance_transactions
+	balance_transactions = []
+	free_credit_balance_created = False
+	for transaction in transactions:
+		amount = transaction.amount * -1 / 100
+		type = (
+			"Applied To Invoice" if transaction.type == "applied_to_invoice" else "Adjustment"
+		)
+		source = ""
+		invoice_name = ""
+		if type == "Adjustment":
+			if amount > 0:
+				free_credits = 1800 if transaction.currency == "inr" else 25
+				source = (
+					"Free Credits"
+					if team.free_credits_allocated
+					and amount == free_credits
+					and not free_credit_balance_created
+					else "Transferred Credits"
 				)
-				free_credits_left -= free_credits_applied
 
-			bt = create_balance_transaction(
-				team,
-				amount=amount,
-				source=source,
-				type=type,
-				invoice=invoice_name,
-				creation=datetime.fromtimestamp(transaction.created),
+		if type == "Applied To Invoice":
+			invoice = frappe.get_doc("Invoice", {"stripe_invoice_id": transaction.invoice})
+			invoice_name = invoice.name
+			free_credits_applied = apply_to_invoice(
+				invoice, amount, free_credits_left, balance_transactions
 			)
-			balance_transactions.append(bt)
-			if bt.source == "Free Credits":
-				free_credit_balance_created = True
+			free_credits_left = frappe.utils.rounded(free_credits_left - free_credits_applied, 2)
 
+		bt = create_balance_transaction(
+			team,
+			amount=amount,
+			source=source,
+			type=type,
+			invoice=invoice_name,
+			creation=datetime.fromtimestamp(transaction.created),
+		)
+		balance_transactions.append(bt)
+		if bt.source == "Free Credits":
+			free_credit_balance_created = True
+
+	stripe_balance = team.get_stripe_balance()
+	team_balance = team.get_balance()
+	if stripe_balance == team_balance:
 		reset_customer_balance_on_stripe(team)
 		frappe.db.commit()
+		print(f"✅ Successful for {team.name}")
+	else:
+		frappe.db.rollback()
+		print(
+			f"❌ Balance mismatch for {team.name}. Team Balance: {team_balance}, Stripe"
+			f" Balance: {stripe_balance}"
+		)
 
 
 def apply_to_invoice(invoice, amount, free_credits_left, balance_transactions):
@@ -106,16 +136,17 @@ def apply_to_invoice(invoice, amount, free_credits_left, balance_transactions):
 				"currency": invoice.currency,
 			},
 		)
-		bt.append(
+		row = bt.append(
 			"allocated_to",
 			{"invoice": invoice.name, "amount": to_apply, "currency": invoice.currency},
 		)
 		bt.save()
+		bt.reload()
 		applied += to_apply
 		not_applied = amount_to_apply - applied
 
 	for row in invoice.credit_allocations:
-		row.db_update()
+		row.db_insert()
 
 	return free_credits_applied
 
@@ -127,6 +158,9 @@ def reset_customer_balance_on_stripe(team):
 		stripe.Customer.create_balance_transaction(
 			team.stripe_customer_id,
 			# multiplied by 100 because Stripe wants amount in cents / paise
+			# to reset the balance we should provide negative value of the original value
+			# but we store the credit balance as a positive and stripe stores it as negative
+			# so a positive value will negate the value to 0 on stripe
 			amount=int(balance * 100),
 			currency=team.currency.lower(),
 			description="Reset customer balance",
@@ -147,6 +181,6 @@ def create_balance_transaction(team, **kwargs):
 	doc = frappe.get_doc(doctype="Balance Transaction", team=team.name)
 	doc.update(kwargs)
 	doc.insert(ignore_permissions=True)
-	# doc.db_set("creation", kwargs.get("creation"), update_modified=False)
 	doc.submit()
+	doc.reload()
 	return doc
