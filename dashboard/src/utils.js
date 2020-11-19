28a92cb6f932b89066e9d0155ce95b194796f404
Gavin D'souza <gavin18d@gmail.com>
2020-11-19 13:25:05 +0530

    fix: Handle negative and undefined via formatBytes, headline alert

--- dashboard/src/utils.js
+++ dashboard/src/utils.js
@@ -27,8 +27,7 @@ let utils = {
 			const k = 1024;
 			const dm = decimals < 0 ? 0 : decimals;
 			const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-
-			const i = Math.floor(Math.log(bytes) / Math.log(k));
+			const i = Math.floor(Math.log(Math.abs(bytes)) / Math.log(k));
 
 			return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i + current];
 		}
