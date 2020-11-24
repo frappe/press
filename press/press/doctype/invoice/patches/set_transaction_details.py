cd558f3a1c5ca7e1df5031ee771bd68943a6e8b6
Faris Ansari <netchamp.faris@gmail.com>
2020-11-24 21:28:35 +0530

    fix: Patch to update transaction details in invoices

---
+++ press/press/doctype/invoice/patches/set_transaction_details.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and Contributors
+# Proprietary License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+	frappe.reload_doc("press", "doctype", "invoice")
+	invoices = frappe.db.get_all(
+		"Invoice",
+		{"status": "Paid", "docstatus": 1, "amount_paid": (">", 0), "transaction_amount": 0},
+		pluck="name",
+	)
+	for name in invoices:
+		print(f"Updating transaction details for {name}")
+		invoice = frappe.get_doc("Invoice", name)
+		invoice.flags.skip_frappe_invoice = True
+		updated = invoice.update_transaction_details()
+		if updated:
+			print(f"✅ Done")
+			frappe.db.commit()
