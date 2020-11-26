8c65993c7fbd655f5043b7f11897835a1ec7cc2c
Gavin D'souza <gavin18d@gmail.com>
2020-11-26 18:53:53 +0530

    fix: Revert correctness to avoid confusion

--- dashboard/src/utils.js
+++ dashboard/src/utils.js
@@ -26,7 +26,7 @@ let utils = {
 
 			const k = 1024;
 			const dm = decimals < 0 ? 0 : decimals;
-			const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+			const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 			const i = Math.floor(Math.log(Math.abs(bytes)) / Math.log(k));
 
 			return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i + current];
