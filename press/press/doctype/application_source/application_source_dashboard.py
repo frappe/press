bd88ad468a79f9df4b4008fd23a912f6be6c0a79
Aditya Hase <aditya@adityahase.com>
2020-12-01 20:42:04 +0530

    fix: Form Dashboard for Application Source

---
+++ press/press/doctype/application_source/application_source_dashboard.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+
+
+def get_data():
+	return {
+		"fieldname": "source",
+		"transactions": [{"items": ["Release Group", "Application Release"]},],
+	}
