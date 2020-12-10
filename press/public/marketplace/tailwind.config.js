8f7aae15ec133857bd9014aac72ab6daed60dd72
Faris Ansari <netchamp.faris@gmail.com>
2020-12-10 15:51:08 +0530

    fix: build command for purging marketplace styles

--- press/public/marketplace/tailwind.config.js
+++ press/public/marketplace/tailwind.config.js
@@ -1,16 +1,14 @@
 const path = require('path');
 const config = require('../../../dashboard/tailwind.config');
 
-// TODO: Figure out purgecss in production
-
 module.exports = {
 	theme: config.theme,
 	plugins: config.plugins,
 	purge: {
-		content: [path.resolve('./press/+(www|templates)/marketplace/**/*.html')],
+		content: ['./press/**/marketplace/**/*.html'],
 		options: {
 			whitelist: ['body', 'html', 'img', 'a'],
-			whitelistPatternsChildren: [/chart-container$/, /graph-svg-tip$/]
-		}
-	}
+			whitelistPatternsChildren: [/chart-container$/, /graph-svg-tip$/],
+		},
+	},
 };
