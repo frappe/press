5c3972812ac87a7bcc67e6aec6ac2f23e1a44838
Aditya Hase <aditya@adityahase.com>
2020-12-11 15:32:38 +0530

    fix: Rename Installed App to Bench Application

--- press/press/doctype/bench_application/bench_application.js
+++ press/press/doctype/bench_application/bench_application.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2019, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Installed App', {
+frappe.ui.form.on('Bench Application', {
 	// refresh: function(frm) {
 
 	// }
