1095dfd9e127a359cc4ef2d543a5abb642275f00
Balamurali M <balamurali@erpnext.com>
2020-12-11 07:05:55 +0530

    feat: Add functions to make test remote files and site backups

--- press/press/doctype/site_backup/test_site_backup.py
+++ press/press/doctype/site_backup/test_site_backup.py
@@ -3,8 +3,33 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
 import unittest
+from datetime import datetime
+
+import frappe
+from press.press.doctype.remote_file.test_remote_file import create_test_remote_file
+
+
+def create_test_site_backup(
+	site: str,
+	creation: datetime,
+	files_availability: str = "Available",
+	offsite: bool = True,
+):
+	"""Create test site backup doc for required timestamp."""
+	params_dict = {
+		"doctype": "Site Backup",
+		"status": "Success",
+		"site": site,
+		"creation": creation,
+		"files_availability": files_availability,
+		"offsite": offsite,
+	}
+	if offsite:
+		params_dict["remote_public_file"] = create_test_remote_file(site, creation).name
+		params_dict["remote_private_file"] = create_test_remote_file(site, creation).name
+		params_dict["remote_database_file"] = create_test_remote_file(site, creation).name
+	return frappe.get_doc(params_dict).insert(ignore_if_duplicate=True)
 
 
 class TestSiteBackup(unittest.TestCase):
