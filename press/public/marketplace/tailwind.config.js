79524afd8720f1743193f5eb923dc0f1bf1c3275
Faris Ansari <netchamp.faris@gmail.com>
2020-12-14 15:50:14 +0530

    fix: purgecss config to include marketplace app html

--- press/public/marketplace/tailwind.config.js
+++ press/public/marketplace/tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
 	theme: config.theme,
 	plugins: config.plugins,
 	purge: {
-		content: ['./press/**/marketplace/**/*.html'],
+		content: [
+			'./press/**/marketplace/**/*.html',
+			'./press/**/marketplace_*.html',
+		],
 		options: {
 			whitelist: ['body', 'html', 'img', 'a'],
 			whitelistPatternsChildren: [/chart-container$/, /graph-svg-tip$/],
