42f68d58fe040ef0b43d7afaadc6ffe4a0bcbd85
Aditya Hase <aditya@adityahase.com>
2020-12-15 22:57:51 +0530

    fix: Remove unnecessary patches

--- press/patches/v0_0_1/rename_frappe_app_to_app.py
+++ press/patches/v0_0_1/rename_frappe_app_to_app.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020, Frappe and contributors
+# Copyright (c) 2019, Frappe and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-
 import frappe
-from frappe.model.utils.rename_field import rename_field
 
 
 def execute():
-	from_doctype = "App Release"
-	to_doctype = "Application Release"
+	from_doctype = "Frappe App"
+	to_doctype = "App"
 	if frappe.db.table_exists(from_doctype) and not frappe.db.table_exists(to_doctype):
 		frappe.rename_doc("DocType", from_doctype, to_doctype, force=True)
-
-	frappe.reload_doctype("Application Release")
-	rename_field("Application Release", "app", "application")
--- press/patches/v0_0_1/rename_release_group_frappe_app_to_release_group_app.py
+++ press/patches/v0_0_1/rename_release_group_frappe_app_to_release_group_app.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2019, Frappe and contributors
+# Copyright (c) 2020, Frappe and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+
 import frappe
 
 
 def execute():
-	from_doctype = "Frappe App"
-	to_doctype = "Application"
+	from_doctype = "Release Group Frappe App"
+	to_doctype = "Release Group App"
 	if frappe.db.table_exists(from_doctype) and not frappe.db.table_exists(to_doctype):
 		frappe.rename_doc("DocType", from_doctype, to_doctype, force=True)
