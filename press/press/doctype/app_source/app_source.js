956a1d65a788d9477f6f16e8b4cadd7059bc0d30
Aditya Hase <aditya@adityahase.com>
2020-12-15 22:38:33 +0530

    refactor: Rename Application Source to App Source

--- press/press/doctype/app_source/app_source.js
+++ press/press/doctype/app_source/app_source.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Application Source', {
+frappe.ui.form.on('App Source', {
 	// refresh: function(frm) {
 
 	// }
