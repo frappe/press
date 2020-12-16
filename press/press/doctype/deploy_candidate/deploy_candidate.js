d7b918de2301a06fdace334bb8f106bbc1be5a0f
Aditya Hase <aditya@adityahase.com>
2020-12-16 19:05:39 +0530

    fix: Move Deploy Candidate action buttons to DocType Action

--- press/press/doctype/deploy_candidate/deploy_candidate.js
+++ press/press/doctype/deploy_candidate/deploy_candidate.js
@@ -3,14 +3,6 @@
 
 frappe.ui.form.on('Deploy Candidate', {
 	refresh: function (frm) {
-		frm.add_custom_button(
-			__('Build'),
-			() => {
-				frm.call("build").then(() => frm.refresh());
-			},
-			__('Actions')
-		);
-
 		frm.fields_dict['apps'].grid.get_field('app').get_query = function (doc) {
 			return {
 				"query": "press.press.doctype.deploy_candidate.deploy_candidate.desk_app",
