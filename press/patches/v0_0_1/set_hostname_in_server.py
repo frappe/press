95e200dec2fed2a7c1e13fbe052da97b273f1e59
Aditya Hase <aditya@adityahase.com>
2020-12-17 18:05:26 +0530

    fix: Add hostname field in Server

---
+++ press/patches/v0_0_1/set_hostname_in_server.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+	frappe.reload_doc("press", "doctype", "server")
+	servers = frappe.get_all("Server", {"hostname": ("is", "not set")})
+	domain = frappe.db.get_single_value("Press Settings", "domain")
+	for server in servers:
+		hostname = server.name.replace(f".{domain}", "")
+		frappe.db.set_value("Server", server.name, "hostname", hostname)
