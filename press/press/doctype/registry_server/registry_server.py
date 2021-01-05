d7af0e4b24c3a0fec4411563aada77492c9c2f3d
Aditya Hase <aditya@adityahase.com>
2021-01-05 09:35:46 +0530

    feat: Registry Server DocType

---
+++ press/press/doctype/registry_server/registry_server.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+from press.runner import Ansible
+from press.utils import log_error
+
+
+class RegistryServer(Document):
+	def autoname(self):
+		if not self.domain:
+			self.domain = frappe.db.get_single_value("Press Settings", "domain")
+		self.name = f"{self.hostname}.{self.domain}"
+
+	def _setup_server(self):
+		certificate_name = frappe.db.get_value(
+			"Press Settings", "Press Settings", "wildcard_tls_certificate"
+		)
+		certificate = frappe.get_doc("TLS Certificate", certificate_name)
+		try:
+			ansible = Ansible(
+				playbook="registry.yml",
+				server=self,
+				variables={
+					"server": self.name,
+					"private_ip": self.private_ip,
+					"registry_username": self.registry_username,
+					"registry_password": self.get_password("registry_password"),
+					"certificate_private_key": certificate.private_key,
+					"certificate_full_chain": certificate.full_chain,
+					"certificate_intermediate_chain": certificate.intermediate_chain,
+				},
+			)
+			play = ansible.run()
+			self.reload()
+			if play.status == "Success":
+				self.status = "Active"
+				self.is_server_setup = True
+			else:
+				self.status = "Broken"
+		except Exception:
+			self.status = "Broken"
+			log_error("Registry Server Setup Exception", server=self.as_dict())
+		self.save()
+
+	def setup_server(self):
+		self.status = "Installing"
+		self.save()
+		frappe.enqueue_doc(
+			self.doctype, self.name, "_setup_server", queue="long", timeout=1200
+		)
+
+	def ping_ansible(self):
+		try:
+			ansible = Ansible(playbook="ping.yml", server=self)
+			ansible.run()
+		except Exception:
+			log_error("Registry Server Ping Exception", server=self.as_dict())
+
+	def on_trash(self):
+		plays = frappe.get_all("Ansible Play", filters={"server": self.name})
+		for play in plays:
+			frappe.delete_doc("Ansible Play", play.name)
