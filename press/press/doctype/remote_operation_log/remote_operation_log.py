db787f2d8ecc6d56e5a08ff256a5420f69166166
Balamurali M <balamurali@erpnext.com>
2021-01-06 14:00:50 +0530

    refactor(RemoteOperationLog): Add decorator for logging

--- press/press/doctype/remote_operation_log/remote_operation_log.py
+++ press/press/doctype/remote_operation_log/remote_operation_log.py
@@ -4,9 +4,35 @@
 
 from __future__ import unicode_literals
 
-# import frappe
+import functools
+import pprint
+
+import frappe
 from frappe.model.document import Document
 
 
 class RemoteOperationLog(Document):
 	pass
+
+
+def make_log(operation_type, status="Success"):
+	"""Make log after remote operation."""
+
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kwargs):
+			req = args[-1]
+			res = func(*args, **kwargs)
+			frappe.get_doc(
+				doctype="Remote Operation Log",
+				operation_type=operation_type,
+				request=pprint.pformat(req),
+				response=pprint.pformat(res),
+				status=status,
+			).insert()
+
+			return res
+
+		return wrapper
+
+	return decorator
