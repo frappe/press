31fe10da127129bc474aaec466f99195c6097386
Balamurali M <balamurali@erpnext.com>
2021-01-21 19:33:55 +0530

    fix: Update path to patched methods

--- press/tests/test_cleanup.py
+++ press/tests/test_cleanup.py
@@ -178,8 +178,8 @@ class TestGFS(unittest.TestCase):
 		self.assertEqual(older_backup.files_availability, "Unavailable")
 		self.assertEqual(newer_backup.files_availability, "Available")
 
-	@patch("press.press.doctype.remote_file.remote_file.delete_remote_backup_objects")
-	@patch("press.press.cleanup.frappe.db.commit")
+	@patch("press.press.doctype.site.backups.delete_remote_backup_objects")
+	@patch("press.press.doctype.site.backups.frappe.db.commit")
 	def test_delete_remote_backup_objects_called(
 		self, mock_frappe_commit, mock_del_remote_backup_objects
 	):
@@ -238,8 +238,8 @@ class TestFIFO(unittest.TestCase):
 		self.assertEqual(old.files_availability, "Available")
 		self.assertEqual(new.files_availability, "Available")
 
-	@patch("press.press.doctype.remote_file.remote_file.delete_remote_backup_objects")
-	@patch("press.press.cleanup.frappe.db.commit")
+	@patch("press.press.doctype.site.backups.delete_remote_backup_objects")
+	@patch("press.press.doctype.site.backups.frappe.db.commit")
 	def test_delete_remote_backup_objects_called(
 		self, mock_frappe_commit, mock_del_remote_backup_objects
 	):
