02686ddc1beec47701b93454b438c09f092f83c1
Aditya Hase <aditya@adityahase.com>
2021-01-25 16:15:33 +0530

    fix: Rename Release Groups to follow naming series

---
+++ press/patches/v0_0_1/rename_release_groups.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+	frappe.reload_doc("press", "doctype", "release_group")
+	frappe.reload_doc("press", "doctype", "release_group_app")
+	frappe.reload_doc("press", "doctype", "release_group_server")
+
+	groups = frappe.get_all("Release Group")
+	for group in groups:
+		old_group_name = group.name
+		group = frappe.get_doc("Release Group", group.name)
+		group.set_new_name(force=True)
+		frappe.rename_doc("Release Group", old_group_name, group.name, force=True)
