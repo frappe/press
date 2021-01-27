8acb5ca7d7f2002bd01ae23bd39495383ddd1aaa
Aditya Hase <aditya@adityahase.com>
2021-01-27 19:03:55 +0530

    fix: Set MarketplaceApp.app from Marketplace.name

---
+++ press/patches/v0_0_1/set_marketplace_app_app_field_from_name.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+	frappe.reload_doc("press", "doctype", "marketplace_app")
+	apps = frappe.get_all("Marketplace App", {"app": ("is", "not set")})
+	for app in apps:
+		frappe.db.set_value(
+			"Marketplace App", app.name, "app", app.name, update_modified=False, modified=False
+		)
