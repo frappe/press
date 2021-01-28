0da4fb2e5fc29775e8b4ab977961687755ac4a3f
Aditya Hase <aditya@adityahase.com>
2021-01-28 10:27:25 +0530

    fix: Remove --progress flag from git clone command

--- press/press/doctype/app_release/app_release.py
+++ press/press/doctype/app_release/app_release.py
@@ -93,7 +93,7 @@ class AppRelease(Document):
 		self.output += self.run(f"git checkout -b {source.branch}")
 		self.output += self.run(f"git remote add origin {url}",)
 		self.output += self.run("git config credential.helper ''")
-		self.output += self.run(f"git fetch --depth 1 --progress origin {self.hash}")
+		self.output += self.run(f"git fetch --depth 1 origin {self.hash}")
 		self.output += self.run(f"git checkout {self.hash}")
 		self.output += self.run(f"git reset --hard {self.hash}")
 
