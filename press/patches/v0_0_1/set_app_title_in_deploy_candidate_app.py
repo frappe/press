4b579c0cd38b2bd539171655f7d79e21c0d483f6
Aditya Hase <aditya@adityahase.com>
2021-01-28 17:23:29 +0530

    fix: Set App title in Deploy Candidate App

---
+++ press/patches/v0_0_1/set_app_title_in_deploy_candidate_app.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+	apps = frappe.db.sql(
+		"""
+		SELECT
+			candidate.name, app.title
+		FROM
+			`tabDeploy Candidate App` candidate
+		LEFT JOIN
+			`tabApp` app
+		ON
+			candidate.app = app.name
+	""",
+		as_dict=True,
+	)
+
+	for app in apps:
+		frappe.db.set_value("Deploy Candidate App", app.name, "title", app.title)
