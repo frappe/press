6896e69e0831a2c430113e9d8490994f8330f7f6
Aditya Hase <aditya@adityahase.com>
2021-02-15 11:15:28 +0530

    fix(permissions): Restrict permissions on Deploy Candidate for Press Admin/Member
    Allow access to candidates of team owned groups.

--- press/press/doctype/deploy_candidate/deploy_candidate.py
+++ press/press/doctype/deploy_candidate/deploy_candidate.py
@@ -360,3 +360,31 @@ def desk_app(doctype, txt, searchfield, start, page_len, filters):
 		fields=["app"],
 		as_list=True,
 	)
+
+
+def get_permission_query_conditions(user):
+	from press.utils import get_current_team
+
+	if not user:
+		user = frappe.session.user
+	if frappe.session.data.user_type == "System User":
+		return ""
+
+	team = get_current_team()
+
+	return f"(`tabDeploy Candidate`.`team` = {frappe.db.escape(team)})"
+
+
+def has_permission(doc, ptype, user):
+	from press.utils import get_current_team
+
+	if not user:
+		user = frappe.session.user
+	if frappe.session.data.user_type == "System User":
+		return True
+
+	team = get_current_team()
+	if doc.team == team:
+		return True
+
+	return False
