bfcdf448c39f803dc0bde8dae9b6e451e112ced8
Aditya Hase <aditya@adityahase.com>
2021-02-15 11:01:59 +0530

    fix(permissions): Restrict permissions on Release Group for Press Admin/Member
    Allow access to public as well as team owned groups.
    Allow write access only to team owned groups.

--- press/hooks.py
+++ press/hooks.py
@@ -85,12 +85,14 @@ permission_query_conditions = {
 	"App Source": (
 		"press.press.doctype.app_source.app_source.get_permission_query_conditions"
 	),
+	"Release Group": "press.press.doctype.release_group.release_group.get_permission_query_conditions",
 }
 has_permission = {
 	"Site": "press.press.doctype.site.site.has_permission",
 	"Team": "press.press.doctype.team.team.has_permission",
 	"Invoice": "press.press.doctype.invoice.invoice.has_permission",
 	"App Source": "press.press.doctype.app_source.app_source.has_permission",
+	"Release Group": "press.press.doctype.release_group.release_group.has_permission",
 }
 
 # Document Events
