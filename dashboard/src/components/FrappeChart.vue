18d172de90bb407ab9e0372a0238d40d8a2a4aa1
Faris Ansari <netchamp.faris@gmail.com>
2021-02-23 18:08:09 +0530

    fix: Frappe Chart popups gets cut in Card

--- dashboard/src/components/FrappeChart.vue
+++ dashboard/src/components/FrappeChart.vue
@@ -34,6 +34,7 @@ export default {
 				});
 
 				this.updateGradient();
+				this.removeOverflowFromContainer();
 			});
 		},
 		updateGradient() {
@@ -48,6 +49,16 @@ export default {
 				linearGradient.children[0].attributes['offset'].value = '0%';
 				linearGradient.children[1].attributes['offset'].value = '25%';
 				linearGradient.children[2].attributes['offset'].value = '50%';
+			}
+		},
+		removeOverflowFromContainer() {
+			// Caution: Do not try this at home
+			// When FrappeChart is rendered inside a Card component, it adds an "overflow-auto" class.
+			// This will hide chart popups that go beyond the container.
+			// To fix it, we check if the parent container has that class and remove it.
+			// This is a very hacky solution. You're welcome.
+			if (this.$el?.parentElement?.classList.contains('overflow-auto')) {
+				this.$el.parentElement.classList.remove('overflow-auto');
 			}
 		}
 	}
