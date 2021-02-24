5811257d2a8f8d9245d4928f56cce7c8b9369ada
Faris Ansari <netchamp.faris@gmail.com>
2021-02-24 18:00:09 +0530

    refactor: ConfirmDialogs and NotificationToasts into separate files

---
+++ dashboard/src/components/NotificationToasts.vue
@@ -0,0 +1,44 @@
+<template>
+	<div
+		class="fixed inset-0 flex items-start justify-end px-4 py-6 pointer-events-none sm:p-6"
+	>
+		<div>
+			<Notification
+				class="mb-4"
+				:key="i"
+				v-for="(props, i) in notifications"
+				v-bind="props"
+				@dismiss="hideNotification"
+			/>
+		</div>
+	</div>
+</template>
+<script>
+import Vue from 'vue';
+import Notification from './Notification.vue';
+
+export default {
+	name: 'NotificationToasts',
+	data() {
+		return {
+			notifications: []
+		};
+	},
+	components: {
+		Notification
+	},
+	created() {
+		Vue.prototype.$notify = this.notify;
+	},
+	methods: {
+		notify(props) {
+			props.id = Math.floor(Math.random() * 1000 + Date.now());
+			this.notifications.push(props);
+			setTimeout(() => this.hideNotification(props.id), props.timeout || 5000);
+		},
+		hideNotification(id) {
+			this.notifications = this.notifications.filter(props => props.id !== id);
+		}
+	}
+};
+</script>
