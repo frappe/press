106bef07755836b1fcd918e27953ca7887968829
Faris Ansari <netchamp.faris@gmail.com>
2021-02-25 13:33:46 +0530

    fix: Remove legacy pdf workaround
    - Invoice PDFs are now generated from frappe.io

--- press/press/doctype/team/team.py
+++ press/press/doctype/team/team.py
@@ -320,13 +320,8 @@ class Team(Document):
 			order_by="due_date desc",
 		)
 
-		print_format = frappe.get_meta("Invoice").default_print_format
 		for invoice in invoices:
 			invoice.formatted_total = frappe.utils.fmt_money(invoice.total, 2, invoice.currency)
-			if invoice.currency == "USD" and not invoice.invoice_pdf:
-				invoice.invoice_pdf = frappe.utils.get_url(
-					f"/api/method/frappe.utils.print_format.download_pdf?doctype=Invoice&name={invoice.name}&format={print_format}&no_letterhead=0"
-				)
 		return invoices
 
 	def allocate_credit_amount(self, amount, source, remark=None):
