33acf3ac2b8b22a175e1af9d7ea16cb5085b78fd
Gavin D'souza <gavin18d@gmail.com>
2021-02-26 21:30:46 +0530

    feat: Team Deletion Request
    * On doc insert, an email for request verification is generated and sent
    to the team owner
    * On clicking of the email's link, the data deletion is triggered
    * The team deletion process consists of 4 distinct stages:
        1. Disabling team and its' users
        2. Data Deletion on frappe.io
        3. Data Deletion on Stripe
        4. Data Deletion on Press

---
+++ press/press/doctype/team_deletion_request/team_deletion_request.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2021, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Team Deletion Request', {
+	onload: function(frm) {
+		frappe.realtime.on("doc_update", data => {
+			if (!frm.is_dirty()) {
+				frm.reload_doc();
+			}
+		});
+	}
+});
