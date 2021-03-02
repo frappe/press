20198414e7fee3c491d5607db85a62cfbc398d80
Faris Ansari <netchamp.faris@gmail.com>
2021-03-02 15:44:08 +0530

    fix: Remove the evil button

--- press/press/doctype/invoice/invoice.js
+++ press/press/doctype/invoice/invoice.js
@@ -10,42 +10,6 @@ frappe.ui.form.on('Invoice', {
 			frm.add_web_link(`https://frappe.io/desk#Form/Sales Invoice/${frm.doc.frappe_invoice}`, 'View Frappe Invoice')
 		}
 
-		if (frm.doc.status === 'Unpaid') {
-			frm.add_custom_button('Mark as Paid', () => {
-				let d = new frappe.ui.Dialog({
-					title: 'Mark as Paid',
-					fields: [
-						{
-							fieldtype: 'HTML',
-							options:
-								'This action will consume credits and mark this invoice as Paid if enough credits are available. Will fail if not.',
-						},
-						{
-							fieldtype: 'Text',
-							fieldname: 'reason',
-							label: 'Reason',
-						},
-					],
-					primary_action({ reason }) {
-						frm
-							.call({
-								doc: frm.doc,
-								method: 'consume_credits_and_mark_as_paid',
-								args: { reason },
-								btn: d.get_primary_btn(),
-							})
-							.then((r) => {
-								if (!r.exc) {
-									frappe.show_alert('Success');
-								}
-								d.hide();
-							});
-					},
-				});
-				d.show();
-			});
-		}
-
 		if (frm.doc.status == "Paid" && !frm.doc.frappe_invoice) {
 			let btn = frm.add_custom_button(
 				"Create Invoice on frappe.io",
