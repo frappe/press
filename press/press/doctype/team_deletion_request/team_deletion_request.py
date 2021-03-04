0b33cee70a362fd6f057ef8f996e232a1f068500
Gavin D'souza <gavin18d@gmail.com>
2021-03-04 12:51:55 +0530

    fix: Use save + commit instead of db_set

--- press/press/doctype/team_deletion_request/team_deletion_request.py
+++ press/press/doctype/team_deletion_request/team_deletion_request.py
@@ -14,26 +14,57 @@ class TeamDeletionRequest(PersonalDataDeletionRequest):
 		super().__init__(*args, **kwargs)
 		self.email = self.team
 
+	def dont_throw(foo):
+		def pass_exception(self):
+			try:
+				foo(self)
+			except Exception:
+				frappe.db.rollback()
+
+				traceback = f"<br><br><pre><code>{frappe.get_traceback()}</pre></code>"
+				self.add_comment(text=f"Failure occurred during Data Deletion:{traceback}")
+
+				frappe.db.commit()
+
+		return pass_exception
+
+	def commit_after_execute(foo):
+		def after_execute(self):
+			try:
+				foo(self)
+			except Exception:
+				pass
+			else:
+				frappe.db.commit()
+
+		return after_execute
+
 	@property
 	def team_doc(self):
 		return frappe.get_cached_doc("Team", self.team)
 
 	def before_insert(self):
-		if frappe.db.exists(self.doctype, self.team):
-			frappe.throw(
-				f"{self.doctype} for {self.team} already exists!", exc=frappe.DuplicateEntryError
-			)
+		self.validate_duplicate_request()
 
 	def validate(self):
 		self.validate_sites_states()
 		self.validate_outstanding_invoices()
-		self.trigger_data_deletion()
 
-	def on_update(self):
-		if self.status == "Deleted" and self.name != self.team:
+	def rename_team_on_data_deletion(self):
+		if (
+			self.status == "Deleted"
+			and self.name != self.team
+			and frappe.db.exists("Team", self.team)
+		):
 			frappe.rename_doc("Team", self.team, self.name)
 
-	def trigger_data_deletion(self):
+	def validate_duplicate_request(self):
+		if frappe.db.exists(self.doctype, {"team": self.team}):
+			frappe.throw(
+				f"{self.doctype} for {self.team} already exists!", exc=frappe.DuplicateEntryError
+			)
+
+	def delete_team_data(self):
 		if self.status == "Processing Deletion":
 			if not self.team_disabled:
 				self.disable_team()
@@ -41,24 +72,47 @@ class TeamDeletionRequest(PersonalDataDeletionRequest):
 				self.delete_data_on_frappeio()
 			if not self.stripe_data_deleted:
 				self.delete_stripe_customer()
-			if not self.data_anonymized:
+			if (
+				True
+				or (self.team_disabled and self.frappeio_data_deleted and self.stripe_data_deleted)
+				and not self.data_anonymized
+			):
 				self.delete_data_on_press()
 			self.finish_up()
 
 	def finish_up(self):
-		self.db_set("status", "Deleted", update_modified=False, commit=True)
+		if (
+			self.team_disabled
+			and self.frappeio_data_deleted
+			and self.stripe_data_deleted
+			and self.data_anonymized
+		):
+			self.status = "Deleted"
+			self.save()
+			self.reload()
+			self.rename_team_on_data_deletion()
 
 	def generate_url_for_confirmation(self):
 		params = get_signed_params({"team": self.team})
 		api = frappe.utils.get_url("/api/method/press.api.account.delete_team")
-		return f"{api}?{params}"
+		url = f"{api}?{params}"
 
+		if frappe.conf.developer_mode:
+			print(f"URL generated for {self.doctype} {self.name}: {url}")
+
+		return url
+
+	@dont_throw
+	@commit_after_execute
 	def disable_team(self):
 		team = self.team_doc
 		team.enabled = False
 		team.save()
-		self.db_set("team_disabled", True, update_modified=False, commit=True)
+		self.team_disabled = True
+		self.save()
 
+	@dont_throw
+	@commit_after_execute
 	def delete_stripe_customer(self):
 		from press.api.billing import get_stripe
 
@@ -72,19 +126,27 @@ class TeamDeletionRequest(PersonalDataDeletionRequest):
 				raise e
 
 		team.db_set("stripe_customer_id", False)
-		self.db_set("stripe_data_deleted", True, update_modified=False, commit=True)
+		self.stripe_data_deleted = True
+		self.save()
 
+	@dont_throw
+	@commit_after_execute
 	def delete_data_on_frappeio(self):
 		"""Anonymize data on frappe.io"""
 		from press.utils.billing import get_frappe_io_connection
+
 		client = get_frappe_io_connection()
 		response = client.session.delete(
-			f"{client.url}/api/method/delete-fc-team",
-			data={"team": self.team},
+			f"{client.url}/api/method/delete-fc-team", data={"team": self.team},
 		)
-		if response.ok:
-			self.db_set("frappeio_data_deleted", True, update_modified=False, commit=True)
+		if not response.ok:
+			response.raise_for_status()
 
+		self.frappeio_data_deleted = True
+		self.save()
+
+	@dont_throw
+	@commit_after_execute
 	def delete_data_on_press(self):
 		# 1. rename team and team members to whatever else
 		team_members = [row.user for row in self.team_doc.team_members]
@@ -95,6 +157,7 @@ class TeamDeletionRequest(PersonalDataDeletionRequest):
 				"Team Member", {"user": user, "parent": ("!=", self.team_doc.name)}
 			)
 		]
+
 		def numerate_email(x, i):
 			user_email, domain = x.split("@")
 			return f"{user_email}-{i + 1}@{domain}"
@@ -106,7 +169,8 @@ class TeamDeletionRequest(PersonalDataDeletionRequest):
 		for now, then in renamed_dict.items():
 			self._anonymize_data(now, then)
 
-		self.db_set("data_anonymized", True, update_modified=False, commit=True)
+		self.data_anonymized = True
+		self.save()
 
 	def validate_sites_states(self):
 		non_archived_sites = frappe.get_all(
@@ -121,3 +185,17 @@ class TeamDeletionRequest(PersonalDataDeletionRequest):
 	def validate_outstanding_invoices(self):
 		if self.team_doc.is_defaulter():
 			frappe.throw(f"You have Unpaid Invoices. Clear them to delete your account")
+
+
+def process_team_deletion_requests():
+	doctype = "Team Deletion Request"
+	for name in frappe.get_all(
+		doctype,
+		filters={"status": ("in", ["Deletion Verified", "Processing Deletion"])},
+		pluck="name",
+	):
+		tdr = frappe.get_doc(doctype, name)
+		tdr.status = "Processing Deletion"
+		tdr.delete_team_data()
+		tdr.reload()
+		tdr.save()
