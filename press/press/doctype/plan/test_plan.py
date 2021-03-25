8548765f9cfec0f303f0b1bd74f6a3b9ab4271d6
Gavin D'souza <gavin18d@gmail.com>
2021-03-25 14:00:22 +0530

    test: Added base tests for Plans

--- press/press/doctype/plan/test_plan.py
+++ press/press/doctype/plan/test_plan.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2020, Frappe and Contributors
 # See license.txt
-from __future__ import unicode_literals
 
 import unittest
+from datetime import date
+from unittest.mock import patch
 
 import frappe
 
@@ -18,10 +19,40 @@ def create_test_plan(document_type: str):
 			"document_type": document_type,
 			"price_inr": 750.0,
 			"price_usd": 10.0,
-			"period": 30,
 		}
 	).insert(ignore_if_duplicate=True)
 
 
 class TestPlan(unittest.TestCase):
-	pass
+	def setUp(self):
+		self.plan = create_test_plan("Site")
+
+	def tearDown(self):
+		self.plan.delete()
+
+	def test_period_int(self):
+		self.assertIsInstance(self.plan.period, int)
+
+	def test_per_day_difference(self):
+		per_day_usd = self.plan.get_price_per_day("USD")
+		per_day_inr = self.plan.get_price_per_day("INR")
+		self.assertIsInstance(per_day_inr, (int, float))
+		self.assertIsInstance(per_day_usd, (int, float))
+		self.assertNotEqual(per_day_inr, per_day_usd)
+
+	def test_dynamic_period(self):
+		month_with_29_days = frappe.utils.get_last_day(date(2020, 2, 3))
+		month_with_30_days = frappe.utils.get_last_day(date(1997, 4, 3))
+
+		with patch.object(frappe.utils, "get_last_day", return_value=month_with_30_days):
+			self.assertEqual(self.plan.period, 30)
+			per_day_for_30_usd = self.plan.get_price_per_day("USD")
+			per_day_for_30_inr = self.plan.get_price_per_day("INR")
+
+		with patch.object(frappe.utils, "get_last_day", return_value=month_with_29_days):
+			self.assertEqual(self.plan.period, 29)
+			per_day_for_29_usd = self.plan.get_price_per_day("USD")
+			per_day_for_29_inr = self.plan.get_price_per_day("INR")
+
+		self.assertNotEqual(per_day_for_29_usd, per_day_for_30_usd)
+		self.assertNotEqual(per_day_for_29_inr, per_day_for_30_inr)
