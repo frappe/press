84a7120334ac01356a538c4b2508a162ed6a4c3b
Aditya Hase <aditya@adityahase.com>
2021-03-26 10:14:25 +0530

    fix: Remove Backbone CA support from TLS Certificates

--- press/press/doctype/tls_certificate/tls_certificate.py
+++ press/press/doctype/tls_certificate/tls_certificate.py
@@ -45,11 +45,7 @@ class TLSCertificate(Document):
 	def _obtain_certificate(self):
 		try:
 			settings = frappe.get_doc("Press Settings", "Press Settings")
-			if frappe.conf.developer_mode and settings.use_backbone_ca:
-				ca_class = Backbone
-			else:
-				ca_class = LetsEncrypt
-			ca = ca_class(settings)
+			ca = LetsEncrypt(settings)
 			(
 				self.certificate,
 				self.full_chain,
@@ -244,92 +240,3 @@ class LetsEncrypt(BaseCA):
 	@property
 	def private_key_file(self):
 		return os.path.join(self.directory, "live", self.domain, "privkey.pem")
-
-
-class Backbone(BaseCA):
-	def __init__(self, settings):
-		super().__init__(settings)
-		ca = settings.backbone_intermediate_ca
-		self.ca = frappe.get_doc("Certificate Authority", ca)
-
-	def run(self, command):
-		try:
-			subprocess.check_output(shlex.split(command), stderr=subprocess.STDOUT)
-		except Exception as e:
-			log_error("Backbone CA Exception", command=command, output=e.output.decode())
-			raise e
-
-	def _obtain(self):
-		self.directory = os.path.join(self.ca.directory, "..", self.domain)
-		if not os.path.exists(self.directory):
-			os.mkdir(self.directory)
-
-		template = "press/press/doctype/tls_certificate/server.conf"
-		with open(self.openssl_config_file, "w") as f:
-			openssl_config = frappe.render_template(template, {"doc": self}, is_path=True)
-			f.write(openssl_config)
-
-		self.generate_private_key()
-		self.generate_certificate_signing_request()
-		self.sign_certificate_signing_request()
-		self.generate_intermediate_chain_certificate()
-		self.generate_full_chain_certificate()
-
-	def generate_private_key(self):
-		self.run(f"openssl genrsa -out {self.private_key_file} {self.rsa_key_size}")
-		os.chmod(self.private_key_file, 400)
-
-	def generate_certificate_signing_request(self):
-		self.run(
-			f"openssl req -new -config {self.openssl_config_file} -key"
-			f" {self.private_key_file} -out {self.certificate_signing_request_file}"
-		)
-		os.chmod(self.certificate_signing_request_file, 444)
-
-	def sign_certificate_signing_request(self):
-		self.run(
-			"openssl ca -batch -notext -config"
-			f" {self.ca.openssl_config_file} -extensions server_cert -in"
-			f" {self.certificate_signing_request_file} -out {self.certificate_file}"
-		)
-		os.chmod(self.certificate_file, 444)
-
-	def generate_intermediate_chain_certificate(self):
-		with open(self.ca.certificate_file) as f:
-			ca_certificate = f.read()
-		with open(self.intermediate_chain_file, "w") as f:
-			f.write(ca_certificate)
-		os.chmod(self.intermediate_chain_file, 444)
-
-	def generate_full_chain_certificate(self):
-		with open(self.certificate_file) as f:
-			fullchain = f.read()
-		with open(self.intermediate_chain_file) as f:
-			fullchain += f.read()
-		with open(self.full_chain_file, "w") as f:
-			f.write(fullchain)
-		os.chmod(self.full_chain_file, 444)
-
-	@property
-	def openssl_config_file(self):
-		return os.path.join(self.directory, "openssl.conf")
-
-	@property
-	def certificate_signing_request_file(self):
-		return os.path.join(self.directory, "csr.pem")
-
-	@property
-	def certificate_file(self):
-		return os.path.join(self.directory, "cert.pem")
-
-	@property
-	def full_chain_file(self):
-		return os.path.join(self.directory, "fullchain.pem")
-
-	@property
-	def intermediate_chain_file(self):
-		return os.path.join(self.directory, "chain.pem")
-
-	@property
-	def private_key_file(self):
-		return os.path.join(self.directory, "key.pem")
