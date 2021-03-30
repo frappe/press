2fa2c434d51cb439cb318af1eb392deda67d11af
Balamurali M <balamurali@erpnext.com>
2021-03-30 16:57:35 +0530

    feat(Audit): Use correct status for bench field check

--- press/press/audit.py
+++ press/press/audit.py
@@ -9,7 +9,7 @@ from press.agent import Agent
 class Audit:
 	"""Base class for all types of Audit."""
 
-	def log(self, output):
+	def log(self, output, status="Success"):
 		output = pprint.pformat(output)
 		frappe.get_doc({"doctype": "Audit Log", "output": output}).insert()
 
@@ -20,6 +20,7 @@ class BenchFieldCheck(Audit):
 	def __init__(self):
 		servers = frappe.get_all("Server", pluck="name")
 		log = {}
+		status = "Success"
 		for server in servers:
 			agent = Agent(server)
 			benches = agent.get("/benches")
@@ -31,8 +32,9 @@ class BenchFieldCheck(Audit):
 					)
 				)
 				if sites_in_press != sites_in_server:
+					status = "Failure"
 					log[bench_name] = {
-						"press": list(sites_in_press.difference(sites_in_server)),
-						"server": list(sites_in_server.difference(sites_in_press)),
+						"Sites on press only": list(sites_in_press.difference(sites_in_server)),
+						"Sites on server only": list(sites_in_server.difference(sites_in_press)),
 					}
-		self.log(log)
+		self.log(log, status)
