312a90435e7a0ca5069898e879fba7d8edc5a316
Aditya Hase <aditya@adityahase.com>
2021-03-31 12:39:45 +0530

    fix: Set default_cluster in Root Domain based on previously determined default Cluster

--- press/patches/v0_0_1/create_root_domain_from_press_settings.py
+++ press/patches/v0_0_1/create_root_domain_from_press_settings.py
@@ -14,10 +14,12 @@ def execute():
 		and press_settings.aws_secret_access_key
 		and not frappe.db.exists("Root Domain", press_settings.domain)
 	):
+		default_cluster = frappe.db.get_value("Cluster", {"default": True})
 		frappe.get_doc(
 			{
 				"doctype": "Root Domain",
 				"name": press_settings.domain,
+				"default_cluster": default_cluster,
 				"dns_provider": press_settings.dns_provider,
 				"aws_access_key_id": press_settings.aws_access_key_id,
 				"aws_secret_access_key": press_settings.get_password("aws_secret_access_key"),
