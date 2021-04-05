d68e900ab277c53e5fc876722b5e832dd059956d
Balamurali M <balamurali@erpnext.com>
2021-04-05 16:47:33 +0530

    feat(OffsiteBackupCheck): Add audit for checking remote files

--- press/press/audit.py
+++ press/press/audit.py
@@ -1,6 +1,7 @@
 """Functions for automated audit of frappe cloud systems."""
 import pprint
 from datetime import datetime, timedelta
+from typing import List
 
 import frappe
 
@@ -75,4 +76,46 @@ class BackupRecordCheck(Audit):
 			):
 				status = "Failure"
 				log[self.site_list_key].append(site)
+		self.log(log, status)
+
+
+class OffsiteBackupCheck(Audit):
+	"""Check if files for offsite backup exists on the offsite backup provider."""
+
+	audit_type = "Offsite Backup Check"
+	list_key = "Offsite Backup Remote File unavailable in remote"
+
+	def _get_all_files_in_s3(self) -> List[str]:
+		all_files = []
+		settings = frappe.get_single("Press Settings")
+		s3 = settings.boto3_offsite_backup_session.resource("s3")
+		for s3_object in s3.Bucket(settings.aws_s3_bucket).objects.all():
+			all_files += s3_object.key
+		return all_files
+
+	def __init__(self):
+		log = {self.list_key: []}
+		status = "Success"
+		all_files = self._get_all_files_in_s3()
+		offsite_remote_files = frappe.db.sql(
+			"""
+			SELECT
+				remote_file.name, remote_file.file_path, site_backup.site
+			FROM
+				`tabRemote File` remote_file
+			JOIN
+				`tabSite Backup` site_backup
+			ON
+				site_backup.site=remote_file.site
+			WHERE
+				site_backup.status="Success" and
+				site_backup.files_availability="Available" and
+				site_backup.offsite=True
+			""",
+			as_dict=True,
+		)
+		for remote_file in offsite_remote_files:
+			if remote_file["file_path"] not in all_files:
+				status = "Failure"
+				log[self.list_key].append(remote_file)
 		self.log(log, status)
