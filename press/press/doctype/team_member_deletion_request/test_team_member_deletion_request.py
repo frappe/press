a6acab2d377fe194a858a869f7287cd41f2450e8
Gavin D'souza <gavin18d@gmail.com>
2021-04-26 18:14:03 +0530

    fix: Delete team data in steps
    * Use Personal Data Deletion's step tracking to remember deletion progress
    * Commit after each stage to avoid loss of progress

---
+++ press/press/doctype/team_member_deletion_request/test_team_member_deletion_request.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Frappe and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+
+class TestTeamMemberDeletionRequest(unittest.TestCase):
+	pass
