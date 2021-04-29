d85cc7065fa645b031449278454665f218b79857
Balamurali M <balamurali@erpnext.com>
2021-04-29 20:46:55 +0530

    fix(ERPNextConsultantRegion): Recreate the doctype
    ERPNext Consultant Region multiselect field kept throwing unique
    exception.

--- press/press/doctype/erpnext_consultant_region/erpnext_consultant_region.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, Frappe and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('ERPNext Consultant Region', {
-	// refresh: function(frm) {
-
-	// }
-});
