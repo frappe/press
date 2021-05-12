809a53b5f2fd589326ebd42b6509f11957ec4f48
Faris Ansari <netchamp.faris@gmail.com>
2021-05-12 15:44:07 +0530

    fix: /files over dev server proxy
    Fixed by removing changeOrigin, frappe server infers the sitename from hostname

--- dashboard/proxyOptions.js
+++ dashboard/proxyOptions.js
@@ -5,7 +5,6 @@ module.exports = {
 	'^/(app|api|assets|files)': {
 		target: `http://localhost:${webserver_port}`,
 		ws: true,
-		changeOrigin: true,
 		router: function(req) {
 			const site_name = req.headers.host.split(':')[0];
 			return `http://${site_name}:${webserver_port}`;
