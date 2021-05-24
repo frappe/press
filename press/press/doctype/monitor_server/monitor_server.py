f282c3f1fa38315df9fbf9b3f22304c537af090e
Aditya Hase <aditya@adityahase.com>
2021-05-24 19:50:52 +0530

    feat: Setup Monitor Server

---
+++ press/press/doctype/monitor_server/monitor_server.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Frappe and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+import json
+
+
+from press.press.doctype.server.server import BaseServer
+from press.runner import Ansible
+from press.utils import log_error
+
+
+class MonitorServer(BaseServer):
+	def validate(self):
+		self.validate_agent_password()
+		self.validate_grafana_password()
+		self.validate_monitoring_password()
+
+	def validate_monitoring_password(self):
+		if not self.monitoring_password:
+			self.monitoring_password = frappe.generate_hash()
+
+	def validate_grafana_password(self):
+		if not self.grafana_password:
+			self.grafana_password = frappe.generate_hash(length=32)
+
+	def _setup_server(self):
+		agent_password = self.get_password("agent_password")
+		monitoring_password = self.get_password("monitoring_password")
+		certificate_name = frappe.db.get_value(
+			"TLS Certificate", {"wildcard": True, "domain": self.domain}, "name"
+		)
+		certificate = frappe.get_doc("TLS Certificate", certificate_name)
+
+		registries = []
+		for registry in frappe.get_all("Registry Server"):
+			registry = frappe.get_doc("Registry Server", registry.name)
+			registries.append(
+				{
+					"name": registry.name,
+					"monitoring_password": registry.get_password("monitoring_password"),
+				}
+			)
+
+		clusters = []
+		for cluster in frappe.get_all("Cluster"):
+			cluster = frappe.get_doc("Cluster", cluster.name)
+			clusters.append(
+				{
+					"name": cluster.name,
+					"monitoring_password": cluster.get_password("monitoring_password"),
+				}
+			)
+		press_url = frappe.utils.get_url()
+		monitor_token = frappe.db.get_single_value("Press Settings", "monitor_token")
+		try:
+			ansible = Ansible(
+				playbook="monitor.yml",
+				server=self,
+				variables={
+					"server": self.name,
+					"workers": 1,
+					"domain": self.domain,
+					"agent_password": agent_password,
+					"monitor": True,
+					"monitoring_password": monitoring_password,
+					"press_url": press_url,
+					"monitor_token": monitor_token,
+					"registries_json": json.dumps(registries),
+					"clusters_json": json.dumps(clusters),
+					"private_ip": self.private_ip,
+					"grafana_password": self.get_password("grafana_password"),
+					"certificate_private_key": certificate.private_key,
+					"certificate_full_chain": certificate.full_chain,
+					"certificate_intermediate_chain": certificate.intermediate_chain,
+				},
+			)
+			play = ansible.run()
+			self.reload()
+			if play.status == "Success":
+				self.status = "Active"
+				self.is_server_setup = True
+			else:
+				self.status = "Broken"
+		except Exception:
+			self.status = "Broken"
+			log_error("Monitor Server Setup Exception", server=self.as_dict())
+		self.save()
+
+	@frappe.whitelist()
+	def reconfigure_monitor_server(self):
+		frappe.enqueue_doc(
+			self.doctype, self.name, "_reconfigure_monitor_server", queue="long", timeout=1200
+		)
+
+	def _reconfigure_monitor_server(self):
+		monitoring_password = self.get_password("monitoring_password")
+
+		registries = []
+		for registry in frappe.get_all("Registry Server"):
+			registry = frappe.get_doc("Registry Server", registry.name)
+			registries.append(
+				{
+					"name": registry.name,
+					"monitoring_password": registry.get_password("monitoring_password"),
+				}
+			)
+
+		clusters = []
+		for cluster in frappe.get_all("Cluster"):
+			cluster = frappe.get_doc("Cluster", cluster.name)
+			clusters.append(
+				{
+					"name": cluster.name,
+					"monitoring_password": cluster.get_password("monitoring_password"),
+				}
+			)
+
+		try:
+			ansible = Ansible(
+				playbook="reconfigure_monitoring.yml",
+				server=self,
+				variables={
+					"server": self.name,
+					"monitoring_password": monitoring_password,
+					"registries_json": json.dumps(registries),
+					"clusters_json": json.dumps(clusters),
+					"grafana_password": self.get_password("grafana_password"),
+				},
+			)
+			ansible.run()
+		except Exception:
+			log_error("Monitoring Server Setup Exception", server=self.as_dict())
--- press/press/doctype/monitor_server/test_monitor_server.py
+++ press/press/doctype/monitor_server/test_monitor_server.py
@@ -7,5 +7,5 @@ from __future__ import unicode_literals
 import unittest
 
 
-class TestMonitoringServer(unittest.TestCase):
+class TestMonitorServer(unittest.TestCase):
 	pass
