c420a72caba76cbe7f4dcf9165135877c8270486
Balamurali M <balamurali@erpnext.com>
2021-05-25 10:27:54 +0530

    perf(Site): Query only sites with domains

--- press/patches/v0_0_1/add_domains_in_site_config_preview.py
+++ press/patches/v0_0_1/add_domains_in_site_config_preview.py
@@ -3,18 +3,29 @@
 # Proprietary License. See license.txt
 
 from __future__ import unicode_literals
+
+from itertools import groupby
+
 import frappe
 
 
 def execute():
-	sites = frappe.get_all("Site", {"status": ("!=", "Archived")}, pluck="name")
-	for site_name in sites:
-		domains = frappe.get_all(
-			"Site Domain",
-			{"site": site_name, "name": ("!=", site_name), "status": "Active"},
-			pluck="name",
-		)
-		if not domains:
-			continue
-		site = frappe.get_doc("Site", site_name)
+	domain_site_list = frappe.db.sql(
+		"""
+		SELECT site_domain.name, site.name
+		FROM `tabSite Domain` site_domain
+		JOIN tabSite site
+		ON site_domain.site = site.name
+		WHERE
+			site_domain.name != site_domain.site and
+			site_domain.status = "Active" and
+			site.status != "Archived"
+		ORDER BY
+			site.name
+		"""
+	)
+	domain_site_list = groupby(domain_site_list, lambda x: x[1])
+	for site, domains_tuple in domain_site_list:
+		domains = [t[0] for t in list(domains_tuple)]
+		site = frappe.get_cached_doc("Site", site)
 		site._update_configuration({"domains": domains})
