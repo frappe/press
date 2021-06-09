851d81d7e9a3fce07a00cd6d34698c8f3cfa35c4
Aditya Hase <aditya@adityahase.com>
2021-06-09 14:10:41 +0530

    feat: Add Prometheus Alert Rule DocType
    Configures prometheus alert rules and alertmanager routes

---
+++ press/press/doctype/prometheus_alert_rule/prometheus_alert_rule.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2021, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Prometheus Alert Rule', {
+	onload_post_render: function(frm) {
+		frm.trigger("resize_code_fields");
+	},
+	resize_code_fields: function(frm) {
+		setTimeout(() => {
+			frm.fields.map(field => {
+				if (field.ace_editor_target) {
+					field.ace_editor_target.css("height", 100);
+					field.editor.resize();
+				}	
+			})
+		}, 1000);
+	}
+});
