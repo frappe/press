849ba8ffc5c50c27caaadee10e41c07bc65e59ce
Aditya Hase <aditya@adityahase.com>
2021-06-15 11:32:51 +0530

    fix: Don't schedule multiple update jobs from the same bench

--- press/press/doctype/site_update/site_update.py
+++ press/press/doctype/site_update/site_update.py
@@ -182,8 +182,11 @@ def schedule_updates():
 	# Shuffle sites list, to achieve this
 	random.shuffle(sites)
 
+	benches = {}
 	update_triggered_count = 0
 	for site in sites:
+		if site.bench in benches:
+			continue
 		if update_triggered_count > queue_size:
 			break
 		try:
@@ -191,6 +194,7 @@ def schedule_updates():
 			site.schedule_update()
 			update_triggered_count += 1
 			frappe.db.commit()
+			benches[site.bench] = True
 		except Exception:
 			log_error("Site Update Exception", site=site)
 			frappe.db.rollback()
