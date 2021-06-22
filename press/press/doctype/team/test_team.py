97615b9739726f830c309fdcdd39f4c0f90c3be8
Balamurali M <balamurali@erpnext.com>
2021-06-22 22:53:25 +0530

    test(Team): Add tests for team creation

--- press/press/doctype/team/test_team.py
+++ press/press/doctype/team/test_team.py
@@ -2,6 +2,11 @@
 # Copyright (c) 2020, Frappe and Contributors
 # See license.txt
 from __future__ import unicode_literals
+from unittest.mock import patch
+from press.press.doctype.account_request.test_account_request import (
+	create_test_account_request,
+)
+from press.press.doctype.team.team import Team
 
 import frappe
 import unittest
@@ -15,4 +20,23 @@ def create_test_team(email: str = frappe.mock("email")):
 
 
 class TestTeam(unittest.TestCase):
-	pass
+	def tearDown(self):
+		frappe.db.rollback()
+
+	def test_create_new_method_works(self):
+		account_request = create_test_account_request("testsubdomain")
+		team_count_before = frappe.db.count("Team")
+		with patch.object(Team, "create_stripe_customer"):
+			Team.create_new(
+				account_request, "first name", "last name", "test@email.com", country="India"
+			)
+		team_count_after = frappe.db.count("Team")
+		self.assertGreater(team_count_after, team_count_before)
+
+	def test_new_team_has_correct_billing_name(self):
+		account_request = create_test_account_request("testsubdomain")
+		with patch.object(Team, "create_stripe_customer"):
+			team = Team.create_new(
+				account_request, "first name", "last name", "test@email.com", country="India"
+			)
+		self.assertEqual(team.billing_name, "first name last name")
