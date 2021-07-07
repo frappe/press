8be27e13e2b3cac2139387f7694927140e3a68dc
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-07 16:28:47 +0530

    feat: Update links UI

--- dashboard/src/components/DeveloperAppLinks.vue
+++ dashboard/src/components/DeveloperAppLinks.vue
@@ -1,5 +1,49 @@
 <template>
-	<Card title="Links" subtitle="Will be shown in marketplace"> </Card>
+	<Card title="Links" subtitle="Will be shown in marketplace">
+		<template #actions>
+			<Button @click="showEditLinksDialog = true">Edit Links</Button>
+		</template>
+		<Dialog title="Update Links for App" v-model="showEditLinksDialog">
+			<div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+				<Input label="Website" type="text" v-model="app.website" />
+				<Input label="Support" type="text" v-model="app.support" />
+				<Input label="Documentation" type="text" v-model="app.documentation" />
+				<Input
+					label="Privacy Policy"
+					type="text"
+					v-model="app.privacy_policy"
+				/>
+				<Input
+					label="Terms of Service"
+					type="text"
+					v-model="app.terms_of_service"
+				/>
+			</div>
+
+			<ErrorMessage class="mt-4" :error="null" />
+
+			<template #actions>
+				<div class="space-x-2">
+					<Button @click="showEditLinksDialog = false">Cancel</Button>
+					<Button
+						type="primary"
+						:loading="$resources.updateAppLinks.loading"
+						loadingText="Saving..."
+						@click="$resources.updateAppLinks.submit()"
+					>
+						Save changes
+					</Button>
+				</div>
+			</template>
+		</Dialog>
+		<div class="divide-y" v-if="app">
+			<ListItem title="Website" :description="app.website" />
+			<ListItem title="Support" :description="app.support" />
+			<ListItem title="Documentation" :description="app.documentation" />
+			<ListItem title="Privacy Policy" :description="app.privacy_policy" />
+			<ListItem title="Terms of Service" :description="app.terms_of_service" />
+		</div>
+	</Card>
 </template>
 
 <script>
@@ -7,6 +51,36 @@ export default {
 	name: 'DeveloperAppLinks',
 	props: {
 		app: Object
+	},
+	data() {
+		return {
+			showEditLinksDialog: false
+		};
+	},
+	resources: {
+		updateAppLinks() {
+			return {
+				method: 'press.api.developer.update_app_links',
+				params: {
+					name: this.app.name,
+					links: {
+						website: this.app.website,
+						support: this.app.support,
+						documentation: this.app.documentation,
+						privacy_policy: this.app.privacy_policy,
+						terms_of_service: this.app.terms_of_service
+					}
+				},
+				onSuccess() {
+					this.showEditLinksDialog = false;
+					this.$notify({
+						title: 'Links Updated!',
+						icon: 'check',
+						color: 'green'
+					});
+				}
+			};
+		}
 	}
 };
 </script>
