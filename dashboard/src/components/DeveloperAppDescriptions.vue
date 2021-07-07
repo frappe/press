4a639f25ae05e81be2b52d365d546b33f4e61448
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-07 14:56:35 +0530

    feat: Developer App Descriptions

---
+++ dashboard/src/components/DeveloperAppDescriptions.vue
@@ -0,0 +1,17 @@
+<template>
+	<Card
+		class="col-span-2"
+		title="App Descriptions"
+		subtitle="Details about your app"
+	>
+	</Card>
+</template>
+
+<script>
+export default {
+	name: 'DeveloperAppDescriptions',
+	props: {
+		app: Object
+	}
+};
+</script>
