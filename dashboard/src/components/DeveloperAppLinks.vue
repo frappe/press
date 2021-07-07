f67b895146dd39866d13abe8848b0a3931e00b4f
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-07 14:51:46 +0530

    feat: Developer App Links

---
+++ dashboard/src/components/DeveloperAppLinks.vue
@@ -0,0 +1,12 @@
+<template>
+	<Card title="Links" subtitle="Will be shown in marketplace"> </Card>
+</template>
+
+<script>
+export default {
+	name: 'DeveloperAppLinks',
+	props: {
+		app: Object
+	}
+};
+</script>
