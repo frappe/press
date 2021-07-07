ff5af57cf8ddcb0839aef020d2239a3b2314a1f9
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-07 14:51:46 +0530

    feat: Developer App Links

--- dashboard/src/views/DeveloperAppOverview.vue
+++ dashboard/src/views/DeveloperAppOverview.vue
@@ -1,11 +1,13 @@
 <template>
 	<div class="grid grid-cols-1 gap-5 md:grid-cols-2">
 		<DeveloperAppProfile :app="app" />
+		<DeveloperAppLinks :app="app" />
 	</div>
 </template>
 
 <script>
 import DeveloperAppProfile from '@/components/DeveloperAppProfile.vue';
+import DeveloperAppLinks from '@/components/DeveloperAppLinks.vue';
 
 export default {
 	name: 'DeveloperAppOverview',
@@ -13,7 +15,8 @@ export default {
 		app: Object
 	},
 	components: {
-		DeveloperAppProfile
+		DeveloperAppProfile,
+		DeveloperAppLinks
 	}
 };
 </script>
