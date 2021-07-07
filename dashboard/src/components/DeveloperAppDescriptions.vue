abe2a0946201487f59b1860963d504154b1d8d8b
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-07 19:18:03 +0530

    feat: App Description Preview

--- dashboard/src/components/DeveloperAppDescriptions.vue
+++ dashboard/src/components/DeveloperAppDescriptions.vue
@@ -4,14 +4,54 @@
 		title="App Descriptions"
 		subtitle="Details about your app"
 	>
+		<div class="divide-y" v-if="app">
+			<ListItem title="Summary" :description="app.description">
+				<template #actions>
+					<Button icon-left="edit" @click="showEditSummaryDialog = true">
+						Edit
+					</Button>
+				</template>
+			</ListItem>
+			<div class="py-3">
+				<ListItem title="Long Description">
+					<template #actions>
+						<Button icon-left="edit" @click="showEditDescriptionDialog = true">
+							Edit
+						</Button>
+					</template>
+				</ListItem>
+				<div
+					class="mt-1 prose text-gray-600"
+					v-if="app.description"
+					v-html="descriptionHTML"
+				></div>
+			</div>
+		</div>
 	</Card>
 </template>
 
 <script>
+import MarkdownIt from 'markdown-it';
+
 export default {
 	name: 'DeveloperAppDescriptions',
 	props: {
 		app: Object
+	},
+	data() {
+		return {
+			showEditSummaryDialog: false,
+			showEditDescriptionDialog: false
+		};
+	},
+	computed: {
+		descriptionHTML() {
+			if (this.app && this.app.long_description) {
+				return MarkdownIt().render(this.app.long_description);
+			}
+
+			return '';
+		}
 	}
 };
 </script>
