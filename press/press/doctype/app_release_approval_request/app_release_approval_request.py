de4d886dc08a399e8806d050880a7ed750815e97
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-12 17:35:26 +0530

    feat: Handle Approval Request status change

--- press/press/doctype/app_release_approval_request/app_release_approval_request.py
+++ press/press/doctype/app_release_approval_request/app_release_approval_request.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2021, Frappe and contributors
 # For license information, please see license.txt
 
+from shutil import ignore_patterns
 import frappe
 
 from frappe.model.document import Document
@@ -11,6 +12,7 @@ from press.press.doctype.app_release.app_release import AppRelease
 class AppReleaseApprovalRequest(Document):
 	@classmethod
 	def create(cls, marketplace_app: str, app_release: str):
+		"""Create a new `App Release Approval Request`"""
 		request: cls = frappe.new_doc("App Release Approval Request")
 		request.marketplace_app = marketplace_app
 		request.app_release = app_release
@@ -28,3 +30,26 @@ class AppReleaseApprovalRequest(Document):
 		release: AppRelease = frappe.get_doc("App Release", self.app_release)
 		release.status = "Awaiting Approval"
 		release.save(ignore_permissions=True)
+
+	def on_update(self):
+		old_doc = self.get_doc_before_save()
+		status_updated = old_doc.status != self.status
+
+		release = frappe.get_doc("App Release", self.app_release)
+
+		if status_updated and self.status == "Rejected":
+			release.status = "Rejected"
+			self.notifty_publisher()
+		elif status_updated and self.status == "Approved":
+			release.status = "Published"
+			self.notifty_publisher()
+		elif status_updated and self.status == "Cancelled":
+			release.status = "Draft"
+
+		release.save(ignore_permissions=True)
+
+	def notifty_publisher(self):
+		if self.status == "Rejected":
+			print("Hey, your app was Rejected. Work hard!")
+		elif self.status == "Approved":
+			print("Congratz! Your app release was approved and is published now!")
