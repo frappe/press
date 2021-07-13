2bacbc9ad98d89359a17699ee7f4cd526d87dae0
Aditya Hase <aditya@adityahase.com>
2021-07-13 20:56:28 +0530

    fix: Use private ip address to route traffic to upstream
    Rename existing upstreams from public to private ip

---
+++ press/patches/v0_0_1/use_private_ip_for_upstreams.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and Contributors
+# Proprietary License. See license.txt
+
+from __future__ import unicode_literals
+
+import frappe
+from press.agent import Agent
+
+
+def execute():
+	servers = frappe.get_all(
+		"Server", {"is_upstream_setup": True, "status": "Active"}, ["name", "proxy_server"],
+	)
+	for server in servers:
+		agent = Agent(server.proxy_server, "Proxy Server")
+		agent.update_upstream_private_ip(server.name)
