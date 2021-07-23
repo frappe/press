da2a5e9cbdeb6c52e0884479e433533cf4730065
Balamurali M <balamurali@erpnext.com>
2021-07-23 10:27:41 +0530

    feat(StagingSite): Use staging plan for staging sites

--- press/press/doctype/site/staging_site.py
+++ press/press/doctype/site/staging_site.py
@@ -2,17 +2,23 @@
 # MIT License. See license.txt
 
 from __future__ import unicode_literals
+
 from datetime import datetime, timedelta
 
 import frappe
 from frappe.model.naming import make_autoname
 
 from press.press.doctype.site.site import Site
+from press.utils import log_error
 
 
 class StagingSite(Site):
 	def __init__(self, bench_name: str):
 		bench = frappe.get_doc("Bench", bench_name)
+		plan = frappe.db.get_value("Press Settings", None, "staging_plan")
+		if not plan:
+			frappe.throw("Staging plan not set in settings")
+			log_error(title="Staging plan not set in settings")
 		super().__init__(
 			{
 				"doctype": "Site",
@@ -21,12 +27,13 @@ class StagingSite(Site):
 				"bench": bench_name,
 				"apps": [{"app": app.app} for app in bench.apps],
 				"team": "Administrator",
+				"subscription_plan": plan,
 			}
 		)
 
 
 def archive_expired_sites():
-	expiry = frappe.db.get_value("Press Settings", None, "staging_site_expiry")
+	expiry = frappe.db.get_value("Press Settings", None, "staging_expiry")
 	sites = frappe.get_all(
 		"Site", {"staging": True, "created_on": ("<", datetime.now() - timedelta(expiry))}
 	)
