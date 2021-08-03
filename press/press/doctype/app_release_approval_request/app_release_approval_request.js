c3acf08ee3ee020652252daa2099a1f299da8171
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-08-03 16:15:47 +0530

    feat: Populate approved by on status change

--- press/press/doctype/app_release_approval_request/app_release_approval_request.js
+++ press/press/doctype/app_release_approval_request/app_release_approval_request.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('App Release Approval Request', {
-	// refresh: function(frm) {
-
-	// }
+	status(frm) {
+		frm.set_value('approved_by', frappe.session.user);
+	},
 });
