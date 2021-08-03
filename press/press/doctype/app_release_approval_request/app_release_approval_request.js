5a48d1fcd5ee34f997e8095503f2adccd681e4ba
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-08-03 16:23:55 +0530

    fix: Button to ease approval

--- press/press/doctype/app_release_approval_request/app_release_approval_request.js
+++ press/press/doctype/app_release_approval_request/app_release_approval_request.js
@@ -2,6 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('App Release Approval Request', {
+	refresh(frm) {
+		frm.add_custom_button('Approve Request', () => {
+			frm.set_value('status', 'Approved');
+			frm.save();
+		});
+	},
 	status(frm) {
 		frm.set_value('approved_by', frappe.session.user);
 	},
