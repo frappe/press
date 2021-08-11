4a2c438d0c2db730fdd34aa499a39b5f01d5afc1
Faris Ansari <netchamp.faris@gmail.com>
2021-08-11 15:55:52 +0530

    chore: remove legacy billing doctypes

---
+++ press/patches/v0_0_4/remove_legacy_billing_doctypes.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and Contributors
+# Proprietary License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+	# these doctypes are only deleted from DocType table, their tables will exist
+	doctypes = ["Payment", "Payment Ledger Entry"]
+	frappe.db.sql("DELETE from tabDocType where name in %s", [doctypes])
