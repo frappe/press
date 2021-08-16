5050107778f818c4e4e848b79c405093db92b00d
Balamurali M <balamurali@erpnext.com>
2021-08-16 08:55:02 +0530

    fix: Handle no frappe.io auth url

--- press/hooks.py
+++ press/hooks.py
@@ -67,7 +67,7 @@ website_route_rules = [
 ]
 
 website_redirects = [
-	{"source": "/dashboard/f-login", "target": get_frappe_io_auth_url() or ""},
+	{"source": "/dashboard/f-login", "target": get_frappe_io_auth_url() or "/"},
 	{"source": "/f-login", "target": "/dashboard/f-login"},
 ]
 
