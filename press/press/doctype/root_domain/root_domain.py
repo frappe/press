bf7003f671d2c49606c1debb403dbe7852fb0ba0
Balamurali M <balamurali@erpnext.com>
2021-08-20 10:16:03 +0530

    feat: Periodically remove unused CNAME records (#181)
    * refactor: Add properties for boto3 client & hosted zone
    
    * refactor(RootDomain): Move cname cleanup to root_domain.py
    
    * feat: Cleanup cname records periodically
    
    * chore: Implement own cache for python 3.6
    
    * fix(RootDomain): Skip sites being renamed when deleting CNAME records

--- press/press/doctype/root_domain/root_domain.py
+++ press/press/doctype/root_domain/root_domain.py
@@ -3,8 +3,14 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from datetime import datetime, timedelta
+import json
+from typing import Iterable, List
+
+import boto3
 import frappe
 from frappe.model.document import Document
+
 from press.utils import log_error
 
 
@@ -33,3 +39,94 @@ class RootDomain(Document):
 			).insert()
 		except Exception:
 			log_error("Root Domain TLS Certificate Exception")
+
+	def boto3_client(self):
+		if not self._boto3_client:
+			self._boto3_client = boto3.client(
+				"route53",
+				aws_access_key_id=self.aws_access_key_id,
+				aws_secret_access_key=self.get_password("aws_secret_access_key"),
+			)
+		return self._boto3_client
+
+	@property
+	def hosted_zone(self):
+		return self.boto3_client.list_hosted_zones_by_name(DNSName=self.name)["HostedZones"][
+			0
+		]["Id"]
+
+	def get_dns_record_pages(self) -> Iterable:
+		try:
+			paginator = self.boto3_client.get_paginator("list_resource_record_sets")
+			return paginator.paginate(
+				PaginationConfig={"MaxItems": 1000, "PageSize": 300, "StartingToken": "0"},
+				HostedZoneId=self.hosted_zone.split("/")[-1],
+			)
+		except Exception:
+			log_error(
+				"Route 53 Pagination Error", domain=self.name,
+			)
+
+	def delete_dns_records(self, records: List[str], proxy: str):
+		try:
+			changes = []
+			for record in records:
+				changes.append(
+					{
+						"Action": "DELETE",
+						"ResourceRecordSet": {
+							"Name": record,
+							"Type": "CNAME",
+							"TTL": 60,
+							"ResourceRecords": [{"Value": proxy}],
+						},
+					}
+				)
+
+			self.boto3_client.change_resource_record_sets(
+				ChangeBatch={"Changes": changes}, HostedZoneId=self.hosted_zone,
+			)
+
+		except Exception:
+			log_error("Route 53 Record Deletion Error", domain=self.name, proxy=proxy)
+
+	def get_sites_being_renamed(self):
+		# get sites renamed in Server but doc not renamed in press
+		last_hour = datetime.now() - timedelta(hours=1)  # very large bound just to be safe
+		renaming_sites = frappe.get_all(
+			"Agent Job",
+			{"job_type": "Rename Site", "creation": (">=", last_hour)},
+			pluck="request_data",
+		)
+		return [json.loads(d_str)["new_name"] for d_str in renaming_sites]
+
+	def get_active_domains(self):
+		active_sites = frappe.get_all(
+			"Site", {"status": ("!=", "Archived"), "domain": self.name}, pluck="name"
+		)
+		active_sites.extend(self.get_sites_being_renamed())
+		return active_sites
+
+	def remove_unused_cname_records(self, proxy: str):
+		for page in self.get_dns_record_pages():
+			to_delete = []
+
+			frappe.db.commit()
+			active = self.get_active_domains()
+
+			for record in page["ResourceRecordSets"]:
+				if record["Type"] == "CNAME" and record["ResourceRecords"][0]["Value"] == proxy:
+					domain = record["Name"].strip(".")
+					if domain not in active:
+						to_delete.append(domain)
+			if to_delete:
+				self.delete_dns_records(to_delete, proxy)
+
+
+def cleanup_cname_records():
+	domains = frappe.get_all("Root Domain", pluck="name")
+	proxies = frappe.get_all("Proxy Server", pluck="name")
+	for proxy in proxies:
+		for domain_name in domains:
+			domain = frappe.get_doc("Root Domain", domain_name)
+			domain.remove_unused_cname_records(proxy)
--- press/press/doctype/root_domain/test_root_domain.py
+++ press/press/doctype/root_domain/test_root_domain.py
@@ -2,14 +2,22 @@
 # Copyright (c) 2021, Frappe and Contributors
 # See license.txt
 from __future__ import unicode_literals
-from press.press.doctype.root_domain.root_domain import RootDomain
+from datetime import datetime, timedelta
 
+import json
 import unittest
 from unittest.mock import Mock, patch
 
 import frappe
 
+from press.press.doctype.agent_job.agent_job import AgentJob
 from press.press.doctype.cluster.test_cluster import create_test_cluster
+from press.press.doctype.database_server.test_database_server import (
+	create_test_database_server,
+)
+from press.press.doctype.proxy_server.test_proxy_server import create_test_proxy_server
+from press.press.doctype.root_domain.root_domain import RootDomain
+from press.press.doctype.server.test_server import create_test_server
 
 
 @patch.object(RootDomain, "after_insert", new=Mock())
@@ -25,5 +33,35 @@ def create_test_root_domain(name: str):
 	).insert(ignore_if_duplicate=True)
 
 
+@patch.object(AgentJob, "after_insert", new=Mock())
 class TestRootDomain(unittest.TestCase):
-	pass
+	def tearDown(self):
+		frappe.db.rollback()
+
+	def _create_fake_rename_job(self, site_name: str, creation=datetime.now()):
+		server = create_test_server(
+			create_test_proxy_server().name, create_test_database_server().name
+		)
+
+		frappe.get_doc(
+			{
+				"doctype": "Agent Job",
+				"job_type": "Rename Site",
+				"request_data": json.dumps({"new_name": site_name}),
+				"server_type": "Server",
+				"server": server.name,
+				"request_path": "fake/rename/path",
+				"creation": creation,
+			}
+		).insert(ignore_if_duplicate=True)
+
+	def test_sites_being_renamed_are_considered_active(self):
+		new_site_name = "new_site.frappe.cloud"
+		old_site_name = "old_site.frappe.cloud"
+		root_domain = create_test_root_domain("frappe.dev")
+
+		self._create_fake_rename_job(new_site_name)
+		self._create_fake_rename_job(old_site_name, datetime.now() - timedelta(hours=2))
+
+		self.assertIn(new_site_name, root_domain.get_active_domains())
+		self.assertNotIn(old_site_name, root_domain.get_active_domains())
