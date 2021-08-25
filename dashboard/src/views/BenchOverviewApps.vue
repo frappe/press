1fb9b3088b04adfd5d1186ec78d4287e0d7e39b3
Faris Ansari <netchamp.faris@gmail.com>
2021-08-25 18:27:58 +0530

    feat: Sites tab in bench to show sites and apps grouped by version

--- dashboard/src/views/BenchOverviewApps.vue
+++ dashboard/src/views/BenchOverviewApps.vue
@@ -15,30 +15,28 @@
 			</Button>
 		</template>
 		<div class="divide-y">
-			<div
-				class="flex items-center py-3"
+			<ListItem
 				v-for="app in apps.data"
 				:key="app.name"
+				:title="app.title"
+				:subtitle="`${app.repository_owner}/${app.repository}:${app.branch}`"
 			>
-				<div class="w-1/3 text-base font-medium">
-					{{ app.title }}
-				</div>
-				<div class="text-base text-gray-700">
-					{{ app.repository_owner }}:{{ app.branch }}
-				</div>
-				<div class="flex items-center ml-auto space-x-2">
-					<Badge v-if="!app.deployed" color="yellow">Not Deployed</Badge>
-					<Badge v-if="app.update_available && app.deployed" color="blue">
-						Update Available
-					</Badge>
-					<Dropdown :items="dropdownItems(app)" right>
-						<template v-slot="{ toggleDropdown }">
-							<Button icon="more-horizontal" @click="toggleDropdown()" />
-						</template>
-					</Dropdown>
-				</div>
-			</div>
+				<template #actions>
+					<div class="flex items-center ml-auto space-x-2">
+						<Badge v-if="!app.deployed" color="yellow">Not Deployed</Badge>
+						<Badge v-if="app.update_available && app.deployed" color="blue">
+							Update Available
+						</Badge>
+						<Dropdown :items="dropdownItems(app)" right>
+							<template v-slot="{ toggleDropdown }">
+								<Button icon="more-horizontal" @click="toggleDropdown()" />
+							</template>
+						</Dropdown>
+					</div>
+				</template>
+			</ListItem>
 		</div>
+
 		<Dialog title="Add apps to your bench" v-model="showAddAppDialog">
 			<Loading class="py-2" v-if="installableApps.loading" />
 			<AppSourceSelector
@@ -72,7 +70,10 @@
 			</p>
 		</Dialog>
 
-		<ChangeAppBranchDialog :bench="bench.name" :app.sync="appToChangeBranchOf" />
+		<ChangeAppBranchDialog
+			:bench="bench.name"
+			:app.sync="appToChangeBranchOf"
+		/>
 	</Card>
 </template>
 <script>
