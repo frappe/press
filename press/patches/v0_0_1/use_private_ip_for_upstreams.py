89818592e1b18b3025bf8cbdbfca90f39b280cb7
Aditya Hase <aditya@adityahase.com>
2021-09-16 12:19:10 +0530

    fix: Update agent before renaming

--- press/patches/v0_0_1/use_private_ip_for_upstreams.py
+++ press/patches/v0_0_1/use_private_ip_for_upstreams.py
@@ -8,11 +8,24 @@ from press.agent import Agent
 from frappe.utils.fixtures import sync_fixtures
 
 
+AGENT_GITHUB_ACCESS_TOKEN = ""
+
+
 def execute():
+	settings = frappe.get_single_doc("Press Settings")
+	settings.agent_repository_owner = "frappe"
+
+	if not AGENT_GITHUB_ACCESS_TOKEN:
+		raise ValueError("GitHub Access Token not found")
+
+	settings.agent_github_access_token = AGENT_GITHUB_ACCESS_TOKEN
+	settings.save()
+
 	sync_fixtures("press")
-	servers = frappe.get_all(
-		"Server", {"is_upstream_setup": True, "status": "Active"}, ["name", "proxy_server"],
-	)
+
+	servers = frappe.get_all("Server", {"is_upstream_setup": True, "status": "Active"})
 	for server in servers:
+		server = frappe.get_doc("Proxy Server", server.name)
+		server.update_agent_ansible()
 		agent = Agent(server.proxy_server, "Proxy Server")
 		agent.update_upstream_private_ip(server.name)
