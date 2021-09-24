2ce8bafcbe50463329cc291eba38081ff84d0767
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-09-24 13:29:26 +0530

    refactor: Bench app update UI

--- dashboard/src/components/AlertBenchUpdate.vue
+++ dashboard/src/components/AlertBenchUpdate.vue
@@ -17,7 +17,10 @@
 			</Button>
 		</template>
 		<Dialog title="Following updates are available" v-model="showDeployDialog">
-			<AppUpdates :apps="deployInformation.apps" />
+			<AppUpdates
+				:apps="deployInformation.apps"
+				:selectedApps.sync="selectedApps"
+			/>
 			<ErrorMessage class="mt-2" :error="$resources.deploy.error" />
 			<template #actions>
 				<Button
@@ -41,7 +44,8 @@ export default {
 	},
 	data() {
 		return {
-			showDeployDialog: false
+			showDeployDialog: false,
+			selectedApps: []
 		};
 	},
 	resources: {
@@ -55,10 +59,20 @@ export default {
 			};
 		},
 		deploy() {
+			console.log('deploy()');
+
+			let appsToIgnore = [];
+			if (this.deployInformation) {
+				appsToIgnore = this.deployInformation.apps.filter(
+					app => app.update_available && !this.selectedApps.includes(app.app)
+				);
+			}
+
 			return {
 				method: 'press.api.bench.deploy',
 				params: {
-					name: this.bench.name
+					name: this.bench.name,
+					apps_to_ignore: appsToIgnore
 				},
 				onSuccess(candidate) {
 					this.$router.push(`/benches/${this.bench.name}/deploys/${candidate}`);
@@ -77,6 +91,15 @@ export default {
 		},
 		deployInformation() {
 			return this.$resources.deployInformation.data;
+		}
+	},
+	// DEBUG ONLY
+	watch: {
+		selectedApps() {
+			const appsToIgnore = this.deployInformation.apps.filter(
+				app => app.update_available && !this.selectedApps.includes(app.app)
+			);
+			console.log(appsToIgnore);
 		}
 	}
 };
