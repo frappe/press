92e098c72c2ddb120c3c599ab67abde700cd0803
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-09-28 23:28:46 +0530

    feat: Utility methods for creating logs

--- press/experimental/doctype/feature_experiment/feature_experiment.py
+++ press/experimental/doctype/feature_experiment/feature_experiment.py
@@ -1,8 +1,34 @@
 # Copyright (c) 2021, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from press.utils import get_current_team
 from frappe.model.document import Document
 
+
 class FeatureExperiment(Document):
-	pass
+	def log_record(self, notes="", log=""):
+		current_team = get_current_team()
+		frappe.get_doc(
+			{
+				"doctype": "Feature Experiment Log",
+				"experiment": self.name,
+				"type": "Record",
+				"team": current_team,
+				"notes": notes,
+				"log": log,
+			}
+		).insert()
+
+	def log_error(self, notes="", log=""):
+		current_team = get_current_team()
+		frappe.get_doc(
+			{
+				"doctype": "Feature Experiment Log",
+				"experiment": self.name,
+				"type": "Error",
+				"team": current_team,
+				"notes": notes,
+				"log": log,
+			}
+		).insert()
