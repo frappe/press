c17e4732780c4718975bde5f48baf28c02d11412
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-09-28 23:41:26 +0530

    chore: Validation and cohort decision

--- press/experimental/doctype/feature_experiment/feature_experiment.py
+++ press/experimental/doctype/feature_experiment/feature_experiment.py
@@ -2,11 +2,22 @@
 # For license information, please see license.txt
 
 import frappe
+import random
 from press.utils import get_current_team
 from frappe.model.document import Document
 
 
 class FeatureExperiment(Document):
+	def validate(self):
+		if self.percentage_for_cohort_a > 100 or self.percentage_for_cohort_a < 0:
+			frappe.throw("Percentage value should be between 1 and 100 (inclusive)!")
+
+	def is_cohort_a(self):
+		return random.random() <= (self.percentage_for_cohort_a / 100)
+
+	def is_cohort_b(self):
+		return not self.is_cohort_a()
+
 	def log_record(self, notes="", log=""):
 		current_team = get_current_team()
 		frappe.get_doc(
