c685f13aa4702bd1ed7631cde991814f9076b69c
Faris Ansari <netchamp.faris@gmail.com>
2021-09-29 12:49:15 +0530

    feat: Impersonate team with button click
    - /impersonate/:team to impersonate a team

---
+++ dashboard/src/views/ImpersonateTeam.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="px-4 py-4 text-base sm:px-8">Impersonating {{ team }}</div>
+</template>
+<script>
+export default {
+	name: 'ImpersonateTeam',
+	props: ['team'],
+	async mounted() {
+		if (this.team) {
+			try {
+				await this.$account.switchTeam(this.team);
+				if (window.history.length === 1) {
+					this.$router.replace('/');
+				} else {
+					this.$router.back();
+				}
+			} catch (error) {
+				this.$router.replace('/');
+			}
+		}
+	}
+};
+</script>
