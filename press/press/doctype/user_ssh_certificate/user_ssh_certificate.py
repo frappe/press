a2580aacd3b4e24196be7929f4cd809fe2e096ab
Balamurali M <balamurali@erpnext.com>
2021-10-05 08:58:44 +0530

    style: black + flake8 + isort

--- press/press/doctype/user_ssh_certificate/user_ssh_certificate.py
+++ press/press/doctype/user_ssh_certificate/user_ssh_certificate.py
@@ -3,16 +3,17 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-from frappe.model.document import Document
-from frappe import safe_decode
 
 import base64
 import binascii
-import frappe
 import hashlib
 import re
 import shlex
 import subprocess
+
+import frappe
+from frappe import safe_decode
+from frappe.model.document import Document
 
 
 class UserSSHCertificate(Document):
@@ -27,14 +28,16 @@ class UserSSHCertificate(Document):
 			frappe.throw("Please ensure that the attached text is a valid public key")
 
 		# check if there is an existing valid certificate for the server
-		if frappe.get_all("User SSH Certificate", {
-			'team': self.team,
-			'valid_until': ['>', frappe.utils.now()],
-			'access_server': self.access_server,
-			'docstatus': 1
-		}):
+		if frappe.get_all(
+			"User SSH Certificate",
+			{
+				"team": self.team,
+				"valid_until": [">", frappe.utils.now()],
+				"access_server": self.access_server,
+				"docstatus": 1,
+			},
+		):
 			frappe.throw("A valid certificate already exists.")
-
 
 	def before_save(self):
 		# decode the ssh key and generate a fingerprint
@@ -44,15 +47,14 @@ class UserSSHCertificate(Document):
 		sha256_sum.update(ssh_key_b64)
 		self.ssh_fingerprint = safe_decode(base64.b64encode(sha256_sum.digest()))
 
-
 	def before_submit(self):
 		# extract key_type (eg: rsa, ecdsa, ed25519.) for naming convention
-		self.key_type = self.ssh_public_key.strip().split()[0].split('-')[1]
+		self.key_type = self.ssh_public_key.strip().split()[0].split("-")[1]
 		if not self.key_type:
 			frappe.throw("Could not guess the key type. Please check your public key.")
 
 		# write the public key to a /tmp file
-		with open(f'/tmp/id_{self.key_type}-{self.name}.pub', 'w') as public_key:
+		with open(f"/tmp/id_{self.key_type}-{self.name}.pub", "w") as public_key:
 			public_key.write(self.ssh_public_key)
 			public_key.flush()
 
@@ -60,19 +62,26 @@ class UserSSHCertificate(Document):
 
 		# try generating a certificate for the /tmp key.
 		try:
-			command = f'ssh-keygen -s ca -I {self.name} -n {principal} -V +{self.validity} /tmp/id_{self.key_type}-{self.name}.pub'
+			command = (
+				f"ssh-keygen -s ca -I {self.name} -n {principal} -V +{self.validity}"
+				f" /tmp/id_{self.key_type}-{self.name}.pub"
+			)
 			subprocess.check_output(shlex.split(command), cwd="/etc/ssh")
 		except subprocess.CalledProcessError:
-			frappe.throw("Failed to generate a certificate for the specified key. Please try again.")
+			frappe.throw(
+				"Failed to generate a certificate for the specified key. Please try again."
+			)
 		return
-		process = subprocess.Popen(shlex.split(f'ssh-keygen -Lf /tmp/id_{self.key_type}-{self.name}-cert.pub'), stdout=subprocess.PIPE)
-		self.certificate_details= safe_decode(process.communicate()[0])
+		process = subprocess.Popen(
+			shlex.split(f"ssh-keygen -Lf /tmp/id_{self.key_type}-{self.name}-cert.pub"),
+			stdout=subprocess.PIPE,
+		)
+		self.certificate_details = safe_decode(process.communicate()[0])
 		# extract the time for until when the key is active
 		regex = re.compile("Valid:.*\n")
 		self.valid_until = regex.findall(self.certificate_details)[0].strip().split()[-1]
 		self.ssh_certificate = read_certificate(self.key_type, self.name)
 		self.ssh_command = get_ssh_command(self.name)
-
 
 	def before_cancel(self):
 		self.delete_key("ssh_certificate")
@@ -80,10 +89,10 @@ class UserSSHCertificate(Document):
 
 @frappe.whitelist()
 def read_certificate(key_type, docname):
-	with open('/tmp/id_{0}-{1}-cert.pub'.format(key_type, docname)) as certificate:
+	with open("/tmp/id_{0}-{1}-cert.pub".format(key_type, docname)) as certificate:
 		try:
 			return certificate.read()
-		except:
+		except Exception:
 			pass
 
 		return
@@ -103,17 +112,21 @@ def get_ssh_command(docname):
 		ssh_port = 2332
 		server = certificate_doc.access_server
 	else:
-		server_url, ip_address, server_port, server_user = frappe.db.get_value("Self Hosted Service", certificate_doc.access_server,
-								["server_url", "ip_address", "ssh_port", "ssh_username"])
+		server_url, ip_address, server_port, server_user = frappe.db.get_value(
+			"Self Hosted Service",
+			certificate_doc.access_server,
+			["server_url", "ip_address", "ssh_port", "ssh_username"],
+		)
 		server = server_url if server_url else ip_address
 		ssh_port = server_port if server_port else ssh_port
 		ssh_user = server_user if server_user else ssh_user
 
-	ssh_command = "ssh {server} -p {ssh_port} -l {ssh_user}".format(server=server,
-						ssh_port=ssh_port, ssh_user=ssh_user)
+	ssh_command = "ssh {server} -p {ssh_port} -l {ssh_user}".format(
+		server=server, ssh_port=ssh_port, ssh_user=ssh_user
+	)
 
 	return ssh_command
 
 
 def set_user_ssh_key(user, ssh_public_key):
-	frappe.db.set_value('User', user, 'ssh_public_key', ssh_public_key)
+	frappe.db.set_value("User", user, "ssh_public_key", ssh_public_key)
