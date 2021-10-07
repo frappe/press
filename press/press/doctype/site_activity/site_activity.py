4323ce255b1f2c40c02b9030b08194587a7f02cf
kamaljohnson <kamaljohnson12345@gmail.com>
2021-10-07 15:21:19 +0530

    feat: send email only if notify email of site is set

--- press/press/doctype/site_activity/site_activity.py
+++ press/press/doctype/site_activity/site_activity.py
@@ -11,17 +11,18 @@ from frappe.model.document import Document
 class SiteActivity(Document):
 	def after_insert(self):
 		if self.action == "Login as Administrator" and self.reason:
-			recipient = frappe.db.get_value("Site", self.site, "team")
-			frappe.sendmail(
-				recipients=[recipient],
-				# TODO: update recipient for email to use custom recipient configured in team <29-09-21, Balamurali M> #
-				subject="Administrator login to your site",
-				template="admin_login",
-				args={"site": self.site, "user": self.owner, "reason": self.reason},
-				reference_doctype=self.doctype,
-				reference_name=self.name,
-				now=True,
-			)
+			recipient = frappe.db.get_value("Site", self.site, "notify_email")
+			if recipient:
+					frappe.sendmail(
+						recipients=[recipient],
+						# TODO: update recipient for email to use custom recipient configured in team <29-09-21, Balamurali M> #
+						subject="Administrator login to your site",
+						template="admin_login",
+						args={"site": self.site, "user": self.owner, "reason": self.reason},
+						reference_doctype=self.doctype,
+						reference_name=self.name,
+						now=True,
+					)
 
 
 def log_site_activity(site, action, reason=None):
