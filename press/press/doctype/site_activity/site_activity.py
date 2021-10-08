26ae52d65fdb296f7c053078513a6dbe0d5fb07e
Balamurali M <balamurali@erpnext.com>
2021-10-08 13:53:09 +0530

    feat(Team): Allow team wise turning off of notifications (#238)
    As of now, only admin login notifications will be turned off

--- press/press/doctype/site_activity/site_activity.py
+++ press/press/doctype/site_activity/site_activity.py
@@ -11,17 +11,19 @@ from frappe.model.document import Document
 class SiteActivity(Document):
 	def after_insert(self):
 		if self.action == "Login as Administrator" and self.reason:
-			recipient = frappe.db.get_value("Site", self.site, "notify_email")
+			d = frappe.get_all("Site", {"name": self.site}, ["notify_email", "team"])[0]
+			recipient = d.notify_email
 			if recipient:
-					frappe.sendmail(
-						recipients=[recipient],
-						subject="Administrator login to your site",
-						template="admin_login",
-						args={"site": self.site, "user": self.owner, "reason": self.reason},
-						reference_doctype=self.doctype,
-						reference_name=self.name,
-						now=True,
-					)
+				team = frappe.get_doc("Team", d.team)
+				team.notify_with_email(
+					[recipient],
+					subject="Administrator login to your site",
+					template="admin_login",
+					args={"site": self.site, "user": self.owner, "reason": self.reason},
+					reference_doctype=self.doctype,
+					reference_name=self.name,
+					now=True,
+				)
 
 
 def log_site_activity(site, action, reason=None):
