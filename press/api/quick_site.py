dc05961c93076aefedaacfb9b5a404eb6106d647
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-10-08 18:56:12 +0530

    chore: Validate if apps exist

--- press/api/quick_site.py
+++ press/api/quick_site.py
@@ -1,5 +1,7 @@
 import frappe
 
+from frappe.utils import comma_and
+
 
 @frappe.whitelist(allow_guest=True)
 def deploy():
@@ -13,7 +15,7 @@ def deploy():
 	# TODO (WIP):
 	# 	1. Check if all apps are valid (fn<validate_app_list>)
 	# 	2. Append the "fc_quick_site" custom app (does not exist yet)
-	# 	3. Create a new site and set browser cookies
+	# 	3. Create a new site and set browser cookies (or maybe localStorage)
 	# 		a. Check out: frappe.local.cookie_manager.set_cookie
 	# 		b. Check out: Site "sid" etc.
 	# 		c. Mark the site 'Quick deploy' (flag in doctype?)
@@ -28,6 +30,17 @@ def deploy():
 def validate_app_list(apps):
 	if not apps:
 		frappe.throw("Param 'apps' is required!")
+
+	apps = apps.split(",")
+	apps_set = set(apps)
+	all_apps_set = set(frappe.get_all("App", pluck="name"))
+	non_existing_apps = apps_set - all_apps_set
+
+	if len(non_existing_apps) > 0:
+		non_existing_apps = comma_and(non_existing_apps)
+		frappe.throw(
+			f"App(s) {frappe.bold(non_existing_apps)} do not exist! Please check app name."
+		)
 
 
 def log_traction_source(source):
