6a68c0188fe6e2e73f8be36a2afd07b024c4cb3e
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-10-14 21:10:36 +0530

    feat: First cut analytics card UI

--- dashboard/src/views/MarketplaceAppAnalytics.vue
+++ dashboard/src/views/MarketplaceAppAnalytics.vue
@@ -1,9 +1,47 @@
 <template>
-	<h2>Analytics Page</h2>
+	<div class="sm:grid sm:grid-cols-2">
+		<Card title="Installs">
+			<div class="divide-y" v-if="this.$resources.analytics.data">
+				<ListItem
+					title="Total Site Installs"
+					:description="`${this.$resources.analytics.data.total_installs}`"
+				>
+				</ListItem>
+				<ListItem
+					title="Number of active Sites with this App"
+					:description="
+						`${this.$resources.analytics.data.num_installs_active_sites}`
+					"
+				>
+				</ListItem>
+				<ListItem
+					title="Number of active Benches with this App"
+					:description="
+						`${this.$resources.analytics.data.num_installs_active_benches}`
+					"
+				>
+				</ListItem>
+			</div>
+		</Card>
+	</div>
 </template>
 
 <script>
 export default {
-	name: 'MarketplaceAppAnalytics'
+	name: 'MarketplaceAppAnalytics',
+	props: {
+		app: Object
+	},
+	resources: {
+		analytics() {
+			return {
+				method: 'press.api.marketplace.analytics',
+				auto: true,
+				params: {
+					name: this.app.app
+				}
+			};
+		}
+	}
 };
 </script>
