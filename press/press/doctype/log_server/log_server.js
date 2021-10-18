7f776498a522820fd09e57678fe07ebd921a18fe
Aditya Hase <aditya@adityahase.com>
2021-10-18 16:42:10 +0530

    fix: Add button to show kibana password

--- press/press/doctype/log_server/log_server.js
+++ press/press/doctype/log_server/log_server.js
@@ -12,6 +12,7 @@ frappe.ui.form.on('Log Server', {
 			[__('Prepare Server'), "prepare_server", true, !frm.doc.is_server_setup],
 			[__('Setup Server'), "setup_server", true, !frm.doc.is_server_setup],
 			[__('Fetch Keys'), "fetch_keys", false, frm.doc.is_server_setup && (!frm.doc.frappe_public_key || !frm.doc.root_public_key)],
+			[__('Show Kibana Password'), "show_kibana_password", false, frm.doc.is_server_setup],
 		].forEach(([label, method, confirm, condition]) => {
 			if (typeof condition === "undefined" || condition) {
 				frm.add_custom_button(
