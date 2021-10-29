1628dfe5c71a35a81e154c9416ce597fe59cd7e1
Balamurali M <balamurali@erpnext.com>
2021-10-29 08:34:57 +0530

    test(ScheduledBackupJob): Add tests for offsite backups schedule
    - refactor(TestScheduledBackupJob): Add utility method for
      interval_hrs_ago

--- press/press/doctype/site/test_backups.py
+++ press/press/doctype/site/test_backups.py
@@ -1,5 +1,5 @@
 import unittest
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from unittest.mock import MagicMock, Mock, patch
 
 import frappe
@@ -7,6 +7,7 @@ import frappe
 from press.press.doctype.agent_job.agent_job import AgentJob
 from press.press.doctype.site.backups import ScheduledBackupJob
 from press.press.doctype.site.test_site import create_test_site
+from press.press.doctype.site_backup.test_site_backup import create_test_site_backup
 
 
 @patch("press.press.doctype.site.backups.frappe.db.commit", new=MagicMock)
@@ -26,9 +27,13 @@ class TestScheduledBackupJob(unittest.TestCase):
 		self.interval = 6
 		frappe.db.set_value("Press Settings", "Press Settings", "backup_interval", 6)
 
+	def _interval_hrs_ago(self):
+		return datetime.now() - timedelta(hours=self.interval)
+
 	def _create_site_requiring_backup(self, **kwargs):
-		interval_hrs_ago = datetime.now() - timedelta(hours=self.interval + 1)
-		return create_test_site(creation=interval_hrs_ago, **kwargs)
+		return create_test_site(
+			creation=self._interval_hrs_ago() - timedelta(hours=1), **kwargs
+		)
 
 	@patch.object(
 		ScheduledBackupJob, "is_backup_hour", new=lambda self, x: True,  # always backup hour
@@ -87,3 +92,36 @@ class TestScheduledBackupJob(unittest.TestCase):
 
 		self.assertLess(sites_num_new, sites_num_old)
 		self.assertEqual(sites_num_old - sites_num_new, limit)
+
+	def test_site_with_no_backup_in_past_interval_shows_up_for_backup(self):
+		site_1 = self._create_site_requiring_backup()
+		site_2 = self._create_site_requiring_backup()
+		create_test_site_backup(
+			site_2.name, creation=self._interval_hrs_ago() + timedelta(hours=1)
+		)
+
+		job = ScheduledBackupJob()
+		sites_for_backup = [site.name for site in job.sites]
+
+		self.assertIn(site_1.name, sites_for_backup)
+		self.assertNotIn(site_2.name, sites_for_backup)
+
+	def test_sites_with_offsite_backup_today_doesnt_show_up(self):
+		site_1 = self._create_site_requiring_backup()
+		create_test_site_backup(
+			site_1.name,
+			offsite=False,
+			creation=date.today()
+		)
+		site_2 = self._create_site_requiring_backup()
+		create_test_site_backup(site_2.name, offsite=True, creation=date.today())
+
+		job = ScheduledBackupJob()
+		sites_for_backup = [site.name for site in job.sites]
+
+		self.assertIn(site_1.name, sites_for_backup)
+		self.assertIn(site_2.name, sites_for_backup)
+		for site in job.sites:
+			if site.name == site_2.name:
+				self.assertEqual(site.offsite, 0)
+
