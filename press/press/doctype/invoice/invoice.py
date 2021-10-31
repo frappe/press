be12dffb1bcd49b09954b2e5ff9b6305d1288bf5
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-10-31 12:30:52 +0530

    fix: Remove legacy unicode import

--- press/press/doctype/invoice/invoice.py
+++ press/press/doctype/invoice/invoice.py
@@ -2,9 +2,8 @@
 # Copyright (c) 2020, Frappe and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
-
 import frappe
+
 from frappe.model.document import Document
 from frappe.utils.data import fmt_money
 from press.api.billing import get_stripe
