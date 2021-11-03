55d6ea8fae87ddd807827994f64b6abe5ba75814
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-11-03 13:27:35 +0530

    test: Add python file for schedule updates tests

---
+++ press/tests/test_scheduled_auto_updates.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+from press.press.doctype.site_update.scheduled_auto_updates import should_update_trigger_for_daily
+
+class TestScheduledAutoUpdates(TestCase):
+    pass
