81c8f7d34cb3b135d88a21acd51dff1f42f1cb5f
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-11-05 12:44:49 +0530

    fix: black + flake8
    * Use isinstance() instead of type()

--- press/api/bench.py
+++ press/api/bench.py
@@ -346,9 +346,9 @@ def deploy_information(name):
 @frappe.whitelist()
 @protected("Release Group")
 def deploy(name, apps_to_ignore=[]):
-	if(type(apps_to_ignore == type(''))):
+	if isinstance(apps_to_ignore, str):
 		apps_to_ignore = json.loads(apps_to_ignore)
-		
+
 	team = get_current_team()
 	rg: ReleaseGroup = frappe.get_doc("Release Group", name)
 
