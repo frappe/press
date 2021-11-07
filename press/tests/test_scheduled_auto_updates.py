e583eb4205eefd42d23d61bd52476c431fdd6fa1
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-11-07 14:24:27 +0530

    test: Case when last triggered on is `None`

--- press/tests/test_scheduled_auto_updates.py
+++ press/tests/test_scheduled_auto_updates.py
@@ -59,3 +59,65 @@ class TestScheduledAutoUpdates(TestCase):
 			self.assertTrue(
 				should_update_trigger_for_monthly(frappe._dict(obj), obj["current_datetime"]), obj
 			)
+
+	def test_true_last_triggered_is_none(self):
+		TEST_DATA_DAILY_TRUE_WITH_NONE = list(
+			map(set_last_triggered_to_none, TEST_DATA_DAILY_TRUE)
+		)
+
+		TEST_DATA_WEEKLY_TRUE_WITH_NONE = list(
+			map(set_last_triggered_to_none, TEST_DATA_WEEKLY_TRUE)
+		)
+
+		TEST_DATA_MONTHLY_TRUE_WITH_NONE = list(
+			map(set_last_triggered_to_none, TEST_DATA_MONTHLY_TRUE)
+		)
+
+		for obj in TEST_DATA_DAILY_TRUE_WITH_NONE:
+			self.assertTrue(
+				should_update_trigger_for_daily(frappe._dict(obj), obj["current_datetime"]), obj
+			)
+
+		for obj in TEST_DATA_WEEKLY_TRUE_WITH_NONE:
+			self.assertTrue(
+				should_update_trigger_for_weekly(frappe._dict(obj), obj["current_datetime"]), obj
+			)
+
+		for obj in TEST_DATA_MONTHLY_TRUE_WITH_NONE:
+			self.assertTrue(
+				should_update_trigger_for_monthly(frappe._dict(obj), obj["current_datetime"]), obj
+			)
+
+	def test_false_last_triggered_is_none(self):
+		TEST_DATA_DAILY_FALSE_WITH_NONE = list(
+			map(set_last_triggered_to_none, TEST_DATA_DAILY_FALSE)
+		)
+
+		TEST_DATA_WEEKLY_FALSE_WITH_NONE = list(
+			map(set_last_triggered_to_none, TEST_DATA_WEEKLY_FALSE)
+		)
+
+		TEST_DATA_MONTHLY_FALSE_WITH_NONE = list(
+			map(set_last_triggered_to_none, TEST_DATA_MONTHLY_FALSE)
+		)
+
+		for obj in TEST_DATA_DAILY_FALSE_WITH_NONE:
+			self.assertFalse(
+				should_update_trigger_for_daily(frappe._dict(obj), obj["current_datetime"]), obj
+			)
+
+		for obj in TEST_DATA_WEEKLY_FALSE_WITH_NONE:
+			self.assertFalse(
+				should_update_trigger_for_weekly(frappe._dict(obj), obj["current_datetime"]), obj
+			)
+
+		for obj in TEST_DATA_MONTHLY_FALSE_WITH_NONE:
+			self.assertFalse(
+				should_update_trigger_for_monthly(frappe._dict(obj), obj["current_datetime"]), obj
+			)
+
+
+def set_last_triggered_to_none(obj):
+	obj_copy = dict(obj)
+	obj_copy["auto_update_last_triggered_on"] = None
+	return obj_copy
