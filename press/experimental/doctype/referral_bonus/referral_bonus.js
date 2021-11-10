09e480d629370470dbc1278dfc16d20f2d111eb5
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-11-10 23:51:13 +0530

    feat: Logic to allocate referral bonus

--- press/experimental/doctype/referral_bonus/referral_bonus.js
+++ press/experimental/doctype/referral_bonus/referral_bonus.js
@@ -2,7 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Referral Bonus', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		frm.add_custom_button('Allocate Credits', () => {
+			frm.call('allocate_credits').then(() => {
+				frm.refresh();
+			});
+		});
+	},
 });
