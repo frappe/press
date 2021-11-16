7bb221bab2118342d6aea2b93cd9ea78652c45ae
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-11-16 13:49:01 +0530

    feat: Usage records for marketplace app subscription
    * Based on the existing code of subscription doctype
    * Reason existing code not used: Decoupling

--- press/marketplace/doctype/marketplace_app_subscription/marketplace_app_subscription.py
+++ press/marketplace/doctype/marketplace_app_subscription/marketplace_app_subscription.py
@@ -4,6 +4,8 @@
 import frappe
 
 
+
+from press.utils import log_error
 from frappe.model.document import Document
 from press.press.doctype.site.site import Site
 
@@ -12,6 +14,7 @@ class MarketplaceAppSubscription(Document):
 	def validate(self):
 		self.validate_plan()
 		self.set_secret_key()
+		# TODO: Validate Duplicate subscription
 
 	def validate_plan(self):
 		doctype_for_plan = frappe.db.get_value("Plan", self.plan, "document_type")
@@ -44,6 +47,57 @@ class MarketplaceAppSubscription(Document):
 
 		site_doc.update_site_config(config)
 
+	def create_usage_record(self):
+		if self.is_usage_record_created():
+			return
+		
+		team_name = frappe.db.get_value("Site", self.site, "team")
+		team = frappe.get_cached_doc("Team", team_name)
+
+		if not team.get_upcoming_invoice():
+			team.create_upcoming_invoice()
+
+		plan = frappe.get_cached_doc("Plan", self.plan)
+		amount = plan.get_price_for_interval(self.interval, team.currency)
+
+		usage_record = frappe.get_doc(
+			doctype="Usage Record",
+			team=team_name,
+			document_type="Marketplace App",
+			document_name=self.app,
+			plan=plan.name,
+			amount=amount,
+			subscription=self.name,
+			interval=self.interval,
+		)
+		usage_record.insert()
+		usage_record.submit()
+		return usage_record
+	
+	def is_usage_record_created(self):
+		team = frappe.db.get_value("Site", self.site, "team")
+		filters = {
+			"team": team,
+			"document_type": "Marketplace App",
+			"document_name": self.app,
+			"subscription": self.name,
+			"interval": self.interval,
+			"plan": self.plan,
+		}
+
+		if self.interval == "Daily":
+			filters.update({"date": frappe.utils.today()})
+
+		if self.interval == "Monthly":
+			date = frappe.utils.getdate()
+			first_day = frappe.utils.get_first_day(date)
+			last_day = frappe.utils.get_last_day(date)
+			filters.update({"date": ("between", (first_day, last_day))})
+
+		result = frappe.db.get_all("Usage Record", filters=filters, limit=1)
+		return bool(result)
+
+
 	@frappe.whitelist()
 	def activate(self):
 		if self.status == "Active":
@@ -51,3 +105,16 @@ class MarketplaceAppSubscription(Document):
 		
 		self.status = "Active"
 		self.save()
+
+def create_usage_records():
+	subscriptions = frappe.db.get_all(
+		"Marketplace App Subscription", filters={"status": "Active"}, pluck="name"
+	)
+	for name in subscriptions:
+		subscription = frappe.get_doc("Marketplace App Subscription", name)
+		try:
+			subscription.create_usage_record()
+			frappe.db.commit()
+		except Exception:
+			frappe.db.rollback()
+			log_error(title="Marketplace App: Create Usage Record Error", name=name)
