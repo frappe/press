1705149af3912001918e90cd89daf1da9debd787
Mohammad Hussain Nagaria <34810212+NagariaHussain@users.noreply.github.com>
2021-12-17 16:03:57 +0530

    refactor: Extract click to copy field component (#318)
    * refactor: Extract click to copy field component
    
    * fix: Remove method, no longer required

---
+++ dashboard/src/components/ClickToCopyField.vue
@@ -0,0 +1,28 @@
+<template>
+	<div
+		class="p-2 items-center rounded-lg flex flex-row border-2 justify-between"
+	>
+		<p class="text-sm text-gray-800 font-mono overflow-hidden">
+			{{ textContent }}
+		</p>
+		<Button icon="copy" @click="copyTextContentToClipboard" />
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['textContent'],
+	methods: {
+		copyTextContentToClipboard() {
+			const clipboard = window.navigator.clipboard;
+			clipboard.writeText(this.textContent).then(() => {
+				this.$notify({
+					title: 'Copied to clipboard!',
+					icon: 'check',
+					color: 'green'
+				});
+			});
+		}
+	}
+};
+</script>
