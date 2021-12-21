8e0d74750ea9f61de1dddf2018df14318b44b020
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-12-21 14:46:10 +0530

    refactor: Sort by number of installs

--- press/www/marketplace/index.py
+++ press/www/marketplace/index.py
@@ -12,14 +12,14 @@ def get_context(context):
 	# TODO: Caching, Pagination, Filtering, Sorting
 	context.no_cache = 1
 	context.apps = frappe.get_all(
-		"Marketplace App",
-		filters={"status": "Published"},
-		order_by="creation asc",
-		fields=["*"],
+		"Marketplace App", filters={"status": "Published"}, fields=["*"],
 	)
 
 	for app in context.apps:
 		app.total_installs = get_total_installs_for_app(app.name)
+
+	# For the time being, sort by number of installs
+	context.apps.sort(key=lambda x: x.total_installs)
 
 	context.metatags = {
 		"title": "Frappe Cloud Marketplace",
