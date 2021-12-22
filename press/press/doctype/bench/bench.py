c39d98519a7bb09af52c0e4792321576480bd546
Aditya Hase <aditya@adityahase.com>
2021-12-22 13:52:37 +0530

    fix: Only fail for sites that have sync issues

--- press/press/doctype/bench/bench.py
+++ press/press/doctype/bench/bench.py
@@ -176,7 +176,12 @@ class Bench(Document):
 		agent = Agent(self.server)
 		data = agent.get_sites_info(self, since=last_synced_time)
 		for site, info in data.items():
-			frappe.get_doc("Site", site).sync_info(info)
+			try:
+				frappe.get_doc("Site", site).sync_info(info)
+				frappe.db.commit()
+			except Exception:
+				log_error("Site Sync Error", site=site, info=info)
+				frappe.db.rollback()
 
 	@frappe.whitelist()
 	def update_all_sites(self):
@@ -402,6 +407,7 @@ def sync_bench(name):
 		frappe.db.commit()
 	except Exception:
 		log_error("Bench Sync Error", bench=bench.name)
+		frappe.db.rollback()
 
 
 get_permission_query_conditions = get_permission_query_conditions_for_doctype("Bench")
