3a7d2dd3ae074d743c236f0e583e35c59b592841
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-12-27 19:10:26 +0530

    feat: Start app plan cards designs

---
+++ dashboard/src/components/ChangeAppPlanCards.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="grid grid-cols-5">
+		<div class="p-5 rounded-2xl shadow-lg">
+			<h4 class="text-gray-900 font-semibold text-xl">
+				₹99<span class="font-normal text-gray-600 text-base">/ Month</span>
+			</h4>
+			<h4 class="mt-1 text-gray-600 text-base line-through">₹299</h4>
+
+			<ul class="mt-5 text-base text-gray-700 space-y-2">
+				<li class="flex flex-row items-center">
+					<CircularCheckIcon class="mr-2" />
+					1 member
+				</li>
+				<li class="flex flex-row items-center">
+					<CircularCheckIcon class="mr-2" />
+					7 day data rentention
+				</li>
+				<li class="flex flex-row items-center">
+					<CircularCheckIcon class="mr-2" />
+					1 GB
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ChangeAppPlanCards',
+	data() {
+		return {};
+	}
+};
+</script>
---
+++ dashboard/src/components/global/CircularCheckIcon.vue
@@ -0,0 +1,21 @@
+<template>
+	<div
+		class="grid w-4 h-4 border rounded-full place-items-center border-green-500 bg-green-50"
+	>
+		<svg
+			width="10"
+			height="8"
+			viewBox="0 0 10 8"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M1.26562 3.86686L3.93229 6.53353L9.26562 1.2002"
+				stroke="#38A160"
+				stroke-miterlimit="10"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	</div>
+</template>
