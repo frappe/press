cede80cdaa03aee122763c5d171d79cb5792c91a
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-12-27 18:26:19 +0530

    feat: Validate app,plan pair in plan subscription

--- press/marketplace/doctype/marketplace_app_plan/marketplace_app_plan.py
+++ press/marketplace/doctype/marketplace_app_plan/marketplace_app_plan.py
@@ -1,8 +1,17 @@
 # Copyright (c) 2021, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
 
+
 class MarketplaceAppPlan(Document):
-	pass
+	def validate(self):
+		self.validate_plan()
+
+	def validate_plan(self):
+		dt = frappe.db.get_value("Plan", self.plan, "doctype_name")
+		
+		if dt != "Marketplace App":
+			frappe.throw("The plan must be a Marketplace App plan.")
