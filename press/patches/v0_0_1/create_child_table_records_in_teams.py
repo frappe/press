7e0323dbc70391058e7c7624f9e4d2f6f1e04f7c
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2022-01-04 19:58:49 +0530

    fix: Temporary fix for failing patch

--- press/patches/v0_0_1/create_child_table_records_in_teams.py
+++ press/patches/v0_0_1/create_child_table_records_in_teams.py
@@ -1,4 +1,5 @@
 import frappe
+from press.utils import log_error
 
 
 def execute():
@@ -6,9 +7,12 @@ def execute():
 	teams = frappe.get_all("Team", pluck="name")
 
 	for name in teams:
-		team = frappe.get_doc("Team", name)
-		team.append("communication_emails", {"type": "invoices", "value": team.name})
-		team.append(
-			"communication_emails", {"type": "marketplace_notifications", "value": team.name}
-		)
-		team.save()
+		try:
+			team = frappe.get_doc("Team", name)
+			team.append("communication_emails", {"type": "invoices", "value": team.name})
+			team.append(
+				"communication_emails", {"type": "marketplace_notifications", "value": team.name}
+			)
+			team.save()
+		except Exception as e:
+			log_error(title="Weird Country Name", data=e)
