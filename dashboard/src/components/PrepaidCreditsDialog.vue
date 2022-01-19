7aa0b592aef01899bee64540bbb0903df1970fe0
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-01-19 19:40:41 +0530

    feat: Handle payment success and failures

--- dashboard/src/components/PrepaidCreditsDialog.vue
+++ dashboard/src/components/PrepaidCreditsDialog.vue
@@ -5,10 +5,45 @@
 		title="Buy Credits"
 	>
 		<BuyPrepaidCredits
+			v-if="paymentGateway === 'stripe'"
 			:minimumAmount="minimumAmount"
 			@success="$emit('success')"
 			@cancel="$emit('update:show', false)"
 		/>
+
+		<div v-if="paymentGateway === 'razorpay'">
+			<Input
+				:label="`Amount (Minimum Amount: ${minimumAmount})`"
+				v-model.number="creditsToBuy"
+				name="amount"
+				autocomplete="off"
+				type="number"
+				min="1"
+			/>
+
+			<div class="flex justify-between w-full mt-4">
+				<Button @click="paymentGateway = null">Go Back</Button>
+				<div>
+					<Button
+						type="primary"
+						:loading="
+							$resources.createRazorpayOrder.loading ||
+								$resources.handlePaymentSuccess.loading
+						"
+						@click="buyCreditsWithRazorpay"
+					>
+						Buy
+					</Button>
+				</div>
+			</div>
+		</div>
+
+		<div>
+			<div v-if="!paymentGateway">
+				<Button @click="paymentGateway = 'stripe'">Stripe</Button>
+				<Button @click="paymentGateway = 'razorpay'">Razorpay</Button>
+			</div>
+		</div>
 	</Dialog>
 </template>
 <script>
@@ -19,12 +54,88 @@ export default {
 	components: {
 		BuyPrepaidCredits
 	},
+	data() {
+		return {
+			paymentGateway: null,
+			creditsToBuy: 0
+		};
+	},
+	mounted() {
+		const razorpayCheckoutJS = document.createElement('script');
+		razorpayCheckoutJS.setAttribute(
+			'src',
+			'https://checkout.razorpay.com/v1/checkout.js'
+		);
+		razorpayCheckoutJS.async = true;
+		document.head.appendChild(razorpayCheckoutJS);
+	},
 	props: {
 		show: {
 			default: true
 		},
 		minimumAmount: {
 			default: 0
+		}
+	},
+	resources: {
+		createRazorpayOrder() {
+			return {
+				method: 'press.api.billing.create_razorpay_order',
+				params: {
+					amount: this.creditsToBuy
+				},
+				onSuccess(data) {
+					this.processOrder(data);
+				}
+			};
+		},
+		handlePaymentSuccess() {
+			return {
+				method: 'press.api.billing.handle_razorpay_payment_success',
+				onSuccess() {
+					this.$emit('success');
+				}
+			};
+		},
+		handlePaymentFailed() {
+			return {
+				method: 'press.api.billing.handle_razorpay_payment_failed',
+				onSuccess() {
+					console.log('Payment Failed.');
+				}
+			};
+		}
+	},
+	methods: {
+		buyCreditsWithRazorpay() {
+			this.$resources.createRazorpayOrder.submit();
+		},
+		processOrder(data) {
+			const options = {
+				key: data.key_id,
+				order_id: data.order_id,
+				prefill: {
+					email: this.$account.team.name
+				},
+				theme: { color: '#2490EF' },
+				handler: this.handlePaymentSuccess
+			};
+
+			const rzp = new Razorpay(options);
+
+			// Opens the payment checkout frame
+			rzp.open();
+
+			// Attach failure handler
+			rzp.on('payment.failed', this.handlePaymentFailed);
+		},
+
+		handlePaymentSuccess(response) {
+			this.$resources.handlePaymentSuccess.submit({ response });
+		},
+
+		handlePaymentFailed(response) {
+			this.$resources.handlePaymentFailed.submit({ response });
 		}
 	}
 };
--- press/press/doctype/razorpay_webhook_log/razorpay_webhook_log.py
+++ press/press/doctype/razorpay_webhook_log/razorpay_webhook_log.py
@@ -1,8 +1,15 @@
 # Copyright (c) 2022, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
 
+
 class RazorpayWebhookLog(Document):
+	pass
+
+
+@frappe.whitelist(allow_guest=True)
+def razorpay_webhook_handler():
 	pass
