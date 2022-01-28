6585f9024847478b8db2aa47bf40d94d17a3b14f
Aditya Hase <aditya@adityahase.com>
2022-01-28 10:25:33 +0530

    feat(ssh): Sign all SSH certificates with default SSH authority

--- press/press/doctype/ssh_certificate/ssh_certificate.py
+++ press/press/doctype/ssh_certificate/ssh_certificate.py
@@ -11,6 +11,7 @@ import re
 import shlex
 import subprocess
 
+import frappe
 from frappe.model.document import Document
 
 
@@ -31,3 +32,45 @@ class SSHCertificate(Document):
 		self.key_type = self.ssh_public_key.strip().split()[0].split("-")[1]
 		if not self.key_type:
 			frappe.throw("Could not guess the key type. Please check your public key.")
+
+	def create_public_key_file(self):
+		with open(self.public_key_file, "w") as file:
+			file.write(self.ssh_public_key)
+
+	def after_insert(self):
+		self.serial_number = self.name[5:]
+		self.create_public_key_file()
+		self.generate_certificate()
+		self.extract_certificate_details()
+		self.save()
+
+	def generate_certificate(self):
+		ca = frappe.get_doc("SSH Certificate Authority", self.ssh_certificate_authority)
+		ca.sign(
+			self.user,
+			[self.group],
+			f"+{self.validity}",
+			self.public_key_file,
+			self.serial_number,
+		)
+		self.read_certificate()
+
+	def run(self, command):
+		return subprocess.check_output(shlex.split(command)).decode()
+
+	def extract_certificate_details(self):
+		self.certificate_details = self.run(f"ssh-keygen -Lf {self.certificate_file}")
+		regex = re.compile("Valid:.*\n")
+		self.valid_until = regex.findall(self.certificate_details)[0].strip().split()[-1]
+
+	def read_certificate(self):
+		with open(self.certificate_file) as file:
+			self.ssh_certificate = file.read()
+
+	@property
+	def public_key_file(self):
+		return os.path.join("/tmp", f"id_{self.key_type}-{self.serial_number}.pub")
+
+	@property
+	def certificate_file(self):
+		return os.path.join("/tmp", f"id_{self.key_type}-{self.serial_number}-cert.pub")
