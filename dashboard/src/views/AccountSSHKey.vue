bde1b57265cbe55de8503fb2762d59d8a10eef7e
Aditya Hase <aditya@adityahase.com>
2022-01-28 12:28:36 +0530

    feat: Modal to add public key

--- dashboard/src/views/AccountSSHKey.vue
+++ dashboard/src/views/AccountSSHKey.vue
@@ -19,10 +19,58 @@
 				}}
 			</div>
 		</div>
+		<template #actions>
+			<Button v-if="!$account.ssh_key" @click="showAddNewKeyDialog = true">
+				New SSH Key
+			</Button>
+		</template>
+		<Dialog title="New SSH Key" v-model="showAddNewKeyDialog">
+			<div class="mt-3">
+				<Input
+					:label="'SSH Key'"
+					type="textarea"
+					placeholder="Begins with 'ssh-rsa', 'ecdsa-sha2-nistp256', 'ecdsa-sha2-nistp384', 'ecdsa-sha2-nistp521', 'ssh-ed25519', 'sk-ecdsa-sha2-nistp256@openssh.com', or 'sk-ssh-ed25519@openssh.com'"
+					required
+					v-model="newKey"
+				/>
+			</div>
+			<ErrorMessage class="mt-2" :error="$resources.saveKey.error" />
+			<template #actions>
+				<Button type="primary" @click="$resources.saveKey.submit()">
+					Add Key
+				</Button>
+			</template>
+		</Dialog>
 	</Card>
 </template>
 <script>
 export default {
 	name: 'AccountSSHKey',
+	data() {
+		return {
+			showAddNewKeyDialog: false,
+			newKey: null
+		};
+	},
+
+	resources: {
+		saveKey() {
+			return {
+				method: 'press.api.account.add_key',
+				params: {
+					key: this.newKey
+				},
+				onSuccess() {
+					this.$account.fetchAccount();
+					this.showAddNewKeyDialog = false;
+					this.$notify({
+						title: 'New SSH Key Added',
+						icon: 'check',
+						color: 'green'
+					});
+				}
+			};
+		}
+	}
 };
 </script>
