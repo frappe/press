0a51ee81f8e3092a60ad2a52de33bc6506b72395
Aditya Hase <aditya@adityahase.com>
2022-02-01 18:44:55 +0530

    style: Black

--- press/press/doctype/ssh_certificate_authority/ssh_certificate_authority.py
+++ press/press/doctype/ssh_certificate_authority/ssh_certificate_authority.py
@@ -57,9 +57,10 @@ class SSHCertificateAuthority(Document):
 		host_flag = "-h " if host_key else ""
 		principals_argument = f"-n {','.join(principals)} " if principals else ""
 		self.run(
-			f"ssh-keygen -s ca -t rsa-sha2-512 -I {identity} {host_flag} {principals_argument} -z"
-			f" {serial_number} -O no-port-forwarding -O no-user-rc -O no-x11-forwarding"
-			f" -O no-agent-forwarding -O permit-pty -V {duration} {public_key_path}",
+			f"ssh-keygen -s ca -t rsa-sha2-512 -I {identity} {host_flag}"
+			f" {principals_argument} -z {serial_number} -O no-port-forwarding -O"
+			" no-user-rc -O no-x11-forwarding -O no-agent-forwarding -O permit-pty -V"
+			f" {duration} {public_key_path}",
 			directory=self.directory,
 		)
 
