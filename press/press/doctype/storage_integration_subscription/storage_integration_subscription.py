78141773ccbcdbf23bdf6603b68111c004f178bc
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2022-02-13 21:11:43 +0530

    chore: Set fields

--- press/press/doctype/storage_integration_subscription/storage_integration_subscription.py
+++ press/press/doctype/storage_integration_subscription/storage_integration_subscription.py
@@ -9,4 +9,40 @@ from frappe.model.document import Document
 
 
 class StorageIntegrationSubscription(Document):
-	pass
+	SERVER_TYPE = "Proxy Server"
+
+	def after_insert(self):
+		self.create_user()
+
+	def validate(self):
+		self.set_access_key_and_policy_name()
+		self.set_secret_key()
+		self.set_policy_json()
+		self.set_minio_server_on()
+
+	def set_access_key_and_policy_name(self):
+		# site.frappe.cloud -> site_frappe_cloud
+		self.access_key = self.name
+		self.policy_name = self.access_key + "_policy"
+
+	def set_secret_key(self):
+		h = blake2b(digest_size=12)
+		h.update(self.name.encode())
+		self.secret_key = h.hexdigest()
+
+	def set_policy_json(self):
+		data = {
+			"Version": "2012-10-17",
+			"Statement": [
+				{
+					"Effect": "Allow",
+					"Action": ["s3:GetObject", "s3:PutObject"],
+					"Resource": f"arn:aws:s3:::dev.storage.frappe.cloud/{self.site}/*",
+				}
+			],
+		}
+		self.policy_json = json.dumps(data, indent=4)
+
+	def set_minio_server_on(self):
+		server = frappe.db.get_value("Site", self.site, "server")
+		self.minio_server_on = frappe.db.get_value("Server", server, "proxy_server")
