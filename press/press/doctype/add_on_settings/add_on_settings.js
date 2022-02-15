3c3424e7e18de10859f6e7275d0b6da5df01d79f
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2022-02-15 13:56:54 +0530

    feat: Temporary restore for etcd storage
    * Minio uses etcd for persistant storage so if something goes wrong use
      this button

--- press/press/doctype/add_on_settings/add_on_settings.js
+++ press/press/doctype/add_on_settings/add_on_settings.js
@@ -2,7 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Add On Settings', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
+		create_custom_button(
+			frm, 
+			"Reset etcd Admin Data", 
+			"init_etcd_data"
+		);
+
+	}
 });
+
+function create_custom_button(frm, title, method) {
+	return frm.add_custom_button(
+		__(title),
+		() => {
+			frappe.prompt(
+				{ fieldtype: 'Data', label: 'Proxy Server Name', fieldname: 'proxy_server', reqd: 1 },
+				({ proxy_server }) => {
+					frm.call(method, { proxy_server: proxy_server });
+				}
+			);
+		},
+		'Actions'
+	);
+}
