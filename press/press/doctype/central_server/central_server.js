5b52358c7f7826ddd14d9d36f435589bef0bc468
Balamurali M <balamurali@frappe.io>
2022-02-16 12:00:33 +0530

    feat: Add doctype for tracking central migration
    - Add playbook to run migrate_2 command

---
+++ press/press/doctype/central_server/central_server.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Central Server', {
+	// refresh: function(frm) {
+
+	// }
+});
