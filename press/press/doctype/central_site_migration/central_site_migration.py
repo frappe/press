61cd167200df7d1997e811fc4c93d8360909cdb4
Balamurali M <balamurali@frappe.io>
2022-02-16 12:00:33 +0530

    feat: Add doctype for tracking central migration
    - Add playbook to run migrate_2 command

---
+++ press/press/doctype/central_site_migration/central_site_migration.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2022, Frappe and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+from press.runner import Ansible
+from press.utils import log_error
+
+central_username = "central@frappecloud.com"
+central_user_password = "wbFzfU62DALuQPZ"
+
+
+def get_ongoing_migration(site: str, scheduled=False):
+	ongoing_statuses = ["Pending", "Running"]
+	if scheduled:
+		ongoing_statuses.append("Scheduled")
+	return frappe.db.exists(
+		"Central Site Migration", {"site": site, "status": ("in", ongoing_statuses)}
+	)
+
+
+class CentralSiteMigration(Document):
+	def before_insert(self):
+		if get_ongoing_migration(self.site, scheduled=True):
+			frappe.throw("Ongoing/Scheduled Site Migration for that site exists.")
+
+	def after_insert(self):
+		if not self.scheduled_time:
+			self.start()
+
+	def start(self):
+		try:
+			ansible = Ansible(
+				playbook="central-site-migration.yml",
+				server=frappe.get_doc("Central Server", self.server),
+				variables={
+					"bench": self.bench,
+					"site": self.site,
+					"version": self.version,
+					"username": central_username,
+					"password": central_user_password
+				},
+			)
+			play = ansible.run()
+			self.reload()
+			if play.status == "Success":
+				self.status = "Success"
+			else:
+				self.status = "Failure"
+		except Exception:
+			self.status = "Failure"
+			log_error("Central Migration Exception", migration=self.as_dict())
+		self.save()
+
+	def _start(self):
+		self.status = "Running"
+		self.save()
+		frappe.enqueue_doc(self.doctype, self.name, "_start", queue="short")
