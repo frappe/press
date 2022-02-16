2c431cc71b03ebba54108209f725c77f0c33376d
Balamurali M <balamurali@frappe.io>
2022-02-16 18:19:24 +0530

    feat(CentralSiteMigration): Schedule one migration every hour

--- press/press/doctype/central_site_migration/central_site_migration.js
+++ press/press/doctype/central_site_migration/central_site_migration.js
@@ -4,7 +4,7 @@
 frappe.ui.form.on('Central Site Migration', {
 	refresh: function (frm) {
 		if (frm.doc.status == 'Failure') {
-			frm.add_custom_button(__('Retry'), () => {
+			frm.add_custom_button(__('Start'), () => {
 				frappe.confirm(
 					'Are you sure you want to retry the migration?',
 					() => frm.call('start')
