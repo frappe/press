ad09ad1b4394d0a64add563bec409d314d43b00d
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2022-02-22 10:33:10 +0530

    feat: Update Storage Integration Sub. doc status
    * update doc status by toggling the checkbox

--- press/press/doctype/storage_integration_subscription/storage_integration_subscription.js
+++ press/press/doctype/storage_integration_subscription/storage_integration_subscription.js
@@ -5,4 +5,13 @@ frappe.ui.form.on('Storage Integration Subscription', {
 	// refresh: function(frm) {
 
 	// }
+	enabled(frm, cdt, cdn) {
+		frappe.call({
+			method: 'press.press.doctype.storage_integration_subscription.storage_integration_subscription.update_user_status',
+			args: {
+				docname: frm.docname,
+				status: frm.fields_dict.enabled.value
+			}
+		})
+	}
 });
