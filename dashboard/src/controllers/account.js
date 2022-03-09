91fdf198c304a4371f62e372a477e7dd3a96ee91
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-03-09 13:50:54 +0530

    refactor: Account, Router & Auth controllers

--- dashboard/src/controllers/account.js
+++ dashboard/src/controllers/account.js
@@ -1,78 +1,75 @@
-import Vue from 'vue';
 import call from './call';
 
-export default new Vue({
-	data() {
-		return {
-			user: null,
-			team: null,
-			ssh_key: null,
-			teams: [],
-			team_members: [],
-			onboarding: null,
-			balance: 0
-		};
-	},
-	created() {
-		this.fetchAccount();
-	},
-	methods: {
-		async fetchAccount() {
-			if (document.cookie.includes('user_id=Guest;')) {
-				return;
-			}
-			try {
-				let result = await call('press.api.account.get');
-				this.user = result.user;
-				this.ssh_key = result.ssh_key;
-				this.team = result.team;
-				this.teams = result.teams;
-				this.team_members = result.team_members;
-				this.onboarding = result.onboarding;
-				this.balance = result.balance;
-			} catch (e) {
-				localStorage.removeItem('current_team');
-			}
-		},
-		hasRole(role) {
-			let roles = this.user.roles.map(d => d.role);
-			return roles.includes(role);
-		},
-		async switchTeam(team) {
-			if (team === this.team.name) {
-				return;
-			}
-			let result = await this.$call('press.api.account.switch_team', { team });
-			this.team = result.team;
-			this.team_members = result.team_members;
-			localStorage.setItem('current_team', team);
-		},
-		async switchToTeam(team) {
-			await this.switchTeam(team);
-			window.location.reload();
+export default class Account {
+	constructor() {
+		this.user = null;
+		this.team = null;
+		this.ssh_key = null;
+		this.teams = [];
+		this.team_members = [];
+		this.onboarding = null;
+		this.balance = 0;
+	}
+
+	async fetchAccount() {
+		console.log('fetchAccount mein hain');
+		if (document.cookie.includes('user_id=Guest;')) {
+			return;
 		}
-	},
-	computed: {
-		needsCard() {
-			return !this.hasBillingInfo;
-		},
-		hasBillingInfo() {
-			if (!this.team) {
-				return true;
-			}
-			if (this.team.free_account) {
-				return true;
-			}
-			if (this.team.erpnext_partner) {
-				return true;
-			}
-			if (this.team.payment_mode == 'Card') {
-				return this.team.default_payment_method;
-			}
-			if (this.team.payment_mode == 'Prepaid Credits') {
-				return this.balance > 0;
-			}
-			return false;
+		try {
+			let result = await call('press.api.account.get');
+			this.user = result.user;
+			this.ssh_key = result.ssh_key;
+			this.team = result.team;
+			this.teams = result.teams;
+			this.team_members = result.team_members;
+			this.onboarding = result.onboarding;
+			this.balance = result.balance;
+		} catch (e) {
+			localStorage.removeItem('current_team');
 		}
 	}
-});
+
+	hasRole(role) {
+		let roles = this.user.roles.map(d => d.role);
+		return roles.includes(role);
+	}
+
+	async switchTeam(team) {
+		if (team === this.team.name) {
+			return;
+		}
+		let result = await this.$call('press.api.account.switch_team', { team });
+		this.team = result.team;
+		this.team_members = result.team_members;
+		localStorage.setItem('current_team', team);
+	}
+
+	async switchToTeam(team) {
+		await this.switchTeam(team);
+		window.location.reload();
+	}
+
+	get needsCard() {
+		return !this.hasBillingInfo;
+	}
+
+	get hasBillingInfo() {
+		if (!this.team) {
+			return true;
+		}
+		if (this.team.free_account) {
+			return true;
+		}
+		if (this.team.erpnext_partner) {
+			return true;
+		}
+		if (this.team.payment_mode == 'Card') {
+			return this.team.default_payment_method;
+		}
+		if (this.team.payment_mode == 'Prepaid Credits') {
+			return this.balance > 0;
+		}
+		return false;
+	}
+}
--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -1,7 +1,4 @@
-import account from '../controllers/account';
-import auth from '../controllers/auth';
 import Home from '../views/Home.vue';
-
 import siteRoutes from './site';
 import benchRoutes from './bench';
 import accountRoute from './account';
@@ -31,36 +28,6 @@ const routes = [
 const router = createRouter({
 	history: createWebHistory('/dashboard/'),
 	routes
-});
-
-router.beforeEach(async (to, from, next) => {
-	if (to.name == 'Home') {
-		next({ name: 'Welcome' });
-		return;
-	}
-
-	if (to.matched.some(record => !record.meta.isLoginPage)) {
-		// this route requires auth, check if logged in
-		// if not, redirect to login page.
-		if (!auth.isLoggedIn) {
-			next({ name: 'Login', query: { route: to.path } });
-		} else {
-			if (!account.user) {
-				await account.fetchAccount();
-			}
-			next();
-		}
-	} else {
-		// if already logged in, route to /welcome
-		if (auth.isLoggedIn) {
-			if (!account.user) {
-				await account.fetchAccount();
-			}
-			next({ name: 'Welcome' });
-		} else {
-			next();
-		}
-	}
 });
 
 export default router;
