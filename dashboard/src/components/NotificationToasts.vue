bf37bb8906370fbcc684c2600a692fd8c79c9d82
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-03-14 11:09:50 +0530

    refactor: NotificationToasts place script before template
    * New Vue 3 convention for `setup` scritps

--- dashboard/src/components/NotificationToasts.vue
+++ dashboard/src/components/NotificationToasts.vue
@@ -1,18 +1,3 @@
-<template>
-	<div
-		class="pointer-events-none fixed inset-0 flex items-start justify-end px-4 py-6 sm:p-6"
-	>
-		<div>
-			<Notification
-				v-bind="props"
-				class="mb-4"
-				:key="i"
-				v-for="(props, i) in notifications"
-				@dismiss="hideNotification"
-			/>
-		</div>
-	</div>
-</template>
 <script setup>
 import { getCurrentInstance, ref } from 'vue';
 import Notification from './Notification.vue';
@@ -33,3 +18,19 @@ const notify = props => {
 // Attach to global instance
 app.appContext.config.globalProperties.$notify = notify;
 </script>
+
+<template>
+	<div
+		class="pointer-events-none fixed inset-0 flex items-start justify-end px-4 py-6 sm:p-6"
+	>
+		<div>
+			<Notification
+				v-bind="props"
+				class="mb-4"
+				:key="i"
+				v-for="(props, i) in notifications"
+				@dismiss="hideNotification"
+			/>
+		</div>
+	</div>
+</template>
