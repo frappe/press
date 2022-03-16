c717a66fda45c4a2fca4a48c9eaf8408f5f731fb
Aditya Hase <aditya@adityahase.com>
2022-03-16 22:07:25 +0530

    feat: Plausible Analytics

---
+++ press/press/doctype/analytics_server/analytics_server.js
@@ -0,0 +1,47 @@
+// Copyright (c) 2022, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Analytics Server', {
+	refresh: function (frm) {
+		[
+			[__('Ping Agent'), "ping_agent", false, frm.doc.is_server_setup],
+			[__('Ping Ansible'), "ping_ansible", true],
+			[__('Ping Ansible Unprepared'), "ping_ansible_unprepared", true],
+			[__('Update Agent'), "update_agent", true, frm.doc.is_server_setup],
+			[__('Prepare Server'), "prepare_server", true, !frm.doc.is_server_setup],
+			[__('Setup Server'), "setup_server", true, !frm.doc.is_server_setup],
+			[__('Fetch Keys'), "fetch_keys", false, frm.doc.is_server_setup && (!frm.doc.frappe_public_key || !frm.doc.root_public_key)],
+			[__('Show Plausible Password'), "show_plausible_password", false, frm.doc.is_server_setup],
+		].forEach(([label, method, confirm, condition]) => {
+			if (typeof condition === "undefined" || condition) {
+				frm.add_custom_button(
+					label,
+					() => {
+						if (confirm) {
+							frappe.confirm(
+								`Are you sure you want to ${label.toLowerCase()}?`,
+								() => frm.call(method).then((r) => {
+									if (r.message) {
+										frappe.msgprint(r.message);
+									} else {
+										frm.refresh();
+									}
+								})
+							);
+
+						} else {
+							frm.call(method).then((r) => {
+								if (r.message) {
+									frappe.msgprint(r.message);
+								} else {
+									frm.refresh();
+								}
+							})
+						}
+					},
+					__('Actions')
+				);
+			}
+		});
+	}
+});
