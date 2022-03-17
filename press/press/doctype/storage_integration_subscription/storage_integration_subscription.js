436cb2755f989f0d9f4918049514b706593ad8e3
Rutwik <rutwikhdev@gmail.com>
2022-03-17 13:12:09 +0530

    chore: Refactor minio method paths

--- press/press/doctype/storage_integration_subscription/storage_integration_subscription.js
+++ press/press/doctype/storage_integration_subscription/storage_integration_subscription.js
@@ -7,7 +7,7 @@ frappe.ui.form.on('Storage Integration Subscription', {
 	// }
 	enabled(frm, cdt, cdn) {
 		frappe.call({
-			method: 'press.press.doctype.storage_integration_subscription.storage_integration_subscription.update_user_status',
+			method: 'press.press.doctype.storage_integration_subscription.storage_integration_subscription.toggle_user_status',
 			args: {
 				docname: frm.docname,
 				status: frm.fields_dict.enabled.value
