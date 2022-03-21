c030bb2fa780183bc2ad18b1590be1110edc4a7c
Rutwik <rutwikhdev@gmail.com>
2022-03-21 13:59:37 +0530

    chore: Return available plans for upgrade component

--- press/saas/doctype/saas_app/saas_app.py
+++ press/saas/doctype/saas_app/saas_app.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
+from press.saas.doctype.saas_app_plan.saas_app_plan import get_app_plan_features
 
 
 class SaasApp(Document):
@@ -12,3 +13,38 @@ class SaasApp(Document):
 	def get_app_versions(self):
 		v = [ver.version_name for ver in self.app_versions]
 		return v
+
+	def get_plans(self):
+		return get_plans_for_app(self.name)
+
+
+def get_plans_for_app(app):
+	plans = []
+	saas_app_plans = frappe.get_all(
+		"Saas App Plan",
+		filters={"app": app, "enabled": 1},
+		fields=["name", "plan", "is_free"],
+	)
+
+	for app_plan in saas_app_plans:
+		plan_data = {}
+		plan_data.update(app_plan)
+
+		plan_prices = get_plan_prices(app_plan.plan)
+		plan_data.update(plan_prices)
+
+		plan_data["features"] = get_app_plan_features(app_plan.name)
+
+		plans.append(plan_data)
+
+	plans.sort(key=lambda x: x["price_usd"])
+
+	return plans
+
+
+def get_plan_prices(plan_name):
+	plan_prices = frappe.db.get_value(
+		"Plan", plan_name, ["plan_title", "price_usd", "price_inr"], as_dict=True
+	)
+
+	return plan_prices
