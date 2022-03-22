1b2d409fc50149c56252f28e2618efc1faec4b5c
Rutwik <rutwikhdev@gmail.com>
2022-03-22 19:57:43 +0530

    feat: Create usage_record for saas subscription

--- press/saas/doctype/saas_app_subscription/saas_app_subscription.py
+++ press/saas/doctype/saas_app_subscription/saas_app_subscription.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
+from press.utils import log_error
 
 
 class SaasAppSubscription(Document):
@@ -32,3 +33,81 @@ class SaasAppSubscription(Document):
 				f"Subscription for app '{frappe.bold(self.app)}' already exists for"
 				f" site '{frappe.bold(self.site)}'!"
 			)
+
+	def create_usage_record(self):
+		if self.is_usage_record_created():
+			return
+
+		team_name = frappe.db.get_value("Site", self.site, "team")
+		team = frappe.get_cached_doc("Team", team_name)
+
+		if not team.get_upcoming_invoice():
+			team.create_upcoming_invoice()
+
+		plan = frappe.get_cached_doc("Plan", self.plan)
+		amount = plan.get_price_for_interval(self.interval, team.currency)
+
+		usage_record = frappe.get_doc(
+			doctype="Usage Record",
+			team=team_name,
+			document_type="Saas App",
+			document_name=self.app,
+			plan=self.plan,
+			amount=amount,
+			subscription=self.name,
+			interval=self.interval,
+		)
+		usage_record.insert()
+		usage_record.submit()
+		return usage_record
+
+	def is_usage_record_created(self):
+		team = frappe.db.get_value("Site", self.site, "team")
+		filters = {
+			"team": team,
+			"document_type": "Saas App",
+			"document_name": self.app,
+			"subscription": self.name,
+			"interval": self.interval,
+			"plan": self.plan,
+		}
+
+		if self.interval == "Daily":
+			filters.update({"date": frappe.utils.today()})
+
+		if self.interval == "Monthly":
+			date = frappe.utils.getdate()
+			first_day = frappe.utils.get_first_day(date)
+			last_day = frappe.utils.get_last_day(date)
+			filters.update({"date": ("between", (first_day, last_day))})
+
+		result = frappe.db.get_all("Usage Record", filters=filters, limit=1)
+		return bool(result)
+
+
+def create_usage_records():
+	subscriptions = frappe.db.get_all(
+		"Saas App Subscription", filters={"status": "Active"}, pluck="name"
+	)
+	for name in subscriptions:
+		subscription = frappe.get_doc("Saas App Subscription", name)
+
+		if not should_create_usage_record(subscription):
+			continue
+
+		try:
+			subscription.create_usage_record()
+			frappe.db.commit()
+		except Exception:
+			frappe.db.rollback()
+			log_error(title="Saas App: Create Usage Record Error", name=name)
+
+
+def should_create_usage_record(subscription):
+	# Don't create for free plans
+	is_free = frappe.db.get_value("Saas App Plan", subscription.saas_app_plan, "is_free")
+
+	if is_free:
+		return False
+
+	return True
