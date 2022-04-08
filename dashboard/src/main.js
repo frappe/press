de122e2980a2f5366152fa80752abd47ae5ca67d
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-04-08 19:21:57 +0530

    refactor: main.js should be small, it is now
    * Move registration of the following in their own files:
    * 1. Plugins: Resource manager and utils
    * 2. Controllers: Auth, call, socket and account
    * 3. Router: Takes in auth and account, does everything on its own then

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,40 +1,16 @@
 import App from './App.vue';
-import utils from './utils';
-import router from './router';
-import call from './controllers/call';
-import Auth from './controllers/auth';
-import socket from './controllers/socket';
-import Account from './controllers/account';
-import resourceManager from './resourceManager';
+import { createApp } from 'vue';
+import registerPlugins from './plugins';
+import registerRouter from './router/register';
+import registerControllers from './controllers/register';
 import registerGlobalComponents from './components/global/register';
 
-import { createApp, reactive } from 'vue';
-
 const app = createApp(App);
-const auth = reactive(new Auth());
-const account = reactive(new Account());
 
+registerPlugins(app);
 registerGlobalComponents(app);
-
-app.use(resourceManager);
-app.use(utils);
-app.use(router);
-
-app.config.globalProperties.$call = call;
-app.provide('$call', call);
-
-// Hack to get $auth working, should be refactored later
-app.config.globalProperties.$auth = auth;
-// Actually, provide-inject is recommended to be used
-app.provide('$auth', auth);
-
-app.config.globalProperties.$socket = socket;
-app.provide('$account', account);
-app.config.globalProperties.$account = account;
-app.provide('$socket', socket);
-
-// global accessor to expose switchToTeam method
-window.$account = account;
+const { auth, account } = registerControllers(app);
+registerRouter(app, auth, account);
 
 app.mount('#app');
 
@@ -49,33 +25,3 @@ app.config.errorHandler = (error, instance) => {
 	}
 	console.error(error);
 };
-
-router.beforeEach(async (to, from, next) => {
-	if (to.name == 'Home') {
-		next({ name: 'Welcome' });
-		return;
-	}
-
-	if (to.matched.some(record => !record.meta.isLoginPage)) {
-		// this route requires auth, check if logged in
-		// if not, redirect to login page.
-		if (!auth.isLoggedIn) {
-			next({ name: 'Login', query: { route: to.path } });
-		} else {
-			if (!account.user) {
-				await account.fetchAccount();
-			}
-			next();
-		}
-	} else {
-		// if already logged in, route to /welcome
-		if (auth.isLoggedIn) {
-			if (!account.user) {
-				await account.fetchAccount();
-			}
-			next({ name: 'Welcome' });
-		} else {
-			next();
-		}
-	}
-});
