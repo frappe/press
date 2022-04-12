506fe369b9da4d3ab0b312b6a1021e6867f1f54d
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-04-12 14:11:36 +0530

    test: FeatureList component
    * First test!

---
+++ dashboard/src/tests/core/FeatureList.test.js
@@ -0,0 +1,19 @@
+import { mount } from '@vue/test-utils';
+import { describe, expect, should, test } from 'vitest';
+import FeatureList from '@/components/FeatureList.vue';
+
+describe('FeatureList Component', () => {
+	test('feature list renders with 2 features in correct order', async () => {
+		expect(FeatureList).toBeTruthy();
+
+		const wrapper = mount(FeatureList, {
+			props: {
+				features: ['Feature 1', 'Feature 2']
+			}
+		});
+
+		expect(wrapper.findAll('li').length).toBe(2);
+        expect(wrapper.findAll('li')[0].text()).toBe('Feature 1');
+        expect(wrapper.findAll('li')[1].text()).toBe('Feature 2');
+	});
+});
