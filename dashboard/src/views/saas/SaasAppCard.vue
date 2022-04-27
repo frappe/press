1c90db8b01848676f056674c255e90846e31e51c
Rutwik <rutwikhdev@gmail.com>
2022-04-27 18:52:21 +0530

    feat: Added SaasAppCard component

---
+++ dashboard/src/views/saas/SaasAppCard.vue
@@ -0,0 +1,31 @@
+<template>
+	<a
+		class="m-2 flex self-stretch rounded-lg border bg-white p-3 shadow hover:border-gray-300 focus:outline-none"
+		:href="app.signup_url"
+	>
+		<Avatar
+			class="shrink-0"
+			size="lg"
+			shape="square"
+			:imageURL="app.image"
+			:label="app.title"
+		/>
+		<div class="ml-3 w-full">
+			<div class="flex items-center justify-between">
+				<h2 class="text-left text-xl font-bold">
+					{{ app.title }}
+				</h2>
+				<!-- <Badge :status="app.status">{{ app.status }}</Badge> -->
+			</div>
+			<p class="pt-1 text-left text-base text-gray-600">
+				{{ app.description }}
+			</p>
+		</div>
+	</a>
+</template>
+
+<script>
+export default {
+	props: ['app']
+};
+</script>
