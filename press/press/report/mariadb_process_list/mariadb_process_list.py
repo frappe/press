fec1e0c5049030bc4dea100e70b80a709e6a7f0c
Aditya Hase <aditya@adityahase.com>
2022-04-27 23:51:25 +0530

    feat: MariaDB Process List report

---
+++ press/press/report/mariadb_process_list/mariadb_process_list.py
@@ -0,0 +1,88 @@
+# Copyright (c) 2022, Frappe and contributors
+# For license information, please see license.txt
+
+import frappe
+
+import sqlparse
+from press.agent import Agent
+
+
+def execute(filters=None):
+	frappe.only_for("System Manager")
+
+	columns = [
+		{
+			"fieldname": "Id",
+			"label": frappe._("ID"),
+			"fieldtype": "Int",
+			"width": 70,
+		},
+		{
+			"fieldname": "User",
+			"label": frappe._("User"),
+			"fieldtype": "Data",
+			"width": 150,
+		},
+		{
+			"fieldname": "Host",
+			"label": frappe._("Host"),
+			"fieldtype": "Data",
+			"width": 150,
+		},
+		{
+			"fieldname": "db",
+			"label": frappe._("Database"),
+			"fieldtype": "Data",
+			"width": 150,
+		},
+		{
+			"fieldname": "Command",
+			"label": frappe._("Command"),
+			"fieldtype": "Data",
+			"width": 400,
+		},
+		{
+			"fieldname": "Time",
+			"label": frappe._("Time"),
+			"fieldtype": "Int",
+			"width": 70,
+		},
+		{
+			"fieldname": "State",
+			"label": frappe._("State"),
+			"fieldtype": "Data",
+			"width": 150,
+		},
+		{
+			"fieldname": "Info",
+			"label": frappe._("Info"),
+			"fieldtype": "Int",
+			"width": 80,
+		},
+		{
+			"fieldname": "Progress",
+			"label": frappe._("Progress"),
+			"fieldtype": "Float",
+			"width": 80,
+		},
+	]
+
+	data = get_data(filters)
+	return columns, data
+
+
+def get_data(filters):
+	server = frappe.get_doc("Database Server", filters.database_server)
+	agent = Agent(server.name, "Database Server")
+
+	data = {
+		"private_ip": server.private_ip,
+		"mariadb_root_password": server.get_password("mariadb_root_password"),
+	}
+	rows = agent.post("database/processes", data=data)
+
+	for row in rows:
+		row["Command"] = sqlparse.format(
+			row["Command"].strip(), keyword_case="upper", reindent=True
+		)
+	return rows
