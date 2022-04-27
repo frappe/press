61267c44e51719b81eee94967232508d8b619e8a
Aditya Hase <aditya@adityahase.com>
2022-04-27 23:52:00 +0530

    fix: Kill processes running longer than given threshold

--- press/press/report/mariadb_process_list/mariadb_process_list.js
+++ press/press/report/mariadb_process_list/mariadb_process_list.js
@@ -3,4 +3,33 @@
 /* eslint-disable */
 
 frappe.query_reports["MariaDB Process List"] = {
+	onload: function(report) {
+		report.page.add_button(
+				__('Kill Processes'),
+				() => {
+					const dialog = new frappe.ui.Dialog({
+						title: __('Kill Processes'),
+						fields: [{
+							fieldtype: 'Int',
+							default: 120,
+							label: __('Kill Proceeses Running Longer Than (Seconds)'),
+							fieldname: 'kill_threshold',
+						}]
+					});
+					
+					dialog.set_primary_action(__('Kill Processes'), args => {
+						frappe.call('press.press.report.mariadb_process_list.mariadb_process_list.kill',
+							{
+								database_server: frappe.query_report.get_filter_value('database_server'),
+								kill_threshold: args.kill_threshold
+							}
+						).then((r) => {
+							dialog.hide();
+							frappe.query_report.refresh()
+						})
+					});
+					dialog.show();
+				},
+			);
+		}
 };
