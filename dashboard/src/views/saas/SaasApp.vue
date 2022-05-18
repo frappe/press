78c23b1702d3524ab10d6984784d00cde1c23dcb
Rutwik <rutwikhdev@gmail.com>
2022-05-18 17:11:09 +0530

    chore: Back link for Manage Apps component

--- dashboard/src/views/saas/SaasApp.vue
+++ dashboard/src/views/saas/SaasApp.vue
@@ -2,6 +2,26 @@
 	<div class="mt-8 flex-1">
 		<div class="text-base text-gray-700">
 			<div class="px-4 sm:px-8">
+				<div class="text-base text-gray-700">
+					<router-link to="/saas/manage" class="hover:text-gray-800">
+						← Back to Apps
+					</router-link>
+				</div>
+
+				<div
+					v-if="app.data"
+					class="my-4 flex flex-col space-y-3 md:flex-row md:items-baseline md:justify-between md:space-y-0"
+				>
+					<div class="mt-2 flex items-center">
+						<h1 class="text-2xl font-bold">{{ app.data.title }}</h1>
+						<Badge
+							class="ml-4 hidden md:inline-block"
+							:status="app.data.status"
+							>{{ app.data.status }}</Badge
+						>
+					</div>
+				</div>
+
 				<Tabs class="pb-8" :tabs="tabs">
 					<router-view v-slot="{ Component, route }">
 						<component
