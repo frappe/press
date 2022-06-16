b6aed719ce9dda61f6b2b18346c247494959f7d3
Rutwik <rutwikhdev@gmail.com>
2022-06-16 16:28:05 +0530

    style: Prettier

--- dashboard/src/router/saas.js
+++ dashboard/src/router/saas.js
@@ -14,7 +14,7 @@ export default [
 		name: 'RemoteLogin',
 		component: () => import('../views/saas/RemoteLogin.vue'),
 		meta: {
-			isLoginPage: true,
+			isLoginPage: true
 		},
 		props: true
 	},
