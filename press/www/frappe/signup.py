a9cedfdb00385b498b17c17436968f5f746b242a
Rutwik <rutwikhdev@gmail.com>
2022-06-16 16:12:46 +0530

    clean: Deleted frappeframework signup templates

--- press/www/frappe/signup.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and Contributors
-# Proprietary License. See license.txt
-
-
-from frappe.geo.country_info import get_country_info
-
-
-def get_context():
-	return {"country_info": get_country_info()}
