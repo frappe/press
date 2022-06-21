1048293cf867acce48b00ec7cd05b7f53ea5d3d7
Rutwik <rutwikhdev@gmail.com>
2022-06-21 10:54:43 +0530

    chore(saas-dashboard): Added Deactivate and Active to subscription

--- dashboard/src/views/saas/SubscriptionSettings.vue
+++ dashboard/src/views/saas/SubscriptionSettings.vue
@@ -1,26 +1,94 @@
-<script setup>
-import { computed, ref } from 'vue';
-import SiteDrop from '../SiteDrop.vue';
-
-const props = defineProps({ site: Object });
-</script>
-
 <template>
 	<div class="sm:grid sm:grid-cols-2">
 		<Card title="Settings">
-			<ListItem
+			<!--<ListItem
 				v-if="props.site.status !== 'Pending'"
 				title="Drop Site"
 				description="Once you drop site your site, there is no going back"
 			>
 				<template v-slot:actions>
-					<SiteDrop :site="props.site" v-slot="{ showDialog }">
+					<SiteDrop :site="this.props.site" v-slot="{ showDialog }">
 						<Button @click="showDialog">
 							<span class="text-red-600">Drop Site</span>
 						</Button>
 					</SiteDrop>
 				</template>
+			</ListItem> -->
+			<ListItem
+				v-if="site.status == 'Active'"
+				title="Deactivate Subscription"
+				description="The subscription will go inactive and site won't be publicly accessible, also the billing will stop"
+			>
+				<template v-slot:actions>
+					<Button @click="onDeactivateClick" class="shrink-0">
+						Deactivate
+					</Button>
+				</template>
+			</ListItem>
+
+			<ListItem
+				v-if="site.status == 'Inactive'"
+				title="Activate Subscription"
+				description="The subscription will become active and billing will be resumed"
+			>
+				<template v-slot:actions>
+					<Button @click="onActivateClick" class="shrink-0"> Activate </Button>
+				</template>
 			</ListItem>
 		</Card>
 	</div>
 </template>
+
+<script>
+export default {
+	name: 'SubscriptionSettings',
+	props: ['site', 'subName'],
+	data() {
+		return {
+			dialogOpen: true
+		};
+	},
+	methods: {
+		onDeactivateClick() {
+			this.$confirm({
+				title: 'Deactivate Site',
+				message: `
+					Are you sure you want to deactivate this site? The site will go in an inactive state.
+					It won't be accessible and background jobs won't run. You will <strong>still be charged</strong> for it.
+				`,
+				actionLabel: 'Deactivate',
+				actionType: 'danger',
+				action: () => this.deactivate()
+			});
+		},
+		onActivateClick() {
+			this.$confirm({
+				title: 'Activate Site',
+				message: 'Are you sure you want to activate this site?',
+				actionLabel: 'Activate',
+				actionType: 'primary',
+				action: () => this.activate()
+			});
+		},
+		deactivate() {
+			return this.$call('press.api.saas.deactivate', {
+				name: this.subName
+			}).then(() => {
+				setTimeout(() => window.location.reload(), 1000);
+			});
+		},
+		activate() {
+			this.$call('press.api.saas.activate', {
+				name: this.subName
+			});
+			this.$notify({
+				title: 'Site activated successfully!',
+				message: 'You can now access your site',
+				icon: 'check',
+				color: 'green'
+			});
+			setTimeout(() => window.location.reload(), 1000);
+		}
+	}
+};
+</script>
