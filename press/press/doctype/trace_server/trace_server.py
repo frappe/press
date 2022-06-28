01bc814c8aa1e21fdcdb9ad993e9f8c253d0b1ef
Aditya Hase <aditya@adityahase.com>
2022-06-28 18:14:14 +0530

    feat: Trace Server powered by Sentry

---
+++ press/press/doctype/trace_server/trace_server.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022, Frappe and contributors
+# For license information, please see license.txt
+
+import frappe
+
+from press.press.doctype.server.server import BaseServer
+from press.runner import Ansible
+from press.utils import log_error
+
+
+class TraceServer(BaseServer):
+	pass
