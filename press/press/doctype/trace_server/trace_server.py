6d8d49bde978965b692d39ef7e632edc8d13232f
Aditya Hase <aditya@adityahase.com>
2022-06-28 18:20:39 +0530

    feat: Ansible playbook for setting up Sentry

--- press/press/doctype/trace_server/trace_server.py
+++ press/press/doctype/trace_server/trace_server.py
@@ -9,4 +9,48 @@ from press.utils import log_error
 
 
 class TraceServer(BaseServer):
-	pass
+	def _setup_server(self):
+		agent_repository_url = self.get_agent_repository_url()
+		certificate_name = frappe.db.get_value(
+			"TLS Certificate", {"wildcard": True, "domain": self.domain}, "name"
+		)
+		certificate = frappe.get_doc("TLS Certificate", certificate_name)
+
+		log_server = frappe.db.get_single_value("Press Settings", "log_server")
+		if log_server:
+			kibana_password = frappe.get_doc("Log Server", log_server).get_password(
+				"kibana_password"
+			)
+		else:
+			kibana_password = None
+
+		try:
+			ansible = Ansible(
+				playbook="trace.yml",
+				server=self,
+				variables={
+					"server": self.name,
+					"workers": 1,
+					"domain": self.domain,
+					"log_server": log_server,
+					"agent_password": self.get_password("agent_password"),
+					"agent_repository_url": agent_repository_url,
+					"kibana_password": kibana_password,
+					"monitoring_password": self.get_password("monitoring_password"),
+					"private_ip": self.private_ip,
+					"certificate_private_key": certificate.private_key,
+					"certificate_full_chain": certificate.full_chain,
+					"certificate_intermediate_chain": certificate.intermediate_chain,
+				},
+			)
+			play = ansible.run()
+			self.reload()
+			if play.status == "Success":
+				self.status = "Active"
+				self.is_server_setup = True
+			else:
+				self.status = "Broken"
+		except Exception:
+			self.status = "Broken"
+			log_error("Trace Server Setup Exception", server=self.as_dict())
+		self.save()
