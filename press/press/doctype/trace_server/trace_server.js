6a2dcfc5da194cc3e0f7879cbf585070602b2827
Aditya Hase <aditya@adityahase.com>
2022-06-28 18:16:48 +0530

    chore: Common server actions

--- press/press/doctype/trace_server/trace_server.js
+++ press/press/doctype/trace_server/trace_server.js
@@ -3,5 +3,45 @@
 
 frappe.ui.form.on('Trace Server', {
 	refresh: function (frm) {
+		[
+			[__('Ping Agent'), "ping_agent", false, frm.doc.is_server_setup],
+			[__('Ping Ansible'), "ping_ansible", true],
+			[__('Ping Ansible Unprepared'), "ping_ansible_unprepared", true],
+			[__('Update Agent'), "update_agent", true, frm.doc.is_server_setup],
+			[__('Prepare Server'), "prepare_server", true, !frm.doc.is_server_setup],
+			[__('Setup Server'), "setup_server", true, !frm.doc.is_server_setup],
+			[__('Reconfigure Monitor Server'), "reconfigure_monitor_server", true, frm.doc.is_server_setup],
+			[__('Fetch Keys'), "fetch_keys", false, frm.doc.is_server_setup && (!frm.doc.frappe_public_key || !frm.doc.root_public_key)],
+		].forEach(([label, method, confirm, condition]) => {
+			if (typeof condition === "undefined" || condition) {
+				frm.add_custom_button(
+					label,
+					() => {
+						if (confirm) {
+							frappe.confirm(
+								`Are you sure you want to ${label.toLowerCase()}?`,
+								() => frm.call(method).then((r) => {
+									if (r.message) {
+										frappe.msgprint(r.message);
+									} else {
+										frm.refresh();
+									}
+								})
+							);
+
+						} else {
+							frm.call(method).then((r) => {
+								if (r.message) {
+									frappe.msgprint(r.message);
+								} else {
+									frm.refresh();
+								}
+							})
+						}
+					},
+					__('Actions')
+				);
+			}
+		});
 	}
 });
