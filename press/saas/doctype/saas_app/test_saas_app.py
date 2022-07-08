52dcb44faa85eba7d8be12ee592c9700d03550c5
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-07-08 06:46:46 +0530

    test: Fix drip email tests after SaaS changes

--- press/saas/doctype/saas_app/test_saas_app.py
+++ press/saas/doctype/saas_app/test_saas_app.py
@@ -1,8 +1,15 @@
 # Copyright (c) 2022, Frappe and Contributors
 # See license.txt
 
-# import frappe
+import frappe
+
 from frappe.tests.utils import FrappeTestCase
+
+
+def create_test_saas_app(app: str):
+	return frappe.get_doc({"doctype": "Saas App", "app": app}).insert(
+		ignore_if_duplicate=True
+	)
 
 
 class TestSaasApp(FrappeTestCase):
