8d27a15d2ac5c1a4d3b2bf49716dacec320a572f
Rutwik <rutwikhdev@gmail.com>
2022-07-27 19:29:32 +0530

    style: Prettier

--- dashboard/src/views/SiteOverview.vue
+++ dashboard/src/views/SiteOverview.vue
@@ -38,8 +38,7 @@ const limitExceeded = computed(() => {
 const isInTrial = computed(() => {
 	let curr = new Date();
 	let trial_end_date = new Date(Date.parse(props.site.trial_end_date));
-	if (trial_end_date > curr) 
-		return props.site?.trial_end_date;
+	if (trial_end_date > curr) return props.site?.trial_end_date;
 	return false;
 });
 
@@ -91,7 +90,8 @@ const marketplacePromotionalBanners = useResource({
 			</Alert>
 		</div>
 		<Alert title="Trial" v-if="!isInTrial">
-			Your trial has ended. Select a plan from the Plan section below to continue using your site.
+			Your trial has ended. Select a plan from the Plan section below to
+			continue using your site.
 		</Alert>
 		<Alert title="Trial" v-if="isInTrial && $account.needsCard">
 			Your trial ends {{ trialEndsText }} after which your site will get
