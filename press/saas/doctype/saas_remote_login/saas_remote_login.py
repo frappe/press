a13fb4cfa7617a320e73f4ebaf971bbdee31c964
Rutwik <rutwikhdev@gmail.com>
2022-08-08 21:13:26 +0530

    chore: Increase Saas Remote Login token expiry time

--- press/saas/doctype/saas_remote_login/saas_remote_login.py
+++ press/saas/doctype/saas_remote_login/saas_remote_login.py
@@ -4,9 +4,9 @@
 import frappe
 from frappe.model.document import Document
 
-EXPIRES_IN_MINUTES = 10
+EXPIRES_IN_HOURS = 6
 
 
 class SaasRemoteLogin(Document):
 	def before_insert(self):
-		self.expires_on = frappe.utils.add_to_date(None, minutes=EXPIRES_IN_MINUTES)
+		self.expires_on = frappe.utils.add_to_date(None, hours=EXPIRES_IN_HOURS)
