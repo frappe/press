7f8bbfd60a2d4a12ba3b3713a04eed3b3ca1e4de
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-08-29 16:45:40 +0530

    chore: Add page header to benches page

--- dashboard/src/views/Benches.vue
+++ dashboard/src/views/Benches.vue
@@ -1,6 +1,17 @@
 <template>
 	<div>
-		<PageHeader title="Benches" />
+		<PageHeader title="Benches" subtitle="Private benches you own">
+			<template v-slot:actions>
+				<Button
+					appearance="primary"
+					iconLeft="plus"
+					class="ml-2 hidden sm:inline-flex"
+					route="/benches/new"
+				>
+					New
+				</Button>
+			</template>
+		</PageHeader>
 	</div>
 </template>
 
