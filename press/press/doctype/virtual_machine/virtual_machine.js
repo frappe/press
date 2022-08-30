ec289590939296694e4878421cd28f38ba41fdc5
Aditya Hase <aditya@adityahase.com>
2022-08-30 17:06:46 +0530

    fix: Allow terminating Virtual Machine using API

--- press/press/doctype/virtual_machine/virtual_machine.js
+++ press/press/doctype/virtual_machine/virtual_machine.js
@@ -5,17 +5,20 @@ frappe.ui.form.on('Virtual Machine', {
 	refresh: function (frm) {
 		[
 			[__('Sync'), 'sync'],
-			[__('Terminate'), 'terminate'],
-			[__('Disable Termination Protection'), 'disable_termination_protection'],
+			[__('Terminate'), 'terminate', !frm.doc.termination_protection],
+			[__('Disable Termination Protection'), 'disable_termination_protection', frm.doc.termination_protection],
+			[__('Enable Termination Protection'), 'enable_termination_protection', !frm.doc.termination_protection],
 			[__('Create Image'), 'create_image'],
-		].forEach(([label, method]) => {
-			frm.add_custom_button(
-				label,
-				() => {
-					frm.call(method).then((r) => frm.refresh())
-				},
-				__('Actions')
-			);
+		].forEach(([label, method, condition]) => {
+			if (typeof condition === "undefined" || condition) {
+				frm.add_custom_button(
+					label,
+					() => {
+						frm.call(method).then((r) => frm.refresh())
+					},
+					__('Actions')
+				);
+			}
 		});
 		if (frm.doc.aws_instance_id){
 			frm.add_web_link(`https://${frm.doc.region}.console.aws.amazon.com/ec2/v2/home?region=${frm.doc.region}#InstanceDetails:instanceId=${frm.doc.aws_instance_id}`, __('Visit AWS Dashboard'));
