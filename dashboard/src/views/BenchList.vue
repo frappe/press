d86d053c240cf6ab47466618950af972de0af869
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-08-30 07:21:37 +0530

    feat: `BenchList` component

---
+++ dashboard/src/views/BenchList.vue
@@ -0,0 +1,82 @@
+<template>
+	<div
+		class="sm:rounded-md sm:border sm:border-gray-100 sm:py-1 sm:px-2 sm:shadow"
+	>
+		<div
+			class="py-2 text-base text-gray-600 sm:px-2"
+			v-if="benches.length === 0"
+		>
+			No benches to show. Go ahead, add a new one 🚀!
+		</div>
+		<div class="py-2" v-for="(bench, index) in benches" :key="bench.name">
+			<div class="flex items-center justify-between">
+				<router-link
+					:to="`/benches/${bench.name}/overview`"
+					class="block w-full rounded-md py-2 hover:bg-gray-50 sm:px-2"
+				>
+					<div class="flex items-center justify-between">
+						<div class="text-base sm:w-4/12">
+							{{ bench.title }}
+						</div>
+						<div class="text-base sm:w-4/12">
+							<Badge
+								:status="`${bench.deploy_information.number_of_apps} Apps`"
+							/>
+						</div>
+						<div class="hidden w-2/12 text-sm text-gray-600 sm:block">
+							{{ bench.version }}
+						</div>
+					</div>
+				</router-link>
+
+				<div class="text-right text-base">
+					<Dropdown :items="dropdownItems(bench)" right>
+						<template v-slot="{ toggleDropdown }">
+							<Button icon="more-horizontal" @click.stop="toggleDropdown()" />
+						</template>
+					</Dropdown>
+				</div>
+			</div>
+			<div
+				class="translate-y-2 transform"
+				:class="{ 'border-b': index < benches.length - 1 }"
+			/>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'BenchList',
+	props: ['benches'],
+	methods: {
+		benchBadge(bench) {
+			let status = bench.status;
+			let color = null;
+
+			if (bench.deploy_information.update_available) {
+				status = 'Update Available';
+				color = 'blue';
+			}
+
+			return {
+				color,
+				status
+			};
+		},
+		dropdownItems(bench) {
+			return [
+				{
+					label: 'View Versions',
+					action: () => {
+						this.$router.push(`/benches/${bench.name}/versions`);
+					}
+				},
+				{
+					label: '',
+					action: () => {}
+				}
+			];
+		}
+	}
+};
+</script>
