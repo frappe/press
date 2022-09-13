84aea22739a90221bec2388c89a964bcf71895e9
Aditya Hase <aditya@adityahase.com>
2022-09-13 21:11:05 +0530

    feat: Server CPU usage chart using data from Promethus

--- dashboard/src/views/server/ServerAnalytics.vue
+++ dashboard/src/views/server/ServerAnalytics.vue
@@ -1,12 +1,80 @@
 <template>
 	<div class="space-y-4">
-		<div class="grid grid-cols-1 gap-5 sm:grid-cols-2"></div>
+		<div class="grid grid-cols-1 gap-5 sm:grid-cols-2">
+			<Card title="CPU">
+				<FrappeChart
+					type="line"
+					:data="cpuData"
+					:options="getChartOptions(d => d + ' s')"
+					:colors="[$theme.colors.yellow[500]]"
+				/>
+			</Card>
+		</div>
 	</div>
 </template>
 
 <script>
+import { DateTime } from 'luxon';
+import FrappeChart from '@/components/FrappeChart.vue';
+
 export default {
 	name: 'ServerAnalytics',
-	props: ['server']
+	props: ['server'],
+	components: {
+		FrappeChart
+	},
+	resources: {
+		cpu() {
+			let localTimezone = DateTime.local().zoneName;
+			return {
+				method: 'press.api.server.analytics',
+				params: {
+					name: this.server?.name,
+					timezone: localTimezone,
+					query: 'cpu'
+				},
+				auto: true
+			};
+		},
+	},
+	computed: {
+		cpuData() {
+			let cpu = this.$resources.cpu.data;
+			if (!cpu) return;
+
+			return {
+				labels: this.formatDate(cpu.labels),
+				datasets: cpu.datasets
+			};
+		},
+	},
+	methods: {
+		getChartOptions(yFormatter) {
+			return {
+				axisOptions: {
+					xIsSeries: true,
+					shortenYAxisNumbers: 1
+				},
+				lineOptions: {
+					hideDots: true,
+					regionFill: true
+				},
+				tooltipOptions: {
+					formatTooltipX: d => {
+						return DateTime.fromSQL(d.date).toLocaleString(
+							DateTime.DATETIME_MED
+						);
+					},
+					formatTooltipY: yFormatter
+				}
+			};
+		},
+		formatDate(labels) {
+			return labels.map(date => ({
+				date: date,
+				toString: () => DateTime.fromSQL(date).toFormat('d MMM')
+			}));
+		}
+	}
 };
 </script>
--- press/api/server.py
+++ press/api/server.py
@@ -2,8 +2,13 @@
 # For license information, please see license.txt
 
 import frappe
+import requests
+
 from press.utils import get_current_team
 from press.api.site import protected
+from frappe.utils import convert_utc_to_timezone
+from frappe.utils.password import get_decrypted_password
+from datetime import datetime
 
 
 @frappe.whitelist()
@@ -45,3 +50,59 @@ def search_list():
 		order_by="creation desc",
 	)
 	return servers + database_servers
+
+
+@frappe.whitelist()
+@protected("Server")
+def analytics(name, query, timezone, duration="7d"):
+	timespan, timegrain = {
+		"1h": (60 * 60, 2 * 60),
+		"6h": (6 * 60 * 60, 5 * 60),
+		"24h": (24 * 60 * 60, 30 * 60),
+		"7d": (7 * 24 * 60 * 60, 3 * 60 * 60),
+		"15d": (15 * 24 * 60 * 60, 6 * 60 * 60),
+	}[duration]
+
+	query_map = {
+		"cpu": f"""sum by (mode)(rate(node_cpu_seconds_total{{instance="{name}", job="node"}}[{timegrain}s])) * 100""",
+	}
+
+	return prometheus_query(query_map[query], timezone, timespan, timegrain)
+
+
+def prometheus_query(query, timezone, timespan, timegrain):
+	monitor_server = frappe.db.get_single_value("Press Settings", "monitor_server")
+	if not monitor_server:
+		return []
+
+	url = f"https://{monitor_server}/prometheus/api/v1/query_range"
+	password = get_decrypted_password("Monitor Server", monitor_server, "grafana_password")
+
+	end = frappe.utils.now_datetime()
+	start = frappe.utils.add_to_date(end, seconds=-timespan)
+	query = {
+		"query": query,
+		"start": start.timestamp(),
+		"end": end.timestamp(),
+		"step": f"{timegrain}s",
+	}
+
+	response = requests.get(url, params=query, auth=("frappe", password)).json()
+
+	datasets = []
+	labels = []
+
+	for timestamp, _ in response["data"]["result"][0]["values"]:
+		labels.append(
+			convert_utc_to_timezone(
+				datetime.fromtimestamp(timestamp).replace(tzinfo=None), timezone
+			)
+		)
+
+	for index in range(len(response["data"]["result"])):
+		dataset = {"name": str(response["data"]["result"][index]["metric"]), "values": []}
+		for _, value in response["data"]["result"][index]["values"]:
+			dataset["values"].append(float(value))
+		datasets.append(dataset)
+
+	return {"datasets": datasets, "labels": labels}
