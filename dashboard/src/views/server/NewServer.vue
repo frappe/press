9d830dfdef92f7b2092b5ad53967a9c5f74c6852
Aditya Hase <aditya@adityahase.com>
2022-09-21 16:42:36 +0530

    feat: Ask for a title for servers

--- dashboard/src/views/server/NewServer.vue
+++ dashboard/src/views/server/NewServer.vue
@@ -3,18 +3,95 @@
 		<div class="mb-6 text-center">
 			<h1 class="text-2xl font-bold">Create a new server</h1>
 		</div>
+		<Steps :steps="steps">
+			<template
+				v-slot="{ active: activeStep, next, previous, hasPrevious, hasNext }"
+			>
+				<div class="mt-8"></div>
+				<Hostname
+					:options="options"
+					v-show="activeStep.name === 'Hostname'"
+					v-model:title="title"
+					v-model:selectedRegion="selectedRegion"
+				/>
+				<ErrorMessage :error="validationMessage" />
+				<div class="mt-4">
+					<!-- Region consent checkbox -->
+					<div class="my-6" v-if="!hasNext">
+						<input
+							id="region-consent"
+							type="checkbox"
+							class="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+							v-model="agreedToRegionConsent"
+						/>
+						<label
+							for="region-consent"
+							class="ml-1 text-sm font-semibold text-gray-900"
+						>
+							I agree that the laws of the region selected by me shall stand
+							applicable to me and Frappe.
+						</label>
+					</div>
+
+
+					<div class="flex justify-between">
+						<Button
+							@click="previous"
+							:class="{
+								'pointer-events-none opacity-0': !hasPrevious
+							}"
+						>
+							Back
+						</Button>
+						<Button
+							appearance="primary"
+							@click="nextStep(activeStep, next)"
+							:class="{
+								'pointer-events-none opacity-0': !hasNext
+							}"
+						>
+							Next
+						</Button>
+						<Button
+							v-show="!hasNext"
+							appearance="primary"
+							@click="$resources.newServer.submit()"
+							:loading="$resources.newServer.loading"
+						>
+							Create Servers
+						</Button>
+					</div>
+				</div>
+			</template>
+		</Steps>
 	</WizardCard>
 </template>
 
 <script>
 import WizardCard from '@/components/WizardCard.vue';
+import Steps from '@/components/Steps.vue';
+import Hostname from './NewServerHostname.vue';
 export default {
 	name: 'NewServer',
 	components: {
 		WizardCard,
+		Steps,
+		Hostname,
 	},
 	data() {
 		return {
+			title: null,
+			options: null,
+			selectedRegion: null,
+			steps: [
+				{
+					name: 'Hostname',
+					validate: () => {
+						return this.title && this.selectedRegion;
+					}
+				},
+			],
+			agreedToRegionConsent: false
 		};
 	},
 	async mounted() {
---
+++ dashboard/src/views/server/NewServerHostname.vue
@@ -0,0 +1,47 @@
+<template>
+	<div>
+		<label class="text-lg font-semibold"> Choose a name for your server </label>
+		<p class="text-base text-gray-700">
+			Name your server based on its purpose. For e.g., Personal Websites,
+			Staging Server, etc.
+		</p>
+		<div class="mt-4 flex">
+			<input
+				class="form-input z-10 w-full rounded-r-none"
+				type="text"
+				v-model="title"
+				@change="titleChange"
+				placeholder="Server"
+			/>
+		</div>
+		<div class="mt-4">
+			<ErrorMessage :error="errorMessage" />
+		</div>
+	</div>
+</template>
+<script>
+
+export default {
+	name: 'Hostname',
+	data() {
+		return {
+			errorMessage: null
+		};
+	},
+	methods: {
+		async titleChange(e) {
+			let title = e.target.value;
+			this.$emit('update:title', title);
+			let error = this.validateTitle(title);
+			this.errorMessage = error;
+			this.$emit('error', error);
+		},
+		validateTitle(title) {
+			if (!title) {
+				return 'Server name cannot be left blank';
+			}
+			return null;
+		}
+	}
+};
+</script>
