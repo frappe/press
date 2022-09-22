d580a7fcdea8242577194b4b217779cc3cc93db9
Aditya Hase <aditya@adityahase.com>
2022-09-23 04:14:46 +0530

    feat: Drop Server component

---
+++ dashboard/src/views/server/ServerDrop.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="shrink-0">
+		<slot v-bind="{ showDialog }"></slot>
+		<FrappeUIDialog :options="{ title: 'Drop Server' }" v-model="dialogOpen">
+			<template v-slot:body-content>
+				<p class="text-base">
+					Are you sure you want to drop your server? The server will be archived
+					and all of the benches and sites on it will be deleted. This action
+					cannot be undone.
+				</p>
+				<p class="mt-4 text-base">
+					Please type
+					<span class="font-semibold">{{ server.name }}</span> to confirm.
+				</p>
+				<Input type="text" class="mt-4 w-full" v-model="confirmServerName" />
+				<ErrorMessage class="mt-2" :error="$resources.dropServer.error" />
+			</template>
+
+			<template v-slot:actions>
+				<div>
+					<Button @click="dialogOpen = false"> Cancel </Button>
+					<Button
+						class="ml-3"
+						appearance="danger"
+						@click="$resources.dropServer.submit()"
+						:loading="$resources.dropServer.loading"
+					>
+						Drop Server
+					</Button>
+				</div>
+			</template>
+		</FrappeUIDialog>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ServerDrop',
+	props: ['server'],
+	data() {
+		return {
+			dialogOpen: false,
+			confirmServerName: null
+		};
+	},
+	resources: {
+		dropServer() {
+			return {
+				method: 'press.api.server.archive',
+				params: {
+					name: this.server?.name
+				},
+				onSuccess() {
+					this.dialogOpen = false;
+					this.$router.push('/servers');
+				},
+				validate() {
+					if (this.server?.name !== this.confirmServerName) {
+						return 'Please type the server name to confirm';
+					}
+				}
+			};
+		}
+	},
+	methods: {
+		showDialog() {
+			this.dialogOpen = true;
+		}
+	}
+};
+</script>
