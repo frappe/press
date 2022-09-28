418f45902228b556a97ad3bab22edf5f0b86d041
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2022-09-28 12:37:52 +0530

    feat: Remote Login from site to Frappe Cloud Dashbaord

--- press/api/developer/marketplace.py
+++ press/api/developer/marketplace.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.utils import get_url
 from typing import Dict
 
 from press.api.developer import raise_invalid_key_error
@@ -50,6 +51,41 @@ class DeveloperApiHandler:
 
 		return filtered_dict
 
+	def get_login_url(self):
+		# check for active tokens
+		team = frappe.db.get_value("Site", self.app_subscription_doc.site, "team")
+		if frappe.db.exists(
+			"Saas Remote Login",
+			{
+				"team": team,
+				"status": "Attempted",
+				"expires_on": (">", frappe.utils.now()),
+			},
+		):
+			doc = frappe.get_doc(
+				"Saas Remote Login",
+				{
+					"team": team,
+					"status": "Attempted",
+					"expires_on": (">", frappe.utils.now()),
+				},
+			)
+			token = doc.token
+		else:
+			token = frappe.generate_hash("Saas Remote Login", 50)
+			frappe.get_doc(
+				{
+					"doctype": "Saas Remote Login",
+					"team": team,
+					"token": token,
+				}
+			).insert(ignore_permissions=True)
+			frappe.db.commit()
+
+		return get_url(
+			f"/api/method/press.api.marketplace.login_via_token?token={token}&team={team}&site={self.app_subscription_doc.site}"
+		)
+
 
 # ------------------------------------------------------------
 # API ENDPOINTS
@@ -64,3 +100,9 @@ def get_subscription_status(secret_key: str) -> str:
 def get_subscription_info(secret_key: str) -> Dict:
 	api_handler = DeveloperApiHandler(secret_key)
 	return api_handler.get_subscription_info()
+
+
+@frappe.whitelist(allow_guest=True)
+def get_login_url(secret_key):
+	api_handler = DeveloperApiHandler(secret_key)
+	return api_handler.get_login_url()
