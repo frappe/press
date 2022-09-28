7cab4e2ec0f91553c233bcc4c6bf98c7f68c3c49
Aditya Hase <aditya@adityahase.com>
2022-09-28 18:57:39 +0530

    feat: Create Press Job Steps from Press Job Type

--- press/press/doctype/press_job/press_job.py
+++ press/press/doctype/press_job/press_job.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2022, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class PressJob(Document):
-	pass
+	def after_insert(self):
+		self.create_press_job_steps()
+
+	def create_press_job_steps(self):
+		job_type = frappe.get_doc("Press Job Type", self.job_type)
+		for step in job_type.steps:
+			doc = frappe.get_doc(
+				{
+					"doctype": "Press Job Step",
+					"job": self.name,
+					"status": "Pending",
+					"step_name": step.step_name,
+					"duration": "00:00:00",
+				}
+			)
+			doc.insert()
