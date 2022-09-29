07475f6fb20de101f0fc950e7293f9602c20bc8e
Aditya Hase <aditya@adityahase.com>
2022-09-29 19:56:47 +0530

    feat: Server Scripts for Job Steps

--- press/press/doctype/press_job_step/press_job_step.py
+++ press/press/doctype/press_job_step/press_job_step.py
@@ -3,12 +3,31 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils.safe_exec import safe_exec
+import json
 
 
 class PressJobStep(Document):
 	@frappe.whitelist()
 	def execute(self):
 		self.status = "Running"
+		script = frappe.db.get_value(
+			"Press Job Type Step",
+			{"parent": self.job_type, "step_name": self.step_name},
+			"script",
+		)
+		arguments = json.loads(frappe.db.get_value("Press Job", self.job, "arguments"))
+		try:
+			local = {"arguments": frappe._dict(arguments), "result": None}
+			safe_exec(script, _locals=local)
+			result = local["result"]
+
+			self.status = "Success"
+			self.result = str(result)
+		except Exception:
+			self.status = "Failure"
+			self.traceback = frappe.get_traceback(with_context=True)
+
 		self.save()
 
 		job = frappe.get_doc("Press Job", self.job)
