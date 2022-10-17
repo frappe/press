8b8fbf4b65339c398debb72e2e473691c18589e8
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-10-17 18:41:29 +0530

    feat: Refund Micro Charge from desk

--- press/press/doctype/stripe_micro_charge_record/stripe_micro_charge_record.js
+++ press/press/doctype/stripe_micro_charge_record/stripe_micro_charge_record.js
@@ -2,7 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Stripe Micro Charge Record', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		if (!frm.doc.has_been_refunded) {
+			const btn = frm.add_custom_button('Refund', () => {
+				frm
+					.call({
+						doc: frm.doc,
+						method: 'refund',
+						btn,
+					})
+					.then((r) => {
+						if (r.message) {
+							frappe.msgprint(`Refunded Successfully.`);
+						}
+						frm.refresh();
+					});
+			});
+		}
+	},
 });
--- press/press/doctype/stripe_micro_charge_record/stripe_micro_charge_record.py
+++ press/press/doctype/stripe_micro_charge_record/stripe_micro_charge_record.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2022, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
+from press.utils.billing import get_stripe
 
 
 class StripeMicroChargeRecord(Document):
-	pass
+	@frappe.whitelist()
+	def refund(self):
+		stripe = get_stripe()
+		refund = stripe.Refund.create(payment_intent=self.stripe_payment_intent_id)
+
+		if refund.status == "succeeded":
+			self.has_been_refunded = True
+			self.save()
