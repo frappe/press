dc9a3426fcc8b531caed515272c062c17f7bc371
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-10-21 16:54:48 +0530

    refactor: Extract out publisher profile card into separate component

---
+++ dashboard/src/components/marketplace/PublisherProfileCard.vue
@@ -0,0 +1,107 @@
+<template>
+	<div>
+		<Card
+			v-if="profileData && profileData.profile_created"
+			title="Publisher Profile"
+			subtitle="Visible on the marketplace website"
+		>
+			<div class="divide-y-2">
+				<ListItem
+					title="Display Name"
+					:description="displayName || 'Not Set'"
+				/>
+				<ListItem
+					title="Contact Email"
+					:description="contactEmail || 'Not Set'"
+				/>
+				<ListItem title="Website" :description="website || 'Not Set'" />
+			</div>
+
+			<template #actions>
+				<Button icon-left="edit" @click="showEditProfileDialog = true"
+					>Edit</Button
+				>
+			</template>
+		</Card>
+
+		<FrappeUIDialog
+			:options="{ title: 'Edit Publisher Profile' }"
+			v-model="showEditProfileDialog"
+		>
+			<template v-slot:body-content>
+				<div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+					<Input label="Display Name" type="text" v-model="displayName" />
+					<Input label="Contact Email" type="email" v-model="contactEmail" />
+					<Input label="Website" type="text" v-model="website" />
+				</div>
+
+				<ErrorMessage
+					class="mt-4"
+					:error="$resources.updatePublisherProfile.error"
+				/>
+			</template>
+
+			<template #actions>
+				<div class="space-x-2">
+					<Button @click="showEditProfileDialog = false">Cancel</Button>
+					<Button
+						appearance="primary"
+						:loading="$resources.updatePublisherProfile.loading"
+						loadingText="Saving..."
+						@click="$resources.updatePublisherProfile.submit()"
+					>
+						Save
+					</Button>
+				</div>
+			</template>
+		</FrappeUIDialog>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['profileData'],
+	emits: ['profileUpdated'],
+	data() {
+		return {
+			showEditProfileDialog: false,
+			displayName: '',
+			contactEmail: '',
+			website: ''
+		};
+	},
+	mounted() {},
+	resources: {
+		updatePublisherProfile() {
+			return {
+				method: 'press.api.marketplace.update_publisher_profile',
+				params: {
+					profile_data: {
+						display_name: this.displayName,
+						contact_email: this.contactEmail,
+						website: this.website
+					}
+				},
+				validate() {
+					if (!this.displayName) {
+						return 'Display Name is required.';
+					}
+				},
+				onSuccess() {
+					this.showEditProfileDialog = false;
+					this.$emit('profile_updated');
+				}
+			};
+		}
+	},
+	watch: {
+		profileData(data) {
+			if (data && data.profile_created) {
+				this.displayName = data.profile_info.display_name;
+				this.contactEmail = data.profile_info.contact_email;
+				this.website = data.profile_info.website;
+			}
+		}
+	}
+};
+</script>
--- dashboard/src/views/marketplace/MarketplacePublisherProfile.vue
+++ dashboard/src/views/marketplace/MarketplacePublisherProfile.vue
@@ -1,15 +1,5 @@
 <template>
 	<div>
-		<div v-if="publisherProfileInfo && !publisherProfileInfo.profile_created">
-			<Alert title="You have not created your profile yet.">
-				<template #actions>
-					<Button appearance="primary" @click="showEditProfileDialog = true"
-						>Create</Button
-					>
-				</template>
-			</Alert>
-		</div>
-
 		<Button
 			v-if="$resources.getPublisherProfileInfo.loading"
 			:loading="true"
@@ -17,114 +7,26 @@
 		></Button>
 
 		<div class="grid grid-cols-1 gap-5 md:grid-cols-2">
-			<Card
-				v-if="publisherProfileInfo && publisherProfileInfo.profile_created"
-				title="Publisher Profile"
-				subtitle="Visible on the marketplace website"
-			>
-				<div class="divide-y-2">
-					<ListItem
-						title="Display Name"
-						:description="displayName || 'Not Set'"
-					/>
-					<ListItem
-						title="Contact Email"
-						:description="contactEmail || 'Not Set'"
-					/>
-					<ListItem title="Website" :description="website || 'Not Set'" />
-				</div>
-
-				<template #actions>
-					<Button icon-left="edit" @click="showEditProfileDialog = true"
-						>Edit</Button
-					>
-				</template>
-			</Card>
+			<PublisherProfileCard
+				:profileData="publisherProfileInfo"
+				@profileUpdated="$resources.getPublisherProfileInfo.fetch()"
+			/>
 		</div>
-
-		<FrappeUIDialog
-			:options="{ title: 'Edit Publisher Profile' }"
-			v-model="showEditProfileDialog"
-		>
-			<template v-slot:body-content>
-				<div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
-					<Input label="Display Name" type="text" v-model="displayName" />
-					<Input label="Contact Email" type="email" v-model="contactEmail" />
-					<Input label="Website" type="text" v-model="website" />
-				</div>
-
-				<ErrorMessage
-					class="mt-4"
-					:error="$resources.updatePublisherProfile.error"
-				/>
-			</template>
-
-			<template #actions>
-				<div class="space-x-2">
-					<Button @click="showEditProfileDialog = false">Cancel</Button>
-					<Button
-						appearance="primary"
-						:loading="$resources.updatePublisherProfile.loading"
-						loadingText="Saving..."
-						@click="$resources.updatePublisherProfile.submit()"
-					>
-						Save
-					</Button>
-				</div>
-			</template>
-		</FrappeUIDialog>
 	</div>
 </template>
 
 <script>
+import PublisherProfileCard from '@/components/marketplace/PublisherProfileCard.vue';
 export default {
 	data() {
-		return {
-			showEditProfileDialog: false,
-			displayName: '',
-			contactEmail: '',
-			website: ''
-		};
+		return {};
 	},
 	resources: {
 		getPublisherProfileInfo() {
 			return {
 				method: 'press.api.marketplace.get_publisher_profile_info',
-				auto: true,
-				onSuccess(data) {
-					this.setProfileInfo(data);
-				}
+				auto: true
 			};
-		},
-		updatePublisherProfile() {
-			return {
-				method: 'press.api.marketplace.update_publisher_profile',
-				params: {
-					profile_data: {
-						display_name: this.displayName,
-						contact_email: this.contactEmail,
-						website: this.website
-					}
-				},
-				validate() {
-					if (!this.displayName) {
-						return 'Display Name is required.';
-					}
-				},
-				onSuccess() {
-					this.showEditProfileDialog = false;
-					this.$resources.getPublisherProfileInfo.fetch();
-				}
-			};
-		}
-	},
-	methods: {
-		setProfileInfo(data) {
-			if (data && data.profile_created) {
-				this.displayName = data.profile_info.display_name;
-				this.contactEmail = data.profile_info.contact_email;
-				this.website = data.profile_info.website;
-			}
 		}
 	},
 	computed: {
@@ -135,9 +37,9 @@ export default {
 			) {
 				return;
 			}
-
 			return this.$resources.getPublisherProfileInfo.data;
 		}
-	}
+	},
+	components: { PublisherProfileCard }
 };
 </script>
