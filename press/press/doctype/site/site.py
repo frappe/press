40d41de556a344b86266a2725c4edb37f7fb43f8
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2023-01-09 18:11:03 +0530

    chore: Remove duplicate code

--- press/press/doctype/site/site.py
+++ press/press/doctype/site/site.py
@@ -912,9 +912,6 @@ class Site(Document):
 		self.reload()
 		if self.status == "Suspended":
 			self.unsuspend_if_applicable()
-		else:
-			# trigger agent job only once
-			self.update_site_config(plan_config)
 
 		if self.trial_end_date:
 			self.reload()
