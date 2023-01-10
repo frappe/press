d4bbd2f4e08f2cadb647dcb85b147cd59f994541
Aditya Hase <aditya@adityahase.com>
2023-01-10 15:12:42 +0530

    fix: Use framework's 404 image

--- dashboard/src/views/general/404.vue
+++ dashboard/src/views/general/404.vue
@@ -1,9 +1,7 @@
 <template>
 	<div class="flex items-center justify-center">
 		<div class="flex flex-col items-center">
-			<span class="text-8xl" @dblclick="() => (surprise = true)">
-				{{ surprise ? '😉' : '👀' }}
-			</span>
+			<img class="md:w-2/5 w-fit" src="../../assets/404.png" />
 			<h2 class="mt-5 text-5xl font-bold text-gray-900">Not Found</h2>
 			<h3 class="text-lg font-semibold text-gray-700">
 				Maybe someone gave you a wrong address?
