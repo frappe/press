2098ee06b681db795ec698dd641e4169598f4204
Shadrak Gurupnor <30501401+shadrak98@users.noreply.github.com>
2023-03-04 15:02:52 +0530

    fix: allow web_search on fc docs

--- press/auth.py
+++ press/auth.py
@@ -23,6 +23,7 @@ ALLOWED_PATHS = [
 	"/api/method/press.press.doctype.razorpay_webhook_log.razorpay_webhook_log.razorpay_webhook_handler",
 	"/api/method/press.press.doctype.stripe_webhook_log.stripe_webhook_log.stripe_webhook_handler",
 	"/api/method/upload_file",
+	"/api/method/frappe.search.web_search",
 ]
 
 ALLOWED_WILDCARD_PATHS = [
