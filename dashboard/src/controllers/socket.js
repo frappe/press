e8cc9c3d481de176d35828d9a8beee4148d7ebf4
Aditya Hase <aditya@adityahase.com>
2023-03-29 00:20:52 +0530

    fix: Use withCredentials with Socket.IO client
    Without this Socket.IO doesn't work locally
    
    References:
    https://socket.io/docs/v4/client-options/#withcredentials
    https://github.com/frappe/frappe/blob/develop/frappe/public/js/frappe/socketio_client.js#L25

--- dashboard/src/controllers/socket.js
+++ dashboard/src/controllers/socket.js
@@ -4,6 +4,6 @@ let host = window.location.hostname;
 let port = window.location.port ? ':9000' : '';
 let protocol = port ? 'http' : 'https';
 let url = `${protocol}://${host}${port}`;
-let socket = io(url);
+let socket = io(url, { withCredentials: true });
 
 export default socket;
