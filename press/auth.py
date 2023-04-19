7637df0fdd95b119cd588eac59900f95ed853cad
Rutwik <rutwikhdev@gmail.com>
2023-04-19 15:48:18 +0530

    fix: Allow email unsubscribe in blocked paths

--- press/auth.py
+++ press/auth.py
@@ -24,6 +24,7 @@ ALLOWED_PATHS = [
 	"/api/method/press.press.doctype.stripe_webhook_log.stripe_webhook_log.stripe_webhook_handler",
 	"/api/method/upload_file",
 	"/api/method/frappe.search.web_search",
+	"/api/method/frappe.email.queue.unsubscribe"
 ]
 
 ALLOWED_WILDCARD_PATHS = [
