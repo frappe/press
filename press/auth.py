e340d62aafb80e759588df733b6205c7e7bd2a37
Balamurali M <balamurali@frappe.io>
2023-04-20 14:33:03 +0530

    fix: typo

--- press/auth.py
+++ press/auth.py
@@ -25,7 +25,7 @@ ALLOWED_PATHS = [
 	"/api/method/upload_file",
 	"/api/method/frappe.search.web_search",
 	"/api/method/frappe.email.queue.unsubscribe",
-	"/api/method/frappe.client.get"
+	"/api/method/frappe.client.get",
 	"/api/method/frappe.client.get_count"
 ]
 
