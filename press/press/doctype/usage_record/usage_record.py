257d17cae889f10f1d4071620171b9e0308b7578
shadrak gurupnor <gurupnor.shadrak@gmail.com>
2023-04-21 13:54:48 +0530

    feat: create common invoice for child and parent team

--- press/press/doctype/usage_record/usage_record.py
+++ press/press/doctype/usage_record/usage_record.py
@@ -29,6 +29,9 @@ class UsageRecord(Document):
 	def update_usage_in_invoice(self):
 		invoice_type = "Summary" if self.prepaid else "Subscription"
 		team = frappe.get_doc("Team", self.team)
+		if team.child_team:
+			team = frappe.get_doc("Team", self.parent_team)
+
 		if team.free_account:
 			return
 		invoice = team.get_upcoming_invoice(invoice_type)
