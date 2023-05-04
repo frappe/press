582d4c2e7335fdb5f319fa44ddfa5811ebd2de16
Balamurali M <balamurali@frappe.io>
2023-05-04 13:32:01 +0530

    fix(AppRename): Add rollback script

--- press/agent.py
+++ press/agent.py
@@ -284,8 +284,10 @@ class Agent:
 			site=site.name,
 		)
 
-	def update_site_recover_move(self, site, target, deploy_type, activate):
-		data = {"target": target, "activate": activate}
+	def update_site_recover_move(
+		self, site, target, deploy_type, activate, rollback_scripts=None
+	):
+		data = {"target": target, "activate": activate, "rollback_scripts": rollback_scripts}
 		return self.create_agent_job(
 			f"Recover Failed Site {deploy_type}",
 			f"benches/{site.bench}/sites/{site.name}/update/{deploy_type.lower()}/recover",
