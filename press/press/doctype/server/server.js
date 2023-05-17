07427f7732bd06794c5b83f7bb946b10c83b19a0
Aditya Hase <aditya@adityahase.com>
2023-05-17 18:16:14 +0530

    style: Prettier

--- press/press/doctype/server/server.js
+++ press/press/doctype/server/server.js
@@ -8,43 +8,105 @@ frappe.ui.form.on('Server', {
 			__('Visit Dashboard')
 		);
 		[
-			[__('Ping Agent'), "ping_agent", false, frm.doc.is_server_setup],
-			[__('Ping Ansible'), "ping_ansible", true, !frm.doc.is_server_prepared],
-			[__('Ping Ansible Unprepared'), "ping_ansible_unprepared", true, !frm.doc.is_server_prepared],
-			[__('Update Agent'), "update_agent", true, frm.doc.is_server_setup],
-			[__('Update Agent Ansible'), "update_agent_ansible", true, frm.doc.is_server_setup],
-			[__('Prepare Server'), "prepare_server", true, !frm.doc.is_server_prepared],
-			[__('Setup Server'), "setup_server", true, !frm.doc.is_server_setup],
-			[__('Add 8GB Swap'), "increase_swap", true, frm.doc.is_server_setup],
-			[__('Add to Proxy'), "add_upstream_to_proxy", true, frm.doc.is_server_setup && !frm.doc.is_upstream_setup],
-			[__('Setup Replication'), "setup_replication", true, frm.doc.is_server_setup && !frm.doc.is_primary && !frm.doc.is_replication_setup],
-			[__('Setup Rename'), "rename_server", true, frm.doc.is_server_setup && frm.doc.is_server_prepared && !frm.doc.is_server_renamed],
-			[__('Fetch Keys'), "fetch_keys", false, frm.doc.is_server_setup && (!frm.doc.frappe_public_key || !frm.doc.root_public_key)],
-			[__('Update TLS Certificate'), "update_tls_certificate", true],
-			[__('Auto Scale Workers'), "auto_scale_workers", true, frm.doc.status == "Active" && frm.doc.is_primary && frm.doc.is_server_setup],
-			[__('Cleanup Unused Files'), "cleanup_unused_files", true, frm.doc.status == "Active" && frm.doc.is_server_setup],
-			[__('Create Image'), 'create_image', true, frm.doc.status == "Active"],
-			[__('Archive'), "archive", true, frm.doc.status !== "Archived"],
-			[__('Setup Fail2ban'), "setup_fail2ban", true, frm.doc.is_server_setup],
-			[__('Whitelist Server'), "whitelist_ipaddress", false, frm.doc.is_server_setup],
-			[__('Show Agent Password'), "show_agent_password", false, frm.doc.is_server_setup],
+			[__('Ping Agent'), 'ping_agent', false, frm.doc.is_server_setup],
+			[__('Ping Ansible'), 'ping_ansible', true, !frm.doc.is_server_prepared],
+			[
+				__('Ping Ansible Unprepared'),
+				'ping_ansible_unprepared',
+				true,
+				!frm.doc.is_server_prepared,
+			],
+			[__('Update Agent'), 'update_agent', true, frm.doc.is_server_setup],
+			[
+				__('Update Agent Ansible'),
+				'update_agent_ansible',
+				true,
+				frm.doc.is_server_setup,
+			],
+			[
+				__('Prepare Server'),
+				'prepare_server',
+				true,
+				!frm.doc.is_server_prepared,
+			],
+			[__('Setup Server'), 'setup_server', true, !frm.doc.is_server_setup],
+			[__('Add 8GB Swap'), 'increase_swap', true, frm.doc.is_server_setup],
+			[
+				__('Add to Proxy'),
+				'add_upstream_to_proxy',
+				true,
+				frm.doc.is_server_setup && !frm.doc.is_upstream_setup,
+			],
+			[
+				__('Setup Replication'),
+				'setup_replication',
+				true,
+				frm.doc.is_server_setup &&
+					!frm.doc.is_primary &&
+					!frm.doc.is_replication_setup,
+			],
+			[
+				__('Setup Rename'),
+				'rename_server',
+				true,
+				frm.doc.is_server_setup &&
+					frm.doc.is_server_prepared &&
+					!frm.doc.is_server_renamed,
+			],
+			[
+				__('Fetch Keys'),
+				'fetch_keys',
+				false,
+				frm.doc.is_server_setup &&
+					(!frm.doc.frappe_public_key || !frm.doc.root_public_key),
+			],
+			[__('Update TLS Certificate'), 'update_tls_certificate', true],
+			[
+				__('Auto Scale Workers'),
+				'auto_scale_workers',
+				true,
+				frm.doc.status == 'Active' &&
+					frm.doc.is_primary &&
+					frm.doc.is_server_setup,
+			],
+			[
+				__('Cleanup Unused Files'),
+				'cleanup_unused_files',
+				true,
+				frm.doc.status == 'Active' && frm.doc.is_server_setup,
+			],
+			[__('Create Image'), 'create_image', true, frm.doc.status == 'Active'],
+			[__('Archive'), 'archive', true, frm.doc.status !== 'Archived'],
+			[__('Setup Fail2ban'), 'setup_fail2ban', true, frm.doc.is_server_setup],
+			[
+				__('Whitelist Server'),
+				'whitelist_ipaddress',
+				false,
+				frm.doc.is_server_setup,
+			],
+			[
+				__('Show Agent Password'),
+				'show_agent_password',
+				false,
+				frm.doc.is_server_setup,
+			],
 		].forEach(([label, method, confirm, condition]) => {
-			if (typeof condition === "undefined" || condition) {
+			if (typeof condition === 'undefined' || condition) {
 				frm.add_custom_button(
 					label,
 					() => {
 						if (confirm) {
 							frappe.confirm(
 								`Are you sure you want to ${label.toLowerCase()}?`,
-								() => frm.call(method).then((r) => {
-									if (r.message) {
-										frappe.msgprint(r.message);
-									} else {
-										frm.refresh();
-									}
-								})
+								() =>
+									frm.call(method).then((r) => {
+										if (r.message) {
+											frappe.msgprint(r.message);
+										} else {
+											frm.refresh();
+										}
+									})
 							);
-
 						} else {
 							frm.call(method).then((r) => {
 								if (r.message) {
@@ -52,12 +114,12 @@ frappe.ui.form.on('Server', {
 								} else {
 									frm.refresh();
 								}
-							})
+							});
 						}
 					},
 					__('Actions')
 				);
 			}
 		});
-	}
+	},
 });
