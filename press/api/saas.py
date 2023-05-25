1490e3aa19caaf7d11f8b5a9bfbdaed0118831a5
Rutwik <rutwikhdev@gmail.com>
2023-05-25 11:08:30 +0530

    fix: Team filter and set default initial plan

--- press/api/saas.py
+++ press/api/saas.py
@@ -350,7 +350,9 @@ def create_marketplace_subscription(account_request):
 					"app": account_request.saas_app,
 					"site": site_name,
 					"marketplace_app_plan": get_saas_plan(account_request.saas_app),
-					"initial_plan": json.loads(account_request.url_args).get("plan"),
+					"initial_plan": json.loads(account_request.url_args).get("plan")
+					if account_request.url_args
+					else "",
 					"interval": "Daily",
 				}
 			).insert(ignore_permissions=True)
@@ -367,7 +369,7 @@ def create_team(account_request, get_stripe_id=False):
 	"""
 	email = account_request.email
 
-	if not frappe.db.exists("Team", email):
+	if not frappe.db.exists("Team", {"user": email}):
 		team_doc = Team.create_new(
 			account_request,
 			account_request.first_name,
