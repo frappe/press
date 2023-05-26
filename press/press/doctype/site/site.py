5869afb2ad77e36cd6ac8dbba63b92d1b973861f
Balamurali M <balamurali@frappe.io>
2023-05-26 18:01:14 +0530

    style(Site): isort

--- press/press/doctype/site/site.py
+++ press/press/doctype/site/site.py
@@ -5,6 +5,7 @@
 import json
 import re
 from collections import defaultdict
+from datetime import datetime
 from typing import Any, Dict, List
 
 import boto3
@@ -16,7 +17,6 @@ from frappe.frappeclient import FrappeClient
 from frappe.model.document import Document
 from frappe.model.naming import append_number_if_name_exists
 from frappe.utils import cint, cstr, get_datetime
-from datetime import datetime
 
 try:
 	from frappe.utils import convert_utc_to_user_timezone
@@ -29,11 +29,11 @@ from frappe.utils.user import is_system_user
 from press.agent import Agent
 from press.api.site import check_dns
 from press.overrides import get_permission_query_conditions_for_doctype
+from press.press.doctype.marketplace_app.marketplace_app import marketplace_app_hook
 from press.press.doctype.plan.plan import get_plan_config
 from press.press.doctype.site_activity.site_activity import log_site_activity
-from press.utils import convert, get_client_blacklisted_keys, guess_type, log_error
 from press.press.doctype.site_analytics.site_analytics import create_site_analytics
-from press.press.doctype.marketplace_app.marketplace_app import marketplace_app_hook
+from press.utils import convert, get_client_blacklisted_keys, guess_type, log_error
 
 
 class Site(Document):
