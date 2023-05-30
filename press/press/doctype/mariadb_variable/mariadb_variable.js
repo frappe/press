1a221c72e77c181517391404629f73b518a0b816
Balamurali M <balamurali@frappe.io>
2023-05-30 15:30:22 +0530

    refactor: Use shorter name for doctype

--- press/press/doctype/mariadb_variable/mariadb_variable.js
+++ press/press/doctype/mariadb_variable/mariadb_variable.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('MariaDB System Variable', {
+frappe.ui.form.on('MariaDB Variable', {
 	refresh(frm) {
 		frm.add_web_link(
 			`https://mariadb.com/kb/en/innodb-system-variables/#${frm.doc.name}`,
--- press/press/doctype/mariadb_variable/test_mariadb_variable.py
+++ press/press/doctype/mariadb_variable/test_mariadb_variable.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestMariaDBSystemVariable(FrappeTestCase):
+class TestMariaDBVariable(FrappeTestCase):
 	pass
