75eb3f7514f5d79cbca9365f162fa3a4fc7c0819
Balamurali M <balamurali@frappe.io>
2023-05-31 12:09:51 +0530

    feat(MariaDBVariable): Add check for replication and binary log variables

--- press/press/doctype/mariadb_variable/mariadb_variable.js
+++ press/press/doctype/mariadb_variable/mariadb_variable.js
@@ -3,8 +3,13 @@
 
 frappe.ui.form.on('MariaDB Variable', {
 	refresh(frm) {
+		let root = 'https://mariadb.com/kb/en/';
+		let prefix = 'innodb-';
+		if (frm.doc.replication_and_binary_log) {
+			prefix = 'replication-and-binary-log-';
+		}
 		frm.add_web_link(
-			`https://mariadb.com/kb/en/innodb-system-variables/#${frm.doc.name}`,
+			`${root}${prefix}system-variables/#${frm.doc.name}`,
 			__('Check MariaDB Documentation')
 		);
 	},
