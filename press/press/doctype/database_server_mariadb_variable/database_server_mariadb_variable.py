c0b8bc5a9cd547de7b1dd491008e6557f6613453
Balamurali M <balamurali@frappe.io>
2023-05-31 11:45:31 +0530

    fix(DatabaseServerMariaDBVariable): Check if value field set matches datatype
    - refactor: Make get_datatype a property

--- press/press/doctype/database_server_mariadb_variable/database_server_mariadb_variable.py
+++ press/press/doctype/database_server_mariadb_variable/database_server_mariadb_variable.py
@@ -8,7 +8,8 @@ from frappe.model.document import Document
 
 
 class DatabaseServerMariaDBVariable(Document):
-	def get_datatype(self) -> str:
+	@property
+	def datatype(self) -> str:
 		return frappe.db.get_value("MariaDB Variable", self.mariadb_variable, "datatype")
 
 	@property
@@ -16,23 +17,36 @@ class DatabaseServerMariaDBVariable(Document):
 		return list(filter(lambda x: x.startswith("value_"), self.as_dict().keys()))
 
 	@property
-	def value(self) -> Any:
+	def value_field(self):
 		"""Return the first value field that has a value"""
 		for f in self.value_fields:
-			if value := self.get(f):
-				return value
+			if self.get(f):
+				return f
+
+	@property
+	def value(self) -> Any:
+		"""Return the value of the first value field that has a value"""
+		return self.get(self.value_field)
 
 	def validate_only_one_value_is_set(self):
 		if sum([bool(self.get(f)) for f in self.value_fields]) > 1:
 			frappe.throw("Only one value can be set for MariaDB system variable")
 
 	def validate_datatype_of_field_is_correct(self):
-		datatype = self.get_datatype()
-		if type(self.value).__name__ != datatype.lower():
-			frappe.throw(f"Value for {self.mariadb_variable} must be {datatype}")
+		if type(self.value).__name__ != self.datatype.lower():
+			frappe.throw(f"Value for {self.mariadb_variable} must be {self.datatype}")
 
-	def validate(self):
+	def validate_value_field_set_is_correct(self):
+		if self.value_field != f"value_{self.datatype.lower()}":
+			frappe.throw(
+				f"Value field for {self.mariadb_variable} must be value_{self.datatype.lower()}"
+			)
+
+	def validate(  # Is not called by FF. Called manually from database_server.py
+		self,
+	):
 		self.validate_only_one_value_is_set()
+		self.validate_value_field_set_is_correct()
 		self.validate_datatype_of_field_is_correct()
 
 
