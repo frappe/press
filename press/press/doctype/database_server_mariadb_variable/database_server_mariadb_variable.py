54087b87d5b36b2ccee7f64ffd794fba13fc9417
Balamurali M <balamurali@frappe.io>
2023-05-31 08:16:21 +0530

    fix(DatabaseServerMariaDBVariable): Make unique(mariadb_variable, parent)

--- press/press/doctype/database_server_mariadb_variable/database_server_mariadb_variable.py
+++ press/press/doctype/database_server_mariadb_variable/database_server_mariadb_variable.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2023, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class DatabaseServerMariaDBVariable(Document):
 	pass
+
+
+def on_doctype_update():
+	frappe.db.add_unique(
+		"Database Server MariaDB Variable", ("mariadb_variable", "parent")
+	)
