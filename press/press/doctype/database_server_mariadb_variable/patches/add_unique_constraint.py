0b739bece664f02230a849bd4d9b8b17a4fc5786
Aditya Hase <aditya@adityahase.com>
2023-06-12 15:20:04 +0530

    fix: Relocate add_unique_constraint patch

--- press/press/doctype/database_server_mariadb_variable/patches/add_unique_constraint.py
+++ press/press/doctype/database_server_mariadb_variable/patches/add_unique_constraint.py
@@ -6,6 +6,7 @@ import frappe
 
 
 def execute():
+	frappe.reload_doc("press", "doctype", "database_server_mariadb_variable")
 	frappe.get_doc("DocType", "Database Server MariaDB Variable").run_module_method(
 		"on_doctype_update"
 	)
