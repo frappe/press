20a0a224954fe22b95b35cf470c097ade43a3bfa
Balamurali M <balamurali@frappe.io>
2023-06-20 17:16:52 +0530

    test(servers): Test for successful press job on create server

--- press/api/tests/test_server.py
+++ press/api/tests/test_server.py
@@ -3,28 +3,42 @@
 # See license.txt
 
 
+from unittest.mock import MagicMock, Mock, patch
+
 import frappe
-from frappe.query_builder.functions import Count
-from press.press.doctype.cluster.test_cluster import create_test_cluster
-from press.press.doctype.plan.test_plan import create_test_plan
-
-from press.press.doctype.team.test_team import create_test_press_admin_team
-
 from frappe.tests.utils import FrappeTestCase
 
 from press.api.server import new
-
+from press.press.doctype.ansible_play.test_ansible_play import create_test_ansible_play
+from press.press.doctype.cluster.test_cluster import create_test_cluster
+from press.press.doctype.plan.test_plan import create_test_plan
 from press.press.doctype.proxy_server.test_proxy_server import create_test_proxy_server
+from press.press.doctype.server.server import BaseServer
+from press.press.doctype.team.test_team import create_test_press_admin_team
 from press.press.doctype.virtual_machine.virtual_machine import VirtualMachine
-
-from unittest.mock import patch, MagicMock
-
 from press.press.doctype.virtual_machine_image.test_virtual_machine_image import (
 	create_test_virtual_machine_image,
 )
 from press.press.doctype.virtual_machine_image.virtual_machine_image import (
 	VirtualMachineImage,
 )
+from press.runner import Ansible
+from press.utils.test import foreground_enqueue_doc
+
+
+def successful_provision(self: VirtualMachine):
+	self.status = "Running"
+	self.save()
+
+
+def successful_sync(self: VirtualMachine):
+	self.status = "Running"
+	self.save()
+	self.update_servers()
+
+
+def successful_ping_ansible(self: BaseServer):
+	create_test_ansible_play("Ping Server", "ping.yml", self.doctype, self.name)
 
 
 @patch.object(VirtualMachineImage, "client", new=MagicMock())
@@ -42,26 +56,19 @@ class TestAPIServer(FrappeTestCase):
 		frappe.set_user("Administrator")
 		frappe.db.rollback()
 
-	def test_create_new_server_creates_pending_server_and_db_server(self):
-		create_test_virtual_machine_image()  # call from here so mocks work
-		frappe.set_user(self.team.user)
-		servers = frappe.qb.DocType("Server")
-		db_servers = frappe.qb.DocType("Database Server")
-		servers_query = (
-			frappe.qb.from_(servers)
-			.select(Count("*"))
-			.where(servers.status == "Pending")
-			.where(servers.team == self.team.name)
-		)
-		db_servers_query = (
-			frappe.qb.from_(db_servers)
-			.select(Count("*"))
-			.where(db_servers.status == "Pending")
-			.where(db_servers.team == self.team.name)
-		)
+	def _get_doc_count(self, doctype: str, status: str, team: str):
+		return frappe.db.count(doctype, filters={"status": status, "team": team})
 
-		servers_before = servers_query.run()
-		db_servers_before = db_servers_query.run()
+	def test_create_new_server_creates_pending_server_and_db_server(self):
+		create_test_virtual_machine_image(cluster=self.cluster, series="m")
+		create_test_virtual_machine_image(
+			cluster=self.cluster, series="f"
+		)  # call from here and not setup, so mocks work
+		frappe.set_user(self.team.user)
+
+		servers_before = self._get_doc_count("Server", "Pending", self.team.name)
+		db_servers_before = self._get_doc_count("Database Server", "Pending", self.team.name)
+
 		new(
 			{
 				"cluster": self.cluster.name,
@@ -70,7 +77,42 @@ class TestAPIServer(FrappeTestCase):
 				"title": "Test Server",
 			}
 		)
-		servers_after = servers_query.run()
-		db_servers_after = db_servers_query.run()
-		self.assertEqual(servers_before[0][0] + 1, servers_after[0][0])
-		self.assertEqual(db_servers_before[0][0] + 1, db_servers_after[0][0])
+
+		servers_after = self._get_doc_count("Server", "Pending", self.team.name)
+		db_servers_after = self._get_doc_count("Database Server", "Pending", self.team.name)
+
+		self.assertEqual(servers_before + 1, servers_after)
+		self.assertEqual(db_servers_before + 1, db_servers_after)
+
+	@patch(
+		"press.press.doctype.press_job.press_job.frappe.enqueue_doc",
+		new=foreground_enqueue_doc,
+	)
+	@patch.object(VirtualMachine, "provision", new=successful_provision)
+	@patch.object(VirtualMachine, "sync", new=successful_sync)
+	@patch.object(Ansible, "run", new=Mock())
+	@patch.object(BaseServer, "ping_ansible", new=successful_ping_ansible)
+	def test_new_fn_creates_active_server_and_db_server_once_press_job_succeeds(self):
+		create_test_virtual_machine_image(cluster=self.cluster, series="m")
+		create_test_virtual_machine_image(
+			cluster=self.cluster, series="f"
+		)  # call from here and not setup, so mocks work
+		frappe.set_user(self.team.user)
+
+		servers_before = self._get_doc_count("Server", "Active", self.team.name)
+		db_servers_before = self._get_doc_count("Database Server", "Active", self.team.name)
+
+		new(
+			{
+				"cluster": self.cluster.name,
+				"db_plan": self.db_plan.name,
+				"app_plan": self.app_plan.name,
+				"title": "Test Server",
+			}
+		)
+
+		servers_after = self._get_doc_count("Server", "Active", self.team.name)
+		db_servers_after = self._get_doc_count("Database Server", "Active", self.team.name)
+
+		self.assertEqual(servers_before + 1, servers_after)
+		self.assertEqual(db_servers_before + 1, db_servers_after)
