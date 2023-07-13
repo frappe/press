bcbe997268a94b1b22656f629f36c45bb1119131
Rutwik <rutwikhdev@gmail.com>
2023-07-13 13:47:17 +0530

    test(marketplace): Add more api test

--- press/marketplace/doctype/marketplace_app_plan/test_marketplace_app_plan.py
+++ press/marketplace/doctype/marketplace_app_plan/test_marketplace_app_plan.py
@@ -3,12 +3,16 @@
 
 import frappe
 import unittest
+from press.press.doctype.app.test_app import create_test_app
 
 from press.press.doctype.plan.test_plan import create_test_plan
 
 
 def create_test_marketplace_app_plan(app: str = "frappe"):
 	"""Create a test marketplace_app_plan"""
+	if not frappe.db.exists("Marketplace App", app):
+		create_test_app(name=app)
+
 	return frappe.get_doc(
 		{
 			"doctype": "Marketplace App Plan",
