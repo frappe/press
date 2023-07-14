89ed56175c7419650080e85bfc0c928cf0cac403
Athul Cyriac Ajay <athul8720@gmail.com>
2023-07-14 16:50:30 +0530

    tests(byod): Add more tests to Self Hosted Server doctypes

--- press/press/doctype/self_hosted_server/test_self_hosted_server.py
+++ press/press/doctype/self_hosted_server/test_self_hosted_server.py
@@ -7,6 +7,7 @@ from press.press.doctype.ansible_play.test_ansible_play import create_test_ansib
 
 from press.press.doctype.self_hosted_server.self_hosted_server import SelfHostedServer
 import frappe
+import json
 from press.runner import Ansible
 from press.press.doctype.team.test_team import create_test_team
 from frappe.tests.utils import FrappeTestCase
@@ -55,12 +56,84 @@ class TestSelfHostedServer(FrappeTestCase):
 		post_setup_count = frappe.db.count("TLS Certificate")
 		self.assertEqual(pre_setup_count, post_setup_count - 1)
 
+	def test_successful_ping_ansible_sets_status_to_pending(self):
+		server = create_test_self_hosted_server("pinger")
+		with patch(
+			"press.press.doctype.self_hosted_server.self_hosted_server.Ansible.run",
+			new=lambda x: create_test_ansible_play(
+				"Ping Server",
+				"ping.yml",
+				server.doctype,
+				server.name,
+				{"server": server.name},
+			),
+		):
+			server.ping_ansible()
+		self.assertEqual(server.status, "Pending")
+
+	def test_failed_ping_ansible_sets_status_to_unreachable(self):
+		server = create_test_self_hosted_server("pinger")
+		with patch(
+			"press.press.doctype.self_hosted_server.self_hosted_server.Ansible.run",
+			new=lambda x: create_test_ansible_play(
+				"Ping Server",
+				"ping.yml",
+				server.doctype,
+				server.name,
+				{"server": server.name},
+				"Failure",
+			),
+		):
+			server.ping_ansible()
+		self.assertEqual(server.status, "Unreachable")
+
+	def _create_test_ansible_play_and_task(
+		self, server: SelfHostedServer, playbook: str, play: str, task: str, task_output: str
+	):  # TODO: Move to AnsiblePlay and Make a generic one for AnsibleTask
+		play = create_test_ansible_play(
+			play,
+			playbook,
+			server.doctype,
+			server.name,
+			{"server": server.name},
+		)
+		task = frappe.get_doc(
+			{
+				"doctype": "Ansible Task",
+				"status": "Success",
+				"play": play.name,
+				"role": play.playbook.split(".")[0],
+				"task": task,
+				"output": task_output,
+			}
+		)
+		task.insert()
+		return play
+
+	def test_get_apps_populates_apps_child_table(self):
+		server = create_test_self_hosted_server("apps")
+		with patch(
+			"press.press.doctype.self_hosted_server.self_hosted_server.Ansible.run",
+			new=lambda x: self._create_test_ansible_play_and_task(
+				server,
+				"get_apps.yml",
+				"Get Bench data from Self Hosted Server",
+				"Get Versions from Current Bench",
+				json.dumps([{'commit': '3672c9f', 'app': 'frappe', 'branch': 'version-14', 'version': '14.30.0'}]),
+			),
+		):
+			server._get_apps()
+		server.reload()
+		self.assertTrue(server.apps)
+		self.assertEqual(len(server.apps), 1)
+		self.assertEqual(server.apps[0].app_name, "frappe")
+		self.assertEqual(server.apps[0].branch, "version-14")
+		self.assertEqual(server.apps[0].version, "14.30.0")
 
 def create_test_self_hosted_server(host) -> SelfHostedServer:
 	server = frappe.get_doc(
 		{
 			"doctype": "Self Hosted Server",
-			"status": "Active",
 			"ip": frappe.mock("ipv4"),
 			"private_ip": frappe.mock("ipv4_private"),
 			"server_url": f"https://{host}.fc.dev",
