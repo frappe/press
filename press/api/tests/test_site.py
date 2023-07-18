48f2d39356a97194a96a3000bf5368f049aac6d6
Bread Genie <msuhailbh07@gmail.com>
2023-07-18 15:03:07 +0530

    chore: temp

--- press/api/tests/test_site.py
+++ press/api/tests/test_site.py
@@ -117,12 +117,18 @@ class TestAPISiteList(FrappeTestCase):
 		frappe.db.rollback()
 
 	def test_list_all_sites(self):
-		self.assertCountEqual(
-			all(), [self.broken_site_dict, self.trial_site_dict, self.tagged_site_dict]
+		self.assertEqual(
+			all(),
+			[self.broken_site_dict, self.trial_site_dict, self.tagged_site_dict],
+			frappe.get_all("Release Group", pluck="name"),
 		)
 
 	def test_list_broken_sites(self):
-		self.assertEqual(all(site_filter="Broken"), [self.broken_site_dict])
+		self.assertEqual(
+			all(site_filter="Broken"),
+			[self.broken_site_dict],
+			frappe.get_all("Site", fields=["name", "group", "status"]),
+		)
 
 	def test_list_trial_sites(self):
 		self.assertEqual(all(site_filter="Trial"), [self.trial_site_dict])
