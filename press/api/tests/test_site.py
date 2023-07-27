991a6ff2d2c75f4317e3a055fef10460d3e9683f
Balamurali M <balamurali@frappe.io>
2023-07-27 14:47:14 +0530

    test(APISite): Reintroduce commented out tests

--- press/api/tests/test_site.py
+++ press/api/tests/test_site.py
@@ -3,11 +3,13 @@
 # See license.txt
 
 
+import datetime
 from unittest.mock import Mock, patch
 
 import frappe
 from frappe.tests.utils import FrappeTestCase
 
+from press.api.site import all
 from press.press.doctype.agent_job.agent_job import AgentJob
 from press.press.doctype.app.test_app import create_test_app
 from press.press.doctype.bench.test_bench import create_test_bench
@@ -113,83 +115,83 @@ class TestAPISite(FrappeTestCase):
 		pass
 
 
-# class TestAPISiteList(FrappeTestCase):
-# 	def setUp(self):
-# 		from press.press.doctype.press_tag.test_press_tag import create_and_add_test_tag
-# 		from press.press.doctype.site.test_site import create_test_site
+class TestAPISiteList(FrappeTestCase):
+	def setUp(self):
+		from press.press.doctype.press_tag.test_press_tag import create_and_add_test_tag
+		from press.press.doctype.site.test_site import create_test_site
 
-# 		app = create_test_app()
-# 		group = create_test_release_group([app])
-# 		bench = create_test_bench(group=group)
+		app = create_test_app()
+		group = create_test_release_group([app])
+		bench = create_test_bench(group=group)
 
-# 		broken_site = create_test_site(bench=bench.name)
-# 		broken_site.status = "Broken"
-# 		broken_site.save()
-# 		self.broken_site_dict = {
-# 			"name": broken_site.name,
-# 			"host_name": broken_site.host_name,
-# 			"status": broken_site.status,
-# 			"creation": broken_site.creation,
-# 			"bench": broken_site.bench,
-# 			"current_cpu_usage": broken_site.current_cpu_usage,
-# 			"current_database_usage": broken_site.current_database_usage,
-# 			"current_disk_usage": broken_site.current_disk_usage,
-# 			"trial_end_date": broken_site.trial_end_date,
-# 			"team": broken_site.team,
-# 			"title": group.title,
-# 			"version": group.version,
-# 		}
+		broken_site = create_test_site(bench=bench.name)
+		broken_site.status = "Broken"
+		broken_site.save()
+		self.broken_site_dict = {
+			"name": broken_site.name,
+			"host_name": broken_site.host_name,
+			"status": broken_site.status,
+			"creation": broken_site.creation,
+			"bench": broken_site.bench,
+			"current_cpu_usage": broken_site.current_cpu_usage,
+			"current_database_usage": broken_site.current_database_usage,
+			"current_disk_usage": broken_site.current_disk_usage,
+			"trial_end_date": broken_site.trial_end_date,
+			"team": broken_site.team,
+			"title": group.title,
+			"version": group.version,
+		}
 
-# 		trial_site = create_test_site(bench=bench.name)
-# 		trial_site.trial_end_date = datetime.datetime.now()
-# 		trial_site.save()
+		trial_site = create_test_site(bench=bench.name)
+		trial_site.trial_end_date = datetime.datetime.now()
+		trial_site.save()
 
-# 		self.trial_site_dict = {
-# 			"name": trial_site.name,
-# 			"host_name": trial_site.host_name,
-# 			"status": trial_site.status,
-# 			"creation": trial_site.creation,
-# 			"bench": trial_site.bench,
-# 			"current_cpu_usage": trial_site.current_cpu_usage,
-# 			"current_database_usage": trial_site.current_database_usage,
-# 			"current_disk_usage": trial_site.current_disk_usage,
-# 			"trial_end_date": trial_site.trial_end_date.date(),
-# 			"team": trial_site.team,
-# 			"title": group.title,
-# 			"version": group.version,
-# 		}
+		self.trial_site_dict = {
+			"name": trial_site.name,
+			"host_name": trial_site.host_name,
+			"status": trial_site.status,
+			"creation": trial_site.creation,
+			"bench": trial_site.bench,
+			"current_cpu_usage": trial_site.current_cpu_usage,
+			"current_database_usage": trial_site.current_database_usage,
+			"current_disk_usage": trial_site.current_disk_usage,
+			"trial_end_date": trial_site.trial_end_date.date(),
+			"team": trial_site.team,
+			"title": group.title,
+			"version": group.version,
+		}
 
-# 		tagged_site = create_test_site(bench=bench.name)
-# 		create_and_add_test_tag(tagged_site.name, "Site")
+		tagged_site = create_test_site(bench=bench.name)
+		create_and_add_test_tag(tagged_site.name, "Site")
 
-# 		self.tagged_site_dict = {
-# 			"name": tagged_site.name,
-# 			"host_name": tagged_site.host_name,
-# 			"status": tagged_site.status,
-# 			"creation": tagged_site.creation,
-# 			"bench": tagged_site.bench,
-# 			"current_cpu_usage": tagged_site.current_cpu_usage,
-# 			"current_database_usage": tagged_site.current_database_usage,
-# 			"current_disk_usage": tagged_site.current_disk_usage,
-# 			"trial_end_date": tagged_site.trial_end_date,
-# 			"team": tagged_site.team,
-# 			"title": group.title,
-# 			"version": group.version,
-# 		}
+		self.tagged_site_dict = {
+			"name": tagged_site.name,
+			"host_name": tagged_site.host_name,
+			"status": tagged_site.status,
+			"creation": tagged_site.creation,
+			"bench": tagged_site.bench,
+			"current_cpu_usage": tagged_site.current_cpu_usage,
+			"current_database_usage": tagged_site.current_database_usage,
+			"current_disk_usage": tagged_site.current_disk_usage,
+			"trial_end_date": tagged_site.trial_end_date,
+			"team": tagged_site.team,
+			"title": group.title,
+			"version": group.version,
+		}
 
-# 	def tearDown(self):
-# 		frappe.db.rollback()
+	def tearDown(self):
+		frappe.db.rollback()
 
-# 	def test_list_all_sites(self):
-# 		self.assertCountEqual(
-# 			all(), [self.broken_site_dict, self.trial_site_dict, self.tagged_site_dict]
-# 		)
+	def test_list_all_sites(self):
+		self.assertCountEqual(
+			all(), [self.broken_site_dict, self.trial_site_dict, self.tagged_site_dict]
+		)
 
-# 	def test_list_broken_sites(self):
-# 		self.assertEqual(all(site_filter="Broken"), [self.broken_site_dict])
+	def test_list_broken_sites(self):
+		self.assertEqual(all(site_filter="Broken"), [self.broken_site_dict])
 
-# 	def test_list_trial_sites(self):
-# 		self.assertEqual(all(site_filter="Trial"), [self.trial_site_dict])
+	def test_list_trial_sites(self):
+		self.assertEqual(all(site_filter="Trial"), [self.trial_site_dict])
 
-# 	def test_list_tagged_sites(self):
-# 		self.assertEqual(all(site_filter="tag:test_tag"), [self.tagged_site_dict])
+	def test_list_tagged_sites(self):
+		self.assertEqual(all(site_filter="tag:test_tag"), [self.tagged_site_dict])
