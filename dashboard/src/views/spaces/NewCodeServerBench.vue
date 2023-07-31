0962c727323014e8a8fd85227beb888ac3697543
rutwik <rutwikhdev@gmail.com>
2023-07-31 11:26:33 +0530

    feat: Add search and loading state to new code-server component

--- dashboard/src/views/spaces/NewCodeServerBench.vue
+++ dashboard/src/views/spaces/NewCodeServerBench.vue
@@ -1,40 +1,43 @@
 <template>
 	<div>
-		<div class="ml-2">
+		<div>
 			<label class="text-lg font-semibold"> Choose a Bench </label>
 			<p class="text-base text-gray-700">
 				Choose a bench where you want to install the code server.
 			</p>
 		</div>
-		<ListItem
-			v-if="benches.length > 0"
-			v-for="(bench, index) in benches"
-			:key="bench"
-			:title="bench"
-			class="border rounded-md m-2 px-2 shadow-sm hover:shadow-md hover:cursor-pointer"
-			:class="[
-				modelValue && modelValue == bench
-					? 'relative ring-2 ring-inset ring-blue-500'
-					: ''
-			]"
-			v-on:click="selectBench(bench)"
-		>
-			<template #actions>
-				<Badge
-					v-if="index === 0"
-					label="Latest Deployed"
-					:colorMap="$badgeStatusColorMap"
-				></Badge>
-			</template>
-		</ListItem>
-		<div v-else class="mt-4 ml-2 text-sm">
-			No bench versions found with a code server. Click
-			<router-link
-				:to="`/benches/${selectedGroup}`"
-				class="text-blue-600 hover:underline"
-				>here</router-link
+		<LoadingText v-if="$resources.options.loading" />
+		<div v-else>
+			<ListItem
+				v-if="benches.length > 0"
+				v-for="(bench, index) in benches"
+				:key="bench"
+				:title="bench"
+				class="border rounded-md m-2 px-2 shadow-sm hover:shadow-md hover:cursor-pointer"
+				:class="[
+					modelValue && modelValue == bench
+						? 'relative ring-2 ring-inset ring-blue-500'
+						: ''
+				]"
+				v-on:click="selectBench(bench)"
 			>
-			to deploy a new available version of your bench.
+				<template #actions>
+					<Badge
+						v-if="index === 0"
+						label="Latest Deployed"
+						:colorMap="$badgeStatusColorMap"
+					></Badge>
+				</template>
+			</ListItem>
+			<div v-else class="mt-4 ml-2 text-sm">
+				No bench versions found with a code server. Click
+				<router-link
+					:to="`/benches/${selectedGroup}`"
+					class="text-blue-600 hover:underline"
+					>here</router-link
+				>
+				to deploy a new available version of your bench.
+			</div>
 		</div>
 	</div>
 </template>
--- dashboard/src/views/spaces/NewCodeServerGroup.vue
+++ dashboard/src/views/spaces/NewCodeServerGroup.vue
@@ -1,16 +1,21 @@
 <template>
 	<div>
-		<div class="ml-2">
+		<div>
 			<label class="text-lg font-semibold"> Choose a Bench </label>
 			<p class="text-base text-gray-700">
 				Choose a bench where you want to install the code server.
 			</p>
 		</div>
+		<Input
+			class="my-2"
+			placeholder="Search for Bench"
+			v-on:input="e => updateSearchTerm(e)"
+		/>
 		<div class="grid grid-cols-1 gap-2 sm:grid-cols-2">
 			<div
-				v-for="group in groups"
+				v-for="group in filteredOptions"
 				v-on:click="selectGroup(group)"
-				class="border rounded-md m-2 px-6 py-5 shadow-sm hover:shadow-md hover:cursor-pointer"
+				class="border rounded-md m-2 px-6 py-5 text-lg shadow-sm hover:shadow-md hover:cursor-pointer"
 				:class="[
 					modelValue && modelValue.name == group.name
 						? 'relative ring-2 ring-inset ring-blue-500'
@@ -24,26 +29,48 @@
 </template>
 
 <script>
+import Fuse from 'fuse.js/dist/fuse.basic.esm';
+
 export default {
 	name: 'NewCodeServerBench',
 	props: ['modelValue'],
 	emits: ['update:modelValue', 'error'],
-	methods: {
-		selectGroup(group) {
-			this.$emit('update:modelValue', group);
-		}
+	data() {
+		return {
+			filteredOptions: []
+		};
 	},
 	resources: {
 		options() {
 			return {
 				method: 'press.api.spaces.code_server_group_options',
-				auto: true
+				auto: true,
+				onSuccess(data) {
+					this.fuse = new Fuse(data, {
+						keys: ['title']
+					});
+					this.filteredOptions = data;
+				}
 			};
 		}
 	},
 	computed: {
 		groups() {
 			return this.$resources.options.data;
+		}
+	},
+	methods: {
+		selectGroup(group) {
+			this.$emit('update:modelValue', group);
+		},
+		updateSearchTerm(value) {
+			if (value) {
+				this.filteredOptions = this.fuse
+					.search(value)
+					.map(result => result.item);
+			} else {
+				this.filteredOptions = this.$resources.options.data;
+			}
 		}
 	}
 };
