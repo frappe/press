ea3b365d635cc30c49d4d492d1d99941c9bd6362
Bread Genie <msuhailbh07@gmail.com>
2023-08-03 16:45:06 +0530

    refactor: extract `SiteCard` component

--- dashboard/src/views/site/SiteList.vue
+++ dashboard/src/views/site/SiteList.vue
@@ -5,55 +5,12 @@
 				{{ groupedSite.releaseGroup }}
 			</h3>
 			<div class="grid grid-cols-4 gap-4">
-				<div
-					class="rounded-md border border-gray-100 p-3"
+				<SiteCard
 					v-for="site in groupedSite.sites"
 					:key="site.name"
-				>
-					<div class="space-y-2">
-						<div class="flex items-center justify-center">
-							<div class="w-full truncate text-lg" :title="site.name">
-								{{ site.name }}
-							</div>
-							<router-link
-								class="space-y-2"
-								:to="`/sites/${site.name}/overview`"
-							>
-								<Button
-									class="ml-auto h-4"
-									variant="ghost"
-									icon="external-link"
-								/>
-							</router-link>
-							<Dropdown
-								v-if="site.status === 'Active' || site.status === 'Updating'"
-								:options="dropdownItems(site)"
-							>
-								<template v-slot="{ open }">
-									<Button variant="ghost" class="ml-2" icon="more-horizontal" />
-								</template>
-							</Dropdown>
-						</div>
-						<div class="flex items-center space-x-3">
-							<Badge :label="site.version" />
-							<Badge
-								class="pointer-events-none"
-								variant="subtle"
-								:label="siteBadge(site)"
-							/>
-							<img
-								v-if="site.server_region_info.image"
-								class="h-4"
-								:src="site.server_region_info.image"
-								:alt="`Flag of ${site.server_region_info.title}`"
-								:title="site.server_region_info.image"
-							/>
-						</div>
-						<div class="hidden text-sm text-gray-600 sm:block">
-							{{ $dayjs(site.creation).fromNow() }}
-						</div>
-					</div>
-				</div>
+					:site="site"
+					:dropdownItems="dropdownItems"
+				/>
 			</div>
 		</div>
 	</div>
@@ -84,6 +41,7 @@
 </template>
 <script>
 import { loginAsAdmin } from '@/controllers/loginAsAdmin';
+import SiteCard from './SiteCard.vue';
 
 export default {
 	name: 'SiteList',
@@ -94,6 +52,9 @@ export default {
 		showBenchInfo: {
 			default: true
 		}
+	},
+	components: {
+		SiteCard
 	},
 	data() {
 		return {
@@ -109,25 +70,6 @@ export default {
 		}
 	},
 	methods: {
-		siteBadge(site) {
-			let status = site.status;
-			if (site.update_available && site.status == 'Active') {
-				status = 'Update Available';
-			}
-
-			let usage = Math.max(
-				site.current_cpu_usage,
-				site.current_database_usage,
-				site.current_disk_usage
-			);
-			if (usage && usage >= 80 && status == 'Active') {
-				status = 'Attention Required';
-			}
-			if (site.trial_end_date) {
-				status = 'Trial';
-			}
-			return status;
-		},
 		dropdownItems(site) {
 			return [
 				{
