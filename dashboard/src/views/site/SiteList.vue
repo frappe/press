7efe2eced47fb62a73d7df8fb2814ed8f14eeae5
Bread Genie <msuhailbh07@gmail.com>
2023-08-08 20:08:33 +0530

    fix: dropdown button redirects to overview when it's open

--- dashboard/src/views/site/SiteList.vue
+++ dashboard/src/views/site/SiteList.vue
@@ -1,49 +1,48 @@
 <template>
-	<router-link
-		:to="{ name: 'SiteOverview', params: { siteName: site.name } }"
-		class="rounded px-3 py-3 hover:bg-gray-100"
-	>
-		<div class="flex items-center">
-			<div class="w-4/12">
-				<div class="flex items-center space-x-2">
-					<div class="truncate text-base font-medium" :title="site.name">
-						{{ site.name }}
+	<div class="flex items-center rounded hover:bg-gray-100">
+		<router-link
+			:to="{ name: 'SiteOverview', params: { siteName: site.name } }"
+			class="w-full px-3 py-3"
+		>
+			<div class="flex items-center">
+				<div class="w-4/12">
+					<div class="flex items-center space-x-2">
+						<div class="truncate text-base font-medium" :title="site.name">
+							{{ site.name }}
+						</div>
+					</div>
+					<div class="mt-1 hidden text-base text-gray-600 sm:block">
+						Created on {{ formatDate(site.creation, 'DATE_MED') }}
 					</div>
 				</div>
-				<div class="mt-1 hidden text-base text-gray-600 sm:block">
-					Created on {{ formatDate(site.creation, 'DATE_MED') }}
+				<div class="w-2/12">
+					<Badge
+						class="pointer-events-none"
+						variant="subtle"
+						:label="siteBadge(site)"
+					/>
+				</div>
+				<div class="w-2/12">
+					<img
+						class="h-4"
+						:src="site.server_region_info.image"
+						:alt="`Flag of ${site.server_region_info.title}`"
+						:title="site.server_region_info.image"
+					/>
+				</div>
+				<div class="w-1/12">
+					<div class="text-base text-gray-700">
+						{{ site.plan ? `${$planTitle(site.plan)}/mo` : 'No Plan Set' }}
+					</div>
 				</div>
 			</div>
-			<div class="w-2/12">
-				<Badge
-					class="pointer-events-none"
-					variant="subtle"
-					:label="siteBadge(site)"
-				/>
-			</div>
-			<div class="w-2/12">
-				<img
-					class="h-4"
-					:src="site.server_region_info.image"
-					:alt="`Flag of ${site.server_region_info.title}`"
-					:title="site.server_region_info.image"
-				/>
-			</div>
-			<div class="w-1/12">
-				<div class="text-base text-gray-700">
-					{{ site.plan ? `${$planTitle(site.plan)}/mo` : 'No Plan Set' }}
-				</div>
-			</div>
-
-			<div class="ml-auto flex items-center">
-				<Dropdown :options="dropdownItems(site)">
-					<template v-slot="{ open }">
-						<Button variant="ghost" class="ml-2" icon="more-horizontal" />
-					</template>
-				</Dropdown>
-			</div>
-		</div>
-	</router-link>
+		</router-link>
+		<Dropdown :options="dropdownItems(site)">
+			<template v-slot="{ open }">
+				<Button variant="ghost" class="mr-2" icon="more-horizontal" />
+			</template>
+		</Dropdown>
+	</div>
 </template>
 
 <script>
