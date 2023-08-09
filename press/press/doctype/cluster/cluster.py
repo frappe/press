e58741e3bbf47e1434c433673f3bc3facf5f2793
Balamurali M <balamurali@frappe.io>
2023-08-09 16:09:57 +0530

    fix(Cluster): Revert hard-coded cidr

--- press/press/doctype/cluster/cluster.py
+++ press/press/doctype/cluster/cluster.py
@@ -51,7 +51,7 @@ class Cluster(Document):
 				"Cluster", ["cidr_block"], pluck="cidr_block"
 			)
 			for block in blocks:
-				cidr_block = "10.133.0.0/16"
+				cidr_block = str(block)
 				if cidr_block not in existing_blocks:
 					self.cidr_block = cidr_block
 					self.subnet_cidr_block = cidr_block
