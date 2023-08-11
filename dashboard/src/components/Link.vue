adc20c20fb4760b62228cc5a64d2ad69bc495e88
Faris Ansari <netchamp.faris@gmail.com>
2023-08-11 13:51:45 +0530

    fix: minor

--- dashboard/src/components/Link.vue
+++ dashboard/src/components/Link.vue
@@ -1,13 +1,15 @@
 <template>
-	<a
-		:href="href"
+	<component
+		:is="to ? 'router-link' : 'a'"
+		v-bind="to ? { to } : { href }"
 		class="border-b border-gray-800 text-gray-900 outline-gray-400 hover:border-gray-600 hover:text-gray-700"
-		><slot
-	/></a>
+	>
+		<slot
+	/></component>
 </template>
 <script>
 export default {
 	name: 'Link',
-	props: ['href']
+	props: ['href', 'to']
 };
 </script>
