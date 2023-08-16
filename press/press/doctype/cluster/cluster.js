b74d7b2f18e004daa7444f66e67122f4a4d95b5d
Balamurali M <balamurali@frappe.io>
2023-08-16 11:41:35 +0530

    feat(Cluster): Link AWS dashboard page in sidebar

--- press/press/doctype/cluster/cluster.js
+++ press/press/doctype/cluster/cluster.js
@@ -2,6 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Cluster', {
-	// refresh: function(frm) {
-	// }
+	refresh: function (frm) {
+		if (frm.doc.aws_vpc_id) {
+			frm.add_web_link(
+				`https://${frm.doc.region}.console.aws.amazon.com/vpc/home?region=${frm.doc.region}#VpcDetails:VpcId=${frm.doc.aws_vpc_id}`,
+				__('Visit AWS Dashboard'),
+			);
+		}
+	},
 });
