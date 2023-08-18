5d5c9de08f681657350a38458c7b8c2554cf15f5
Aditya Hase <aditya@adityahase.com>
2023-08-18 16:23:24 +0530

    fix(containers): Isolate stacks using VXLAN VNIs

--- press/infrastructure/doctype/stack/stack.py
+++ press/infrastructure/doctype/stack/stack.py
@@ -6,6 +6,11 @@ from frappe.model.document import Document
 
 
 class Stack(Document):
+	def validate(self):
+		if not self.vxlan_id:
+			# Use the last 3 bytes of the name as the VXLAN ID
+			self.vxlan_id = int(self.name[-6:], 16)
+
 	@frappe.whitelist()
 	def deploy(self):
 		deployment = frappe.new_doc("Deployment")
