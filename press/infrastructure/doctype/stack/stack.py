da3a29f5740b0b832f8c2656a6fefd44d9762ff6
Aditya Hase <aditya@adityahase.com>
2023-08-29 22:27:17 +0530

    feat: Define volumes for Service and Stack

--- press/infrastructure/doctype/stack/stack.py
+++ press/infrastructure/doctype/stack/stack.py
@@ -10,6 +10,16 @@ class Stack(Document):
 		if not self.vxlan_id:
 			# Use the last 3 bytes of the name as the VXLAN ID
 			self.vxlan_id = int(self.name[-6:], 16)
+		service_volumes = set()
+		for service in self.services:
+			service = frappe.get_doc("Service", service.service)
+			service_volumes |= set(mount.volume for mount in service.mounts)
+
+		stack_volumes = set(volume.volume for volume in self.volumes)
+		if service_volumes != stack_volumes:
+			self.volumes = []
+			for volume in service_volumes:
+				self.append("volumes", {"volume": volume})
 
 	@frappe.whitelist()
 	def deploy(self):
