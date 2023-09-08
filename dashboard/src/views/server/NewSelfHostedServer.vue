e1df58d7a83c654c48591c324e26aede3aca1eef
Jufri <134145164+optibizpro@users.noreply.github.com>
2023-09-08 16:49:06 +0700

    Update NewSelfHostedServer.vue

--- dashboard/src/views/server/NewSelfHostedServer.vue
+++ dashboard/src/views/server/NewSelfHostedServer.vue
@@ -82,7 +82,7 @@
 							id="region-consent"
 							type="checkbox"
 							label="I agree that the laws of the region selected by me shall stand
-							applicable to me and Frappe."
+							applicable to me and OptiBizPro."
 							class="rounded border-gray-300 focus:ring-blue-500"
 							v-model="agreedToRegionConsent"
 						/>
