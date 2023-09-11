70604403c4655dbfd772dbcc33755512a66cfb6d
Balamurali M <balamurali@frappe.io>
2023-09-11 19:53:48 +0530

    chore(UserSSHKey): Prettier error msg

--- press/press/doctype/user_ssh_key/user_ssh_key.py
+++ press/press/doctype/user_ssh_key/user_ssh_key.py
@@ -31,7 +31,7 @@ class UserSSHKey(Document):
 		try:
 			key_type, key, *comment = self.ssh_public_key.strip().split()
 			if key_type not in self.valid_key_types:
-				raise ValueError(f"Key type has to be one of {','.join(self.valid_key_types)}")
+				raise ValueError(f"Key type has to be one of {', '.join(self.valid_key_types)}")
 			key_bytes = base64.b64decode(key)
 			self.check_embedded_key_type(key_type, key_bytes)
 			self.generate_ssh_fingerprint(key_bytes)
