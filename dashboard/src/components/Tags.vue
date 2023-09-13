bed8e346950ba648bc6edbd3036750c375bd9475
Bread Genie <msuhailbh07@gmail.com>
2023-09-13 12:45:30 +0530

    refactor(Tags): Tag creation/adding flow

--- dashboard/src/components/Tags.vue
+++ dashboard/src/components/Tags.vue
@@ -1,47 +1,45 @@
 <template>
 	<Card title="Tags">
 		<template #actions>
-			<Dropdown :options="dropdownItems">
-				<template v-slot="{ open }">
-					<Button icon-right="chevron-down">Actions</Button>
-				</template>
-			</Dropdown>
+			<Button icon="plus" @click="showAddDialog = true" />
 		</template>
 		<div class="divide-y" v-if="addedTags?.length">
 			<ListItem v-for="tag in addedTags" :key="tag.name" :title="tag.tag">
 				<template #actions>
-					<Button icon="trash-2" @click="removeTag(tag.name)" />
+					<Button icon="x" @click="removeTag(tag.name)" />
 				</template>
 			</ListItem>
 		</div>
 		<div v-else class="m-4 text-center">
 			<p class="text-base text-gray-500">No tags added yet</p>
 		</div>
+		<ErrorMessage
+			:message="
+				$resources.addTag.error ||
+				$resources.createTag.error ||
+				$resources.removeTag.error
+			"
+		/>
 	</Card>
 	<Dialog :options="{ title: `Add a new tag` }" v-model="showAddDialog">
-		<template v-slot:body-content>
-			<ListItem v-for="tag in tags" :key="tag.name" :title="tag.tag">
-				<template #actions>
-					<Button icon="plus" @click="addTag(tag.name)" />
-				</template>
-			</ListItem>
+		<template #body-content>
+			<Autocomplete
+				placeholder="Tags"
+				:options="getAutocompleteOptions"
+				v-model="chosenTag"
+				@update:modelValue="handleAutocompleteSelection"
+			/>
+			<FormControl
+				v-if="showNewTagInput"
+				v-model="newTag"
+				class="mt-4"
+				placeholder="Enter New Tag's name"
+			/>
 		</template>
-	</Dialog>
-	<Dialog
-		:options="{
-			title: `Create a new tag for ${doctype}`,
-			actions: [
-				{
-					label: 'Create',
-					variant: 'solid',
-					onClick: () => $resources.createTag.submit()
-				}
-			]
-		}"
-		v-model="showNewDialog"
-	>
-		<template v-slot:body-content>
-			<FormControl label="Enter Tag name" v-model="newTag" />
+		<template #actions>
+			<Button variant="solid" class="w-full" @click="addTag()">{{
+				showNewTagInput ? 'Create a New Tag' : 'Add Tag'
+			}}</Button>
 		</template>
 	</Dialog>
 </template>
@@ -53,21 +51,9 @@ export default {
 	props: ['name', 'doctype', 'resourceTags', 'tags'],
 	data() {
 		return {
-			dropdownItems: [
-				{
-					label: 'Add',
-					onClick: () =>
-						this.tags.length > 0
-							? (this.showAddDialog = true)
-							: (this.showNewDialog = true)
-				},
-				{
-					label: 'New',
-					onClick: () => (this.showNewDialog = true)
-				}
-			],
 			showAddDialog: false,
-			showNewDialog: false,
+			showNewTagInput: false,
+			chosenTag: '',
 			newTag: '',
 			addedTags: [],
 			createErrorMessage: ''
@@ -82,24 +68,24 @@ export default {
 					doctype: this.doctype,
 					tag: this.newTag
 				},
+				validate() {
+					if (this.addedTags.find(t => t.name == this.newTag)) {
+						return 'Tag already added';
+					}
+				},
 				onSuccess(d) {
 					this.addedTags.push(this.tags.find(t => t.name == d));
 					this.showAddDialog = false;
 					this.newTag = '';
+					this.chosenTag = '';
 				}
 			};
 		},
 		removeTag() {
 			return {
 				url: 'press.api.dashboard.remove_tag',
-				params: {
-					name: this.name,
-					doctype: this.doctype,
-					tag: this.newTag
-				},
 				onSuccess(d) {
 					this.addedTags = this.addedTags.filter(t => t.name != d);
-					this.newTag = '';
 				}
 			};
 		},
@@ -111,34 +97,61 @@ export default {
 					doctype: this.doctype,
 					tag: this.newTag
 				},
+				validate() {
+					if (this.tags.find(t => t.tag === this.newTag)) {
+						return 'Tag already exists';
+					}
+				},
 				onSuccess(d) {
 					this.addedTags.push({ name: d.name, tag: d.tag });
-					this.showNewDialog = false;
+					this.showNewTagInput = false;
 					this.newTag = '';
-				},
-				onError(e) {
-					this.showNewDialog = false;
-					notify({
-						title: e,
-						color: 'red',
-						icon: 'x'
-					});
+					this.chosenTag = '';
 				}
 			};
 		}
 	},
 	methods: {
-		addTag(tagName) {
-			this.newTag = tagName;
-			this.$resources.addTag.submit();
+		addTag() {
+			if (this.showNewTagInput) {
+				this.$resources.createTag.submit();
+			} else {
+				this.$resources.addTag.submit();
+			}
+			this.showAddDialog = false;
 		},
 		removeTag(tagName) {
-			this.newTag = tagName;
-			this.$resources.removeTag.submit();
+			this.$resources.removeTag.submit({
+				name: this.name,
+				doctype: this.doctype,
+				tag: tagName
+			});
+		},
+		handleAutocompleteSelection() {
+			if (this.chosenTag.value === 'new_tag') {
+				this.showNewTagInput = true;
+			} else {
+				this.newTag = this.chosenTag.value;
+				this.showNewTagInput = false;
+			}
 		}
 	},
 	mounted() {
 		this.addedTags = this.resourceTags;
+	},
+	computed: {
+		getAutocompleteOptions() {
+			return [
+				{
+					group: 'New Tag',
+					items: [{ label: 'Create a New Tag', value: 'new_tag' }]
+				},
+				{
+					group: 'Existing Tags',
+					items: this.tags.map(t => ({ label: t.tag, value: t.name }))
+				}
+			];
+		}
 	}
 };
 </script>
