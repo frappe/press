3fde00628213824be78940882fa29450df9323bc
rutwik <rutwikhdev@gmail.com>
2023-09-20 18:31:53 +0530

    fix: Show marketplace filters only if selected

--- press/public/js/marketplace.bundle.js
+++ press/public/js/marketplace.bundle.js
@@ -100,10 +100,9 @@ function updateCategories(category) {
 	var button = document.querySelector(`button[value="${category}"]`);
 	if (button) {
 		button.classList.add('bg-gray-200');
+		removeCategoryBtn.classList.remove('hidden');
+		document.getElementById('remove-category-name').innerText = category;
 	}
-
-	removeCategoryBtn.classList.remove('hidden');
-	document.getElementById('remove-category-name').innerText = category;
 }
 
 var category = new URLSearchParams(window.location.search).get('category');
