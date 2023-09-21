40de07f1a603a88b0e0717b306bce7eee818ccaa
Bread Genie <msuhailbh07@gmail.com>
2023-09-21 14:43:24 +0530

    refactor: monitor
    - Move monitor logs as children to `/monitor`
    - Fix process list
    - styling in binary logs

--- dashboard/src/views/site/SiteMonitorsList.vue
+++ dashboard/src/views/site/SiteMonitorsList.vue
@@ -1,40 +1,34 @@
 <template>
 	<!-- <div class="grid grid-cols-1 gap-5 sm:grid-cols-3"> -->
-	<div class="flex flex-col gap-3">
+	<div v-if="$route.fullPath.endsWith('/monitor')" class="flex flex-col gap-3">
 		<div
-			class="sm:rounded-md sm:border sm:border-gray-100 sm:py-1 sm:px-2 sm:shadow hover:bg-gray-100"
+			class="hover:bg-gray-100 sm:rounded-md sm:border sm:border-gray-100 sm:px-2 sm:py-1 sm:shadow"
 		>
-			<router-link
-				:to="{ name: 'SiteRequestLogs', params: { siteName: site.name } }"
-			>
+			<router-link :to="{ name: 'SiteRequestLogs', params: { siteName } }">
 				<div class="py-2 text-gray-900 sm:px-2">Request Logs</div>
 			</router-link>
 		</div>
 		<div
-			class="sm:rounded-md sm:border sm:border-gray-100 sm:py-1 sm:px-2 sm:shadow hover:bg-gray-100"
+			class="hover:bg-gray-100 sm:rounded-md sm:border sm:border-gray-100 sm:px-2 sm:py-1 sm:shadow"
 		>
-			<router-link
-				:to="{ name: 'SiteBinaryLogs', params: { siteName: site.name } }"
-			>
+			<router-link :to="{ name: 'SiteBinaryLogs', params: { siteName } }">
 				<div class="py-2 text-gray-900 sm:px-2">Mariadb Binary Log Report</div>
 			</router-link>
 		</div>
 		<div
-			class="sm:rounded-md sm:border sm:border-gray-100 sm:py-1 sm:px-2 sm:shadow hover:bg-gray-100"
+			class="hover:bg-gray-100 sm:rounded-md sm:border sm:border-gray-100 sm:px-2 sm:py-1 sm:shadow"
 		>
-			<router-link
-				:to="{ name: 'MariaDBProcessList', params: { siteName: site.name } }"
-			>
+			<router-link :to="{ name: 'MariaDBProcessList', params: { siteName } }">
 				<div class="py-2 text-gray-900 sm:px-2">Mariadb Process List</div>
 			</router-link>
 		</div>
 		<div
-			class="sm:rounded-md sm:border sm:border-gray-100 sm:py-1 sm:px-2 sm:shadow hover:bg-gray-100"
+			class="hover:bg-gray-100 sm:rounded-md sm:border sm:border-gray-100 sm:px-2 sm:py-1 sm:shadow"
 		>
 			<router-link
 				:to="{
 					name: 'SiteMariaDBSlowQueries',
-					params: { siteName: site.name }
+					params: { siteName }
 				}"
 			>
 				<div class="py-2 text-gray-900 sm:px-2">
@@ -43,22 +37,23 @@
 			</router-link>
 		</div>
 		<div
-			class="sm:rounded-md sm:border sm:border-gray-100 sm:py-1 sm:px-2 sm:shadow hover:bg-gray-100"
+			class="hover:bg-gray-100 sm:rounded-md sm:border sm:border-gray-100 sm:px-2 sm:py-1 sm:shadow"
 		>
-			<router-link
-				:to="{ name: 'SiteDeadlockReport', params: { siteName: site.name } }"
-			>
+			<router-link :to="{ name: 'SiteDeadlockReport', params: { siteName } }">
 				<div class="py-2 text-gray-900 sm:px-2">Deadlock Report</div>
 			</router-link>
 		</div>
 	</div>
+	<router-view v-slot="{ Component }">
+		<component v-if="site" :is="Component" :site="site"></component>
+	</router-view>
 </template>
 <script>
 import Report from '@/components/Report.vue';
 
 export default {
 	name: 'SiteMonitorsList',
-	props: ['site'],
+	props: ['site', 'siteName'],
 	components: {
 		Report
 	}
