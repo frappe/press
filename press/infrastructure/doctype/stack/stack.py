c35a9d8b14527c094f389907c5bdd698d77630e6
Aditya Hase <aditya@adityahase.com>
2023-09-25 16:08:06 +0530

    feat: Create stack from Compose file

--- press/infrastructure/doctype/stack/stack.py
+++ press/infrastructure/doctype/stack/stack.py
@@ -3,10 +3,17 @@
 
 import frappe
 from frappe.model.document import Document
+import json
+import shlex
+import subprocess
+import tempfile
 
 
 class Stack(Document):
 	def validate(self):
+		if self.compose and not self.services:
+			self.create_services_from_compose()
+
 		if not self.vxlan_id:
 			# Use the last 3 bytes of the name as the VXLAN ID
 			self.vxlan_id = int(self.name[-6:], 16)
@@ -31,3 +38,58 @@ class Stack(Document):
 		deployments = frappe.get_all("Deployment", filters={"stack": self.name})
 		for deployment in deployments:
 			frappe.get_doc("Deployment", deployment.name).delete()
+
+	@classmethod
+	def parse_compose(kls, compose):
+		loaded = {}
+		with tempfile.NamedTemporaryFile(mode="w", delete=False) as file:
+			file.write(compose)
+			file.flush()
+			command = f"docker compose -f {file.name} config --format json"
+			loaded = json.loads(subprocess.check_output(shlex.split(command)).decode())
+
+		parsed = []
+		for service_name, service in loaded["services"].items():
+			short = service["image"].split(":")[0]
+			image = short
+			if "/" not in image:
+				image = f"library/{image}"
+			if "." not in image.split("/")[0]:
+				image = f"docker.io/{image}"
+
+			parsed_service = frappe._dict(
+				{
+					"image": image,
+					"title": service_name,
+					"tag": service["image"].split(":")[1],
+				}
+			)
+			# for port in service.get("expose", []):
+			# 	parsed_service.setdefault("ports", []).append({"port": port})
+
+			for port in service.get("ports", []):
+				parsed_service.setdefault("ports", []).append(
+					{"port": port["published"], "protocol": port["protocol"]}
+				)
+
+			for key, value in service.get("environment", {}).items():
+				parsed_service.setdefault("environment_variables", []).append(
+					{"key": key, "required": True, "default_value": value}
+				)
+
+			for volume in service.get("volumes", []):
+				if volume["type"] == "volume":
+					parsed_service.setdefault("mounts", []).append(
+						{"volume": volume["source"], "source": "data", "destination": volume["target"]}
+					)
+
+			parsed.append(parsed_service)
+		return parsed
+
+	def create_services_from_compose(self):
+		parsed = self.__class__.parse_compose(self.compose)
+		for service in parsed:
+			service.doctype = "Service"
+			doc = frappe.get_doc(service)
+			doc.insert()
+			self.append("services", {"service": doc.name})
