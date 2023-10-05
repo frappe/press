698efdf0e40ace41d8fdf74402a625fdc12e3780
Bread Genie <msuhailbh07@gmail.com>
2023-10-05 15:40:55 +0530

    refactor(ServerAnalytics): use echarts
    - `LineChart` now takes array of colors
    - datasets are now array too

--- dashboard/src/components/charts/LineChart.vue
+++ dashboard/src/components/charts/LineChart.vue
@@ -57,9 +57,18 @@ const props = defineProps({
 		default: () => 'category'
 	},
 	chartTheme: {
-		type: String,
+		type: Array,
 		required: false,
-		default: () => theme.colors.blue[500]
+		default: () => [
+			theme.colors.red[500],
+			theme.colors.blue[500],
+			theme.colors.green[500],
+			theme.colors.purple[500],
+			theme.colors.yellow[500],
+			theme.colors.teal[500],
+			theme.colors.pink[500],
+			theme.colors.cyan[500]
+		]
 	},
 	loading: {
 		type: Boolean,
@@ -89,20 +98,39 @@ const initOptions = {
 	renderer: 'svg'
 };
 
+function formatBytes(bytes, decimals = 2, current = 0) {
+	if (bytes === 0) return '0 Bytes';
+
+	const k = 1024;
+	const dm = decimals < 0 ? 0 : decimals;
+	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+	const i = Math.floor(Math.log(Math.abs(bytes)) / Math.log(k));
+
+	return (
+		parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i + current]
+	);
+}
+
+function getUnit(value, seriesName) {
+	if (seriesName === 'bytes') return formatBytes(value);
+	else return `${+value.toFixed(2)} ${seriesName}`;
+}
+
 const options = ref({
 	tooltip: {
 		trigger: 'axis',
 		formatter: params => {
 			// for the dot to follow the same color as the line 🗿
 			let tooltip = `<p>${params[0].axisValueLabel}</p>`;
-			params.forEach(({ value, seriesName }) => {
+			params.forEach(({ value, seriesName }, i) => {
 				let colorSpan = color =>
 					'<span style="display:inline-block;margin-right:4px;border-radius:10px;width:10px;height:10px;background-color:' +
 					color +
 					'"></span>';
-				tooltip += `<p>${colorSpan(chartTheme.value)} ${+value[1].toFixed(
-					2
-				)} ${seriesName}</p>`;
+				tooltip += `<p>${colorSpan(chartTheme.value[i])}  ${getUnit(
+					value[1],
+					seriesName
+				)}</p>`;
 			});
 			return tooltip;
 		}
@@ -116,17 +144,20 @@ const options = ref({
 		},
 		axisTick: {
 			show: false
-		},
-		axisLabel: {
-			formatter: '{d} {MMM}'
 		}
 	},
 	yAxis: {
 		type: 'value',
 		axisLabel: {
 			formatter: value => {
-				if (value >= 1000) return `${value / 1000}K`;
-				return value;
+				if (unit.value === 'bytes') {
+					return formatBytes(value, 0);
+				} else {
+					if (value >= 1000000000) return `${value / 1000000000}B`;
+					else if (value >= 1000000) return `${value / 1000000}M`;
+					else if (value >= 1000) return `${value / 1000}K`;
+					return value;
+				}
 			}
 		}
 	},
@@ -135,12 +166,12 @@ const options = ref({
 		length: 10,
 		length2: 20
 	},
-	series: [
-		{
-			name: unit,
+	series: data.value.datasets.map((dataset, i) => {
+		return {
+			name: dataset.name || unit,
 			type: 'line',
 			showSymbol: false,
-			data: data.value.datasets,
+			data: dataset.dataset || dataset,
 			markLine: data.value.markLine,
 			emphasis: {
 				itemStyle: {
@@ -150,16 +181,16 @@ const options = ref({
 				}
 			},
 			lineStyle: {
-				color: chartTheme
+				color: chartTheme.value[i]
 			},
 			itemStyle: {
-				color: chartTheme
+				color: chartTheme.value[i]
 			},
 			areaStyle: {
 				color: new graphic.LinearGradient(0, 0, 0, 1, [
 					{
 						offset: 0,
-						color: chartTheme
+						color: chartTheme.value[i]
 					},
 					{
 						offset: 1,
@@ -168,7 +199,7 @@ const options = ref({
 				]),
 				opacity: 0.3
 			}
-		}
-	]
+		};
+	})
 });
 </script>
