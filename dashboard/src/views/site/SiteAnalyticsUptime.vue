afd1139af7e64c011b761e0e28d695ac790b8883
Bread Genie <msuhailbh07@gmail.com>
2023-10-11 09:58:57 +0530

    style(SiteUptimeBar): thicker bars and average as subtitle

--- dashboard/src/views/site/SiteAnalyticsUptime.vue
+++ dashboard/src/views/site/SiteAnalyticsUptime.vue
@@ -1,14 +1,14 @@
 <template>
-	<Card title="Uptime" :loading="loading">
+	<Card title="Uptime" :subtitle="subtitle" :loading="loading">
 		<div v-if="!data" class="flex h-full items-center justify-center">
 			<div class="text-base text-gray-600">No data</div>
 		</div>
 		<div v-else class="mt-8" v-for="type in uptimeTypes" :key="type.key">
-			<div class="flex h-8 justify-between">
+			<div class="flex h-10 justify-between">
 				<div
 					v-for="d in data"
 					:key="d.date"
-					style="width: 2.5px"
+					class="w-1.5 rounded"
 					:class="[
 						d[type.key] === undefined
 							? 'bg-white'
@@ -18,9 +18,11 @@
 							? 'bg-red-500'
 							: 'bg-yellow-500'
 					]"
-					:title="`${formatDate(d.date)} | Uptime: ${(d.value * 100).toFixed(
-						2
-					)}%`"
+					:title="
+						d[type.key]
+							? `${formatDate(d.date)} | Uptime: ${(d.value * 100).toFixed(2)}%`
+							: ''
+					"
 				></div>
 			</div>
 		</div>
@@ -34,6 +36,19 @@ export default {
 	computed: {
 		uptimeTypes() {
 			return [{ key: 'value', label: 'Web' }];
+		},
+		subtitle() {
+			let total = 0;
+			let i = 0;
+			for (; i < this.data.length; i++) {
+				// there could be empty objects at the end of the array
+				// so we don't have to count them
+				if (!this.data[i].value) break;
+
+				total += this.data[i].value;
+			}
+
+			return this.data ? `Average: ${((total / i) * 100).toFixed(2)}%` : '';
 		}
 	},
 	methods: {
