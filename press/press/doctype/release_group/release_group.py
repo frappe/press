1df3dfc34a9bdea6949bdb136ca0da172881ada0
Bread Genie <msuhailbh07@gmail.com>
2023-10-17 16:18:07 +0530

    fix(release-group): requiring approved status for own marketplace app

--- press/press/doctype/release_group/release_group.py
+++ press/press/doctype/release_group/release_group.py
@@ -416,7 +416,7 @@ class ReleaseGroup(Document):
 			only_approved_for_sources = (
 				frappe.qb.from_(AppSource)
 				.where(AppSource.name.isin(marketplace_app_sources))
-				.where(AppSource.team.isin(app_publishers_team))
+				.where(AppSource.team.notin(app_publishers_team))
 				.select(AppSource.name)
 				.run(as_dict=True, pluck="name")
 			)
