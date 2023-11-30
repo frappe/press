7d23a213a9af72a127f0a8ba3f960907a640f83e
rutwik <rutwikhdev@gmail.com>
2023-11-30 17:11:38 +0530

    chore: Send all dispute updates

--- press/press/doctype/payment_dispute/payment_dispute.py
+++ press/press/doctype/payment_dispute/payment_dispute.py
@@ -8,14 +8,14 @@ from press.telegram_utils import Telegram
 
 class PaymentDispute(Document):
 	def after_insert(self):
-		if self.event_type == "Created":
-			telegram = Telegram(topic="Dispute", group="Billing")
-			telegram.send(
-				f"""
-				⚠️  New dispute has been raised!
+		telegram = Telegram(topic="Dispute", group="Billing")
+		telegram.send(
+			f"""
+			Dispute Update!
 
-				Email: {self.email}
-				Dispute ID: `{self.dispute_id}`
-				[Payment reference on Stripe Dashboard](https://dashboard.stripe.com/payments/{self.payment_intent})
-			"""
-			)
+			Email: {self.email}
+			Dispute ID: `{self.dispute_id}`
+			Event: `{self.event_type}`
+			[Payment reference on Stripe Dashboard](https://dashboard.stripe.com/payments/{self.payment_intent})
+		"""
+		)
