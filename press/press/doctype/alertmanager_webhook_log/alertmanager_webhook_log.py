b2f08d462d668239a7e31749181e81d8e4ffc037
Balamurali M <balamurali@frappe.io>
2023-12-15 13:33:03 +0530

    feat(Incident): Detect incident from firing alerts

--- press/press/doctype/alertmanager_webhook_log/alertmanager_webhook_log.py
+++ press/press/doctype/alertmanager_webhook_log/alertmanager_webhook_log.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2021, Frappe and contributors
 # For license information, please see license.txt
 
+from functools import cached_property
 import frappe
 import json
 from frappe.model.document import Document
@@ -29,6 +30,9 @@ Labels:
 
 """
 
+INCIDENT_ALERT = "Sites Down"  # TODO: make it a field or child table somewhere #
+INCIDENT_SCOPE = "server"
+
 
 class AlertmanagerWebhookLog(Document):
 	def validate(self):
@@ -48,11 +52,61 @@ class AlertmanagerWebhookLog(Document):
 		self.common_labels = json.dumps(self.parsed["commonLabels"], indent=2, sort_keys=True)
 
 		self.payload = json.dumps(self.parsed, indent=2, sort_keys=True)
-		if self.status is "Firing" and self.severity is "Critical":
-			self.create_incident()
 		frappe.enqueue_doc(
 			self.doctype, self.name, "send_telegram_notification", enqueue_after_commit=True
 		)
+
+	def after_insert(self):
+		self.validate_and_create_incident()
+
+	def get_past_alert_instances(self):
+		past_alerts = frappe.get_all(
+			self.doctype,
+			fields=["payload"],
+			filters={
+				"alert": self.alert,
+				"severity": self.severity,
+				"status": self.status,
+				"group_key": ("like", f"%{self.group_labels.get(INCIDENT_SCOPE)}%"),
+				"creation": [
+					">",
+					add_to_date(frappe.utils.now(), hours=-self.get_repeat_interval()),
+				],
+			},
+			group_by="group_key",
+		)  # get site down alerts grouped by benches
+
+		instances = []
+		for alert in past_alerts:
+			payload = json.loads(alert["payload"])
+			instances.extend(
+				[alert["alerts"]["labels"]["instance"] for alert in payload["alerts"]]
+			)
+		return set(instances)
+
+	def total_instances(self) -> int:
+		return frappe.db.count(
+			"Site", {"status": "Active", INCIDENT_SCOPE: self.group_labels.get(INCIDENT_SCOPE)}
+		)
+
+	def validate_and_create_incident(self):
+		if not (
+			self.alert == INCIDENT_ALERT
+			and self.severity == "Critical"
+			and self.status == "Firing"
+		):
+			return
+
+		instances = self.get_past_alert_instances()
+		if len(instances) > 0.8 * self.total_instances():
+			self.create_incident()
+
+	def get_repeat_interval(self):
+		repeat_interval = frappe.db.get_value(
+			"Prometheus Alert Rule", self.alert, "repeat_interval"
+		)
+		hours = repeat_interval.split("h")[0]  # assume hours
+		return int(hours)
 
 	def generate_telegram_message(self):
 		context = self.as_dict()
@@ -104,41 +158,51 @@ class AlertmanagerWebhookLog(Document):
 		client = Telegram(self.severity)
 		client.send(message)
 
-	def create_incident(self):
-		# incident = frappe.new_doc("Incident")
-		# incident.alert = self.alert
+	@property
+	def bench(self):
+		return self.group_labels.get("bench")
+
+	@property
+	def cluster(self):
+		return self.group_labels.get("cluster")
+
+	@property
+	def server(self):
+		return self.group_labels.get("server")
+
+	@cached_property
+	def group_labels(self) -> dict:
 		parsed = json.loads(self.payload)
+		return parsed["groupLabels"]
+
+	def create_incident(self):
 		try:
-			bench = parsed["groupLabels"].get("bench")
-			cluster = parsed["groupLabels"].get("cluster")
-			server = parsed["groupLabels"].get("server")
+			if incident_exists := frappe.db.exists(
+				"Incident",
+				filters={
+					"alert": self.alert,
+					"bench": self.bench,
+					"server": self.server,
+					"cluster": self.cluster,
+					"status": "Validating",
+					"creation": ["<=", add_to_date(frappe.utils.now(), minutes=-10)],
+				},
+			):
+				incident = frappe.get_doc("Incident", incident_exists)
+			else:
+				incident = frappe.new_doc("Incident")
+				incident.alert = self.alert
+				incident.bench = self.bench
+				incident.server = self.server
+				incident.cluster = self.cluster
+
+			incident.append(
+				"alerts",
+				{
+					"alert": self.name,
+					"combined_alerts": self.combined_alerts,
+				},
+			)
+			incident.save()
 		except Exception as e:
 			log_error("Failed to create incident", e)
-			return
-		if incident_exists := frappe.db.exists(
-			"Incident",
-			filters={
-				"alert": self.alert,
-				"bench": bench,
-				"server": server,
-				"cluster": cluster,
-				"status": "Validating",
-				"creation": ["<=", add_to_date(frappe.utils.now(), minutes=-10)],
-			},
-		):
-			incident = frappe.get_doc("Incident", incident_exists)
-		else:
-			incident = frappe.new_doc("Incident")
-			incident.alert = self.alert
-			incident.bench = bench
-			incident.server = server
-			incident.cluster = cluster
-
-		incident.append(
-			"alerts",
-			{
-				"alert": self.name,
-				"combined_alerts": self.combined_alerts,
-			},
-		)
-		incident.save()
