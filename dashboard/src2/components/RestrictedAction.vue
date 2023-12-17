f471ea47fb3983f649bc0ad209d637fd194c48bc
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-12-17 12:51:41 +0530

    fix: cache method permissions per user

--- dashboard/src2/components/RestrictedAction.vue
+++ dashboard/src2/components/RestrictedAction.vue
@@ -15,12 +15,17 @@
 <script setup>
 import { Button, createResource, Tooltip } from 'frappe-ui';
 import { getTeam } from '../data/team';
+import session from '../data/session';
 
 const props = defineProps({
 	doctype: { type: String, required: true },
 	docname: { type: String, required: true },
 	method: { type: String, required: true }
 });
+
+if (!props.doctype || !props.docname || !props.method) {
+	console.warn('doctype, docname and method are required');
+}
 
 const hasMethodPermission = createResource({
 	url: 'press.api.account.has_method_permission',
@@ -32,6 +37,7 @@ const hasMethodPermission = createResource({
 	cache: [
 		'SiteActionPermission',
 		getTeam().doc.name,
+		session.user,
 		props.doctype,
 		props.docname,
 		props.method
