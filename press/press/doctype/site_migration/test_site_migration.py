ad40dfbeef9a68e34a53363808d4288561e6aad5
Balamurali M <balamurali@frappe.io>
2024-01-12 15:17:30 +0530

    test(SiteMigration): Add test for activate on failure

--- press/press/doctype/site_migration/test_site_migration.py
+++ press/press/doctype/site_migration/test_site_migration.py
@@ -14,8 +14,8 @@ from press.press.doctype.agent_job.test_agent_job import fake_agent_job
 from press.press.doctype.site.test_site import create_test_bench, create_test_site
 
 
+@patch.object(RemoteFile, "download_link", new="http://test.com")
 class TestSiteMigration(FrappeTestCase):
-	@patch.object(RemoteFile, "download_link", new="http://test.com")
 	def test_in_cluster_site_migration_goes_through_all_steps_and_updates_site(self):
 		site = create_test_site()
 		bench = create_test_bench()
@@ -89,3 +89,64 @@ class TestSiteMigration(FrappeTestCase):
 		self.assertEqual(site.status, "Active")
 		self.assertEqual(site.bench, bench.name)
 		self.assertEqual(site.server, bench.server)
+
+	def test_site_is_activated_on_failure_when_possible(self):
+		site = create_test_site()
+		bench = create_test_bench()
+		site_migration = frappe.get_doc(
+			{
+				"doctype": "Site Migration",
+				"site": site.name,
+				"destination_bench": bench.name,
+			}
+		).insert()
+
+		with fake_agent_job("Update Site Configuration"), fake_agent_job(
+			"Backup Site",
+			data={
+				"backups": {
+					"database": {
+						"file": "a.sql.gz",
+						"path": "/home/frappe/a.sql.gz",
+						"size": 1674818,
+						"url": "https://a.com/a.sql.gz",
+					},
+					"public": {
+						"file": "b.tar",
+						"path": "/home/frappe/b.tar",
+						"size": 1674818,
+						"url": "https://a.com/b.tar",
+					},
+					"private": {
+						"file": "a.tar",
+						"path": "/home/frappe/a.tar",
+						"size": 1674818,
+						"url": "https://a.com/a.tar",
+					},
+					"site_config": {
+						"file": "a.json",
+						"path": "/home/frappe/a.json",
+						"size": 595,
+						"url": "https://a.com/json",
+					},
+				},
+				"offsite": {
+					"a.sql.gz": "bucket.frappe.cloud/2023-10-10/a.sql.gz",
+					"a.tar": "bucket.frappe.cloud/2023-10-10/a.tar",
+					"b.tar": "bucket.frappe.cloud/2023-10-10/b.tar",
+					"a.json": "bucket.frappe.cloud/2023-10-10/a.json",
+				},
+			},
+		), fake_agent_job("New Site from Backup"), fake_agent_job(
+			"Archive Site", status="Failure"
+		), fake_agent_job(
+			"Update Site Configuration"
+		):
+			site_migration.start()
+			poll_pending_jobs()
+			poll_pending_jobs()
+			poll_pending_jobs()
+			site_migration.reload()
+			self.assertEqual(site_migration.status, "Failure")
+			site.reload()
+			self.assertEqual(site.status, "Active")
