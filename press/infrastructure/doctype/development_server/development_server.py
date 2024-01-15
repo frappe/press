5f4256ee6036ec79b874047df2dde8099872b18c
Aditya Hase <aditya@adityahase.com>
2024-01-15 15:17:35 +0530

    feat: Development Server

---
+++ press/infrastructure/doctype/development_server/development_server.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Frappe and contributors
+# For license information, please see license.txt
+
+from press.press.doctype.server.server import BaseServer
+
+
+class DevelopmentServer(BaseServer):
+	pass
