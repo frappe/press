4581c96b92d870a6d56fbd8a1e00f780857dad1a
Aditya Hase <aditya@adityahase.com>
2024-01-15 18:35:47 +0530

    feat: Add suspended builds status to prometheus metrics

--- press/metrics.py
+++ press/metrics.py
@@ -3,18 +3,27 @@
 
 from prometheus_client import (
 	CollectorRegistry,
+	Gauge,
 	generate_latest,
 )
 from werkzeug.wrappers import Response
+import frappe
+from frappe.utils import cint
 
 
 class MetricsRenderer:
-	def __init__(self, path):
+	def __init__(self, path, status_code=None):
 		self.path = path
+		self.registry = CollectorRegistry(auto_describe=True)
 
 	def metrics(self):
-		registry = CollectorRegistry(auto_describe=True)
-		return generate_latest(registry).decode("utf-8")
+		suspended_builds = Gauge(
+			"press_builds_suspended", "Are docker builds suspended", registry=self.registry
+		)
+		suspended_builds.set(
+			cint(frappe.db.get_value("Press Settings", None, "suspend_builds"))
+		)
+		return generate_latest(self.registry).decode("utf-8")
 
 	def can_render(self):
 		if self.path in ("metrics",):
