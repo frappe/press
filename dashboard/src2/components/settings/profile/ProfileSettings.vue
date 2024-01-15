49182d6fb02782c480f7ae7f25c64f83bd135fad
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-01-15 17:21:11 +0530

    feat(d2): Profile Settings (#1347)

---
+++ dashboard/src2/components/settings/profile/ProfileSettings.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="p-5 space-y-5">
+		<AccountProfile />
+		<AccountEmails />
+		<AccountReferral />
+	</div>
+</template>
+
+<script setup>
+import AccountEmails from './AccountEmails.vue';
+import AccountProfile from './AccountProfile.vue';
+import AccountReferral from './AccountReferral.vue';
+
+import { inject } from 'vue';
+const breadcrumbs = inject('breadcrumbs');
+breadcrumbs.value = [
+	{ label: 'Settings', route: '/settings' },
+	{ label: 'Profile', route: '/settings/profile' }
+];
+
+document.title = 'Settings - Profile';
+</script>
