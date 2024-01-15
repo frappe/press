5219d7d170d72606d8e2d8a3bc1fa8bcc4981c5d
Shadrak Gurupnor <30501401+shadrak98@users.noreply.github.com>
2024-01-15 14:47:43 +0530

    fix: removed enabled filter

--- press/press/doctype/team/team.py
+++ press/press/doctype/team/team.py
@@ -382,7 +382,7 @@ class Team(Document):
 
 		client = get_frappe_io_connection()
 		data = client.get_value(
-			"Partner", "start_date", {"email": self.partner_email, "enabled": 1}
+			"Partner", "start_date", {"email": self.partner_email}
 		)
 		if not data:
 			frappe.throw("Partner not found on frappe.io")
