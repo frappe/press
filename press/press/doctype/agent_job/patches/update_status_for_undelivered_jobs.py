5fba38467fd3a5134e83ec1ccd7e96e07c81969b
Saurabh <saurabh6790@gmail.com>
2024-01-16 19:01:38 +0530

    feat: patch to update agent job delivery status having id 0

---
+++ press/press/doctype/agent_job/patches/update_status_for_undelivered_jobs.py
@@ -0,0 +1,16 @@
+import frappe
+from frappe.utils import now_datetime, add_to_date
+
+
+def execute():
+	one_minute_ago = add_to_date(now_datetime(), minutes=-1)
+	print(one_minute_ago)
+
+	frappe.db.sql(
+		"""
+		UPDATE `tabAgent Job`
+			SET status = 'Delivery Failure'
+		WHERE job_id = 0 and status = 'Undelivered' and creation < %s
+	""",
+		one_minute_ago,
+	)
