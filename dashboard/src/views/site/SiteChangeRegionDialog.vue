fbb1225ec647196ed52a0fa4c48d843538c10024
Bread Genie <msuhailbh07@gmail.com>
2024-01-25 13:00:43 +0530

    refactor(SiteChangeRegion): only allow changing region and not adding region
    since we can do it from dashboard anyway

--- dashboard/src/views/site/SiteChangeRegionDialog.vue
+++ dashboard/src/views/site/SiteChangeRegionDialog.vue
@@ -4,25 +4,10 @@
 			title: 'Change Region',
 			actions: [
 				{
-					label: 'Add Region to Bench',
-					loading: $resources.addRegionToReleaseGroup.loading,
-					disabled:
-						!selectedRegion ||
-						$account.team.name !== changeRegionOptions?.data?.group_team ||
-						$resources.changeRegionOptions.data.group_regions.includes(
-							selectedRegion
-						),
-					onClick: () => $resources.addRegionToReleaseGroup.submit()
-				},
-				{
 					label: 'Change Region',
 					loading: $resources.changeRegion.loading,
 					variant: 'solid',
-					disabled:
-						!selectedRegion ||
-						!$resources.changeRegionOptions.data.group_regions.includes(
-							selectedRegion
-						),
+					disabled: !selectedRegion,
 					onClick: () =>
 						$resources.changeRegion.submit({
 							name: site?.name,
@@ -41,34 +26,39 @@
 				v-if="$resources.changeRegionOptions.loading"
 			/>
 			<div v-else>
-				<RichSelect
-					:value="selectedRegion"
-					@change="selectedRegion = $event"
+				<FormControl
+					type="autocomplete"
+					label="Choose Region"
+					v-model="selectedRegion"
 					:options="
 						$resources.changeRegionOptions.data.regions.map(r => ({
 							label: r.title || r.name,
 							value: r.name,
-							image: r.image,
-							beta: r.beta
+							image: r.image
 						}))
 					"
-				/>
+				>
+					<template #prefix>
+						<img :src="selectedRegion?.image" class="mr-2 h-4" />
+					</template>
+					<template #item-prefix="{ active, selected, option }">
+						<img v-if="option?.image" :src="option.image" class="mr-2 h-4" />
+					</template>
+				</FormControl>
 				<FormControl
 					class="mt-4"
 					v-if="
 						$resources.changeRegionOptions.data?.regions?.length > 0 &&
-						selectedRegion &&
-						$resources.changeRegionOptions.data.group_regions.includes(
-							selectedRegion
-						)
+						selectedRegion
 					"
 					label="Schedule Site Migration"
 					type="datetime-local"
 					:min="new Date().toISOString().slice(0, 16)"
 					v-model="targetDateTime"
 				/>
-				<p class="mt-4 text-sm text-gray-600">
-					{{ message }}
+				<p v-else class="mt-4 text-sm text-gray-600">
+					If the region you're looking for isn't available, please add from the
+					Bench dashboard.
 				</p>
 			</div>
 			<ErrorMessage class="mt-3" :message="$resources.changeRegion.error" />
@@ -78,15 +68,11 @@
 
 <script>
 import { notify } from '@/utils/toast';
-import RichSelect from '@/components/RichSelect.vue';
 
 export default {
 	name: 'SiteChangeRegionDialog',
 	props: ['site', 'modelValue'],
 	emits: ['update:modelValue'],
-	components: {
-		RichSelect
-	},
 	data() {
 		return {
 			targetDateTime: null,
@@ -107,27 +93,6 @@ export default {
 				this.$emit('update:modelValue', value);
 			}
 		},
-		message() {
-			if (
-				this.$resources.changeRegionOptions.data.group_regions.includes(
-					this.selectedRegion
-				)
-			) {
-				return 'Selected region is available in the bench. You can schedule the site migration to this region.';
-			} else if (
-				!this.$resources.changeRegionOptions.data.group_regions.includes(
-					this.selectedRegion
-				)
-			) {
-				if (
-					this.$resources.changeRegionOptions?.data?.group_name !==
-					this.$account.team.name
-				)
-					return 'Selected region is not available in the bench. Get in touch with support to add this region.';
-				else
-					return 'Selected region is not available in the bench. You can add this region to the bench and then schedule the site migration later.';
-			} else return '';
-		},
 		datetimeInIST() {
 			if (!this.targetDateTime) return null;
 			const datetimeInIST = this.$dayjs(this.targetDateTime)
@@ -143,43 +108,6 @@ export default {
 				url: 'press.api.site.change_region_options',
 				params: {
 					name: this.site?.name
-				},
-				onSuccess(data) {
-					if (data.regions.find(r => r.name === data.current_region))
-						this.selectedRegion = data.current_region;
-				}
-			};
-		},
-		addRegionToReleaseGroup() {
-			return {
-				url: 'press.api.site.add_region_to_group',
-				params: {
-					name: this.site?.group,
-					region: this.selectedRegion
-				},
-				validate() {
-					if (
-						this.$resources.changeRegionOptions.data.group_regions.includes(
-							this.selectedRegion
-						)
-					)
-						return 'Region is already added to the release group';
-				},
-				onSuccess(data) {
-					notify({
-						title: 'Region Added to Release Group',
-						message: `Please wait till the deploy in the region ${this.selectedRegion} is done.`,
-						color: 'green',
-						icon: 'check'
-					});
-					this.$emit('update:modelValue', false);
-					this.$router.push({
-						name: 'BenchDeploys',
-						params: {
-							benchName: this.site?.group,
-							candidateName: data
-						}
-					});
 				}
 			};
 		},
