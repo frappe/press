aed8d02f140730a858e73eb06ae86fdf2733441c
Aditya Hase <aditya@adityahase.com>
2024-02-02 16:37:20 +0530

    feat: Playbook to setup Development server

--- press/infrastructure/doctype/development_server/development_server.js
+++ press/infrastructure/doctype/development_server/development_server.js
@@ -1,8 +1,76 @@
 // Copyright (c) 2024, Frappe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Development Server", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Development Server', {
+	refresh: function (frm) {
+		[
+			[__('Ping Agent'), 'ping_agent', false, frm.doc.is_server_setup],
+			[__('Ping Ansible'), 'ping_ansible', true, !frm.doc.is_server_prepared],
+			[
+				__('Ping Ansible Unprepared'),
+				'ping_ansible_unprepared',
+				true,
+				!frm.doc.is_server_prepared,
+			],
+			[__('Update Agent'), 'update_agent', true, frm.doc.is_server_setup],
+			[
+				__('Update Agent Ansible'),
+				'update_agent_ansible',
+				true,
+				frm.doc.is_server_setup,
+			],
+			[
+				__('Prepare Server'),
+				'prepare_server',
+				true,
+				!frm.doc.is_server_prepared,
+			],
+			[__('Setup Server'), 'setup_server', true, !frm.doc.is_server_setup],
+			[
+				__('Fetch Keys'),
+				'fetch_keys',
+				false,
+				frm.doc.is_server_setup &&
+					(!frm.doc.frappe_public_key || !frm.doc.root_public_key),
+			],
+			[__('Update TLS Certificate'), 'update_tls_certificate', true],
+			[__('Archive'), 'archive', true, frm.doc.status !== 'Archived'],
+			[
+				__('Show Agent Password'),
+				'show_agent_password',
+				false,
+				frm.doc.is_server_setup,
+			],
+		].forEach(([label, method, confirm, condition]) => {
+			if (typeof condition === 'undefined' || condition) {
+				frm.add_custom_button(
+					label,
+					() => {
+						if (confirm) {
+							frappe.confirm(
+								`Are you sure you want to ${label.toLowerCase()}?`,
+								() =>
+									frm.call(method).then((r) => {
+										if (r.message) {
+											frappe.msgprint(r.message);
+										} else {
+											frm.refresh();
+										}
+									}),
+							);
+						} else {
+							frm.call(method).then((r) => {
+								if (r.message) {
+									frappe.msgprint(r.message);
+								} else {
+									frm.refresh();
+								}
+							});
+						}
+					},
+					__('Actions'),
+				);
+			}
+		});
+	},
+});
--- press/infrastructure/doctype/development_server/development_server.py
+++ press/infrastructure/doctype/development_server/development_server.py
@@ -1,8 +1,47 @@
 # Copyright (c) 2024, Frappe and contributors
 # For license information, please see license.txt
+import frappe
 
 from press.press.doctype.server.server import BaseServer
+from press.runner import Ansible
+from press.utils import log_error
 
 
 class DevelopmentServer(BaseServer):
-	pass
+	def validate(self):
+		self.validate_agent_password()
+
+	def _setup_server(self):
+		agent_password = self.get_password("agent_password")
+		agent_repository_url = self.get_agent_repository_url()
+		certificate_name = frappe.db.get_value(
+			"TLS Certificate", {"wildcard": True, "domain": self.domain}, "name"
+		)
+		certificate = frappe.get_doc("TLS Certificate", certificate_name)
+		try:
+			ansible = Ansible(
+				playbook="development.yml",
+				server=self,
+				user=self.ssh_user or "root",
+				port=self.ssh_port or 22,
+				variables={
+					"server": self.name,
+					"workers": "2",
+					"agent_password": agent_password,
+					"agent_repository_url": agent_repository_url,
+					"certificate_private_key": certificate.private_key,
+					"certificate_full_chain": certificate.full_chain,
+					"certificate_intermediate_chain": certificate.intermediate_chain,
+				},
+			)
+			play = ansible.run()
+			self.reload()
+			if play.status == "Success":
+				self.status = "Active"
+				self.is_server_setup = True
+			else:
+				self.status = "Broken"
+		except Exception:
+			self.status = "Broken"
+			log_error("Development Server Setup Exception", server=self.as_dict())
+		self.save()
