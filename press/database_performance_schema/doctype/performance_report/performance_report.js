18a31da576c2ec97984de2b2e5bcc06bd180e7a5
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-02-03 14:26:29 +0530

    feat: button to multiply billion with count and time field has been added

--- press/database_performance_schema/doctype/performance_report/performance_report.js
+++ press/database_performance_schema/doctype/performance_report/performance_report.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2024, Frappe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Performance Report", {
-// 	refresh(frm) {
+frappe.ui.form.on("Performance Report", {
+	refresh(frm) {
+		frm.add_custom_button(__('Multiply 10^9 with Count & Time Field'), function(){
+			format_fields_helper(frm.get_doc());
+			frm.refresh_fields();
+			msgprint("All the count & time fields with billion and billion_ps suffixes have been multiplied by 10^9");
+			frm.remove_custom_button(__('Multiply 10^9 with Count & Time Field'));
+    });
+	},
+});
 
-// 	},
-// });
+function format_fields_helper(fields_map){
+	for (const [key, value] of Object.entries(fields_map)) {
+		if (typeof value === 'object') {
+			format_fields_helper(value);
+		} else {
+			if (key.endsWith('_billion') || key.endsWith('_billion_ps')){
+				if (typeof value === 'number'){
+					fields_map[key] = value*1000000000;
+				}
+			}
+		}
+	}
+	return fields_map;
+}
