9f4284d43eefef868fce291f15ee612f282ee8a9
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2024-02-07 14:24:41 +0530

    chore: whitelist webform access

--- press/auth.py
+++ press/auth.py
@@ -34,6 +34,7 @@ ALLOWED_PATHS = [
 	"/api/method/validate_plan_change",
 	"/api/method/marketplace-apps",
 	"/api/method/press.www.dashboard.get_context_for_dev",
+	"/api/method/frappe.website.doctype.web_form.web_form.accept",
 ]
 
 ALLOWED_WILDCARD_PATHS = [
