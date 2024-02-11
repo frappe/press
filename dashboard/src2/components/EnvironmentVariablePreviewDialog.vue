7493d65ead53e6663202fd4bf4d6ab4c8f6ddb29
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-02-11 15:10:39 +0530

    feat: added support to edit env variables from dashboard

---
+++ dashboard/src2/components/EnvironmentVariablePreviewDialog.vue
@@ -0,0 +1,35 @@
+<template>
+	<Dialog
+		:options="{
+			title: 'Config Environment Variable',
+		}"
+		v-model="showDialog"
+	>
+		<template #body-content>
+			<pre
+				class="overflow-x-scroll rounded bg-gray-100 p-4 text-base"
+				v-html="configEnvironmentVariable"
+			></pre>
+		</template>
+	</Dialog>
+</template>
+
+<script>
+export default {
+	props: ['environment_variables'],
+	data() {
+		return {
+			showDialog: true
+		};
+	},
+	computed: {
+		configEnvironmentVariable() {
+			let obj = {};
+			for (let d of this.environment_variables) {
+				obj[d.key] = d.value;
+			}
+			return JSON.stringify(obj, null, '&nbsp; ');
+		}
+	}
+};
+</script>
--- press/press/doctype/release_group_variable/release_group_variable.py
+++ press/press/doctype/release_group_variable/release_group_variable.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2023, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ReleaseGroupVariable(Document):
-	pass
+	dashboard_fields = ["key", "value"]
+
+	@staticmethod
+	def get_list_query(query, filters=None, **list_args):
+		Config = frappe.qb.DocType("Release Group Variable")
+		query = query.where(Config.internal == 0).orderby(Config.key, order=frappe.qb.asc)
+		configs = query.run(as_dict=True)
+		return configs
