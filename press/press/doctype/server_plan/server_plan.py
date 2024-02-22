fb23a2eb49bb4996a7bbb43944dabd4da94cda80
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2024-02-22 16:56:30 +0530

    fix: Broken usage record creation for server plans

--- press/press/doctype/server_plan/server_plan.py
+++ press/press/doctype/server_plan/server_plan.py
@@ -2,8 +2,8 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from press.press.doctype.site_plan.plan import Plan
 
 
-class ServerPlan(Document):
+class ServerPlan(Plan):
 	pass
