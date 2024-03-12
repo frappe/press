b61d5c9e667fa3a6f1c402c046d058db6e42f2eb
Faris Ansari <netchamp.faris@gmail.com>
2024-03-12 14:54:49 +0530

    fix: fetch plans when getPlans runs

--- dashboard/src2/data/plans.js
+++ dashboard/src2/data/plans.js
@@ -4,8 +4,8 @@ export let plans = createResource({
 	url: 'press.api.site.get_plans',
 	cache: 'site.plans'
 });
-plans.fetch();
 
 export function getPlan(planName) {
+	if (!plans.data) plans.fetch();
 	return (plans.data || []).find(plan => plan.name === planName);
 }
