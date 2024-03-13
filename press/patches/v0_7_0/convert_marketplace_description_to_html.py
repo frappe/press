d652735532c7706845f096df9c1dd0c6f015866a
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2024-03-13 14:03:56 +0530

    revert: Use md_to_html instead of markdown lib
    * reverted some changes from https://github.com/frappe/press/pull/1553

--- press/patches/v0_7_0/convert_marketplace_description_to_html.py
+++ press/patches/v0_7_0/convert_marketplace_description_to_html.py
@@ -1,13 +1,9 @@
 import frappe
-import markdown
 
 
 def execute():
 	apps = frappe.get_all("Marketplace App", fields=["name", "long_description"])
 
 	for app in apps:
-		text = app["long_description"].replace("'", "'")
-		html = markdown.markdown(text)
-		html = html.replace("\n", "")
-
+		html = frappe.utils.md_to_html(app["long_description"])
 		frappe.db.set_value("Marketplace App", app["name"], "long_description", html)
