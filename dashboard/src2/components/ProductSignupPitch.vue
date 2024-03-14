5c63a1eca14b672b8798bcea1b0e78c7bb48921d
Faris Ansari <netchamp.faris@gmail.com>
2024-03-14 12:04:50 +0530

    fix: remove layout restrictions for product pitch

--- dashboard/src2/components/ProductSignupPitch.vue
+++ dashboard/src2/components/ProductSignupPitch.vue
@@ -1,8 +1,9 @@
 <template>
 	<div class="shrink-0 bg-white" v-if="saasProduct">
-		<div class="max-w-[480px]">
-			<div class="prose prose-sm mt-4" v-html="saasProduct.description" />
-		</div>
+		<div
+			class="prose prose-sm mt-4 max-w-none"
+			v-html="saasProduct.description"
+		/>
 	</div>
 </template>
 <script>
