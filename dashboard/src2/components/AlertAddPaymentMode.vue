e6a5b008bba2c05280c181d88598fd7670e126ec
Bread Genie <msuhailbh07@gmail.com>
2024-03-22 11:30:45 +0530

    Revert "fix(AlertPaymentBanner): route to billing if onbarding is done"
    This reverts commit d1649557399c0b86960d5d66f77359159b08b048.

--- dashboard/src2/components/AlertAddPaymentMode.vue
+++ dashboard/src2/components/AlertAddPaymentMode.vue
@@ -3,11 +3,7 @@
 		title="Add a payment mode to start creating sites, benches, and more."
 		type="warning"
 	>
-		<Button
-			class="ml-auto"
-			:route="$team.doc.onboarding.complete ? '/billing' : '/welcome'"
-			variant="outline"
-		>
+		<Button class="ml-auto" route="/welcome" variant="outline">
 			Add payment mode
 		</Button>
 	</AlertBanner>
