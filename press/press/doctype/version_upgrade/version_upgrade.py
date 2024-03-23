f112a3e56bfa3569fbb5674512d56386cb51d24d
Ankush Menat <ankush@frappe.io>
2024-03-23 22:11:58 +0530

    chore: keep actual fix

--- press/press/doctype/version_upgrade/version_upgrade.py
+++ press/press/doctype/version_upgrade/version_upgrade.py
@@ -181,7 +181,7 @@ def update_from_site_update():
 			version_upgrade.save()
 			frappe.db.commit()
 		except Exception as e:
-			frappe.log_error(f"Error while updating Version Upgrade {version_upgrade.name}", e)
+			frappe.log_error(f"Error while updating Version Upgrade {version_upgrade.name}")
 			frappe.db.rollback()
 
 
