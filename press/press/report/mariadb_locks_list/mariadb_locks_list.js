195f6f3c21d0e5edf6729eb33bab4704e1977eb1
Ankush Menat <ankush@frappe.io>
2024-03-24 18:41:53 +0530

    fix: use after_refresh hook on reports

--- press/press/report/mariadb_locks_list/mariadb_locks_list.js
+++ press/press/report/mariadb_locks_list/mariadb_locks_list.js
@@ -2,19 +2,19 @@
 // For license information, please see license.txt
 
 frappe.query_reports['MariaDB Locks List'] = {
-	after_datatable_render() {
-		let should_poll = frappe.query_report.get_filter_value('poll');
-		if (!should_poll) return;
+	after_refresh(report) {
+		let should_poll = report.get_filter_value('poll');
+		if (!should_poll || report.polling_interval) return;
 
 		frappe.toast(
 			'This report will be auto-refreshed every 5 seconds till we find a lock wait.',
 		);
 
-		frappe.query_report.polling_interval = setInterval(() => {
-			if (!frappe.query_report.data.length) {
-				frappe.query_report.refresh();
+		report.polling_interval = setInterval(() => {
+			if (!report.data.length) {
+				report.refresh();
 			} else {
-				clearInterval(frappe.query_report.polling_interval);
+				clearInterval(report.polling_interval);
 			}
 		}, 5000);
 	},
