e682e883aae604aae4777ce70ceaa9df62104f00
Ankush Menat <ankush@frappe.io>
2024-03-24 12:11:53 +0530

    chore: remove bad default filters

--- press/press/report/mariadb_locks_list/mariadb_locks_list.js
+++ press/press/report/mariadb_locks_list/mariadb_locks_list.js
@@ -1,6 +1,4 @@
 // Copyright (c) 2024, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.query_reports['MariaDB Locks List'] = {
-	filters: [],
-};
+frappe.query_reports['MariaDB Locks List'] = {};
