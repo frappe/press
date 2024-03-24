dcf2808e05667323dee6664c6a55a71d1846efd7
Ankush Menat <ankush@frappe.io>
2024-03-24 12:56:44 +0530

    feat: poll lock timeout report

--- press/press/report/mariadb_locks_list/mariadb_locks_list.js
+++ press/press/report/mariadb_locks_list/mariadb_locks_list.js
@@ -1,4 +1,21 @@
 // Copyright (c) 2024, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.query_reports['MariaDB Locks List'] = {};
+frappe.query_reports['MariaDB Locks List'] = {
+	after_datatable_render() {
+		let should_poll = frappe.query_report.get_filter_value('poll');
+		if (!should_poll) return;
+
+		frappe.toast(
+			'This report will be auto-refreshed every 5 seconds till we find a lock wait.',
+		);
+
+		frappe.query_report.polling_interval = setInterval(() => {
+			if (!frappe.query_report.data.length) {
+				frappe.query_report.refresh();
+			} else {
+				clearInterval(frappe.query_report.polling_interval);
+			}
+		}, 5000);
+	},
+};
