9fd5c21e52b203d0ba40801364f9109f2bd32f30
Suhail <63963181+BreadGenie@users.noreply.github.com>
2024-03-26 17:53:04 +0530

    refactor(ServerOverview): minor revamp (#1633)
    * refactor(ServerOverview): minor revamp
    
    * docs: link load average docs
    
    * refactor: proper colors and sort averages
    
    * fix: type error

--- dashboard/src2/components/server/ServerOverview.vue
+++ dashboard/src2/components/server/ServerOverview.vue
@@ -3,52 +3,55 @@
 		v-if="$appServer?.doc"
 		class="grid grid-cols-1 items-start gap-5 sm:grid-cols-2"
 	>
-		<div class="flex flex-col space-y-4">
-			<div class="rounded-md border">
-				<div class="flex h-12 items-center justify-between border-b px-5">
-					<h2 class="text-lg font-medium text-gray-900">
-						Application Server Plan
-					</h2>
-					<Button @click="showPlanChangeDialog('Server')"> Change </Button>
-				</div>
-				<div v-if="$appServer.doc.current_plan">
+		<div
+			v-for="server in ['Server', 'Database Server']"
+			class="col-span-1 rounded-md border lg:col-span-2"
+		>
+			<div class="grid grid-cols-2 lg:grid-cols-4">
+				<template v-for="(d, i) in currentUsage(server)" :key="d.value">
 					<div
-						v-for="d in current_usage('Server')"
-						:key="d.label"
-						class="flex items-center px-5 py-3 last:pb-5 even:bg-gray-50/70"
+						class="border-b p-5 lg:border-b-0"
+						:class="{ 'border-r': i + 1 != currentUsage(server).length }"
 					>
-						<div class="w-1/3 text-base text-gray-700">{{ d.label }}</div>
-						<div class="w-2/3 text-base font-medium" v-html="d.value" />
+						<div
+							v-if="d.type === 'info'"
+							class="flex items-center justify-between"
+						>
+							<div
+								v-if="d.type === 'info'"
+								class="mt-2 flex flex-col space-y-2"
+							>
+								<div class="text-base text-gray-700">{{ d.label }}</div>
+								<div>
+									<div class="text-base text-gray-900">
+										{{ d.value }}
+									</div>
+								</div>
+							</div>
+							<Button
+								v-if="d.type === 'info'"
+								@click="showPlanChangeDialog(server)"
+								label="Change"
+							/>
+						</div>
+						<div v-else-if="d.type === 'progress'">
+							<div class="text-base text-gray-700">{{ d.label }}</div>
+							<div class="mt-2">
+								<Progress size="md" :value="d.progress_value" />
+								<div>
+									<div class="mt-2 flex justify-between">
+										<div class="text-sm text-gray-600">
+											{{ d.value }}
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div v-else class="flex items-center justify-center p-3">
-					<div class="text-base text-gray-700">No Plan Selected</div>
-				</div>
-			</div>
-			<div class="rounded-md border">
-				<div class="flex h-12 items-center justify-between border-b px-5">
-					<h2 class="text-lg font-medium text-gray-900">
-						Database Server Plan
-					</h2>
-					<Button @click="showPlanChangeDialog('Database Server')">
-						Change
-					</Button>
-				</div>
-				<div v-if="$dbServer.doc?.current_plan">
-					<div
-						v-for="d in current_usage('Database Server')"
-						:key="d.label"
-						class="flex items-center px-5 py-3 last:pb-5 even:bg-gray-50/70"
-					>
-						<div class="w-1/3 text-base text-gray-700">{{ d.label }}</div>
-						<div class="w-2/3 text-base font-medium" v-html="d.value" />
-					</div>
-				</div>
-				<div v-else class="flex items-center justify-center p-3">
-					<div class="text-base text-gray-700">No Plan Selected</div>
-				</div>
+				</template>
 			</div>
 		</div>
+
 		<div class="rounded-md border">
 			<div class="h-12 border-b px-5 py-4">
 				<h2 class="text-lg font-medium text-gray-900">Server Information</h2>
@@ -64,6 +67,8 @@
 				</div>
 			</div>
 		</div>
+
+		<ServerLoadAverage :server="server" />
 	</div>
 </template>
 
@@ -72,11 +77,13 @@ import { h, defineAsyncComponent } from 'vue';
 import { getCachedDocumentResource } from 'frappe-ui';
 import { renderDialog } from '../../utils/components';
 import ServerPlansDialog from './ServerPlansDialog.vue';
+import ServerLoadAverage from './ServerLoadAverage.vue';
 import { getDocResource } from '../../utils/resource';
 
 export default {
 	props: ['server'],
 	components: {
+		ServerLoadAverage,
 		ServerPlansDialog
 	},
 	methods: {
@@ -94,7 +101,10 @@ export default {
 				})
 			);
 		},
-		current_usage(serverType) {
+		currentUsage(serverType) {
+			if (!this.$appServer.doc) return [];
+			if (!this.$dbServer.doc) return [];
+
 			let formatBytes = v => this.$format.bytes(v, 0, 2);
 
 			let currentPlan =
@@ -114,9 +124,9 @@ export default {
 			let planDescription = '';
 			if (currentPlan.price_usd > 0) {
 				if (this.$team.doc.currency === 'INR') {
-					planDescription = `₹${currentPlan.price_inr} /month (₹${currentPlan.price_per_day_inr} /day)`;
+					planDescription = `₹${currentPlan.price_inr} /month`;
 				} else {
-					planDescription = `$${currentPlan.price_usd} /month ($${currentPlan.price_per_day_usd} /day)`;
+					planDescription = `$${currentPlan.price_usd} /month`;
 				}
 			} else {
 				planDescription = currentPlan.plan_title;
@@ -124,32 +134,38 @@ export default {
 
 			return [
 				{
-					label: 'Current Plan',
-					value: planDescription
+					label:
+						serverType === 'Server'
+							? 'Application Server Plan'
+							: 'Database Server Plan',
+					value: planDescription,
+					type: 'info'
 				},
 				{
 					label: 'CPU',
+					type: 'progress',
+					progress_value: (currentUsage.vcpu / currentPlan.vcpu) * 100,
 					value: `${currentUsage.vcpu || 0} vCPU / ${
 						currentPlan.vcpu
 					} ${this.$format.plural(currentPlan.vcpu, 'vCPU', 'vCPUs')}`
 				},
 				{
 					label: 'Memory',
+					type: 'progress',
+					progress_value: (currentUsage.memory / currentPlan.memory) * 100,
 					value: `${formatBytes(currentUsage.memory || 0)} / ${formatBytes(
 						currentPlan.memory
 					)}`
 				},
 				{
 					label: 'Storage',
+					type: 'progress',
+					progress_value:
+						(currentUsage.disk / (diskSize ? diskSize : currentPlan.disk)) *
+						100,
 					value: `${currentUsage.disk || 0} GB / ${
 						diskSize ? diskSize : currentPlan.disk
-					} GB ${
-						diskSize
-							? `<span class="text-gray-600">(${currentPlan.disk} GB + ${
-									diskSize - currentPlan.disk
-							  }) GB</span>`
-							: ''
-					}`
+					} GB`
 				}
 			];
 		}
