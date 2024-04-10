34bc7d54910c34360e3329d42dbc72ad448aa21f
Saurabh <saurabh6790@gmail.com>
2024-04-10 13:06:50 +0530

    chore: commit server records while setting hybrid servers

--- press/press/doctype/self_hosted_server/self_hosted_server.py
+++ press/press/doctype/self_hosted_server/self_hosted_server.py
@@ -369,6 +369,8 @@ class SelfHostedServer(Document):
 			if not frappe.flags.in_test:
 				db_server.create_dns_record()
 
+			frappe.db.commit()
+
 			frappe.msgprint(f"Databse server record {db_server.name} created")
 		except Exception:
 			frappe.throw("Adding Server to Database Server Doctype failed")
@@ -444,6 +446,8 @@ class SelfHostedServer(Document):
 
 			if not frappe.flags.in_test:
 				server.create_dns_record()
+
+			frappe.db.commit()
 
 		except Exception as e:
 			self.status = "Broken"
