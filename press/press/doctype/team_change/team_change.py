a471182d92336593857eb586ad469d8afbb727a5
Bread Genie <msuhailbh07@gmail.com>
2024-04-10 22:16:34 +0530

    fix(site): unset `notify_email` on team change

--- press/press/doctype/team_change/team_change.py
+++ press/press/doctype/team_change/team_change.py
@@ -29,7 +29,10 @@ class TeamChange(Document):
 
 	def on_update(self):
 		if self.document_type == "Site" and self.transfer_completed:
-			frappe.db.set_value("Site", self.document_name, "team", self.to_team)
+			notify_email = frappe.get_value("Team", self.to_team, "user")
+			frappe.db.set_value(
+				"Site", self.document_name, {"team": self.to_team, "notify_email": notify_email}
+			)
 			frappe.db.set_value(
 				"Subscription",
 				{"document_name": self.document_name},
