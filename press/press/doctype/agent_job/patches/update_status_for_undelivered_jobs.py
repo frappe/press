df50f7e2cf952ed1395433e0a0fffa4869d30622
Saurabh <saurabh6790@gmail.com>
2024-04-23 23:05:47 +0530

    chore: re-execute patch

--- press/press/doctype/agent_job/patches/update_status_for_undelivered_jobs.py
+++ press/press/doctype/agent_job/patches/update_status_for_undelivered_jobs.py
@@ -3,7 +3,7 @@ from frappe.utils import now_datetime, add_to_date
 
 
 def execute():
-	one_minute_ago = add_to_date(now_datetime(), minutes=-1)
+	five_minute_ago = add_to_date(now_datetime(), minutes=-5)
 
 	frappe.db.sql(
 		"""
@@ -11,5 +11,5 @@ def execute():
 			SET status = 'Delivery Failure'
 		WHERE job_id = 0 and status = 'Undelivered' and creation <= %s
 	""",
-		one_minute_ago,
+		five_minute_ago,
 	)
