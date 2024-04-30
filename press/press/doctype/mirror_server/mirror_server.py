fcc0209fbc7cd0bbe7ae0f50437fe6c40300fd3b
Saurabh <saurabh6790@gmail.com>
2024-04-30 12:04:04 +0530

    feat: setup mirror server

---
+++ press/press/doctype/mirror_server/mirror_server.py
@@ -0,0 +1,93 @@
+# Copyright (c) 2024, Frappe and contributors
+# For license information, please see license.txt
+
+import frappe
+
+
+from press.press.doctype.server.server import BaseServer
+from press.runner import Ansible
+from press.utils import log_error
+
+
+class MirrorServer(BaseServer):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		agent_password: DF.Password | None
+		cluster: DF.Link | None
+		domain: DF.Link | None
+		frappe_public_key: DF.Code | None
+		frappe_user_password: DF.Data | None
+		hostname: DF.Data | None
+		is_server_setup: DF.Check
+		os: DF.Data | None
+		os_version: DF.Data | None
+		private_ip: DF.Data | None
+		provider: DF.Literal["Generic", "Scaleway", "AWS EC2", "OCI"]
+		public_ip: DF.Data | None
+		root_public_key: DF.Code | None
+		ssh_port: DF.Int
+		ssh_user: DF.Data | None
+		status: DF.Literal["Pending", "Installing", "Active", "Broken", "Archived"]
+		virtual_machine: DF.Link | None
+	# end: auto-generated types
+
+	def validate(self):
+		self.validate_agent_password()
+
+	def _setup_server(self):
+		agent_password = self.get_password("agent_password")
+		agent_repository_url = self.get_agent_repository_url()
+		certificate_name = frappe.db.get_value(
+			"TLS Certificate", {"wildcard": True, "domain": self.domain}, "name"
+		)
+		certificate = frappe.get_doc("TLS Certificate", certificate_name)
+		monitoring_password = frappe.get_doc("Cluster", self.cluster).get_password(
+			"monitoring_password"
+		)
+
+		log_server = frappe.db.get_single_value("Press Settings", "log_server")
+		if log_server:
+			kibana_password = frappe.get_doc("Log Server", log_server).get_password(
+				"kibana_password"
+			)
+		else:
+			kibana_password = None
+
+		try:
+			ansible = Ansible(
+				playbook="mirrors.yml",
+				server=self,
+				user=self.ssh_user or "root",
+				port=self.ssh_port or 22,
+				variables={
+					"server": self.name,
+					"workers": "2",
+					"agent_password": agent_password,
+					"agent_repository_url": agent_repository_url,
+					"monitoring_password": monitoring_password,
+					"log_server": log_server,
+					"kibana_password": kibana_password,
+					"private_ip": self.private_ip,
+					"certificate_private_key": certificate.private_key,
+					"certificate_full_chain": certificate.full_chain,
+					"certificate_intermediate_chain": certificate.intermediate_chain,
+				},
+			)
+			play = ansible.run()
+			self.reload()
+			if play.status == "Success":
+				self.status = "Active"
+				self.is_server_setup = True
+
+			else:
+				self.status = "Broken"
+		except Exception:
+			self.status = "Broken"
+			log_error("Database Server Setup Exception", server=self.as_dict())
+		self.save()
