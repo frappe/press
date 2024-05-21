31580ea3ed206bf09ec09edb3af1eb54040f7262
Aditya Hase <aditya@adityahase.com>
2024-05-21 17:06:57 +0530

    feat(telegram): Telegram Message DocType to queue message

---
+++ press/press/doctype/telegram_message/telegram_message.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2024, Frappe and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TelegramMessage(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		group: DF.Data | None
+		message: DF.Code
+		parse_mode: DF.Literal["Markdown", "HTML"]
+		priority: DF.Literal["High", "Medium", "Low"]
+		status: DF.Literal["Queued", "Sent", "Error", "Cancelled", "Skipped"]
+		topic: DF.Data | None
+	# end: auto-generated types
+
+	pass
