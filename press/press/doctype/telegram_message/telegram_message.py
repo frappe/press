896464fbe96dae34d4a589c928624be3e4dc7254
Aditya Hase <aditya@adityahase.com>
2024-05-22 16:32:06 +0530

    fix(telegram): Send one message at a time and log error

--- press/press/doctype/telegram_message/telegram_message.py
+++ press/press/doctype/telegram_message/telegram_message.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2024, Frappe and contributors
 # For license information, please see license.txt
 
+import traceback
+
 import frappe
 from frappe.model.document import Document
+from press.telegram_utils import Telegram
 
 
 class TelegramMessage(Document):
@@ -14,6 +17,7 @@ class TelegramMessage(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		error: DF.Code | None
 		group: DF.Data | None
 		message: DF.Code
 		parse_mode: DF.Literal["Markdown", "HTML"]
@@ -21,6 +25,20 @@ class TelegramMessage(Document):
 		status: DF.Literal["Queued", "Sent", "Error", "Cancelled", "Skipped"]
 		topic: DF.Data | None
 	# end: auto-generated types
+
+	def send(self):
+		try:
+			telegram = Telegram()
+			telegram.send(self.message)
+			self.status = "Sent"
+		except Exception:
+			# It's unlinkely that this error will be resolved by retrying
+			# Fail immediately
+			self.error = traceback.format_exc()
+			self.status = "Error"
+			raise
+		finally:
+			self.save()
 
 	@staticmethod
 	def enqueue(
@@ -34,4 +52,31 @@ class TelegramMessage(Document):
 			}
 		).insert(ignore_permissions=True)
 
-	pass
+	@staticmethod
+	def get_one() -> "TelegramMessage | None":
+		first = frappe.get_all(
+			"Telegram Message",
+			filters={"status": "Queued"},
+			limit=1,
+			pluck="name",
+		)
+		if first:
+			return frappe.get_doc("Telegram Message", first[0])
+
+	@staticmethod
+	def send_one() -> None:
+		message = TelegramMessage.get_one()
+		if message:
+			return message.send()
+
+
+def send_telegram_message():
+	"""Send one queued telegram message"""
+
+	while message := TelegramMessage.get_one():
+		try:
+			message.send()
+			return
+		except Exception:
+			# Try next message
+			pass
--- press/press/doctype/telegram_message/test_telegram_message.py
+++ press/press/doctype/telegram_message/test_telegram_message.py
@@ -5,7 +5,10 @@ from unittest.mock import Mock, patch
 
 import frappe
 from frappe.tests.utils import FrappeTestCase
-from press.press.doctype.telegram_message.telegram_message import TelegramMessage
+from press.press.doctype.telegram_message.telegram_message import (
+	TelegramMessage,
+	send_telegram_message,
+)
 from press.telegram_utils import Telegram
 
 
@@ -26,3 +29,25 @@ class TestTelegramMessage(FrappeTestCase):
 		"""Test if enqueue method creates Telegram Message with Queued status"""
 		message = TelegramMessage.enqueue(message="Test Message")
 		self.assertEqual(message.status, "Queued")
+
+	def test_send_calls_telegram_send(self, mock_send: Mock):
+		"""Test if send method calls Telegram send method"""
+		TelegramMessage.enqueue(message="Test Message")
+		send_telegram_message()
+		mock_send.assert_called_once()
+
+	def test_successful_send_call_sets_sent_status(self, mock_send: Mock):
+		"""Test if successful send call sets status to Sent"""
+		first = TelegramMessage.enqueue(message="Test Message")
+		send_telegram_message()
+		first.reload()
+		self.assertEqual(first.status, "Sent")
+
+	def test_failed_send_call_sets_error_status(self, mock_send: Mock):
+		"""Test if failed send call sets status to Error"""
+		mock_send.side_effect = Exception()
+		first = TelegramMessage.enqueue(message="Test Message")
+		self.assertRaises(Exception, TelegramMessage.send_one)
+		first.reload()
+		self.assertEqual(first.status, "Error")
+		self.assertIn("Exception", first.error)
