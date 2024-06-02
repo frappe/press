f978df15b54eec283de7f16d6363e6072a35b77c
Bread Genie <msuhailbh07@gmail.com>
2024-06-02 17:07:40 +0530

    test(press-role): add bench

--- press/press/doctype/press_role/test_press_role.py
+++ press/press/doctype/press_role/test_press_role.py
@@ -98,7 +98,7 @@ class TestPressRole(FrappeTestCase):
 		frappe.set_user(self.team_user.name)
 
 		# permission for site1 added in the role
-		self.assertEqual(get_list("Site"), [{"name": site1.name}])
+		self.assertEqual(get_list("Site"), [{"name": site1.name, "bench": site1.bench}])
 
 		frappe.set_user("Administrator")
 		perm2 = frappe.new_doc("Press Role Permission")
@@ -109,7 +109,13 @@ class TestPressRole(FrappeTestCase):
 		frappe.set_user(self.team_user.name)
 
 		# permission for site2 added in another role
-		self.assertCountEqual(get_list("Site"), [{"name": site1.name}, {"name": site2.name}])
+		self.assertCountEqual(
+			get_list("Site"),
+			[
+				{"name": site1.name, "bench": site1.bench},
+				{"name": site2.name, "bench": site2.bench},
+			],
+		)
 
 	def test_get_with_permissions(self):
 		from press.api.client import get
