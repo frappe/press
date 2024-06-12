d33e182502dd7ddea211ed7e3d67668751696a0a
Bread Genie <msuhailbh07@gmail.com>
2024-06-12 12:33:38 +0530

    refactor(Billing): guide user to add another card on certain errors

--- dashboard/src2/components/StripeCard.vue
+++ dashboard/src2/components/StripeCard.vue
@@ -32,7 +32,7 @@
 				/>
 			</div>
 
-			<div class="mt-3" v-show="tryingMicroCharge">
+			<div class="mt-3 space-y-4" v-show="tryingMicroCharge">
 				<p class="text-base text-gray-700">
 					We are attempting to charge your card with
 					<strong>{{ formattedMicroChargeAmount }}</strong> to make sure the
@@ -40,14 +40,34 @@
 					account.
 				</p>
 
-				<Button class="mt-2" :loading="true">Attempting Test Charge</Button>
+				<Button
+					:loading="!microChargeCompleted"
+					:loadingText="'Attempting Test Charge'"
+				>
+					Test Charge Initiated
+					<template #prefix>
+						<GreenCheckIcon class="h-4 w-4" />
+					</template>
+				</Button>
 			</div>
 
 			<ErrorMessage class="mt-2" :message="errorMessage" />
 
 			<div class="mt-6 flex items-center justify-between">
 				<StripeLogo />
-				<Button variant="solid" @click="submit" :loading="addingCard">
+				<Button
+					@click="clearForm"
+					v-if="showAddAnotherCardButton"
+					iconLeft="plus"
+				>
+					Add Another Card
+				</Button>
+				<Button
+					v-else-if="!tryingMicroCharge"
+					variant="solid"
+					@click="submit"
+					:loading="addingCard"
+				>
 					Save Card
 				</Button>
 			</div>
@@ -82,15 +102,13 @@ export default {
 			},
 			gstNotApplicable: false,
 			addingCard: false,
-			tryingMicroCharge: false
+			tryingMicroCharge: false,
+			showAddAnotherCardButton: false,
+			microChargeCompleted: false
 		};
 	},
 	async mounted() {
-		await this.$resources.setupIntent.submit();
-
-		let { first_name, last_name = '' } = this.$team.doc.user_info;
-		let fullname = first_name + ' ' + last_name;
-		this.billingInformation.cardHolderName = fullname.trimEnd();
+		await this.setupStripeIntent();
 	},
 	resources: {
 		setupIntent() {
@@ -177,6 +195,13 @@ export default {
 		}
 	},
 	methods: {
+		async setupStripeIntent() {
+			await this.$resources.setupIntent.submit();
+
+			let { first_name, last_name = '' } = this.$team.doc.user_info;
+			let fullname = first_name + ' ' + last_name;
+			this.billingInformation.cardHolderName = fullname.trimEnd();
+		},
 		async submit() {
 			this.addingCard = true;
 
@@ -213,9 +238,20 @@ export default {
 
 			if (error) {
 				this.addingCard = false;
+				let declineCode = error.decline_code;
 				let errorMessage = error.message;
+
+				if (declineCode === 'do_not_honor') {
+					this.errorMessage =
+						"Your card was declined. It might be due to insufficient funds or you might've exceeded your daily limit. Please try with another card or contact your bank.";
+					this.showAddAnotherCardButton = true;
+				} else if (declineCode === 'transaction_not_allowed') {
+					this.errorMessage =
+						'Your card was declined. It might be due to restrictions on your card, like international transactions or online payments. Please try with another card or contact your bank.';
+					this.showAddAnotherCardButton = true;
+				}
 				// fix for duplicate error message
-				if (errorMessage != 'Your card number is incomplete.') {
+				else if (errorMessage != 'Your card number is incomplete.') {
 					this.errorMessage = errorMessage;
 				}
 			} else {
@@ -270,9 +306,8 @@ export default {
 							payment_method: { card: this.card }
 						});
 						if (payload.paymentIntent.status === 'succeeded') {
-							this.$emit('complete');
+							this.microChargeCompleted = true;
 						}
-						this.tryingMicroCharge = false;
 					},
 					onError: error => {
 						console.error(error);
@@ -287,6 +322,13 @@ export default {
 				d => d.name === country
 			).code;
 			return code.toUpperCase();
+		},
+		async clearForm() {
+			this.ready = false;
+			this.errorMessage = null;
+			this.showAddAnotherCardButton = false;
+			this.card = null;
+			this.setupStripeIntent();
 		}
 	},
 	computed: {
