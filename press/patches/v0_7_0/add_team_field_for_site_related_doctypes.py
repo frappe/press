ec06e4e698f74f7ec7e3adee6c72574ffba73e2d
Bread Genie <msuhailbh07@gmail.com>
2024-06-14 10:41:58 +0530

    refactor: faster patch

--- press/patches/v0_7_0/add_team_field_for_site_related_doctypes.py
+++ press/patches/v0_7_0/add_team_field_for_site_related_doctypes.py
@@ -10,14 +10,6 @@ def execute():
 	sites = frappe.get_all("Site", fields=["name", "team"])
 
 	for site in tqdm(sites):
-		site_updates = frappe.get_all("Site Update", filters={"site": site["name"]})
-		for site_update in site_updates:
-			frappe.db.set_value("Site Update", site_update["name"], "team", site["team"])
-
-		site_backups = frappe.get_all("Site Backup", filters={"site": site["name"]})
-		for site_backup in site_backups:
-			frappe.db.set_value("Site Backup", site_backup["name"], "team", site["team"])
-
-		site_migrations = frappe.get_all("Site Migration", filters={"site": site["name"]})
-		for site_migration in site_migrations:
-			frappe.db.set_value("Site Migration", site_migration["name"], "team", site["team"])
+		frappe.db.set_value("Site Update", {"site": site["name"]}, "team", site["team"])
+		frappe.db.set_value("Site Backup", {"site": site["name"]}, "team", site["team"])
+		frappe.db.set_value("Site Activity", {"site": site["name"]}, "team", site["team"])
