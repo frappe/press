9367644bccfd5f8380a5cdf4cf900ec0f14b16e7
Bread Genie <msuhailbh07@gmail.com>
2024-06-14 11:05:19 +0530

    fix: patch only non archived sites

--- press/patches/v0_7_0/add_team_field_for_site_related_doctypes.py
+++ press/patches/v0_7_0/add_team_field_for_site_related_doctypes.py
@@ -7,7 +7,9 @@ from tqdm import tqdm
 
 
 def execute():
-	sites = frappe.get_all("Site", fields=["name", "team"])
+	sites = frappe.get_all(
+		"Site", fields=["name", "team"], filters={"status": ("!=", "Archived")}
+	)
 
 	for site in tqdm(sites):
 		frappe.db.set_value("Site Update", {"site": site["name"]}, "team", site["team"])
