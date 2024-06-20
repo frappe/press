e1a602c5fc01e84ba33f4725eea0c776850b2fc8
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2024-06-20 12:35:27 +0530

    feat: show alert banner if billing address is incomplete

---
+++ dashboard/src2/components/AlertAddressDetails.vue
@@ -0,0 +1,18 @@
+<template>
+	<AlertBanner
+		title="Please add your billing address details to complete your Frappe Cloud profile."
+		type="warning"
+	>
+		<Button class="ml-auto" route="/billing" variant="outline">
+			Update Address
+		</Button>
+	</AlertBanner>
+</template>
+<script>
+import AlertBanner from './AlertBanner.vue';
+
+export default {
+	name: 'AlertAddressDetails',
+	components: { AlertBanner }
+};
+</script>
