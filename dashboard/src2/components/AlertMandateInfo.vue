dfc4985267cd5dffbbc204c7278fec8a6d72c2de
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2024-06-21 21:55:12 +0530

    fix: update the message

--- dashboard/src2/components/AlertMandateInfo.vue
+++ dashboard/src2/components/AlertMandateInfo.vue
@@ -1,18 +1,34 @@
 <template>
-	<AlertBanner
-		title="Due to RBI regulations, we need to collect a mandate/consent from you to process payments from your card. Please remove and add your card again. Contact support for any further details."
-		type="warning"
-	>
-		<Button class="ml-auto shrink-0" route="/billing" variant="outline">
-			Update Card
-		</Button>
-	</AlertBanner>
+	<div>
+		<AlertBanner
+			title="Due to RBI regulations, we need to collect a mandate/consent from you to process payments from your card."
+			type="warning"
+		>
+			<Button
+				class="ml-auto shrink-0"
+				@click="showAddCardDialog = true"
+				variant="outline"
+			>
+				Update Card
+			</Button>
+		</AlertBanner>
+		<StripeCardDialog
+			v-model="showAddCardDialog"
+			message="Due to RBI regulations, we need to collect mandate/consent from Indian customers to process auto debit from your card. Please add your card again. Contact support@frappe.io for further details."
+		/>
+	</div>
 </template>
 <script>
 import AlertBanner from './AlertBanner.vue';
+import StripeCardDialog from './StripeCardDialog.vue';
 
 export default {
 	name: 'AlertMandateInfo',
-	components: { AlertBanner }
+	components: { AlertBanner, StripeCardDialog },
+	data() {
+		return {
+			showAddCardDialog: false
+		};
+	}
 };
 </script>
