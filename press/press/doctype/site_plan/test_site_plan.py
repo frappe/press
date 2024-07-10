c29a38728cc612290f34b307e4f8e9b0701fef22
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-07-10 15:24:58 +0530

    tests: ad testcase to check auto update of provate bench

--- press/press/doctype/site_plan/test_site_plan.py
+++ press/press/doctype/site_plan/test_site_plan.py
@@ -5,6 +5,7 @@ from frappe.tests.utils import FrappeTestCase
 
 from datetime import date
 from unittest.mock import patch
+from typing import Optional
 
 import frappe
 from frappe.model.naming import make_autoname
@@ -18,6 +19,8 @@ def create_test_plan(
 	plan_title: str = None,
 	plan_name: str = None,
 	allow_downgrading_from_other_plan: bool = True,
+	allowed_apps: Optional[list[str]] = None,
+	release_groups: Optional[list[str]] = None,
 ):
 	"""Create test Plan doc."""
 	plan_name = plan_name or f"Test {document_type} plan {make_autoname('.#')}"
@@ -35,7 +38,15 @@ def create_test_plan(
 			"disk": 50,
 			"instance_type": "t2.micro",
 		}
-	).insert(ignore_if_duplicate=True)
+	)
+	if allowed_apps:
+		for app in allowed_apps:
+			plan.append("allowed_apps", {"app": app})
+	if release_groups:
+		for release_group in release_groups:
+			plan.append("release_groups", {"release_group": release_group})
+
+	plan.insert(ignore_if_duplicate=True)
 	plan.reload()
 	return plan
 
