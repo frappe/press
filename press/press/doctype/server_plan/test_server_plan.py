a627063fd2eb2d678897653c2db3a7c5d2be87ae
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2024-07-11 17:27:53 +0530

    test: server plan test and new subs creation test on team update

--- press/press/doctype/server_plan/test_server_plan.py
+++ press/press/doctype/server_plan/test_server_plan.py
@@ -1,8 +1,30 @@
 # Copyright (c) 2024, Frappe and Contributors
 # See license.txt
 
-# import frappe
+import frappe
+import typing
 from frappe.tests.utils import FrappeTestCase
+from frappe.model.naming import make_autoname
+
+if typing.TYPE_CHECKING:
+	from press.press.doctype.server_plan.server_plan import ServerPlan
+
+
+def create_test_server_plan(server_type: str = "Server") -> "ServerPlan":
+	"""Create test Server Plan doc."""
+	server_plan = frappe.get_doc(
+		{
+			"doctype": "Server Plan",
+			"name": make_autoname("SP-.####"),
+			"server_type": server_type,
+			"title": frappe.mock("name"),
+			"price_inr": 1000,
+			"price_usd": 200,
+			"enabled": 1,
+		}
+	).insert()
+	server_plan.reload()
+	return server_plan
 
 
 class TestServerPlan(FrappeTestCase):
