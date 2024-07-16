be2bb90759ec56f692ad1e58ecc4f2319a58fe6e
Bread Genie <msuhailbh07@gmail.com>
2024-07-16 12:12:32 +0530

    fix(StripeCard): reading property of undefined error when paymentIntent in undefined

--- dashboard/src2/components/StripeCard.vue
+++ dashboard/src2/components/StripeCard.vue
@@ -255,7 +255,7 @@ export default {
 					this.errorMessage = errorMessage;
 				}
 			} else {
-				if (setupIntent.status === 'succeeded') {
+				if (setupIntent?.status === 'succeeded') {
 					this.$resources.setupIntentSuccess.submit(
 						{
 							setupIntent
@@ -305,7 +305,7 @@ export default {
 						let payload = await this.stripe.confirmCardPayment(client_secret, {
 							payment_method: { card: this.card }
 						});
-						if (payload.paymentIntent.status === 'succeeded') {
+						if (payload.paymentIntent?.status === 'succeeded') {
 							this.microChargeCompleted = true;
 						}
 					},
