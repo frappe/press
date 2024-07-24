a823ddae1ac9fb450b69efcebe55ce671f36f601
Bread Genie <msuhailbh07@gmail.com>
2024-07-24 10:55:38 +0530

    fix(marketplace-app): fix patch

--- press/press/doctype/marketplace_app/patches/change_field_from_first_site_creation_to_site_creation.py
+++ press/press/doctype/marketplace_app/patches/change_field_from_first_site_creation_to_site_creation.py
@@ -2,9 +2,13 @@
 # For license information, please see license.txt
 
 import frappe
+import pymysql
 
 
 def execute():
-	frappe.db.sql(
-		"UPDATE `tabMarketplace App` SET show_for_first_site_creation = show_for_site_creation"
-	)
+	try:
+		frappe.db.sql(
+			"UPDATE `tabMarketplace App` SET show_for_site_creation = show_for_first_site_creation"
+		)
+	except pymysql.err.OperationalError:
+		pass
