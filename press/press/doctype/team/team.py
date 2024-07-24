07fac3bee4e694237cf70ab629d78d2a0a58f529
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-07-24 20:38:10 +0530

    fix: issue

--- press/press/doctype/team/team.py
+++ press/press/doctype/team/team.py
@@ -385,7 +385,7 @@ class Team(Document):
 
 	def validate_payment_mode(self):
 		if not self.payment_mode and self.get_balance() > 0:
-			frappe.db.set_value("Team", self.name, "payment_mode", "Prepaid Credits")
+			self.payment_mode = "Prepaid Credits"
 
 		if self.has_value_changed("payment_mode"):
 			if self.payment_mode == "Card":
@@ -799,7 +799,7 @@ class Team(Document):
 		self.reload()
 		if not self.default_payment_method:
 			# change payment mode to prepaid credits if default is card or not set
-			self.payment_mode = self.payment_mode
+			self.payment_mode = "Prepaid Credits"
 			self.save(ignore_permissions=True)
 		return doc
 
