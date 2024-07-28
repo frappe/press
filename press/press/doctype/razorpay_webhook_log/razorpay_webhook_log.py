3f41176d23f6cc52911b70260589910919226a3a
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-07-28 11:21:48 +0530

    fix: dont raise error in rzp authorized handler if payment already captured (#1981)

--- press/press/doctype/razorpay_webhook_log/razorpay_webhook_log.py
+++ press/press/doctype/razorpay_webhook_log/razorpay_webhook_log.py
@@ -74,7 +74,13 @@ def razorpay_authorized_payment_handler():
 
 		# Capture the authorized payment
 		client.payment.capture(payment_id, amount)
-	except Exception:
+	except Exception as e:
+		error_message = str(e)
+		if (
+			"payment has already been captured" in error_message
+			or "the order is already paid" in error_message
+		):
+			return
 		log_error(
 			title="Razorpay Authorized Payment Webhook Handler",
 			payment_id=form_dict["payload"]["payment"]["entity"]["id"],
