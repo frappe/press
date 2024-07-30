166ff2e407422fdda49659a65e25717645991618
Saurabh <saurabh6790@gmail.com>
2024-07-30 14:49:04 +0530

    fix: validate database service configuration

--- press/press/doctype/server/server.py
+++ press/press/doctype/server/server.py
@@ -186,11 +186,19 @@ class BaseServer(Document, TagHelpers):
 	def validate(self):
 		self.validate_cluster()
 		self.validate_agent_password()
+		self.validate_managed_database_instance()
 		if self.doctype == "Database Server" and not self.self_hosted_mariadb_server:
 			self.self_hosted_mariadb_server = self.private_ip
 
 		if not self.hostname_abbreviation:
 			self._set_hostname_abbreviation()
+
+	def validate_managed_database_instance(self):
+		if hasattr(self, "is_managed_database") and self.is_managed_database:
+			if not self.managed_database_service:
+				frappe.throw(_("Managed Database Service is required"))
+
+			self.database_server = ""
 
 	def _set_hostname_abbreviation(self):
 		self.hostname_abbreviation = get_hostname_abbreviation(self.hostname)
