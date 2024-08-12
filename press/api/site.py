9db794000d13d63169b6e788a4e621f4a4e69e14
Balamurali M <balamurali@frappe.io>
2024-08-12 18:04:14 +0530

    fix(Site): Handle edge case

--- press/api/site.py
+++ press/api/site.py
@@ -112,7 +112,10 @@ def get_protected_doctype_name(args: list, kwargs: dict, doctypes: list[str]):
 	return kwargs.get(key)
 
 
-def get_name_from_filters(filters: dict):
+def get_name_from_filters(filters: dict | None):
+	if not filters:
+		return None
+
 	values = [v for v in filters.values()]
 	if len(values) == 0:
 		return None
