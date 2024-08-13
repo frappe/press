0f782be909e55df71fbac141490b7645117d2305
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-08-13 12:53:59 +0530

    feat: verification mail link for product trial fixed

--- press/press/doctype/account_request/account_request.py
+++ press/press/doctype/account_request/account_request.py
@@ -184,6 +184,10 @@ class AccountRequest(Document):
 			return get_url(
 				f"/api/method/press.api.saas.validate_account_request?key={self.request_key}"
 			)
+		if self.product_trial:
+			return get_url(
+				f"/api/method/press.api.saas.setup_account?key={self.request_key}"
+			)
 		return get_url(f"/dashboard/setup-account/{self.request_key}")
 
 	@property
