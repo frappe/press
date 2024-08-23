a990651bb91d8c2fa4f966e492debfbbed583d37
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-08-23 11:23:40 +0530

    feat: move app selector to component

---
+++ dashboard/src2/components/OnboardingAppSelector.vue
@@ -0,0 +1,24 @@
+<template>
+	<div v-if="apps" class="mt-4 grid grid-cols-1 gap-2">
+		<div v-for="app in apps">
+			<a
+				:href="`/dashboard/install-app/${app.name}`"
+				class="focus:shadow-outline group flex cursor-pointer justify-start rounded-lg border p-2 no-underline transition hover:bg-gray-50"
+			>
+				<img :src="app.image" class="mr-4 h-12 w-12 rounded-md border" />
+				<div>
+					<span class="text-base font-semibold">{{ app.title }}</span>
+					<p class="mt-1 line-clamp-1 text-sm text-gray-600">
+						{{ app.description }}
+					</p>
+				</div>
+			</a>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'OnboardingAppSelector',
+	props: ['apps']
+};
+</script>
--- dashboard/src2/components/OnboardingWithoutPayment.vue
+++ dashboard/src2/components/OnboardingWithoutPayment.vue
@@ -16,31 +16,18 @@
 				</div>
 			</div>
 			<!-- App Chooser -->
-			<div
-				v-if="$resources.availableApps.data"
-				class="mt-4 grid grid-cols-1 gap-2"
-			>
-				<div v-for="app in $resources.availableApps.data">
-					<a
-						:href="`/dashboard/install-app/${app.name}`"
-						class="focus:shadow-outline group flex cursor-pointer justify-start rounded-lg border p-2 no-underline transition hover:bg-gray-50"
-					>
-						<img :src="app.image" class="mr-4 h-12 w-12 rounded-md border" />
-						<div>
-							<span class="text-base font-semibold">{{ app.title }}</span>
-							<p class="mt-1 line-clamp-1 text-sm text-gray-600">
-								{{ app.description }}
-							</p>
-						</div>
-					</a>
-				</div>
-			</div>
+			<OnboardingAppSelector :apps="$resources.availableApps.data" />
 		</div>
 	</div>
 </template>
 <script>
+import OnboardingAppSelector from './OnboardingAppSelector.vue';
+
 export default {
 	name: 'Onboarding',
+	components: {
+		OnboardingAppSelector
+	},
 	mounted() {
 		if (window.posthog?.__loaded) {
 			window.posthog.identify(this.$team.doc.user, {
