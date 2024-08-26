3f37a3bd50b5f20a16b0358b9234a9be13ff3936
Aditya Hase <aditya@adityahase.com>
2024-08-26 14:22:02 +0530

    fix: Avoid rate limits for OCI block volume snapshots
    Keep trying snapshots every hour till we succeed
    
    Fixes: https://github.com/frappe/press/issues/2076
    
    References:
    - https://trace.frappe.cloud/organizations/frappe/issues/15360/events/78d4c7f13be444d0925de3b58638e74e/
    - https://support.frappe.io/helpdesk/tickets/20095

--- press/hooks.py
+++ press/hooks.py
@@ -193,7 +193,6 @@ scheduler_events = {
 		"press.press.doctype.payout_order.payout_order.create_marketplace_payout_orders",
 		"press.press.doctype.root_domain.root_domain.cleanup_cname_records",
 		"press.press.doctype.remote_file.remote_file.poll_file_statuses",
-		"press.press.doctype.virtual_machine.virtual_machine.snapshot_virtual_machines",
 		"press.press.doctype.site_domain.site_domain.update_dns_type",
 	],
 	"hourly": [
@@ -215,6 +214,7 @@ scheduler_events = {
 		"press.press.doctype.deploy_candidate.deploy_candidate.cleanup_build_directories",
 		"press.press.doctype.deploy_candidate.deploy_candidate.delete_draft_candidates",
 		"press.press.doctype.deploy_candidate.deploy_candidate.check_builds_status",
+		"press.press.doctype.virtual_machine.virtual_machine.snapshot_virtual_machines",
 		"press.press.doctype.virtual_disk_snapshot.virtual_disk_snapshot.delete_old_snapshots",
 		"press.press.doctype.app_release.app_release.cleanup_unused_releases",
 	],
--- press/press/doctype/virtual_machine/virtual_machine.py
+++ press/press/doctype/virtual_machine/virtual_machine.py
@@ -25,6 +25,7 @@ from oci.core.models import (
 	UpdateInstanceShapeConfigDetails,
 	UpdateVolumeDetails,
 )
+from oci.exceptions import TransientServiceError
 
 from press.overrides import get_permission_query_conditions_for_doctype
 from press.utils import log_error
@@ -612,27 +613,31 @@ class VirtualMachine(Document):
 
 	def _create_snapshots_oci(self):
 		for volume in self.volumes:
-			if ".bootvolume." in volume.volume_id:
-				snapshot = (
-					self.client(BlockstorageClient)
-					.create_boot_volume_backup(
-						CreateBootVolumeBackupDetails(
-							boot_volume_id=volume.volume_id,
-							type="INCREMENTAL",
-							display_name=f"Frappe Cloud - {self.name} - {volume.name} - {frappe.utils.now()}",
-						)
-					)
-					.data
-				)
-			else:
-				self.client(BlockstorageClient).create_volume_backup(
-					CreateVolumeBackupDetails(
-						volume_id=volume.volume_id,
-						type="INCREMENTAL",
-						display_name=f"Frappe Cloud - {self.name} - {volume.name} - {frappe.utils.now()}",
-					)
-				).data
 			try:
+				if ".bootvolume." in volume.volume_id:
+					snapshot = (
+						self.client(BlockstorageClient)
+						.create_boot_volume_backup(
+							CreateBootVolumeBackupDetails(
+								boot_volume_id=volume.volume_id,
+								type="INCREMENTAL",
+								display_name=f"Frappe Cloud - {self.name} - {volume.name} - {frappe.utils.now()}",
+							)
+						)
+						.data
+					)
+				else:
+					snapshot = (
+						self.client(BlockstorageClient)
+						.create_volume_backup(
+							CreateVolumeBackupDetails(
+								volume_id=volume.volume_id,
+								type="INCREMENTAL",
+								display_name=f"Frappe Cloud - {self.name} - {volume.name} - {frappe.utils.now()}",
+							)
+						)
+						.data
+					)
 				frappe.get_doc(
 					{
 						"doctype": "Virtual Disk Snapshot",
@@ -640,6 +645,10 @@ class VirtualMachine(Document):
 						"snapshot_id": snapshot.id,
 					}
 				).insert()
+			except TransientServiceError:
+				# We've hit OCI rate limit for creating snapshots
+				# Let's try again later
+				pass
 			except Exception:
 				log_error(
 					title="Virtual Disk Snapshot Error", virtual_machine=self.name, snapshot=snapshot
@@ -1026,6 +1035,13 @@ def sync_virtual_machines():
 def snapshot_virtual_machines():
 	machines = frappe.get_all("Virtual Machine", {"status": "Running"})
 	for machine in machines:
+		# Skip if a snapshot has already been created today
+		if frappe.get_all(
+			"Virtual Disk Snapshot",
+			{"virtual_machine": machine.name, "creation": (">=", frappe.utils.today())},
+			limit=1,
+		):
+			continue
 		try:
 			frappe.get_doc("Virtual Machine", machine.name).create_snapshots()
 			frappe.db.commit()
