34d7ba5125d97f6c945d65250c6bbe007df9e831
Bread Genie <msuhailbh07@gmail.com>
2024-09-04 10:18:03 +0530

    feat(RolePermissions): admin roles

--- dashboard/src2/components/settings/RoleConfigureDialog.vue
+++ dashboard/src2/components/settings/RoleConfigureDialog.vue
@@ -69,20 +69,30 @@
 					<div class="space-y-3">
 						<div class="space-y-3 rounded border p-4">
 							<Switch
+								v-model="adminAccess"
+								label="Admin Access"
+								description="Grant users belonging to this role admin access"
+							/>
+						</div>
+						<div class="space-y-3 rounded border p-4">
+							<Switch
 								v-model="allowBilling"
 								label="Allow Billing Access"
 								description="Grant users belonging to this role access to billing page"
+								:disabled="adminAccess"
 							/>
 							<Switch
 								v-model="allowApps"
 								label="Allow Apps Access"
 								description="Grant users belonging to this role access to apps page"
+								:disabled="adminAccess"
 							/>
 							<Switch
 								v-if="$team.doc.erpnext_partner"
 								v-model="allowPartner"
 								label="Allow Partner Access"
 								description="Grant users belonging to this role access to partner page"
+								:disabled="adminAccess"
 							/>
 						</div>
 						<div class="space-y-3 rounded border p-4">
@@ -90,16 +100,19 @@
 								v-model="allowSiteCreation"
 								label="Allow Site Creation"
 								description="Newly created sites will be given access to users of this role"
+								:disabled="adminAccess"
 							/>
 							<Switch
 								v-model="allowBenchCreation"
 								label="Allow Bench Creation"
 								description="Newly created benches will be given access to users of this role"
+								:disabled="adminAccess"
 							/>
 							<Switch
 								v-model="allowServerCreation"
 								label="Allow Server Creation"
 								description="Newly created servers will be given access to users of this role"
+								:disabled="adminAccess"
 							/>
 						</div>
 					</div>
@@ -157,6 +170,19 @@ export default {
 			return this.$team.doc.team_members
 				?.filter(({ user }) => isNotGroupMember(user))
 				.map(({ user }) => ({ label: user, value: user }));
+		},
+		adminAccess: {
+			get() {
+				return !!this.role?.admin_access;
+			},
+			set(value) {
+				this.$resources.role.setValue.submit(
+					{
+						admin_access: value
+					},
+					{ onSuccess: this.$session.roles.reload }
+				);
+			}
 		},
 		allowBilling: {
 			get() {
