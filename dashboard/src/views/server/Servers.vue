846f60da0f44963b946d0568bd1c166d70ff972d
Jufri <134145164+optibizpro@users.noreply.github.com>
2024-09-19 23:27:48 +0700

    Update Servers.vue
    frappe > OptiBizPro

--- dashboard/src/views/server/Servers.vue
+++ dashboard/src/views/server/Servers.vue
@@ -185,7 +185,7 @@ export default {
 	},
 	pageMeta() {
 		return {
-			title: 'Servers - Frappe Cloud'
+			title: 'Servers - OptiBizPro Cloud'
 		};
 	},
 	data() {
@@ -196,7 +196,7 @@ export default {
 			server_tag: '',
 			dropDownOptions: [
 				{
-					label: 'Frappe Cloud Server',
+					label: 'OptiBizPro Cloud Server',
 					onClick: () => this.$router.replace('/servers/new')
 				},
 				{
