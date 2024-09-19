de35b08cfbd2408b664966cabe7606f1c6a2d3c3
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-09-19 12:09:13 +0530

    feat: webhook endpoint pre-validation added

--- press/press/doctype/press_webhook/press_webhook.js
+++ press/press/doctype/press_webhook/press_webhook.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, Frappe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Press Webhook", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Press Webhook', {
+	refresh(frm) {
+		frm.add_custom_button(__('Activate'), () => {
+			frm.call('activate').then((r) => {
+				if (r.message) {
+					frappe.msgprint(r.message);
+				}
+			});
+		});
+	},
+});
--- press/press/doctype/press_webhook/press_webhook.py
+++ press/press/doctype/press_webhook/press_webhook.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2024, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import requests
 from frappe.model.document import Document
 
 
@@ -24,4 +25,40 @@ class PressWebhook(Document):
 		team: DF.Link
 	# end: auto-generated types
 
-	pass
+	def validate(self):
+		if self.has_value_changed("callback_url"):
+			self.enabled = 0
+
+	def check_callback_url(self) -> dict:
+		response = ""
+		response_status_code = 0
+		try:
+			req = requests.post(
+				self.callback_url,
+				timeout=5,
+				json={"event": "Webhook Test", "data": {}},
+				headers={"X-Webhook-Secret": self.secret},
+			)
+			response = req.text or ""
+			response_status_code = req.status_code
+		except Exception as e:
+			response = e.__str__()
+
+		return frappe._dict(
+			{
+				"success": response_status_code >= 200 and response_status_code < 300,
+				"response": response,
+				"response_status_code": response_status_code,
+			}
+		)
+
+	@frappe.whitelist()
+	def activate(self):
+		result = self.check_callback_url()
+		if result.get("success"):
+			self.enabled = 1
+			self.save()
+			frappe.msgprint("Webhook activated successfully")
+		else:
+			message = f"<b>Status Code -</b> {result.response_status_code}</br><b>Response -</b></br>{result.response}"
+			frappe.throw(title="Webhook endpoint is invalid", msg=message)
