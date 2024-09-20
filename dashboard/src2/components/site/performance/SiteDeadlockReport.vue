fc8b39150941f508129813c28bacd7050539b9fb
Bread Genie <msuhailbh07@gmail.com>
2024-09-20 11:27:47 +0530

    fix(SIteDeadlockReport): filters

--- dashboard/src2/components/site/performance/SiteDeadlockReport.vue
+++ dashboard/src2/components/site/performance/SiteDeadlockReport.vue
@@ -1,45 +1,5 @@
 <template>
 	<div v-if="$site.doc?.current_plan?.monitor_access" class="m-5">
-		<!-- <Card>
-			<Report
-				:filters="filters"
-				:columns="[
-					{ label: 'Timestamp', name: 'timestamp', class: 'w-2/12' },
-					{ label: 'Query', name: 'query', class: 'w-10/12' }
-				]"
-				:data="formatDeadlockData"
-				title="MariaDB Deadlock Report"
-			/>
-			<div
-				class="px-2 py-2 text-base text-gray-600"
-				v-if="
-					$resources.deadlockReport.loading &&
-					$resources.deadlockReport.data.length == 0
-				"
-			>
-				<LoadingText />
-			</div>
-			<div
-				class="content-center py-2 text-base"
-				v-if="
-					!$resources.deadlockReport.loading &&
-					$resources.deadlockReport.data.length == 0
-				"
-			>
-				No data
-			</div>
-			<Button
-				v-if="
-					$resources.deadlockReport.data &&
-					$resources.deadlockReport.data.length
-				"
-				:loading="$resources.deadlockReport.loading"
-				loadingText="Loading..."
-				@click="max_lines += 10"
-			>
-				Load more
-			</Button>
-		</Card> -->
 		<ObjectList :options="deadlockReportOptions" />
 	</div>
 	<div class="flex justify-center" v-else>
@@ -49,69 +9,24 @@
 	</div>
 </template>
 <script>
-import Report from '@/components/Report.vue';
 import { getCachedDocumentResource } from 'frappe-ui';
-import { DateTime } from 'luxon';
 import ObjectList from '../../ObjectList.vue';
+import dayjs from '../../../utils/dayjs';
 
 export default {
 	name: 'SiteDeadlockReport',
 	props: ['name'],
 	components: {
-		Report,
 		ObjectList
 	},
 	data() {
 		return {
-			filters: [
-				{
-					name: 'start_datetime',
-					label: 'From:',
-					type: 'date',
-					value: this.today
-				},
-				{ name: 'end_datetime', label: 'To:', type: 'date', value: this.today }
-			],
+			today: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+			yesterday: dayjs().subtract(1, 'day').format('YYYY-MM-DD HH:mm:ss'),
 			max_lines: 20
 		};
 	},
-	watch: {
-		startTime() {
-			this.reset();
-		},
-		endTime() {
-			this.reset();
-		}
-	},
-	resources: {
-		deadlockReport() {
-			return {
-				url: 'press.api.analytics.deadlock_report',
-				params: {
-					site: this.name,
-					start: this.startTime || this.today,
-					end: this.endTime || this.today
-				},
-				auto: true,
-				initialData: []
-			};
-		}
-	},
-	methods: {
-		reset() {
-			this.$resources.deadlockReport.reset();
-		}
-	},
 	computed: {
-		today() {
-			return DateTime.local().toISODate();
-		},
-		startTime() {
-			return this.filters[0].value;
-		},
-		endTime() {
-			return this.filters[1].value;
-		},
 		$site() {
 			return getCachedDocumentResource('Site', this.name);
 		},
@@ -120,18 +35,31 @@ export default {
 				resource: () => {
 					return {
 						url: 'press.api.analytics.deadlock_report',
-						params: {
-							site: this.name,
-							start: this.startTime || this.today,
-							end: this.endTime || this.today
+						makeParams: params => {
+							// need to return params if it exists for filterControls to work
+							if (params) return params;
+
+							return {
+								site: this.name,
+								start: this.yesterday,
+								end: this.today,
+								max_lines: this.max_lines
+							};
 						},
 						auto: true,
 						initialData: []
 					};
 				},
 				columns: [
-					{ label: 'Timestamp', fieldname: 'timestamp' },
-					{ label: 'Query', fieldname: 'query' }
+					{
+						label: 'Timestamp',
+						fieldname: 'timestamp',
+						width: '12rem',
+						format: value => {
+							return this.$format.date(value, 'YYYY-MM-DD HH:mm:ss');
+						}
+					},
+					{ label: 'Query', fieldname: 'query', class: 'font-mono' }
 				],
 				actions: () => {
 					return [
@@ -143,24 +71,29 @@ export default {
 							}
 						}
 					];
+				},
+				filterControls: () => {
+					return [
+						{
+							type: 'datetime-local',
+							label: 'Start Time',
+							fieldname: 'start',
+							default: this.yesterday
+						},
+						{
+							type: 'datetime-local',
+							label: 'End Time',
+							fieldname: 'end',
+							default: this.today
+						},
+						{
+							label: 'Max Lines',
+							fieldname: 'max_lines',
+							default: this.max_lines
+						}
+					];
 				}
 			};
-		},
-		formatDeadlockData() {
-			let data = [];
-			this.$resources.deadlockReport.data.forEach(row => {
-				let timestamp = this.formatDate(
-					row.timestamp,
-					'TIME_24_WITH_SHORT_OFFSET'
-				);
-				let out = [
-					{ name: 'Timestamp', value: timestamp, class: 'w-2/12' },
-					{ name: 'Query', value: row.query, class: 'w-10/12' }
-				];
-				data.push(out);
-			});
-
-			return data;
 		}
 	}
 };
