9269e7d77e4b62d1065905308479be00c6c60f89
Bread Genie <msuhailbh07@gmail.com>
2024-09-20 11:20:50 +0530

    fix(SiteBinaryLogs): filters

--- dashboard/src2/components/site/performance/SiteBinaryLogs.vue
+++ dashboard/src2/components/site/performance/SiteBinaryLogs.vue
@@ -1,34 +1,5 @@
 <template>
 	<div v-if="$site.doc.current_plan.monitor_access" class="m-5">
-		<!-- <Report
-			:filters="filters"
-			:columns="[
-				{ label: 'Timestamp', name: 'timestamp', class: 'w-3/12' },
-				{ label: 'Query', name: 'query', class: 'w-9/12' }
-			]"
-			:data="formatData"
-			title="MariaDB Binary Log Report"
-		/>
-		<div
-			class="px-2 py-2 text-base text-gray-600"
-			v-if="$resources.binaryLogs.loading"
-		>
-			<LoadingText />
-		</div>
-		<div
-			class="py-2 text-base text-gray-600"
-			v-else-if="$resources.binaryLogs.data.length == 0"
-		>
-			No data
-		</div>
-		<Button
-			v-if="$resources.binaryLogs.data && $resources.binaryLogs.data.length"
-			:loading="$resources.binaryLogs.loading"
-			loadingText="Loading..."
-			@click="max_lines += 10"
-		>
-			Load more
-		</Button> -->
 		<ObjectList :options="binaryLogsOptions" />
 	</div>
 	<div class="flex justify-center" v-else>
@@ -40,56 +11,19 @@
 
 <script>
 import { getCachedDocumentResource } from 'frappe-ui';
-import Report from '@/components/Report.vue';
 import ObjectList from '../../ObjectList.vue';
 import dayjs from '../../../utils/dayjs';
 
 export default {
 	name: 'SiteBinaryLogs',
 	props: ['name'],
-	components: { Report, ObjectList },
+	components: { ObjectList },
 	data() {
 		return {
-			filters: [
-				{
-					name: 'pattern',
-					label: 'Search:',
-					type: 'text',
-					value: this.pattern
-				},
-				{
-					name: 'start_datetime',
-					label: 'From:',
-					type: 'datetime-local',
-					value: ''
-				},
-				{
-					name: 'end_datetime',
-					label: 'To:',
-					type: 'datetime-local',
-					value: ''
-				}
-			],
 			today: dayjs().format('YYYY-MM-DD HH:mm:ss'),
 			yesterday: dayjs().subtract(1, 'day').format('YYYY-MM-DD HH:mm:ss'),
 			max_lines: 4000
 		};
-	},
-	watch: {
-		patternFilter() {
-			this.reset();
-		},
-		startTime() {
-			this.reset();
-		},
-		endTime() {
-			this.reset();
-		}
-	},
-	methods: {
-		reset() {
-			this.$resources.binaryLogs.reset();
-		}
 	},
 	computed: {
 		$site() {
@@ -100,12 +34,16 @@ export default {
 				resource: () => {
 					return {
 						url: 'press.api.analytics.binary_logs',
-						params: {
-							name: this.name,
-							start_time: this.startTime || this.yesterday,
-							end_time: this.endTime || this.today,
-							pattern: this.patternFilter,
-							max_lines: this.max_lines
+						makeParams: params => {
+							if (params) return params;
+
+							return {
+								name: this.name,
+								start_time: this.yesterday,
+								end_time: this.today,
+								pattern: '.*',
+								max_lines: this.max_lines
+							};
 						},
 						auto: true,
 						pageLength: 10,
@@ -135,28 +73,33 @@ export default {
 						}
 					}
 				],
-				filterControls() {
+				filterControls: () => {
 					return [
 						{
-							type: 'datetime',
-							label: 'Version',
-							fieldname: 'version',
-							options: {
-								doctype: 'Frappe Version'
-							}
+							type: 'datetime-local',
+							label: 'Start Time',
+							fieldname: 'start_time',
+							default: this.yesterday
+						},
+						{
+							type: 'datetime-local',
+							label: 'End Time',
+							fieldname: 'end_time',
+							default: this.today
+						},
+						{
+							label: 'Pattern',
+							fieldname: 'pattern',
+							default: '.*'
+						},
+						{
+							label: 'Max Lines',
+							fieldname: 'max_lines',
+							default: 4000
 						}
 					];
 				}
 			};
-		},
-		startTime() {
-			return this.filters[1].value;
-		},
-		endTime() {
-			return this.filters[2].value;
-		},
-		patternFilter() {
-			return this.filters[0].value;
 		},
 		formatData() {
 			let binaryData = this.$resources.binaryLogs.data;
