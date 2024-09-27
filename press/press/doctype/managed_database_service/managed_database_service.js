fc288bd20bc3bdf787f1c3e6947f7dffe286f976
Aditya Hase <aditya@adityahase.com>
2024-09-27 15:45:35 +0530

    fix: Show console access help for Managed Databases

--- press/press/doctype/managed_database_service/managed_database_service.js
+++ press/press/doctype/managed_database_service/managed_database_service.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2024, Frappe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Managed Database Service", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Managed Database Service', {
+	refresh(frm) {
+		let command = `mysql -h ${frm.doc.name} -p -u ${frm.doc.database_root_user} -P ${frm.doc.port}`;
+		frm.add_custom_button('Console Access', () => {
+			frappe.msgprint(`<pre>${command}</pre>`);
+		});
+		frm.add_custom_button('Show Root Password', () => {
+			frm.call('show_root_password').then((r) => {
+				frappe.msgprint(`<pre>${r.message}<pre>`);
+			});
+		});
+	},
+});
