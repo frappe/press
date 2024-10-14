849b8fac013e4c3db6302def9fc49b1c77fb56b8
Jufri <134145164+optibizpro@users.noreply.github.com>
2024-10-14 11:08:18 +0700

    Update NewServer.vue
    Naming

--- dashboard/src2/pages/NewServer.vue
+++ dashboard/src2/pages/NewServer.vue
@@ -254,7 +254,7 @@
 				<FormControl
 					type="checkbox"
 					v-model="agreedToRegionConsent"
-					:label="`I agree that the laws of the region selected by me shall stand applicable to me and Frappe.`"
+					:label="`I agree that the laws of the region selected by me shall stand applicable to me and OptiBizPro.`"
 				/>
 				<ErrorMessage
 					class="my-2"
