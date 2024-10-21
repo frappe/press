6bc4f548312de2d430680754e3aa5f34c786150d
Arun <me@arunmathaisk.in>
2024-10-21 11:47:23 +0530

    feat(devboxes): Added Jobs and made changes to the Devbox doctype

--- press/press/doctype/devbox/devbox.py
+++ press/press/doctype/devbox/devbox.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2024, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+from __future__ import annotations
+
 import frappe
 from frappe.model.document import Document
 
@@ -17,21 +18,28 @@ class Devbox(Document):
 
 		cpu_cores: DF.Int
 		disk_mb: DF.Int
+		domain: DF.Link | None
+		initialized: DF.Check
 		ram: DF.Int
 		server: DF.Link
 		status: DF.Literal["Pending", "Starting", "Paused", "Running", "Archived", "Exited"]
+		subdomain: DF.Data
 	# end: auto-generated types
 	pass
 
+	def _get_devbox_name(self, subdomain: str):
+		"""Get full devbox domain name given subdomain."""
+		if not self.domain:
+			self.domain = frappe.db.get_single_value("Press Settings", "domain")
+		return f"{subdomain}.{self.domain}"
+
+	def autoname(self):
+		self.name = self._get_devbox_name(subdomain=self.subdomain)
+
 	@frappe.whitelist()
-	def get_available_cpu_and_ram(self):	
+	def get_available_cpu_and_ram(self):
 		print("meow")
 
 	@frappe.whitelist()
 	def pull_latest_image(self):
 		print("meow")
-
-
-
-
-x
