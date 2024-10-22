4668655697adcf48df2bb710ea4095c6285b1790
Bread Genie <msuhailbh07@gmail.com>
2024-10-22 10:08:49 +0530

    fix(SiteTags): show tags in overview

---
+++ dashboard/src2/dialogs/TagsDialog.vue
@@ -0,0 +1,92 @@
+<template>
+	<Dialog v-model="showDialog" :options="{ title: 'Add Tag' }">
+		<template #body-content>
+			<div class="space-y-4">
+				<div class="flex items-center space-x-2">
+					<FormControl
+						v-if="selectedTag?.value !== '__new__'"
+						class="w-full"
+						label="Select or create a tag"
+						type="autocomplete"
+						v-model="selectedTag"
+						:options="tagOptions"
+					/>
+					<FormControl
+						v-if="selectedTag?.value === '__new__'"
+						class="w-full"
+						v-model="newTag"
+						label="Enter new tag"
+						placeholder="production, staging, testing"
+					/>
+					<Button
+						label="Add"
+						icon-left="plus"
+						@click="addTag"
+						class="mt-5"
+						:disabled="!selectedTag"
+					/>
+				</div>
+			</div>
+		</template>
+	</Dialog>
+</template>
+
+<script>
+import { ErrorMessage, getCachedDocumentResource } from 'frappe-ui';
+import { toast } from 'vue-sonner';
+
+export default {
+	name: 'TagsDialog',
+	props: ['docname', 'doctype'],
+	components: { ErrorMessage },
+	data() {
+		return {
+			showDialog: true,
+			newTag: '',
+			selectedTag: null
+		};
+	},
+	resources: {
+		availableTags() {
+			return {
+				type: 'list',
+				doctype: 'Press Tag',
+				filters: { doctype_name: this.doctype },
+				fields: ['tag'],
+				pageLength: 1000,
+				auto: true
+			};
+		}
+	},
+	computed: {
+		$doc() {
+			return getCachedDocumentResource(this.doctype, this.docname);
+		},
+		tagOptions() {
+			return [
+				{ label: 'Create new tag', value: '__new__' },
+				...(this.$resources.availableTags.data || []).map(d => ({
+					label: d.tag,
+					value: d.tag
+				}))
+			];
+		}
+	},
+	methods: {
+		addTag() {
+			const tag =
+				this.selectedTag?.value === '__new__'
+					? this.newTag
+					: this.selectedTag.value;
+			toast.promise(this.$doc.addTag.submit({ tag }), {
+				success: () => {
+					this.selectedTag = null;
+					return 'Tag added successfully';
+				},
+				loading: 'Adding tag...',
+				error: 'Failed to add tag'
+			});
+		}
+	}
+};
+</script>
