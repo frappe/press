803e6517453d664218d974264e6b27ba57fd17cf
KerollesFathy <kerolles.f@outlook.com>
2024-10-31 16:31:39 +0300

    refactor: Payment Partner Balance Transaction
    - rename field actual_amount to paid_amount  and actual_currency to payment_currency
    - enhance remark description

--- press/press/doctype/payment_partner_balance_transaction/payment_partner_balance_transaction.py
+++ press/press/doctype/payment_partner_balance_transaction/payment_partner_balance_transaction.py
@@ -14,12 +14,12 @@ class PaymentPartnerBalanceTransaction(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		actual_amount: DF.Currency
-		actual_currency: DF.Link | None
 		amended_from: DF.Link | None
 		amount: DF.Currency
 		currency: DF.Link | None
 		exchange_rate: DF.Float
+		paid_amount: DF.Currency
+		payment_currency: DF.Link | None
 		payment_gateway: DF.Link | None
 		payment_partner: DF.Link | None
 		payment_transaction_details: DF.Code | None
@@ -27,4 +27,6 @@ class PaymentPartnerBalanceTransaction(Document):
 	# end: auto-generated types
 	def on_submit(self):
 		team = frappe.get_doc("Team", self.team)
-		team.allocate_credit_amount(self.amount, "Prepaid Credits", remark=f"Via Payment Partner {self.payment_partner} document {self.name}")
+		payment_partner_user = frappe.db.get_value("Team", self.payment_partner, "user")
+		remark = f"Credit allocated via Payment Partner: {payment_partner_user} using {self.payment_gateway} gateway ({self.doctype} ID: {self.name})"
+		team.allocate_credit_amount(self.amount, "Prepaid Credits", remark=remark)
