f8cebc7dd57311977ce08f1fcbf8240dc58069df
Shariq Ansari <sharique.rik@gmail.com>
2024-11-11 13:01:13 +0530

    fix: use frappe.local.team_name

--- press/saas/api/billing.py
+++ press/saas/api/billing.py
@@ -72,7 +72,6 @@ def handle_razorpay_payment_failed():
 # Invoice Related APIs
 @whitelist_saas_api
 def get_invoices():
-	team = frappe.local.get_team()
 	return frappe.get_list(
 		"Invoice",
 		fields=[
@@ -91,7 +90,7 @@ def get_invoices():
 			"amount_due",
 			"stripe_payment_failed",
 		],
-		filters={"team": team.name},
+		filters={"team": frappe.local.team_name},
 	)
 
 
