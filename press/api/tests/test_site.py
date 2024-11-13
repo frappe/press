1acafdb30e22d053a996ef80ca38098a7a8f6c58
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-11-13 16:30:43 +0530

    testcase(SiteVersionUpgrade): set a plan on test site to fix issues  while comparing cpu_time_per_day

--- press/api/tests/test_site.py
+++ press/api/tests/test_site.py
@@ -819,7 +819,8 @@ erpnext 0.8.3	    HEAD
 
 		v14_bench = create_test_bench(group=v14_group, server=server)
 		create_test_bench(group=v15_group, server=server)
-		site = create_test_site(bench=v14_bench.name)
+
+		site = create_test_site(bench=v14_bench.name, subscription_plan=create_test_plan("Site"))
 
 		self.assertEqual(
 			get_private_groups_for_upgrade(site.name, v14_group.version),
