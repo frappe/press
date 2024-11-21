59af954bf5af5140ce8f1181b6af43b31a707e39
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-11-21 09:44:13 +0530

    chore(saas): misc saas flow fixes and tweaks (#2293)

--- press/saas/api/site.py
+++ press/saas/api/site.py
@@ -25,7 +25,25 @@ def change_plan(plan: str):
 
 @whitelist_saas_api
 def get_plans():
-	return site_api.get_site_plans()
+	site = frappe.get_value("Site", frappe.local.site_name, ["server", "group", "plan"], as_dict=True)
+	is_site_on_private_bench = frappe.db.get_value("Release Group", site.group, "public") is False
+	is_site_on_shared_server = frappe.db.get_value("Server", site.server, "public")
+	plans = site_api.get_site_plans()
+	filtered_plans = []
+
+	for plan in plans:
+		if plan.name != site.plan:
+			if plan.restricted_plan or plan.is_frappe_plan or plan.is_trial_plan:
+				continue
+			if is_site_on_private_bench and not plan.private_benches:
+				continue
+			if plan.dedicated_server_plan and is_site_on_shared_server:
+				continue
+			if not plan.dedicated_server_plan and not is_site_on_shared_server:
+				continue
+		filtered_plans.append(plan)
+
+	return filtered_plans
 
 
 @whitelist_saas_api
