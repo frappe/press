9767383bd140744b50532eecf8460853efa35c56
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-11-25 14:13:07 +0530

    chore(site): on db user archive, dont raise error if it's already archived

--- press/press/doctype/site/site.py
+++ press/press/doctype/site/site.py
@@ -1278,7 +1278,7 @@ class Site(Document, TagHelpers):
 		)
 
 		for db_user in db_users:
-			frappe.get_doc("Site Database User", db_user).archive(raise_error=True)
+			frappe.get_doc("Site Database User", db_user).archive(raise_error=False)
 
 	@frappe.whitelist()
 	def cleanup_after_archive(self):
