6901ed20d457307fe94ae6f65ae0844ec814d5a9
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-11-28 19:55:34 +0530

    fix(marketplace-install-app): dont allow to create site for returning user having no payment mode

--- dashboard/src2/pages/InstallApp.vue
+++ dashboard/src2/pages/InstallApp.vue
@@ -317,6 +317,12 @@ export default {
 					};
 				},
 				validate() {
+					if (
+						!this.$team.doc.payment_mode &&
+						this.$team.doc.onboarding.site_created
+					) {
+						throw new DashboardError('Please add a valid payment mode');
+					}
 					if (!this.selectedPlan && this.plans.length > 0) {
 						throw new DashboardError('Please select a plan');
 					}
