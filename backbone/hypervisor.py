3c01e3bf25882425d88c6d11a6d6c32278466b49
Aditya Hase <aditya@adityahase.com>
2024-12-17 14:34:03 +0530

    fix(backbone): Use mkisofs to create cloud-init image
    cloud-localds isn't available on macOS
    
    mkisofs is a drop-in replacement for genisoimage
    Reference: https://github.com/canonical/cloud-utils/blob/49e5dd7849ee3c662f3db35e857148d02e72694b/bin/cloud-localds#L235-L237
    
    And we need to do a few more steps. Explicitly create user-data and meta-data files
    
    Reference: https://github.com/canonical/cloud-utils/blob/49e5dd7849ee3c662f3db35e857148d02e72694b/bin/cloud-localds#L168-L210

--- backbone/hypervisor.py
+++ backbone/hypervisor.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2020, Frappe and contributors
 # For license information, please see license.txt
 import platform
@@ -20,6 +19,13 @@ class Hypervisor:
 			self.verify_mac()
 
 	def build(self, size):
+		system = platform.system()
+		if system == "Linux":
+			self.build_linux(size=size)
+		elif system == "Darwin":
+			self.build_mac(size=size)
+
+	def build_linux(self, size):
 		cloud_init_yml = str(Path(__file__).parent.joinpath("packer", "cloud-init.yml"))
 		cloud_init_image = str(Path(__file__).parent.joinpath("packer", "cloud-init.img"))
 		self.shell.execute(f"cloud-localds {cloud_init_image} {cloud_init_yml}")
@@ -33,6 +39,35 @@ class Hypervisor:
 		add = self.shell.execute(f"vagrant box add {box} --name backbone --force")
 		if add.returncode:
 			raise Exception(f"Cannot add box {box}")
+
+	def build_mac(self, size):
+		# cloud-localds isn't availble on macOS.
+		# So we do what it does ourselves
+		# user-data is the same as cloud-init.yml
+		# https://github.com/canonical/cloud-utils/blob/49e5dd7849ee3c662f3db35e857148d02e72694b/bin/cloud-localds#L168-L187
+		cloud_init_yml = str(Path(__file__).parent.joinpath("packer", "cloud-init.yml"))
+		user_data = str(Path(__file__).parent.joinpath("packer", "user-data"))
+		self.shell.execute(f"cp {cloud_init_yml} {user_data}")
+
+		# meta-data has some inconsequential values
+		# but the file is needed
+		meta_data = str(Path(__file__).parent.joinpath("packer", "meta-data"))
+		self.shell.execute(f"touch {meta_data}")
+
+		cloud_init_image = str(Path(__file__).parent.joinpath("packer", "cloud-init.img"))
+		# Reference: https://github.com/canonical/cloud-utils/blob/49e5dd7849ee3c662f3db35e857148d02e72694b/bin/cloud-localds#L235-L237
+		self.shell.execute(f"mkisofs -joliet -rock -volid cidata -output {cloud_init_image} {user_data} {meta_data}")
+
+		packer_template = str(Path(__file__).parent.joinpath("packer", "backbone.json"))
+		packer = self.shell.execute(f"packer build -var 'disk_size={size}' {packer_template}")
+		if packer.returncode:
+			raise Exception("Build Failed")
+
+		box = str(Path(__file__).parent.joinpath("packer", "builds", "backbone.box"))
+		add = self.shell.execute(f"vagrant box add {box} --name backbone --force")
+		if add.returncode:
+			raise Exception(f"Cannot add box {box}")
+
 
 	def build_scaleway(self, size):
 		cloud_init_yml = str(
