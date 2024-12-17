077080b847a8c94b71cf041e87313ed56a47a8d5
Aditya Hase <aditya@adityahase.com>
2024-12-17 16:07:33 +0530

    fix(backbone): Minor changes to get vagrant to run on MacOS

--- backbone/setup_mac.py
+++ backbone/setup_mac.py
@@ -6,9 +6,7 @@ shell = Shell()
 
 
 def brew_install(packages):
-	shell.execute(
-		f"brew install {packages}"
-	)
+	shell.execute(f"brew install {packages}")
 
 
 def main(args):
@@ -21,16 +19,23 @@ def main(args):
 
 def prepare():
 	shell.execute("brew update")
+	brew_install("cdrtools iproute2mac")
 
 
 def setup_qemu():
 	brew_install("qemu")
+	# We might need to disable a few things
+	# echo 'security_driver = "none"' >> /opt/homebrew/etc/libvirt/qemu.conf
+	# echo "dynamic_ownership = 0" >> /opt/homebrew/etc/libvirt/qemu.conf
+	# echo "remember_owner = 0" >> /opt/homebrew/etc/libvirt/qemu.conf
+
 
 def setup_vagrant():
 	# At the time of writing hashicorp tap has older 2.4.2 version
 	# We need 2.4.3
 	# Reference: https://github.com/vagrant-libvirt/vagrant-libvirt/issues/1831
 	brew_install("vagrant")
+
 
 def setup_libvirt():
 	brew_install("libvirt")
@@ -39,6 +44,7 @@ def setup_libvirt():
 	# It overrides pkg-config, and we won't find brew libvirt packages
 	shell.execute("vagrant plugin install vagrant-libvirt")
 	shell.execute("vagrant plugin install vagrant-hostmanager")
+
 
 def setup_packer():
 	shell.execute("brew tap hashicorp/tap")
