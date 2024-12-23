c755b89e9551192f19fb247292eff82a596b259e
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-12-23 15:21:23 +0530

    feat(site-database-user): add ssl instructions for database user (#2383)

--- dashboard/src2/components/site_database_user/SiteDatabaseUserCredentialDialog.vue
+++ dashboard/src2/components/site_database_user/SiteDatabaseUserCredentialDialog.vue
@@ -43,7 +43,7 @@
 				</div>
 				<div class="pb-2 pt-5">
 					<p class="mb-2 text-base font-semibold text-gray-700">
-						Using MariaDB Client
+						Using MariaDB CLI
 					</p>
 					<p class="mb-2 text-base">
 						Run this command in your terminal to access MariaDB console
@@ -55,12 +55,43 @@
 						computer.
 					</p>
 				</div>
+				<div class="pb-2 pt-5">
+					<p class="mb-2 text-base font-semibold text-gray-700">SSL Info</p>
+					<p class="text-sm">
+						You need to <b>use SSL</b> to connect to the database.
+					</p>
+					<Button
+						class="mt-3"
+						:loading="this.$resources.downloadSSLCert.loading"
+						loading-text="Downloading SSL Certificate (.pem)"
+						@click="this.$resources.downloadSSLCert.submit()"
+						>Download SSL Certificate (.pem)</Button
+					>
+					<ErrorMessage
+						class="mt-2"
+						:message="this.$resources.downloadSSLCert.error"
+					/>
+				</div>
+				<div class="pb-2 pt-5">
+					<p class="mb-2 text-base font-semibold text-gray-700">Need Help?</p>
+					<p class="text-sm">
+						Please check out the
+						<a
+							href="https://frappecloud.com/docs/database-users-and-permission-manager#faq"
+							target="_blank"
+							class="underline"
+							>documentation</a
+						>
+						to get guides on troubleshooting.
+					</p>
+				</div>
 			</div>
 		</template>
 	</Dialog>
 </template>
 <script>
 import ClickToCopyField from '../ClickToCopyField.vue';
+import { toast } from 'vue-sonner';
 
 export default {
 	name: 'SiteDatabaseUserCredentialDialog',
@@ -83,6 +114,28 @@ export default {
 				},
 				initialData: {},
 				auto: true
+			};
+		},
+		downloadSSLCert() {
+			return {
+				url: 'press.api.download_ssl_cert',
+				makeParams: () => {
+					return {
+						domain: this.databaseCredential?.host ?? ''
+					};
+				},
+				auto: false,
+				onSuccess: data => {
+					// create a blob and trigger a download
+					const blob = new Blob([data], { type: 'text/plain' });
+					const filename = `${this.databaseCredential?.host ?? ''}.pem`;
+					const link = document.createElement('a');
+					link.href = URL.createObjectURL(blob);
+					link.download = filename;
+					link.click();
+					URL.revokeObjectURL(link.href);
+					toast.success('SSL Certificate downloaded successfully');
+				}
 			};
 		}
 	},
