3a5353fccf80f822b352f250975f5dcdfe802120
Aditya Hase <aditya@adityahase.com>
2025-01-02 14:10:12 +0530

    feat(vm-migration): Reset root_disk_size on the new machine
    Root Disk Size now stores the size of the root disk

--- press/infrastructure/doctype/virtual_machine_migration/virtual_machine_migration.py
+++ press/infrastructure/doctype/virtual_machine_migration/virtual_machine_migration.py
@@ -385,7 +385,7 @@ class VirtualMachineMigration(Document):
 		# Set new machine image and machine type
 		machine.virtual_machine_image = self.virtual_machine_image
 		machine.machine_type = self.machine_type
-		machine.disk_size = 10  # Default disk size for new machines
+		machine.root_disk_size = 10  # Default root disk size for new machines
 		machine.save()
 		return StepStatus.Success
 
