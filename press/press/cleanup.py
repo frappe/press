a77c2c644ba889e475a31b0065d1fe602ad8d6ec
Aditya Hase <aditya@adityahase.com>
2025-01-14 14:36:57 +0530

    style: Ruff

--- press/press/cleanup.py
+++ press/press/cleanup.py
@@ -21,7 +21,7 @@ def unlink_remote_files_from_site():
 		"remote_private_file",
 	]
 	sites = frappe.get_all(
-		"Site", fields=["name"] + fields, filters=filters, or_filters=or_filters, pluck="name"
+		"Site", fields=["name", *fields], filters=filters, or_filters=or_filters, pluck="name"
 	)
 
 	# s3 uploads.frappe.cloud has a 1 day expiry rule for all objects, so we'll unset those files here
