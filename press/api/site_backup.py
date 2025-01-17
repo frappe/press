c699c65b74c26794089fe0d767672c0249b789be
tanmoysrt <57363826+tanmoysrt@users.noreply.github.com>
2025-01-17 14:26:26 +0530

    refactor(site-backup): In snapshot create api set admin user temporarily
    We are validating the requests by provided request key
    Instead of adding ignore_permissions, temporarily set user to Admin
    Then, reset it back

--- press/api/site_backup.py
+++ press/api/site_backup.py
@@ -14,11 +14,18 @@ def create_snapshot(name: str, key: str):
 	Because we need to hold the lock on the database for the duration of the backup.
 	Only after the snapshot is created, the agent will release the lock on the database.
 	"""
-	site_backup: SiteBackup = frappe.get_doc("Site Backup", name)
-	if not (key and site_backup.snapshot_request_key == key):
-		frappe.throw("Invalid key for snapshot creation")
-	site_backup.create_database_snapshot()
-	site_backup.reload()
-	# Re-verify if the snapshot was created and linked to the site backup
-	if not site_backup.database_snapshot:
-		frappe.throw("Failed to create a snapshot for the database server")
+	current_user = frappe.session.user
+	try:
+		frappe.set_user("Administrator")
+		site_backup: SiteBackup = frappe.get_doc("Site Backup", name)
+		if not (key and site_backup.snapshot_request_key == key):
+			frappe.throw("Invalid key for snapshot creation")
+		site_backup.create_database_snapshot()
+		site_backup.reload()
+		# Re-verify if the snapshot was created and linked to the site backup
+		if not site_backup.database_snapshot:
+			frappe.throw("Failed to create a snapshot for the database server")
+	except Exception as e:
+		raise e
+	finally:
+		frappe.set_user(current_user)
