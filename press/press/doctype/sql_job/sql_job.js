c5fb018cad6a1d3ebba1d0755523a2255048ef0d
tanmoysrt <57363826+tanmoysrt@users.noreply.github.com>
2025-02-05 17:14:48 +0530

    feat(sql-job): Add some buttons for frontend

--- press/press/doctype/sql_job/sql_job.js
+++ press/press/doctype/sql_job/sql_job.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2025, Frappe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("SQL Job", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('SQL Job', {
+	refresh(frm) {
+		[
+			[__('Start'), 'start', frm.doc.status === 'Pending', true],
+			[__('Cancel'), 'cancel', frm.doc.status === 'Running', true],
+		].forEach(([label, method, condition, grouped]) => {
+			if (condition) {
+				frm.add_custom_button(
+					label,
+					() => {
+						frappe.confirm(
+							`Are you sure you want to ${label.toLowerCase()}?`,
+							() => frm.call(method).then(() => frm.refresh()),
+						);
+					},
+					grouped ? __('Actions') : null,
+				);
+			}
+		});
+	},
+});
