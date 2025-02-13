0becaed2672b6f5493ba1b080c9dcc821c2fc13e
Arun Mathai <me@arunmathaisk.in>
2025-02-13 09:46:01 +0300

    feat(marketplace): Change marketplace app github repository visibility

--- press/press/report/marketplace_app_repository_visibility/marketplace_app_repository_visibility.js
+++ press/press/report/marketplace_app_repository_visibility/marketplace_app_repository_visibility.js
@@ -3,4 +3,17 @@
 
 frappe.query_reports['Marketplace App Repository Visibility'] = {
 	filters: [],
+	onload: async function (report) {
+		report.page.add_inner_button(__('Send Email to Developers'), () => {
+			frappe.confirm('Are you sure you want to send out the e-mails?', () => {
+				frappe.xcall(
+					'press.press.report.marketplace_app_repository_visibility.marketplace_app_repository_visibility.send_emails',
+					{
+						columns: JSON.stringify(report.columns),
+						data: JSON.stringify(report.data),
+					},
+				);
+			});
+		});
+	},
 };
