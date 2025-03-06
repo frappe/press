4cf75cb9b8afdabd671b17c01670922293ece92a
tanmoysrt <57363826+tanmoysrt@users.noreply.github.com>
2025-03-06 18:03:15 +0530

    Revert "refactor: Use single query to check for 2fa"
    This reverts commit 51003f9c53f59c2489379fbd446af177314618ca.

--- press/overrides.py
+++ press/overrides.py
@@ -78,12 +78,10 @@ def on_session_creation():
 
 def on_login(login_manager):
 	user = login_manager.user
-	has_2fa = frappe.db.get_value(
-		"User 2FA", {"user": user, "enabled": 1}, ["last_verified_at"], as_dict=True
-	)
-	if (has_2fa and not has_2fa.get("last_verified_at")) or has_2fa.get(
-		"last_verified_at"
-	) < frappe.utils.add_to_date(None, seconds=-10):
+	if frappe.db.get_value("User 2FA", {"user": user, "enabled": 1}) and not frappe.db.get_value(
+		"User 2FA",
+		{"user": user, "enabled": 1, "last_verified_at": (">", frappe.utils.add_to_date(None, seconds=-30))},
+	):
 		frappe.throw("Please re-login to verify your identity.")
 
 
