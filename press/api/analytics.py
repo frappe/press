e4a5c45562edcdeb26ffae2f3c5408aa367c01fa
Balamurali M <balamurali@frappe.io>
2025-03-11 11:21:03 +0530

    chore(analytics): Add other bucket only when enough data is there

--- press/api/analytics.py
+++ press/api/analytics.py
@@ -245,7 +245,7 @@ class StackedGroupByChart:
 		for path_bucket in aggs.method_path.buckets:
 			datasets.append(self.get_histogram_chart(path_bucket, labels))
 
-		if datasets:
+		if len(datasets) >= self.MAX_NO_OF_PATHS:
 			datasets.append(self.get_other_bucket(datasets, labels))
 
 		if self.normalize_slow_logs:
