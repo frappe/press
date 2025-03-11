48bac217e69564e26d5bf79ca13a7fa69e615d9e
Saurabh <saurabh6790@gmail.com>
2025-03-11 19:52:54 +0530

    feat: Provision to configure custom TLS certificate

--- press/press/doctype/tls_certificate/tls_certificate.js
+++ press/press/doctype/tls_certificate/tls_certificate.js
@@ -3,13 +3,6 @@
 
 frappe.ui.form.on('TLS Certificate', {
 	refresh: function (frm) {
-		frm.add_custom_button(__('Obtain Certificate'), () => {
-			frm.call({
-				method: 'obtain_certificate',
-				doc: frm.doc,
-				callback: (result) => frm.refresh(),
-			});
-		});
 		if (frm.doc.wildcard) {
 			frm.add_custom_button(__('Trigger Callback'), () => {
 				frm.call({
@@ -19,6 +12,21 @@ frappe.ui.form.on('TLS Certificate', {
 				});
 			});
 		}
+
+		frm.trigger('show_obtain_certificate');
+		frm.trigger('toggle_read_only');
+		frm.trigger('toggle_hidden');
+		frm.trigger('toggle_copy_private_key');
+	},
+
+	custom: function (frm) {
+		frm.trigger('show_obtain_certificate');
+		frm.trigger('toggle_read_only');
+		frm.trigger('toggle_hidden');
+		frm.trigger('toggle_copy_private_key');
+	},
+
+	toggle_copy_private_key: function (frm) {
 		if (!frm.doc.wildcard) {
 			frm.add_custom_button('Copy Private Key', () => {
 				frappe.confirm(
@@ -31,5 +39,41 @@ frappe.ui.form.on('TLS Certificate', {
 				);
 			});
 		}
+
+		if (frm.doc.custom) {
+			frm.remove_custom_button('Copy Private Key');
+		}
+	},
+
+	show_obtain_certificate: function (frm) {
+		if (!frm.doc.custom) {
+			frm.add_custom_button(__('Obtain Certificate'), () => {
+				frm.call({
+					method: 'obtain_certificate',
+					doc: frm.doc,
+					callback: (result) => frm.refresh(),
+				});
+			});
+		}
+	},
+
+	toggle_read_only: function (frm) {
+		let fields = [
+			'certificate',
+			'private_key',
+			'intermediate_chain',
+			'full_chain',
+			'issued_on',
+			'expires_on',
+		];
+		fields.forEach(function (field) {
+			frm.set_df_property(field, 'read_only', !frm.doc.custom);
+			frm.refresh_field(field);
+		});
+	},
+
+	toggle_hidden: function (frm) {
+		frm.set_df_property('private_key', 'hidden', !frm.doc.custom);
+		frm.refresh_field('private_key');
 	},
 });
