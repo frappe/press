8b8952fbfdcd8e0c9eec4925f64214d56e353812
Ankush Menat <ankush@frappe.io>
2025-03-26 19:32:42 +0530

    fix: Periodically cleanup ansible tmp files
    extends https://github.com/frappe/press/pull/2559
    alternate to https://github.com/frappe/press/commit/d90dfb50eba4bea18c54ea6f4372f22d1928b310
    
    - Ansible creates tmp files while running plays
    - The files are usually deleted but tmp directories in which the file got
      created is often left out, so that slowly grows (not THAT fast afaik)
    
    This new job clears all directories older than 1 hour, every hour. This
    avoids problem with racey deletes since most ansible plays won't be
    taking >1 hour.
    
    Things to review:
    - threshold logic
    - "growth rate" - if it's too high then frequency might not be enough.

--- press/hooks.py
+++ press/hooks.py
@@ -200,6 +200,7 @@ scheduler_events = {
 		"press.press.doctype.site.backups.schedule_for_sites_with_backup_time",
 		"press.press.doctype.tls_certificate.tls_certificate.renew_tls_certificates",
 		"press.saas.doctype.product_trial_request.product_trial_request.expire_long_pending_trial_requests",
+		"press.overrides.cleanup_ansible_tmp_files",
 	],
 	"hourly_long": [
 		"press.press.doctype.release_group.release_group.prune_servers_without_sites",
