28ba600665eff61c297cc4afa4a4f18fcb964a36
Aditya Hase <aditya@adityahase.com>
2025-03-26 12:18:41 +0530

    fix(deploy-candidate): Divide large list of packages into smaller chunks
    We're using Data field for storing package/dependency names
    
    Large list of dependencies doesn't fit in this field
    e.g. https://source.chromium.org/chromium/chromium/src/+/main:chrome/installer/linux/debian/dist_package_versions.json
    
    Fixes https://github.com/frappe/press/issues/2487

--- press/press/doctype/deploy_candidate/deploy_candidate.py
+++ press/press/doctype/deploy_candidate/deploy_candidate.py
@@ -1070,6 +1070,7 @@ class DeployCandidate(Document):
 
 			app_packages = []
 			for p in pkgs:
+				p = p.strip()
 				if p in existing_apt_packages:
 					continue
 				existing_apt_packages.add(p)
@@ -1078,7 +1079,30 @@ class DeployCandidate(Document):
 			if not app_packages:
 				continue
 
-			package = dict(package_manager="apt", package=" ".join(app_packages))
+			self._add_packages(app_packages)
+
+	def __prepare_chunks(self, packages: list[str]):
+		"""Chunk packages into groups of 140 characters"""
+		# Start with one empty chunk
+		chunks = [[]]
+		for package in packages:
+			# Appending the package to the last chunk will keep it under 140
+			# Append package to last chunk
+			if len(" ".join(chunks[-1] + [package])) < 140:
+				chunks[-1].append(package)
+			# Appending the package to the last chunk will make it larger than 140
+			# Add package in a new chunk
+			else:
+				if len(package) > 140:
+					raise frappe.ValidationError(
+						f"Package {package} is too long to be added to the Dockerfile"
+					)
+				chunks.append([package])
+		return chunks
+
+	def _add_packages(self, packages: list[str]):
+		for chunk in self.__prepare_chunks(packages):
+			package = dict(package_manager="apt", package=" ".join(chunk))
 			self.append("packages", package)
 
 	def _set_additional_packages(self):
