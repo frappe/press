f0070f125457415fc2dd06012a3da9e4a7676e62
Aradhya-Tripathi <aradhya@frappe.io>
2025-04-07 17:39:56 +0530

    fix(clone): Repo clones

--- press/press/doctype/remote_build/remote_build.py
+++ press/press/doctype/remote_build/remote_build.py
@@ -15,7 +15,9 @@ from press.press.doctype.deploy_candidate.utils import get_package_manager_files
 from press.utils import get_current_team
 
 if typing.TYPE_CHECKING:
+	from press.press.doctype.app_release.app_release import AppRelease
 	from press.press.doctype.deploy_candidate.deploy_candidate import DeployCandidate
+	from press.press.doctype.deploy_candidate_app.deploy_candidate_app import DeployCandidateApp
 
 
 class RemoteBuild(Document):
@@ -37,6 +39,40 @@ class RemoteBuild(Document):
 		no_push: DF.Check
 		status: DF.Literal["Pending", "Preparing", "Running", "Success", "Failure"]
 	# end: auto-generated types
+
+	def set_build_step(
+		self,
+		step_name: str,
+		step_status: str,
+		step_cached: bool = False,
+		step_output: str | None = None,
+		step_command: str | None = None,
+	):
+		"""Update the step or add the step to the build steps list"""
+		step_data = {
+			"step": step_name,
+			"step_output": step_output,
+			"step_cached": step_cached,
+			"step_status": step_status,
+			"step_command": step_command,
+		}
+
+		for idx, step in enumerate(self.build_steps):
+			if step.get("step") == step_name:
+				step_data["step_output"] = step_output if step_output is not None else step.get("step_output")
+				step_data["step_cached"] = (
+					step_cached if step_cached is not None else step.get("step_cached", False)
+				)
+				step_data["step_status"] = (
+					step_status if step_status is not None else step.get("step_status", "Pending")
+				)
+				step_data["step_command"] = (
+					step_command if step_command is not None else step.get("step_command")
+				)
+				self.build_steps[idx] = step_data
+				return
+
+		self.append("build_steps", step_data)
 
 	@cached_property
 	def candidate(self) -> DeployCandidate:
@@ -72,10 +108,46 @@ class RemoteBuild(Document):
 
 		return None
 
+	def _clone_app(self, app: DeployCandidateApp):
+		step_name = f"Clone Repository {app.app}"
+		command = f"git clone {app.app}"
+		source, cloned = frappe.get_value("App Release", app.release, ["clone_directory", "cloned"])
+
+		if cloned and os.path.exists(source):
+			self.set_build_step(
+				step_name=step_name,
+				step_status="Success",
+				step_cached=True,
+				step_command=command,
+				step_output="",
+			)
+		else:
+			self.set_build_step(step_name, "Running", command=command)
+			release: AppRelease = frappe.get_doc("App Release", app.release, for_update=True)
+			source = release._clone(force=True)
+			self.set_build_step(step_name, "Success", command=command)
+
+		target = os.path.join(self.candidate.build_directory, "apps", app.app)
+		shutil.copytree(source, target, symlinks=True)
+
+		if app.pullable_release:
+			source = frappe.get_value("App Release", app.pullable_release, "clone_directory")
+			target = os.path.join(self.build_directory, "app_updates", app.app)
+			# don't know why
+			shutil.copytree(source, target, symlinks=True)
+
+		return target
+
+	def _clone_repositories(self):
+		repo_path_map = {}
+
+		for app in self.candidate.apps:
+			repo_path_map[app.app] = self._clone_app(app)
+
+		return repo_path_map
+
 	def _prepare_build_context(self):
-		# TODO: Fix this we need to find a way to bypass build step in this case
-		# And add our own step
-		repo_path_map = self.candidate._clone_repos()
+		repo_path_map = self._clone_repositories()
 		pmf = get_package_manager_files(repo_path_map)
 		self.candidate._run_prebuild_validations_and_update_step(pmf)
 
@@ -112,7 +184,7 @@ class RemoteBuild(Document):
 			self.candidate._set_app_cached_flags()
 
 		self.candidate._prepare_build_directory()
-		self.candidate._prepare_build_context(self.no_push)
+		self._prepare_build_context()
 
 	def _run_agent_jobs(self):
 		for file in os.scandir(self.candidate.build_directory):
@@ -128,11 +200,11 @@ class RemoteBuild(Document):
 
 	def _build(self):
 		self.status = "Preparing"
-		try:
-			self._prepare_build()
-			self._start_build()
-		except Exception as exc:
-			print(exc)
+		# try:
+		self._prepare_build()
+		self._start_build()
+		# except Exception as exc:
+		# 	print(exc)
 
 	def reset_build_steps(self):
 		self.build_steps.clear()
