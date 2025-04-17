7600cfa39f95dbb4baf5398a01a087e4c18058ae
Suhail <63963181+BreadGenie@users.noreply.github.com>
2025-04-17 13:53:54 +0530

    refactor(signup): Pre-fill setup wizard data on signup (#2608)
    * refactor(signup): Pre-fill setup wizard data on signup
    
    * fix: remove unused code
    
    * fix: add currency to setup wizard
    
    * fix: send more info for setup wizard
    
    * refactor: rename endpoint for prefilling setup wizard
    
    * refactor: pass in params explicitly
    
    * fix: handle case for non standby sites

--- press/saas/doctype/product_trial_request/product_trial_request.py
+++ press/saas/doctype/product_trial_request/product_trial_request.py
@@ -3,7 +3,6 @@
 
 from __future__ import annotations
 
-import json
 import urllib
 import urllib.parse
 from contextlib import suppress
@@ -12,20 +11,14 @@ from typing import TYPE_CHECKING
 import frappe
 from frappe.model.document import Document
 from frappe.utils.data import add_to_date, now_datetime
-from frappe.utils.momentjs import get_all_timezones
-from frappe.utils.password import decrypt as decrypt_password
-from frappe.utils.password import encrypt as encrypt_password
-from frappe.utils.password_strength import test_password_strength
-from frappe.utils.safe_exec import safe_exec
 from frappe.utils.telemetry import init_telemetry
 
-from press.agent import Agent
 from press.api.client import dashboard_whitelist
-from press.saas.doctype.product_trial.product_trial import ProductTrial
 from press.utils import log_error
 
 if TYPE_CHECKING:
 	from press.press.doctype.site.site import Site
+	from press.saas.doctype.product_trial.product_trial import ProductTrial
 
 
 class ProductTrialRequest(Document):
@@ -41,14 +34,13 @@ class ProductTrialRequest(Document):
 		agent_job: DF.Link | None
 		domain: DF.Data | None
 		product_trial: DF.Link | None
-		signup_details: DF.JSON | None
 		site: DF.Link | None
 		site_creation_completed_on: DF.Datetime | None
 		site_creation_started_on: DF.Datetime | None
 		status: DF.Literal[
 			"Pending",
 			"Wait for Site",
-			"Completing Setup Wizard",
+			"Prefilling Setup Wizard",
 			"Adding Domain",
 			"Site Created",
 			"Error",
@@ -107,8 +99,8 @@ class ProductTrialRequest(Document):
 					self.capture_posthog_event("product_trial_request_failed")
 				case "Wait for Site":
 					self.capture_posthog_event("product_trial_request_initiated_site_creation")
-				case "Completing Setup Wizard":
-					self.capture_posthog_event("product_trial_request_started_setup_wizard_completion")
+				case "Prefilling Setup Wizard":
+					self.capture_posthog_event("product_trial_request_started_setup_wizard_prefilled")
 				case "Site Created":
 					self.capture_posthog_event("product_trial_request_site_created")
 
@@ -119,49 +111,36 @@ class ProductTrialRequest(Document):
 
 	@frappe.whitelist()
 	def get_setup_wizard_payload(self):
-		data = frappe.get_value(
-			"Product Trial",
-			self.product_trial,
-			["setup_wizard_completion_mode", "setup_wizard_payload_generator_script"],
-			as_dict=True,
-		)
-		if data.setup_wizard_completion_mode != "auto":
-			frappe.throw("In manual Setup Wizard Completion Mode, the payload cannot be generated")
-		if not data.setup_wizard_payload_generator_script:
-			return {}
-		signup_details = json.loads(self.signup_details)
-		team_details = frappe.get_value(
-			"Team", self.team, ["name", "user", "country", "currency"], as_dict=True
-		)
-		team_user = frappe.get_doc("User", team_details.user)
-		try:
-			_locals = {
-				"decrypt_password": decrypt_password,
-				"team": frappe._dict(
-					{
-						"name": team_details.name,
-						"user": frappe._dict(
-							{
-								"email": team_user.email,
-								"full_name": team_user.full_name or "",
-								"first_name": team_user.first_name or "",
-								"last_name": team_user.last_name or "",
-							}
-						),
-						"country": team_details.country,
-						"currency": team_details.currency,
-					}
-				),
-				"signup_details": frappe._dict(signup_details),
-				"payload": frappe._dict(),
-			}
-			safe_exec(
-				data.setup_wizard_payload_generator_script,
-				_locals=_locals,
-				restrict_commit_rollback=True,
-			)
-			return frappe._dict(_locals.get("payload", {}))
+		import json
 
+		try:
+			team_details = frappe.db.get_value(
+				"Team", self.team, ["name", "user", "country", "currency"], as_dict=True
+			)
+			team_user = frappe.db.get_value(
+				"User", team_details.user, ["first_name", "last_name", "email"], as_dict=True
+			)
+			account_request_geo_data = frappe.db.get_value(
+				"Account Request", self.account_request, "geo_location"
+			)
+			timezone = frappe.parse_json(account_request_geo_data).get("timezone", "Asia/Kolkata")
+
+			return json.dumps(
+				{
+					"email": team_user.email,
+					"first_name": team_user.first_name,
+					"last_name": team_user.last_name,
+				}
+			), json.dumps(
+				{
+					"country": team_details.country,
+					"time_zone": timezone,
+					"language": "en",
+					"currency": team_details.currency,
+					# setup wizard will override currency anyway
+					# but adding this since ERPNext will throw an error otherwise
+				}
+			)
 		except Exception as e:
 			log_error(
 				title="Product Trial Request Setup Wizard Payload Generation Error",
@@ -171,68 +150,19 @@ class ProductTrialRequest(Document):
 			)
 			frappe.throw(f"Failed to generate payload for Setup Wizard: {e}")
 
-	def get_user_login_password_from_signup_details(self) -> str | None:
-		"""
-		Handling the exception because without the password also
-		the site can be created and user can login through saas flow
-
-		Better than failing the site creation process
-		"""
-		try:
-			signup_details = json.loads(self.signup_details)
-			encrypted_password = signup_details.get(ProductTrial.USER_LOGIN_PASSWORD_FIELD)
-			if encrypted_password:
-				return decrypt_password(encrypted_password)
-		except Exception as e:
-			log_error("Failed to get user login password from signup details", data=e)
-		return None
-
-	def validate_signup_fields(self):
-		signup_values = json.loads(self.signup_details)
-		product = frappe.get_doc("Product Trial", self.product_trial)
-		# Validate signup values
-		for field in product.signup_fields:
-			if field.fieldname not in signup_values:
-				if field.required:
-					frappe.throw(f"Required field {field.label} is missing")
-				else:
-					signup_values[field.fieldname] = None
-
-			# validate select field
-			if (
-				field.fieldtype == "Select"
-				and field.fieldname.endswith("_tz")
-				and signup_values[field.fieldname] is not None
-			):
-				# validate timezone specific select field
-				if field.fieldname.endswith("_tz"):
-					if signup_values[field.fieldname] not in get_all_timezones():
-						frappe.throw(f"Invalid timezone {signup_values[field.fieldname]}")
-				# validate generic select field
-				else:
-					options = [option for option in ((field.options or "").split("\n")) if option]
-					if signup_values[field.fieldname] not in options:
-						frappe.throw(f"Invalid value for {field.label}. Please choose a valid option")
-
 	@dashboard_whitelist()
-	def create_site(self, subdomain: str, cluster: str | None = None, signup_values: dict | None = None):
-		if not signup_values:
-			signup_values = {}
+	def create_site(self, subdomain: str, cluster: str | None = None):
+		"""
+		Trigger the site creation process for the product trial request.
+		Args:
+			subdomain (str): The subdomain for the new site.
+			cluster (str | None): The cluster to use for site creation.
+		"""
+		if not subdomain:
+			frappe.throw("Subdomain is required to create a site.")
 
 		try:
-			product = frappe.get_doc("Product Trial", self.product_trial)
-			for field in product.signup_fields:
-				if field.fieldtype == "Password" and field.fieldname in signup_values:
-					password_analysis = test_password_strength(signup_values[field.fieldname])
-					if int(field.min_password_score) > password_analysis["score"]:
-						suggestions = password_analysis["feedback"]["suggestions"]
-						suggestion = suggestions[0] if suggestions else ""
-						frappe.throw(f"{field.label} is easy to guess.\n{suggestion}")
-					signup_values[field.fieldname] = encrypt_password(signup_values[field.fieldname])
-
-			self.signup_details = json.dumps(signup_values)
-			self.validate_signup_fields()
-			product = frappe.get_doc("Product Trial", self.product_trial)
+			product: ProductTrial = frappe.get_doc("Product Trial", self.product_trial)
 			self.status = "Wait for Site"
 			self.site_creation_started_on = now_datetime()
 			self.domain = f"{subdomain}.{product.domain}"
@@ -243,8 +173,8 @@ class ProductTrialRequest(Document):
 			self.site = site.name
 			self.save()
 
-			if is_standby_site and product.setup_wizard_completion_mode == "auto":
-				self.complete_setup_wizard()
+			if is_standby_site:
+				self.prefill_setup_wizard_data()
 
 			user_mail = frappe.db.get_value("Team", self.team, "user")
 			frappe.get_doc(
@@ -269,7 +199,7 @@ class ProductTrialRequest(Document):
 
 	@dashboard_whitelist()
 	def get_progress(self, current_progress=None):  # noqa: C901
-		current_progress = current_progress or 0
+		current_progress = current_progress or 10
 		if self.agent_job:
 			filters = {"name": self.agent_job, "site": self.site}
 		else:
@@ -287,7 +217,6 @@ class ProductTrialRequest(Document):
 			return {"progress": 80, "current_step": self.status}
 
 		if status == "Running":
-			mode = frappe.get_value("Product Trial", self.product_trial, "setup_wizard_completion_mode")
 			steps = frappe.db.get_all(
 				"Agent Job Step",
 				filters={"agent_job": job_name},
@@ -296,8 +225,7 @@ class ProductTrialRequest(Document):
 			)
 			done = [s for s in steps if s.status in ("Success", "Skipped", "Failure")]
 			steps_count = len(steps)
-			if mode == "auto":
-				steps_count += 1
+			steps_count += 1
 			progress = (len(done) / steps_count) * 100
 			progress = max(progress, current_progress)
 			current_running_step = ""
@@ -315,15 +243,25 @@ class ProductTrialRequest(Document):
 		# If agent job is undelivered, pending
 		return {"progress": current_progress + 0.1}
 
-	def complete_setup_wizard(self):
-		if self.status == "Completing Setup Wizard":
+	def prefill_setup_wizard_data(self):
+		if self.status == "Prefilling Setup Wizard":
 			return
 		site = frappe.get_doc("Site", self.site)
-		agent = Agent(site.server)
-		agent.complete_setup_wizard(site, self.get_setup_wizard_payload())
-		self.reload()
-		self.status = "Completing Setup Wizard"
-		self.save(ignore_permissions=True)
+		try:
+			user_payload, system_settings_payload = self.get_setup_wizard_payload()
+			site.prefill_setup_wizard(system_settings_payload, user_payload)
+			if self.site != self.domain:
+				self.status = "Prefilling Setup Wizard"
+				self.save()
+		except Exception as e:
+			log_error(
+				title="Product Trial Request Prefill Setup Wizard Error",
+				data=e,
+				reference_doctype=self.doctype,
+				reference_name=self.name,
+			)
+			self.status = "Error"
+			self.save()
 
 	@dashboard_whitelist()
 	def get_login_sid(self):
