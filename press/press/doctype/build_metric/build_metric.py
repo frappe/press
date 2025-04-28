5a322c20e20a3bf3b0b88bb0aad1da9f8c12add0
Aradhya-Tripathi <aradhya@frappe.io>
2025-04-28 11:46:10 +0530

    feat(metrics): Add metric for context and package durations

--- press/press/doctype/build_metric/build_metric.py
+++ press/press/doctype/build_metric/build_metric.py
@@ -13,6 +13,13 @@ from frappe.model.document import Document
 if typing.TYPE_CHECKING:
 	from frappe.utils import DateTimeLikeObject
 
+	from press.press.doctype.build_metric.build_metric_types import (
+		ContextDurationType,
+		DeployCandidateBuildType,
+		DurationType,
+		FailedBuildType,
+	)
+
 
 class BuildMetric(Document):
 	# begin: auto-generated types
@@ -25,46 +32,6 @@ class BuildMetric(Document):
 
 		metric_dump: DF.JSON | None
 	# end: auto-generated types
-
-
-class DeployCandidateBuildType(typing.TypedDict):
-	name: str
-	group: str
-	status: typing.Literal[
-		"Scheduled",
-		"Success",
-		"Failure",
-		"Preparing",
-		"Pending",
-		"Running",
-	]
-	creation: DateTimeLikeObject
-	error_key: str
-	retry_count: int
-	build_output: str
-	build_error: str
-	build_duration: DateTimeLikeObject
-	manually_failed: bool
-	user_addressable_failure: bool
-	pending_duration: DateTimeLikeObject
-	user_addressable_failure: bool
-	platform: str
-
-
-class FailedBuildType(typing.TypedDict):
-	user_failure: list[DeployCandidateBuildType]
-	fc_failure: list[DeployCandidateBuildType]
-	fc_manually_failed: list[DeployCandidateBuildType]
-
-
-class DurationType(typing.TypedDict):
-	avg_build_duration: float
-	avg_pending_duration: float
-
-
-class ContextDurationType(typing.TypedDict):
-	avg_package_duration: float
-	avg_upload_duration: float
 
 
 @dataclass
