c4774d1f6e22e3c7419236e1c46989da4ab815af
Aradhya-Tripathi <aradhya@frappe.io>
2025-05-11 14:45:25 +0530

    fix(env-var): Changed deploy candidate env var field from data to text
    to accommodate large text values

--- press/press/doctype/deploy_candidate_variable/deploy_candidate_variable.py
+++ press/press/doctype/deploy_candidate_variable/deploy_candidate_variable.py
@@ -18,7 +18,7 @@ class DeployCandidateVariable(Document):
 		parent: DF.Data
 		parentfield: DF.Data
 		parenttype: DF.Data
-		value: DF.Data
+		value: DF.Text
 	# end: auto-generated types
 
 	pass
