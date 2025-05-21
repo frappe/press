b20d32786f8de4718b7ce58f28ce49e5698a121f
Aradhya-Tripathi <aradhya@frappe.io>
2025-05-21 13:16:46 +0530

    feat(image): Pull docker images for all builds on server

--- press/infrastructure/doctype/arm_build_record/arm_build_record.py
+++ press/infrastructure/doctype/arm_build_record/arm_build_record.py
@@ -3,10 +3,16 @@
 
 from __future__ import annotations
 
+import typing
+
 import frappe
 from frappe.model.document import Document
 
+from press.agent import Agent
 from press.press.doctype.deploy_candidate_build.deploy_candidate_build import Status
+
+if typing.TYPE_CHECKING:
+	from press.press.doctype.agent_job.agent_job import AgentJob
 
 
 class ARMBuildRecord(Document):
@@ -23,7 +29,6 @@ class ARMBuildRecord(Document):
 		)
 
 		arm_images: DF.Table[VirtualMachineARMImage]
-		name: DF.Int | None
 		virtual_machine: DF.Link
 	# end: auto-generated types
 
@@ -38,10 +43,16 @@ class ARMBuildRecord(Document):
 				arm_image.status = current_status
 				arm_image.save()
 
-	@frappe.whitelist()
-	def pull_images(self):
-		for arm_image in self.arm_images:
-			if arm_image.status != Status.SUCCESS.value:
-				continue
+	def _pull_images(self, image_tags: list[str]) -> AgentJob:
+		return Agent(self.virtual_machine).pull_docker_images(
+			image_tags, reference_doctype=self.doctype, reference_name=self.name
+		)
 
-			# Pull here in queue
+	@frappe.whitelist()
+	def pull_images(self) -> None:
+		builds = [image.build for image in self.arm_images if image.status == Status.SUCCESS.value]
+		image_tags = frappe.db.get_all(
+			"Deploy Candidate Build", {"name": ("in", builds)}, pluck="docker_image"
+		)
+
+		self._pull_images(image_tags)
