1b71f3423a418aaccd8893134ec03d84de197fe7
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2025-05-23 16:18:52 +0530

    feat(prm): Add Partner lead followup

--- press/partner/doctype/partner_lead/partner_lead.py
+++ press/partner/doctype/partner_lead/partner_lead.py
@@ -15,8 +15,12 @@ class PartnerLead(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		from press.partner.doctype.lead_followup.lead_followup import LeadFollowup
+
+		additional_comments: DF.SmallText | None
 		company_name: DF.Data | None
 		contact_no: DF.Data | None
+		conversion_date: DF.Date | None
 		country: DF.Link | None
 		domain: DF.Literal[
 			"",
@@ -31,16 +35,40 @@ class PartnerLead(Document):
 			"Other",
 		]
 		email: DF.Data | None
-		engagement_stage: DF.Literal["Demo"]
+		engagement_stage: DF.Literal[
+			"",
+			"Demo",
+			"Qualification",
+			"Learning",
+			"Follow-up",
+			"Quotation",
+			"Negotiation",
+			"Ready for Closing",
+		]
+		erpnext_success_pack_interested_in: DF.Data | None
 		estimated_closure_date: DF.Date | None
 		feedback_for_frappe: DF.SmallText | None
 		feedback_to_partner: DF.SmallText | None
+		followup: DF.Table[LeadFollowup]
 		full_name: DF.Data | None
 		hosting: DF.Literal["Frappe Cloud", "Self Hosted"]
 		lead_name: DF.Data | None
 		lead_rating: DF.Rating
 		lead_source: DF.Literal["", "Partner Owned", "Passed to Partner", "Partner Listing", "Public Webinar"]
 		lead_type: DF.Link | None
+		lost_reason: DF.Literal[
+			"Lost to Competitor",
+			"No Response",
+			"Budget Constraints",
+			"Partner Rejected",
+			"Trash Lead",
+			"Free User",
+			"Not Interested Anymore",
+			"Other",
+		]
+		lost_reason_specify: DF.Data | None
+		need_demo_before_implementation: DF.Check
+		next_action_date: DF.Date | None
 		organization_name: DF.Data | None
 		origin: DF.Link | None
 		partner_email: DF.Data | None
@@ -52,7 +80,7 @@ class PartnerLead(Document):
 		requirement: DF.Text | None
 		site_url: DF.Data | None
 		state: DF.Data | None
-		status: DF.Literal[None]
+		status: DF.Literal["Open", "In Process", "Won", "Lost", "Junk", "Pass to Other Partner"]
 		territory: DF.Data | None
 	# end: auto-generated types
 
