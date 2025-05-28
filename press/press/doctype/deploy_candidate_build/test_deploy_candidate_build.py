f146ceb306bd2143c580b91b9fe6bd30bbe5edb2
Aradhya-Tripathi <aradhya@frappe.io>
2025-05-28 14:07:02 +0530

    test(deploy-candidate-build): Add test for build flow with multiple server platforms

--- press/press/doctype/deploy_candidate_build/test_deploy_candidate_build.py
+++ press/press/doctype/deploy_candidate_build/test_deploy_candidate_build.py
@@ -2,6 +2,7 @@
 # See license.txt
 
 
+import typing
 import unittest
 from unittest.mock import Mock, patch
 
@@ -19,6 +20,9 @@ from press.press.doctype.release_group.test_release_group import (
 	create_test_release_group,
 )
 from press.press.doctype.server.test_server import create_test_server
+
+if typing.TYPE_CHECKING:
+	from press.press.doctype.deploy_candidate.deploy_candidate import DeployCandidate
 
 
 @patch("press.press.doctype.deploy_candidate.deploy_candidate.frappe.db.commit")
@@ -81,3 +85,56 @@ class TestDeployCandidateBuild(unittest.TestCase):
 		with self.assertRaises(frappe.ValidationError):
 			# Since wkhtmltopdf version is not supported!
 			self.deploy_candidate_build.create_arm_build()
+
+	@patch("press.press.doctype.deploy_candidate.deploy_candidate.frappe.enqueue_doc", new=Mock())
+	@patch("press.press.doctype.deploy_candidate.deploy_candidate.frappe.db.commit", new=Mock())
+	@patch.object(DeployCandidateBuild, "_process_run_build", new=Mock())
+	def test_correct_build_flow(self, mock_enqueue):
+		import json
+
+		app = create_test_app()
+
+		group = create_test_release_group([app], servers=[self.x86_build_server, self.arm_build_server])
+
+		dc: DeployCandidate = group.create_deploy_candidate()
+		deploy_candidate_build_name = dc.build().get("message")
+
+		deploy_candidate_build: DeployCandidateBuild = frappe.get_doc(
+			"Deploy Candidate Build", deploy_candidate_build_name
+		)
+
+		deploy_candidate_build.set_build_server()
+		build_server_platform = frappe.get_value("Server", deploy_candidate_build.build_server, "platform")
+		self.assertEqual(build_server_platform, "x86_64")
+		deploy_candidate_build.status = "Success"
+		deploy_candidate_build.save()
+
+		job = frappe._dict(
+			{"request_data": json.dumps({"deploy_candidate_build": deploy_candidate_build_name})}
+		)
+		deploy_candidate_build.process_run_build(job, response_data=None)
+
+		# Intel build has finished and been processed
+		dc: DeployCandidate = dc.reload()
+		self.assertEqual(dc.intel_build, deploy_candidate_build_name)
+
+		# Auto triggered from process run build of the last dc
+		newly_created_build: DeployCandidateBuild = frappe.get_last_doc(
+			"Deploy Candidate Build", {"deploy_candidate": dc.name}
+		)
+		self.assertEqual(newly_created_build.platform, "arm64")
+
+		job = frappe._dict({"request_data": json.dumps({"deploy_candidate_build": newly_created_build.name})})
+		newly_created_build.status = "Success"
+		newly_created_build.save()
+		newly_created_build.process_run_build(job, response_data=None)
+
+		# ARM Build has finished and been processed
+		dc: DeployCandidate = dc.reload()
+		self.assertEqual(dc.arm_build, newly_created_build.name)
+
+		# Assert no more builds are created
+		test_build: DeployCandidateBuild = frappe.get_last_doc(
+			"Deploy Candidate Build", {"deploy_candidate": dc.name}
+		)
+		self.assertEqual(test_build.name, newly_created_build.name)
