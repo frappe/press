a98a99206d03e30a82865d9b941074043742dcd0
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2025-05-29 16:21:32 +0530

    fix(database): Check for /dev/sda1 as well for db disk

--- press/utils/database.py
+++ press/utils/database.py
@@ -12,7 +12,7 @@ def find_db_disk_info(df_output: str) -> tuple[int, int] | None:
 		data_disk_available = True
 
 	for line in df_output.strip().split("\n"):
-		if not data_disk_available and "/dev/root" not in line:
+		if not data_disk_available and not ("/dev/root" in line or "/dev/sda1" in line):
 			continue
 		if data_disk_available and "/opt/volumes/mariadb" not in line:
 			continue
