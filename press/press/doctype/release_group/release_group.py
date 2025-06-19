ba447786a04bc4c57ae7b96bda9dbce3670ebbfd
Bread Genie <msuhailbh07@gmail.com>
2025-06-19 13:52:24 +0530

    fix(change-branch): for custom apps add new version to the app source if not present

--- press/press/doctype/release_group/release_group.py
+++ press/press/doctype/release_group/release_group.py
@@ -1191,10 +1191,23 @@ class ReleaseGroup(Document, TagHelpers):
 			filters={"repository_url": current_app_source.repository_url, "branch": to_branch},
 			or_filters={"team": current_app_source.team, "public": 1},
 			limit=1,
+			fields=["name", "team", "public"],
 		)
 
 		if required_app_source:
 			required_app_source = required_app_source[0]
+			if not required_app_source.public:
+				required_app_source = frappe.get_doc("App Source", required_app_source.name)
+				# check if the version already exists
+				if not any(vs.version == self.version for vs in required_app_source.versions):
+					required_app_source.append(
+						"versions",
+						{
+							"version": self.version,
+						},
+					)
+					required_app_source.save()
+
 		else:
 			versions = frappe.get_all(
 				"App Source Version", filters={"parent": current_app_source.name}, pluck="version"
