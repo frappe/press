c8c0f2ac8500eac26c7d6bdf2f58af2f66152d29
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2025-06-30 13:03:07 +0530

    feat(prm): Add Website Info on Partner overview

---
+++ dashboard/src/components/partners/WebsiteInfoDialog.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="flex flex-col gap-4">
+		<FormControl
+			label="Website URL"
+			type="data"
+			name="website_url"
+			v-model="domain"
+		/>
+		<FormControl
+			label="Introduction"
+			type="textarea"
+			name="website_url"
+			v-model="domain"
+		/>
+		<FormControl
+			label="Customers"
+			type="textarea"
+			name="website_url"
+			description="Customers should be separated by commas"
+			placeholder="Tesla, Micro Corp, Ford"
+			v-model="domain"
+		/>
+		<Button
+			variant="solid"
+			label="Update Website Info"
+			@click="console.log('Update Website Info')"
+		/>
+	</div>
+</template>
+<script setup>
+import { FormControl } from 'frappe-ui';
+</script>
