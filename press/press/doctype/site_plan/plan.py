dca835ee29f9d3187fe89e485e838e0f4bb32619
Aradhya-Tripathi <aradhya@frappe.io>
2025-07-05 12:22:56 +0530

    feat(server-plan): Use plan change for disabled plans

--- press/press/doctype/site_plan/plan.py
+++ press/press/doctype/site_plan/plan.py
@@ -35,8 +35,11 @@ class Plan(Document):
 		if not fields:
 			fields = ["*"]
 
+		filters.update(
+			{"use_for_plan_change" if filters.get("platform") else "enabled": True},
+		)
+
 		fields.append("`tabHas Role`.role")
-		filters.update({"enabled": True})
 		plans = frappe.get_all(doctype, filters=filters, fields=fields, order_by="price_usd asc")
 		return filter_by_roles(plans)
 
