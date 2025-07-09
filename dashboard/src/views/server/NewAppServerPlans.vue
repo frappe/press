ddad59ec61f15220a71b6d64ec3d634ba7af8153
Aradhya-Tripathi <aradhya@frappe.io>
2025-07-09 18:40:32 +0530

    chore(new-server): Filter arm servers in frankfurt

--- dashboard/src/views/server/NewAppServerPlans.vue
+++ dashboard/src/views/server/NewAppServerPlans.vue
@@ -31,7 +31,10 @@ export default {
 	},
 	computed: {
 		appPlanOptions() {
-			if (this.selectedRegion === 'Mumbai') {
+			if (
+				this.selectedRegion === 'Mumbai' ||
+				this.selectedRegion === 'Frankfurt'
+			) {
 				return this.options.app_plans.filter(
 					(plan) =>
 						plan.cluster == this.selectedRegion && plan.platform == 'arm64',
