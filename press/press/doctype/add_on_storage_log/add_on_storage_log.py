b86c72576e1d13e736094da8e9c9a6a50c1253a7
Aradhya-Tripathi <aradhya@frappe.io>
2025-07-17 22:50:42 +0530

    fix(storage): Ingore insertion if storage log exists

--- press/press/doctype/add_on_storage_log/add_on_storage_log.py
+++ press/press/doctype/add_on_storage_log/add_on_storage_log.py
@@ -25,10 +25,10 @@ class AddOnStorageLog(Document):
 		current_disk_usage: DF.Float
 		database_server: DF.Link | None
 		extend_ec2_play: DF.Link | None
-		has_auto_increased: DF.Check
+		is_auto_triggered: DF.Check
+		is_warning: DF.Check
 		mountpoint: DF.Data | None
 		notification_sent: DF.Check
-		reason: DF.SmallText | None
 		server: DF.Link | None
 	# end: auto-generated types
 
@@ -47,14 +47,15 @@ class AddOnStorageLog(Document):
 				),
 			},
 		)
-		if logs_created_today:
+		if logs_created_today and self.is_auto_triggered:
+			# Don't allow creation of more than 1 auto triggered log a day
 			frappe.throw("A log for this machine already exists")
 
 	def validate(self):
 		self.validate_existing_logs()
 
 	def send_notification(self):
-		"""Send add on storage notification"""
+		"""Send add on storage notification / warning"""
 		server: Server | DatabaseServer = frappe.get_cached_doc(
 			"Server" if self.server else "Database Server", self.server or self.database_server
 		)
@@ -63,15 +64,87 @@ class AddOnStorageLog(Document):
 		frappe.sendmail(
 			recipients=notify_email,
 			subject=f"Important: Server {server.name} storage space at 90%",
-			template="enabled_auto_disk_expansion",
+			template="enabled_auto_disk_expansion" if not self.is_warning else "disabled_auto_disk_expansion",
 			args={
 				"server": server.name,
 				"current_disk_usage": f"{self.current_disk_usage} GiB",
 				"available_disk_space": f"{self.available_disk_space} GiB",
 				"increase_by": f"{self.adding_storage} GiB",
+				"used_storage_percentage": "90%",
 			},
 		)
 
+		self.notification_sent = True
+		self.save()
 
-def get_teams_to_send_notifications_to():
-	frappe.get_all("Add On Storage Log", {"notification_sent": 0}, pluck="name")
+
+def check_existing_logs(server: str, database_server: str, is_auto_triggered: bool) -> bool:
+	logs_created_today = frappe.get_value(
+		"Add On Storage",
+		{
+			"server": server,
+			"database_server": database_server,
+			"creation": (
+				"between",
+				[
+					frappe.utils.add_to_date(days=-1),
+					frappe.utils.now(),
+				],
+			),
+		},
+	)
+	return logs_created_today and is_auto_triggered
+
+
+def insert_addon_storage_log(
+	adding_storage: float,
+	available_disk_space: float,
+	current_disk_usage: float,
+	mountpoint: str,
+	notification_sent: bool,
+	is_auto_triggered: bool,
+	is_warning: bool,
+	database_server: str | None = None,
+	server: str | None = None,
+	ignore_if_exists: bool = False,
+) -> AddOnStorageLog | None:
+	if ignore_if_exists and check_existing_logs(server, database_server, is_auto_triggered):
+		return None
+
+	add_on_storage_log: AddOnStorageLog = frappe.get_doc(
+		{
+			"doctype": "Add On Storage Log",
+			"adding_storage": adding_storage,
+			"available_disk_space": available_disk_space,
+			"current_disk_usage": current_disk_usage,
+			"mountpoint": mountpoint,
+			"notification_sent": notification_sent,
+			"is_auto_triggered": is_auto_triggered,
+			"is_warning": is_warning,
+			"server": server,
+			"database_server": database_server,
+		}
+	)
+	add_on_storage_log.insert()
+	return add_on_storage_log
+
+
+def send_disk_extention_notification():
+	logs = frappe.get_all(
+		"Add On Storage Log",
+		{
+			"notification_sent": 0,
+			"creation": ("between", [frappe.utils.add_to_date(days=-1), frappe.utils.now()]),
+		},
+		pluck="name",
+		limit=100,
+	)
+
+	for log in logs:
+		add_on_storage_log: AddOnStorageLog = frappe.get_doc("Add On Storage Log", log)
+		if (
+			add_on_storage_log.extend_ec2_play
+			and frappe.get_value("Ansible Play", add_on_storage_log.extend_ec2_play, "status") == "Success"
+		):
+			# Only send notification if extend ec2 play has run successfully
+			add_on_storage_log.send_notification()
--- press/press/doctype/server/server.py
+++ press/press/doctype/server/server.py
@@ -25,6 +25,9 @@ from press.agent import Agent
 from press.api.client import dashboard_whitelist
 from press.exceptions import VolumeResizeLimitError
 from press.overrides import get_permission_query_conditions_for_doctype
+from press.press.doctype.add_on_storage_log.add_on_storage_log import (
+	insert_addon_storage_log,
+)
 from press.press.doctype.ansible_console.ansible_console import AnsibleAdHoc
 from press.press.doctype.resource_tag.tag_helpers import TagHelpers
 from press.runner import Ansible
@@ -33,7 +36,6 @@ from press.utils import fmt_timedelta, log_error
 
 if typing.TYPE_CHECKING:
 	from press.infrastructure.doctype.arm_build_record.arm_build_record import ARMBuildRecord
-	from press.press.doctype.add_on_storage_log.add_on_storage_log import AddOnStorageLog
 	from press.press.doctype.ansible_play.ansible_play import AnsiblePlay
 	from press.press.doctype.bench.bench import Bench
 	from press.press.doctype.database_server.database_server import DatabaseServer
@@ -133,7 +135,7 @@ class BaseServer(Document, TagHelpers):
 		server: str,
 		increment: int,
 		mountpoint: str | None = None,
-		is_auto_increase: bool = False,
+		is_auto_triggered: bool = False,
 		current_disk_usage: int | None = None,
 	) -> None:
 		storage_parameters = {
@@ -143,29 +145,50 @@ class BaseServer(Document, TagHelpers):
 			"current_disk_usage": current_disk_usage
 			or round((self.disk_capacity(mountpoint) - self.free_space(mountpoint)) / 1024 / 1024 / 1024, 2),
 			"mountpoint": mountpoint or self.guess_data_disk_mountpoint(),
-			"reason": "Auto trigged by frappe cloud"
-			if is_auto_increase
-			else f"Triggered by {frappe.session.user}",
+			is_auto_triggered: is_auto_triggered,
 		}
 
 		storage_parameters.update({"database_server" if server[0] == "m" else "server": server})
 
 		if server == self.name:
 			if increment:
-				add_on_storage_log: AddOnStorageLog = frappe.get_doc(storage_parameters)
-				add_on_storage_log.insert(ignore_permissions=True)
+				add_on_storage_log = insert_addon_storage_log(
+					adding_storage=increment,
+					available_disk_space=round((self.disk_capacity(mountpoint) / 1024 / 1024 / 1024), 2),
+					current_disk_usage=current_disk_usage
+					or round(
+						(self.disk_capacity(mountpoint) - self.free_space(mountpoint)) / 1024 / 1024 / 1024, 2
+					),
+					mountpoint=mountpoint or self.guess_data_disk_mountpoint(),
+					is_auto_triggered=is_auto_triggered,
+					ignore_if_exists=True,
+				)
 
-			self.increase_disk_size(increment=increment, mountpoint=mountpoint, log=add_on_storage_log.name)
+			self.increase_disk_size(
+				increment=increment,
+				mountpoint=mountpoint,
+				log=add_on_storage_log.name if add_on_storage_log else None,
+			)
 			self.create_subscription_for_storage(increment)
 		else:
 			server_doc: DatabaseServer = frappe.get_doc("Database Server", server)
-
 			if increment:
-				add_on_storage_log: AddOnStorageLog = frappe.get_doc(storage_parameters)
-				add_on_storage_log.insert(ignore_permissions=True)
+				add_on_storage_log = insert_addon_storage_log(
+					adding_storage=increment,
+					available_disk_space=round((self.disk_capacity(mountpoint) / 1024 / 1024 / 1024), 2),
+					current_disk_usage=current_disk_usage
+					or round(
+						(self.disk_capacity(mountpoint) - self.free_space(mountpoint)) / 1024 / 1024 / 1024, 2
+					),
+					mountpoint=mountpoint or self.guess_data_disk_mountpoint(),
+					is_auto_triggered=is_auto_triggered,
+					ignore_if_exists=True,
+				)
 
 			server_doc.increase_disk_size(
-				increment=increment, mountpoint=mountpoint, log=add_on_storage_log.name
+				increment=increment,
+				mountpoint=mountpoint,
+				log=add_on_storage_log.name if add_on_storage_log else None,
 			)
 			server_doc.create_subscription_for_storage(increment)
 
@@ -662,8 +685,9 @@ class BaseServer(Document, TagHelpers):
 				variables={"restart_mariadb": restart_mariadb, "device": device},
 			)
 			play = ansible.run()
-			frappe.db.set_value("Add On Storage Log", log, "extend_ec2_play", play.name)
-			frappe.db.commit()
+			if log:
+				frappe.db.set_value("Add On Storage Log", log, "extend_ec2_play", play.name)
+				frappe.db.commit()
 		except Exception:
 			log_error("EC2 Volume Extend Exception", server=self.as_dict())
 
@@ -1581,7 +1605,6 @@ node_filesystem_avail_bytes{{instance="{self.name}", mountpoint="{mountpoint}"}}
 
 		buffer = self.size_to_increase_by_for_20_percent_available(mountpoint)
 		server: Server | DatabaseServer = frappe.get_doc(self.doctype, self.name)
-		notify_email = frappe.get_value("Team", server.team, "notify_email")
 		disk_capacity = self.disk_capacity(mountpoint)
 		current_disk_usage = disk_capacity - self.free_space(mountpoint)
 
@@ -1589,38 +1612,28 @@ node_filesystem_avail_bytes{{instance="{self.name}", mountpoint="{mountpoint}"}}
 		disk_capacity = round(disk_capacity / 1024 / 1024 / 1024, 2)
 
 		if not server.auto_increase_storage:
-			storage_parameters = {
-				"doctype": "Add On Storage Log",
-				"adding_storage": additional + buffer,
-				"available_disk_space": round((self.disk_capacity(mountpoint) / 1024 / 1024 / 1024), 2),
-				"current_disk_usage": current_disk_usage
-				or round(
-					(self.disk_capacity(mountpoint) - self.free_space(mountpoint)) / 1024 / 1024 / 1024, 2
-				),
-				"mountpoint": mountpoint or self.guess_data_disk_mountpoint(),
-				"reason": "[Alert] Auto trigged by frappe cloud",
-				"notification_sent": 1,
-				"has_auto_increased": 0,
-			}
-			storage_parameters.update({"database_server" if server[0] == "m" else "server": server})
-
 			telegram.send(
 				f"Not increasing disk (mount point {mountpoint}) on "
 				f"[{self.name}]({frappe.utils.get_url_to_form(self.doctype, self.name)}) "
 				f"by {buffer + additional}G as auto disk increase disabled by user"
 			)
-			frappe.sendmail(
-				recipients=notify_email,
-				subject=f"Important: Server {server.name} has used 90% of the available space",
-				template="disabled_auto_disk_expansion",
-				args={
-					"server": server.name,
-					"current_disk_usage": f"{current_disk_usage} GiB",
-					"available_disk_space": f"{disk_capacity} GiB",
-					"increase_by": f"{buffer + additional} GiB",
-					"used_storage_percentage": "90%",
-				},
+
+			insert_addon_storage_log(
+				adding_storage=additional + buffer,
+				available_disk_space=round((self.disk_capacity(mountpoint) / 1024 / 1024 / 1024), 2),
+				current_disk_usage=current_disk_usage
+				or round(
+					(self.disk_capacity(mountpoint) - self.free_space(mountpoint)) / 1024 / 1024 / 1024, 2
+				),
+				mountpoint=mountpoint or self.guess_data_disk_mountpoint(),
+				notification_sent=False,
+				is_auto_triggered=True,
+				is_warning=True,
+				database_server=server if server[0] == "m" else None,
+				server=server if server[0] == "f" else None,
+				ignore_if_exists=True,  # Create one warning log in a day,
 			)
+
 			return
 
 		telegram.send(
@@ -1633,7 +1646,7 @@ node_filesystem_avail_bytes{{instance="{self.name}", mountpoint="{mountpoint}"}}
 			self.name,
 			buffer + additional,
 			mountpoint,
-			is_auto_increase=True,
+			is_auto_triggered=True,
 			current_disk_usage=current_disk_usage,
 		)
 
