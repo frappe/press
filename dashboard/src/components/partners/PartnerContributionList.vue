c5a0bfda494285f89efc011ced53f50e2b1c3861
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2025-07-28 20:49:33 +0530

    fix(prm): Show partner contributions

---
+++ dashboard/src/components/partners/PartnerContributionList.vue
@@ -0,0 +1,110 @@
+<template>
+	<div class="p-4">
+		<ObjectList :options="partnerInvoices" />
+		<Dialog
+			v-model="invoiceDialog"
+			:options="{
+				size: '4xl',
+				title: 'Invoice Details',
+			}"
+		>
+			<template #body-content>
+				<template v-if="showInvoice">
+					<div
+						v-if="showInvoice.status === 'Empty'"
+						class="text-base text-gray-600"
+					>
+						Nothing to show
+					</div>
+					<InvoiceDetail v-else :invoiceId="showInvoice.name" />
+				</template>
+			</template>
+		</Dialog>
+	</div>
+</template>
+<script setup>
+import ObjectList from '../ObjectList.vue';
+import { computed, ref } from 'vue';
+import InvoiceDetail from './InvoiceDetail.vue';
+
+let showInvoice = ref(null);
+const invoiceDialog = ref(false);
+const partnerInvoices = computed(() => {
+	return {
+		resource() {
+			return {
+				url: 'press.api.partner.get_partner_invoices',
+				auto: true,
+				initialData: [],
+				transform(data) {
+					// console.log(data)
+					return data.map((d) => {
+						return {
+							name: d.name,
+							customer_name: d.customer_name,
+							due_date: d.due_date || '',
+							total_before_discount: d.total_before_discount || '',
+							currency: d.currency || '',
+							status: d.status || '',
+						};
+					});
+				},
+			};
+		},
+		columns: [
+			{
+				label: 'Customer Name',
+				fieldname: 'customer_name',
+			},
+			{
+				label: 'Date',
+				fieldname: 'due_date',
+				format(value) {
+					if (!value) {
+						return '';
+					}
+					return Intl.DateTimeFormat('en-US', {
+						year: 'numeric',
+						month: 'short',
+						day: 'numeric',
+					}).format(new Date(value));
+				},
+			},
+			{
+				label: 'Amount',
+				fieldname: 'total_before_discount',
+			},
+			{
+				label: 'Status',
+				fieldname: 'status',
+				type: 'Badge',
+			},
+			{
+				label: 'Currency',
+				fieldname: 'currency',
+			},
+		],
+		filterControls() {
+			return [
+				{
+					type: 'select',
+					fieldname: 'status',
+					label: 'Status',
+					options: ['', 'Draft', 'Unpaid', 'Paid'],
+				},
+				{
+					type: 'date',
+					fieldname: 'due_date',
+					label: 'Due Date',
+				},
+			];
+		},
+		orderBy: 'creation desc',
+		onRowClick(row) {
+			console.log(row);
+			showInvoice = row;
+			invoiceDialog.value = true;
+		},
+	};
+});
+</script>
