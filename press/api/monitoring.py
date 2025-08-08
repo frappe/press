f7c295464c862a8bef9a2912ffa432db94395f43
Aradhya-Tripathi <aradhya@frappe.io>
2025-08-08 16:46:26 +0530

    chore(monitor): Move to frappe request object

--- press/api/monitoring.py
+++ press/api/monitoring.py
@@ -114,8 +114,8 @@ def alert(*args, **kwargs):
 		user = str(frappe.session.user)
 		monitor_token = frappe.db.get_single_value("Press Settings", "monitor_token", cache=True)
 
-		if kwargs.get("monitor_token") != monitor_token:
-			raise frappe.ValidationError("Invalid credentials")
+		if frappe.request.args.get("monitor_token") != monitor_token:
+			raise Exception
 
 		frappe.set_user("Administrator")
 
