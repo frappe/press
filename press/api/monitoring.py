fd33e4e897728ae2b506f0dafdb77d449102db22
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2025-08-10 10:16:39 +0530

    fix(monitoring): Store the session user at the beginning of function
    Sentry : PRESS-AGJ

--- press/api/monitoring.py
+++ press/api/monitoring.py
@@ -110,13 +110,13 @@ def targets(token=None):
 
 @frappe.whitelist(allow_guest=True, xss_safe=True)
 def alert(*args, **kwargs):
+	user = frappe.session.user
 	try:
 		monitor_token = frappe.db.get_single_value("Press Settings", "monitor_token", cache=True)
 
 		if frappe.request.args.get("key") != monitor_token:
 			raise frappe.ValidationError("Invalid credentials")
 
-		user = str(frappe.session.user)
 		monitor_token = frappe.db.get_single_value("Press Settings", "monitor_token", cache=True)
 
 		if frappe.request.args.get("monitor_token") != monitor_token:
