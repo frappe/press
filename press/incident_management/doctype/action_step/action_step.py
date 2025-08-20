25e744c68efdf30e29f85e0429e0c4ff882cd14e
Aradhya-Tripathi <aradhya@frappe.io>
2025-08-20 15:58:09 +0530

    feat(incident-action): Added action doctype responding to investigation
    (cherry picked from commit bdd67a46ae2a72478ba9e2acb29cce394114cbd9)

---
+++ press/incident_management/doctype/action_step/action_step.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2025, Frappe and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ActionStep(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		method: DF.Data | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		reference_doctype: DF.Link | None
+		reference_name: DF.DynamicLink | None
+	# end: auto-generated types
+
+	pass
