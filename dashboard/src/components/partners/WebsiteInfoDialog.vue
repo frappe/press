972ea4c14e9c333c00f25f6b49a8bf3472991528
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2025-08-26 06:57:05 +0530

    fix(prm): Update website info

--- dashboard/src/components/partners/WebsiteInfoDialog.vue
+++ dashboard/src/components/partners/WebsiteInfoDialog.vue
@@ -1,32 +1,80 @@
 <template>
-	<div class="flex flex-col gap-4">
-		<FormControl
-			label="Website URL"
-			type="data"
-			name="website_url"
-			v-model="domain"
-		/>
-		<FormControl
-			label="Introduction"
-			type="textarea"
-			name="website_url"
-			v-model="domain"
-		/>
-		<FormControl
-			label="Customers"
-			type="textarea"
-			name="website_url"
-			description="Customers should be separated by commas"
-			placeholder="Tesla, Micro Corp, Ford"
-			v-model="domain"
-		/>
-		<Button
-			variant="solid"
-			label="Update Website Info"
-			@click="console.log('Update Website Info')"
-		/>
+	<div class="flex flex-col gap-5">
+		<div class="flex flex-col gap-4">
+			<div v-for="field in fields" :key="field.fieldname">
+				<FormControl
+					v-model="websiteInfo[field.fieldname]"
+					:label="field.label"
+					:type="getInputType(field)"
+					:name="field.fieldname"
+					:required="field.required"
+					:rows="10"
+				/>
+			</div>
+		</div>
+		<div>
+			<Button
+				class="w-full"
+				variant="solid"
+				label="Update Website Info"
+				@click="_updateWebsiteInfo()"
+			/>
+		</div>
 	</div>
 </template>
 <script setup>
-import { FormControl } from 'frappe-ui';
+import { FormControl, createResource } from 'frappe-ui';
+import { computed } from 'vue';
+import { toast } from 'vue-sonner';
+
+const websiteInfo = defineModel();
+const emit = defineEmits(['success']);
+
+const updateWebsiteInfo = createResource({
+	url: 'press.api.partner.update_website_info',
+	params: {
+		website_info: websiteInfo.value,
+	},
+	onSuccess: () => {
+		toast.success('Website info updated successfully');
+		emit('success');
+	},
+});
+
+function _updateWebsiteInfo() {
+	updateWebsiteInfo.submit();
+}
+
+const fields = computed(() => {
+	return [
+		{
+			fieldname: 'partner_website',
+			label: 'Website URL',
+			fieldtype: 'Data',
+			required: true,
+		},
+		{
+			fieldname: 'introduction',
+			label: 'Introduction',
+			fieldtype: 'Text',
+		},
+		{
+			fieldname: 'customers',
+			label: 'Customers',
+			fieldtype: 'Text',
+		},
+	];
+});
+
+function getInputType(field) {
+	return {
+		Data: 'text',
+		Int: 'number',
+		Select: 'select',
+		Check: 'checkbox',
+		Password: 'password',
+		Text: 'textarea',
+		Date: 'date',
+	}[field.fieldtype || 'Data'];
+}
 </script>
