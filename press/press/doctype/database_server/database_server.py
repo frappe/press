ac55d82670f85800621300c374bde28f0e3a5cdd
Balamurali M <balamurali@frappe.io>
2025-08-29 09:01:45 +0530

    fix(hetzner): Add extra steps for create server

--- press/press/doctype/database_server/database_server.py
+++ press/press/doctype/database_server/database_server.py
@@ -1641,6 +1641,23 @@ Latest binlog : {latest_binlog.get("name", "")} - {last_binlog_size_mb} MB {last
 		except Exception:
 			frappe.throw("Failed to fetch storage usage. Try again later.")
 
+	def set_mariadb_mount_dependency(self):
+		if not self.mariadb_depends_on_mounts:
+			return
+		frappe.enqueue_doc(self.doctype, self.name, "_set_mariadb_mount_dependency", timeout=1800)
+
+	def _set_mariadb_mount_dependency(self):
+		try:
+			ansible = Ansible(
+				playbook="set_mariadb_mount_dependency.yml",
+				server=self,
+				user=self._ssh_user(),
+				port=self._ssh_port(),
+			)
+			ansible.run()
+		except Exception:
+			log_error("Set MariaDB Mount Dependency Exception", server=self.as_dict())
+
 
 get_permission_query_conditions = get_permission_query_conditions_for_doctype("Database Server")
 
