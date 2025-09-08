9b6cda76d550c5eef2aa381523e393bc44c0c00a
Balamurali M <balamurali@frappe.io>
2025-09-08 21:08:19 +0530

    fix(agent-job): Show Get Status option for failed jobs
    Old jobs that were delivered get marked Failure by default. Correct
    status may not have been fetched due to some other reason.

--- press/press/doctype/agent_job/agent_job.js
+++ press/press/doctype/agent_job/agent_job.js
@@ -14,7 +14,7 @@ frappe.ui.form.on('Agent Job', {
 			__('Visit Dashboard'),
 		);
 
-		if (!['Success', 'Failure', 'Delivery Failure'].includes(frm.doc.status)) {
+		if (!['Success', 'Delivery Failure'].includes(frm.doc.status)) {
 			frm.add_custom_button(
 				__('Get Status'),
 				() => {
@@ -52,7 +52,11 @@ frappe.ui.form.on('Agent Job', {
 				__('Succeed and Process Job Updates'),
 				'succeed_and_process_job_updates',
 			],
-			[__('Cancel Job'), 'cancel_job', ['Pending', "Running"].includes(frm.doc.status)],
+			[
+				__('Cancel Job'),
+				'cancel_job',
+				['Pending', 'Running'].includes(frm.doc.status),
+			],
 		].forEach(([label, method, condition]) => {
 			frm.add_custom_button(
 				label,
