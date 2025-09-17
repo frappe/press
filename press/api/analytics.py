frappe/press

commit e753a951e2e55c6fd7300bf57979608863ec0686

fix(site-analytics): Don't treat null values as 0

press/api/analytics.py
@@ -220,7 +220,7 @@ class StackedGroupByChart:
 	):
 		path_data = {
 			"path": path_bucket.key,
-			"values": [0] * len(labels),
+			"values": [None] * len(labels),
 			"stack": "path",
 		}
 		hist_bucket: HistBucket

dashboard/src/components/charts/BarChart.vue
@@ -133,7 +133,7 @@ const options = ref({
 			).toLocaleString(DateTime.DATETIME_MED)}</p>`;
 
 			params.forEach(({ value, seriesName }, i) => {
-				if (value === 0) {
+				if (value === null) {
 					return;
 				}
 				let colorSpan = (color) =>

commit 8f9b779b95d85d1dea7a3a93b11f68249963e8ed

fix(normalized-slow-charts): Handle None values in dataset
Broke it in e753a951e2e55c6fd7300bf57979608863ec0686

press/api/analytics.py
@@ -813,7 +813,8 @@ def normalize_datasets(datasets: list[Dataset]) -> list[Dataset]:
 		n_query = normalize_query(data_dict["path"])
 		if n_datasets.get(n_query):
 			n_datasets[n_query]["values"] = [
-				x + y for x, y in zip(n_datasets[n_query]["values"], data_dict["values"], strict=False)
+				x + y if x and y else x or y
+				for x, y in zip(n_datasets[n_query]["values"], data_dict["values"], strict=True)
 			]
 		else:
 			data_dict["path"] = n_query