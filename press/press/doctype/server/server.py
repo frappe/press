1d4df3fd7ab4a31c3ba36bfc17d4756a5cd0f8cb
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2025-09-21 18:27:00 +0530

    test(server): Don't update db server if not set

--- press/press/doctype/server/server.py
+++ press/press/doctype/server/server.py
@@ -2022,6 +2022,8 @@ class Server(BaseServer):
 			)
 
 	def update_db_server(self):
+		if not self.database_server:
+			return
 		db_server = frappe.get_doc("Database Server", self.database_server)
 		if self.team == db_server.team:
 			return
