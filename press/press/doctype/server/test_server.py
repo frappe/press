754f91a0a722bf9e28415f206535bbd94c4ac62b
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2025-09-21 18:04:35 +0530

    test(server): Fix duplicate server sub tests
    (cherry picked from commit ec00491199491780531e493053693af992f67526)

--- press/press/doctype/server/test_server.py
+++ press/press/doctype/server/test_server.py
@@ -119,7 +119,6 @@ class TestServer(FrappeTestCase):
 		create_test_press_settings()
 		server_plan = create_test_server_plan()
 		server = create_test_server(plan=server_plan.name)
-		server.create_subscription(server.plan)
 		subscription = frappe.get_doc(
 			"Subscription",
 			{"document_type": "Server", "document_name": server.name, "enabled": 1},
@@ -131,19 +130,15 @@ class TestServer(FrappeTestCase):
 		create_test_press_settings()
 		server_plan = create_test_server_plan()
 		server = create_test_server(plan=server_plan.name)
-		server.create_subscription(server.plan)
-		subscription = frappe.get_doc(
-			"Subscription",
-			{"document_type": "Server", "document_name": server.name, "enabled": 1},
-		)
-		self.assertEqual(server.team, subscription.team)
-		self.assertEqual(server.plan, subscription.plan)
+
+		self.assertEqual(server.team, server.subscription.team)
+		self.assertEqual(server.plan, server.subscription.plan)
 
 		# update server team
 		team2 = create_test_team()
 		server.team = team2.name
 		server.save()
-		self.assertEqual(server.team, subscription.team)
+		self.assertEqual(server.team, server.subscription.team)
 
 	def test_db_server_team_update_on_server_team_update(self):
 		create_test_press_settings()
@@ -154,16 +149,16 @@ class TestServer(FrappeTestCase):
 		db_server.plan = db_server_plan.name
 		db_server.save()
 
-		server.create_subscription(server.plan)
-		db_server.create_subscription(db_server.plan)
 		self.assertEqual(server.team, db_server.team)
 
 		# update server team
 		team2 = create_test_team()
 		server.team = team2.name
 		server.save()
+		server.reload()
 		db_server.reload()
 		self.assertEqual(server.team, db_server.team)
+		self.assertEqual(server.subscription.team, server.team)
 		self.assertEqual(server.subscription.team, db_server.subscription.team)
 
 	def tearDown(self):
