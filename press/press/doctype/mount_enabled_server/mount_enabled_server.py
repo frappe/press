840ad871bcaa8a0c76214a73e9a26e847ca42900
Aradhya-Tripathi <aradhya@frappe.io>
2025-09-24 00:50:55 +0530

    feat(nfs): Add doctype to manage nfs servers

---
+++ press/press/doctype/mount_enabled_server/mount_enabled_server.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2025, Frappe and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MountEnabledServer(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		private_ip: DF.Data
+		server: DF.Link
+	# end: auto-generated types
+
+	pass
