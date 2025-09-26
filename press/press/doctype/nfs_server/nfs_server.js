786617b274d49e65eed67f53b77cde5d7f2de7cb
Aradhya-Tripathi <aradhya@frappe.io>
2025-09-26 14:07:07 +0530

    feat(nfs): Allow removing mount enabled server

--- press/press/doctype/nfs_server/nfs_server.js
+++ press/press/doctype/nfs_server/nfs_server.js
@@ -79,5 +79,29 @@ frappe.ui.form.on('NFS Server', {
 				);
 			});
 		}
+		if (frm.doc.status === 'Active') {
+			frm.add_custom_button('Remove Mount Enabled Server', () => {
+				frappe.prompt(
+					[
+						{
+							fieldtype: 'Link',
+							fieldname: 'server',
+							label: 'Server',
+							options: 'Server',
+							reqd: 1,
+						},
+					],
+					({ server }) => {
+						frm
+							.call('remove_mount_enabled', {
+								server: server,
+							})
+							.then((r) => {
+								frm.refresh();
+							});
+					},
+				);
+			});
+		}
 	},
 });
