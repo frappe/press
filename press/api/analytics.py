d987820068c6ce4b63bfa6f3111a43192acf67fa
Aradhya-Tripathi <aradhya@frappe.io>
2025-09-30 11:56:02 +0530

    fix(analytics): Replace group with name for protected to work
    (cherry picked from commit 31458cd55f44a99985163daa2c5a49242eb77e96)
    
    # Conflicts:
    #	press/api/analytics.py

--- press/api/analytics.py
+++ press/api/analytics.py
@@ -498,6 +498,7 @@ def get_metrics(
 	promql_query: str,
 	timezone: str,
 	response_key: str,
+<<<<<<< HEAD
 	group: str | None = None,
 	bench: str | None = None,
 	duration: str = "24h",
@@ -508,6 +509,15 @@ def get_metrics(
 	benches = (
 		frappe.get_all("Bench", {"status": "Active", "group": group}, pluck="name") if group else [bench]
 	)
+=======
+	name: str | None = None,
+	duration: str = "24h",
+):
+	if not name:
+		frappe.throw("No release group found!")
+
+	benches = frappe.get_all("Bench", {"status": "Active", "group": name}, pluck="name")
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 
 	if not benches:
 		frappe.throw("No active benches found!")
@@ -525,9 +535,13 @@ def get_metrics(
 
 @frappe.whitelist()
 @protected("Release Group")
+<<<<<<< HEAD
 def get_fs_read_bytes(
 	timezone: str, group: str | None = None, bench: str | None = None, duration: str = "24h"
 ):
+=======
+def get_fs_read_bytes(name: str, timezone: str, duration: str = "24h"):
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 	promql_query = (
 		'sum by (name) (rate(container_fs_reads_bytes_total{{job="cadvisor", name=~"{benches}"}}[5m]))'
 	)
@@ -535,17 +549,25 @@ def get_fs_read_bytes(
 		promql_query=promql_query,
 		timezone=timezone,
 		response_key="read_bytes_fs",
+<<<<<<< HEAD
 		group=group,
 		bench=bench,
+=======
+		name=name,
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 		duration=duration,
 	)
 
 
 @frappe.whitelist()
 @protected("Release Group")
+<<<<<<< HEAD
 def get_fs_write_bytes(
 	timezone: str, group: str | None = None, bench: str | None = None, duration: str = "24h"
 ):
+=======
+def get_fs_write_bytes(name: str, timezone: str, duration: str = "24h"):
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 	promql_query = (
 		'sum by (name) (rate(container_fs_writes_bytes_total{{job="cadvisor", name=~"{benches}"}}[5m]))'
 	)
@@ -553,33 +575,49 @@ def get_fs_write_bytes(
 		promql_query=promql_query,
 		timezone=timezone,
 		response_key="write_bytes_fs",
+<<<<<<< HEAD
 		group=group,
 		bench=bench,
+=======
+		name=name,
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 		duration=duration,
 	)
 
 
 @frappe.whitelist()
 @protected("Release Group")
+<<<<<<< HEAD
 def get_outgoing_network_traffic(
 	timezone: str, group: str | None = None, bench: str | None = None, duration: str = "24h"
 ):
+=======
+def get_outgoing_network_traffic(name: str, timezone: str, duration: str = "24h"):
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 	promql_query = 'sum by (name) (rate(container_network_transmit_bytes_total{{job="cadvisor", name=~"{benches}"}}[5m]))'
 	return get_metrics(
 		promql_query=promql_query,
 		timezone=timezone,
 		response_key="network_traffic_outward",
+<<<<<<< HEAD
 		group=group,
 		bench=bench,
+=======
+		name=name,
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 		duration=duration,
 	)
 
 
 @frappe.whitelist()
 @protected("Release Group")
+<<<<<<< HEAD
 def get_incoming_network_traffic(
 	timezone: str, group: str | None = None, bench: str | None = None, duration: str = "24h"
 ):
+=======
+def get_incoming_network_traffic(name: str, timezone: str, duration: str = "24h"):
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 	promql_query = (
 		'sum by (name) (rate(container_network_receive_bytes_total{{job="cadvisor", name=~"{benches}"}}[5m]))'
 	)
@@ -587,31 +625,47 @@ def get_incoming_network_traffic(
 		promql_query=promql_query,
 		timezone=timezone,
 		response_key="network_traffic_inward",
+<<<<<<< HEAD
 		group=group,
 		bench=bench,
+=======
+		name=name,
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 		duration=duration,
 	)
 
 
 @frappe.whitelist()
 @protected("Release Group")
+<<<<<<< HEAD
 def get_memory_usage(
 	timezone: str, group: str | None = None, bench: str | None = None, duration: str = "24h"
 ):
+=======
+def get_memory_usage(name: str, timezone: str, duration: str = "24h"):
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 	promql_query = 'sum by (name) (avg_over_time(container_memory_usage_bytes{{job="cadvisor", name=~"{benches}"}}[5m]) / 1024 / 1024 / 1024)'
 	return get_metrics(
 		promql_query=promql_query,
 		timezone=timezone,
 		response_key="memory",
+<<<<<<< HEAD
 		group=group,
 		bench=bench,
+=======
+		name=name,
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 		duration=duration,
 	)
 
 
 @frappe.whitelist()
 @protected("Release Group")
+<<<<<<< HEAD
 def get_cpu_usage(timezone: str, group: str | None = None, bench: str | None = None, duration: str = "24h"):
+=======
+def get_cpu_usage(name: str, timezone: str, duration: str = "24h"):
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 	promql_query = (
 		'sum by (name) ( rate(container_cpu_usage_seconds_total{{job="cadvisor", name=~"{benches}"}}[5m]))'
 	)
@@ -619,8 +673,12 @@ def get_cpu_usage(timezone: str, group: str | None = None, bench: str | None = N
 		promql_query=promql_query,
 		timezone=timezone,
 		response_key="cpu",
+<<<<<<< HEAD
 		group=group,
 		bench=bench,
+=======
+		name=name,
+>>>>>>> 31458cd55 (fix(analytics): Replace group with name for protected to work)
 		duration=duration,
 	)
 
