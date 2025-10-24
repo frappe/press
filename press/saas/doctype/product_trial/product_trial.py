445811849a3bc8bc66f9a6489be3ad08cf39a685
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2025-10-24 23:08:51 +0530

    Revert "fix(site-creation): Add check to pick server with bench > 0"

--- press/saas/doctype/product_trial/product_trial.py
+++ press/saas/doctype/product_trial/product_trial.py
@@ -406,7 +406,6 @@ class ProductTrial(Document):
 
 		ReleaseGroupServer = frappe.qb.DocType("Release Group Server")
 		Server = frappe.qb.DocType("Server")
-		Bench = frappe.qb.DocType("Bench")
 
 		servers = (
 			frappe.qb.from_(ReleaseGroupServer)
@@ -415,11 +414,6 @@ class ProductTrial(Document):
 			.join(Server)
 			.on(Server.name == ReleaseGroupServer.server)
 			.where(Server.cluster == cluster)
-			.where(
-				frappe.qb.exists(
-					frappe.qb.from_(Bench).select(Bench.name).where(Bench.server == ReleaseGroupServer.server)
-				)
-			)
 			.run(pluck="server")
 		)
 
