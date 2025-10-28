88f58fba212189ccf0ef5a1d7f498e50c2849ace
Aradhya-Tripathi <aradhya@frappe.io>
2025-10-28 16:27:12 +0530

    fix(conflict): Fix conflicts
    (cherry picked from commit 0ce4ba37282d9ffe6950a94d9533fc090e50c468)

--- press/press/doctype/server/server.js
+++ press/press/doctype/server/server.js
@@ -236,6 +236,7 @@ frappe.ui.form.on('Server', {
 					frm.doc.status === 'Active' &&
 					frm.doc.platform === 'x86_64',
 			],
+			[__('Scale Up'), 'scale_up', true, !frm.doc.scaled_up],
 		].forEach(([label, method, confirm, condition]) => {
 			if (typeof condition === 'undefined' || condition) {
 				frm.add_custom_button(
