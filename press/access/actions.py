fc34ef3b355ebb93a93ca7cfe6cc278e3682f1cd
Sabu Siyad <hello@ssiyad.com>
2025-10-29 22:19:06 +0530

    chore: rename enum members

--- press/access/actions.py
+++ press/access/actions.py
@@ -1,23 +1,23 @@
 from enum import Enum
 
 
-class ReleaseGroupActions(Enum):
-	GENERATE_SSH_CERTIFICATE = "Generate SSH Certificate"
+class ReleaseGroupActions(str, Enum):
+	GenerateSSHCertificate = "Generate SSH Certificate"
 
 
 class SiteActions(str, Enum):
-	VISIT_SITE = "Visit Site"
-	VIEW_IN_DESK = "View in Desk"
-	LOGIN_AS_ADMINISTRATOR = "Login As Administrator"
+	VisitSite = "Visit Site"
+	ViewInDesk = "View in Desk"
+	LoginAsAdmin = "Login As Administrator"
 
 
 ACTIONS_RULES = {
 	"Release Group": {
-		ReleaseGroupActions.GENERATE_SSH_CERTIFICATE: False,
+		ReleaseGroupActions.GenerateSSHCertificate: False,
 	},
 	"Site": {
-		SiteActions.VISIT_SITE: True,
-		SiteActions.VIEW_IN_DESK: False,
-		SiteActions.LOGIN_AS_ADMINISTRATOR: False,
+		SiteActions.VisitSite: True,
+		SiteActions.ViewInDesk: False,
+		SiteActions.LoginAsAdmin: False,
 	},
 }
