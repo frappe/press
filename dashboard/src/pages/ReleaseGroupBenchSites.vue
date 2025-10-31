19ccd30512e19e3c9e99b054dcffcc859927960f
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2025-10-31 13:16:03 +0530

    feat(dashboard-banners): Add CustomAlerts to ReleaseGroupBenchSites view
    Resolves #3745
    
    (cherry picked from commit d31d1f31c3a7047a4a768c2124acc792af599252)
    
    # Conflicts:
    #	dashboard/src/pages/ReleaseGroupBenchSites.vue

--- dashboard/src/pages/ReleaseGroupBenchSites.vue
+++ dashboard/src/pages/ReleaseGroupBenchSites.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
+		<CustomAlerts ctx_type="List Page" />
 		<DismissableBanner
 			v-if="$releaseGroup.doc.eol_versions.includes($releaseGroup.doc.version)"
 			class="col-span-1 lg:col-span-2"
@@ -45,11 +46,17 @@ import {
 import { confirmDialog, icon, renderDialog } from '../utils/components';
 import { getToastErrorMessage } from '../utils/toast';
 import DismissableBanner from '../components/DismissableBanner.vue';
+import CustomAlerts from '../components/CustomAlerts.vue';
 
 export default {
 	name: 'ReleaseGroupBenchSites',
+<<<<<<< HEAD
 	props: ['releaseGroup', 'actionsAccess'],
 	components: { ObjectList, DismissableBanner },
+=======
+	props: ['releaseGroup'],
+	components: { ObjectList, DismissableBanner, CustomAlerts },
+>>>>>>> d31d1f31c (feat(dashboard-banners): Add CustomAlerts to ReleaseGroupBenchSites view)
 	data() {
 		return {
 			showAppVersionDialog: false,
