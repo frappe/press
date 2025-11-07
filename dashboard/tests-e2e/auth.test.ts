643817af51a02db57246adcae727be4eeb166f23
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2025-11-07 12:21:45 +0530

    test(playwright): Add playwright tests to CI

---
+++ dashboard/tests-e2e/auth.test.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+const localCloudBaseUrl = new URL(process.env.BASE_URL || 'http://localhost:8010').hostname;
+
+test('Login', async ({ page }) => {
+    await page.goto(`${localCloudBaseUrl}/dashboard/login`);
+    await page.locator('#scrollContainer div').filter({ hasText: 'Log in to your account' }).nth(2).click({
+      button: 'right'
+    });
+    await page.getByRole('textbox', { name: 'Email (required)' }).fill('Administrator');
+    await page.getByRole('button', { name: 'Continue with password' }).click();
+    await page.getByRole('textbox', { name: 'Password (required)' }).fill('abc123');
+    await page.getByRole('button', { name: 'Log In' }).click();
+    await expect(page).toHaveURL(`**/dashboard/`);
+});
+
+export { test };
