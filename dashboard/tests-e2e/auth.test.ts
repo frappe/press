72c81c7620c077d9b7bbc8f76a0d1f55e21ae65e
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2025-11-07 18:26:25 +0530

    wip

--- dashboard/tests-e2e/auth.test.ts
+++ dashboard/tests-e2e/auth.test.ts
@@ -4,14 +4,11 @@ const localCloudBaseUrl = new URL(process.env.BASE_URL || 'http://localhost:8000
 
 test('Login', async ({ page }) => {
     await page.goto(`${localCloudBaseUrl}/dashboard/login`);
-    await page.locator('#scrollContainer div').filter({ hasText: 'Log in to your account' }).nth(2).click({
-      button: 'right'
-    });
     await page.getByRole('textbox', { name: 'Email (required)' }).fill('Administrator');
     await page.getByRole('button', { name: 'Continue with password' }).click();
     await page.getByRole('textbox', { name: 'Password (required)' }).fill('admin');
     await page.getByRole('button', { name: 'Log In' }).click();
-    await expect(page).toHaveURL(`**/dashboard/sites`);
+    await expect(page).toHaveURL(/\/dashboard\/sites$/);
 });
 
 export { test };
