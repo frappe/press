90781cb8e580f443abe01a7cd872cc2c3f3911a7
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2025-11-09 15:02:59 +0530

    wip

--- dashboard/tests-e2e/auth.test.ts
+++ dashboard/tests-e2e/auth.test.ts
@@ -1,17 +1,27 @@
 import { expect, test } from '@playwright/test';
 
 const localCloudBaseUrl = process.env.BASE_URL || 'http://localhost:8000';
-const userEmail = process.env.PRESS_ADMIN_USER_EMAIL || 'Frappe';
+const userEmail = process.env.PRESS_ADMIN_USER_EMAIL || 'playwright@example.com';
 
 test('Login', async ({ page }) => {
     await page.goto(`${localCloudBaseUrl}/dashboard/login`);
     await page.getByRole('textbox', { name: 'Email (required)' }).click();
+    await page.waitForTimeout(500);
+
     await page.getByRole('textbox', { name: 'Email (required)' }).fill(userEmail);
+    await page.waitForTimeout(500);
+
     await page.getByRole('button', { name: 'Send verification code' }).click();
+    await page.waitForTimeout(500);
     await page.getByRole('textbox', { name: 'Verification code (required)' }).click();
+    await page.waitForTimeout(500);
+
     await page.getByRole('textbox', { name: 'Verification code (required)' }).fill('111111');
+    await page.waitForTimeout(500);
+
     await page.getByRole('button', { name: 'Submit verification code' }).click();
-    
+    await page.waitForTimeout(2000);
+
     const url = new URL(page.url());
     expect(url.pathname).toBe("/dashboard/welcome");
 });
