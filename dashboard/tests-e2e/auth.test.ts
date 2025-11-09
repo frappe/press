89963427f4b9461b4c914205ace1505644be94fa
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2025-11-09 14:08:55 +0530

    wip

--- dashboard/tests-e2e/auth.test.ts
+++ dashboard/tests-e2e/auth.test.ts
@@ -1,14 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 const localCloudBaseUrl = process.env.BASE_URL || 'http://localhost:8000';
 const userEmail = process.env.PRESS_ADMIN_USER_EMAIL || 'Frappe';
-const userPwd = process.env.PRESS_ADMIN_USER_PWD || 'admin';
 
 test('Login', async ({ page }) => {
     await page.goto(`${localCloudBaseUrl}/dashboard/login`);
+    await page.getByRole('textbox', { name: 'Email (required)' }).click();
     await page.getByRole('textbox', { name: 'Email (required)' }).fill(userEmail);
-    await page.getByRole('button', { name: 'Continue with password' }).click();
-    await page.getByRole('textbox', { name: 'Password (required)' }).fill(userPwd);
-    await page.getByRole('button', { name: 'Log In' }).click();
-    await expect(page).toHaveURL(/\/dashboard\/welcome$/);
+    await page.getByRole('button', { name: 'Send verification code' }).click();
+    await page.getByRole('textbox', { name: 'Verification code (required)' }).click();
+    await page.getByRole('textbox', { name: 'Verification code (required)' }).fill('111111');
+    await page.getByRole('button', { name: 'Submit verification code' }).click();
+    
+    const url = new URL(page.url());
+    expect(url.pathname).toBe("/dashboard/welcome");
 });
