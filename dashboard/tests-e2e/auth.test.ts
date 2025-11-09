d481a8548d6680ff70bb88c4bf0045aba1550887
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2025-11-09 11:49:26 +0530

    wip

--- dashboard/tests-e2e/auth.test.ts
+++ dashboard/tests-e2e/auth.test.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-const localCloudBaseUrl = new URL(process.env.BASE_URL || 'http://localhost:8000').hostname;
+const localCloudBaseUrl = process.env.BASE_URL || 'http://localhost:8000';
 
 test('Login', async ({ page }) => {
     await page.goto(`${localCloudBaseUrl}/dashboard/login`);
     await page.getByRole('textbox', { name: 'Email (required)' }).fill('Administrator');
     await page.getByRole('button', { name: 'Continue with password' }).click();
-    await page.getByRole('textbox', { name: 'Password (required)' }).fill('admin');
+    await page.getByRole('textbox', { name: 'Password (required)' }).fill('abc123');
     await page.getByRole('button', { name: 'Log In' }).click();
     await expect(page).toHaveURL(/\/dashboard\/sites$/);
 });
