a90e20b6fd6006536c1aad0c280be9db843afca0
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2025-11-09 11:59:14 +0530

    wip

--- dashboard/tests-e2e/auth.test.ts
+++ dashboard/tests-e2e/auth.test.ts
@@ -6,7 +6,7 @@ test('Login', async ({ page }) => {
     await page.goto(`${localCloudBaseUrl}/dashboard/login`);
     await page.getByRole('textbox', { name: 'Email (required)' }).fill('Administrator');
     await page.getByRole('button', { name: 'Continue with password' }).click();
-    await page.getByRole('textbox', { name: 'Password (required)' }).fill('abc123');
+    await page.getByRole('textbox', { name: 'Password (required)' }).fill('admin');
     await page.getByRole('button', { name: 'Log In' }).click();
     await expect(page).toHaveURL(/\/dashboard\/sites$/);
 });
