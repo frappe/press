2d18535fadf91b9dc7df81a8cad6f9bafc49c5e9
Sabu Siyad <hello@ssiyad.com>
2025-11-12 18:29:02 +0530

    chore: Remove `Email Provider` from fixtures

--- press/hooks.py
+++ press/hooks.py
@@ -362,7 +362,6 @@ deploy_hours = [1, 2, 3, 4, 5, 21, 22, 23]  # Purposefully avoiding 0
 
 fixtures = [
 	"Agent Job Type",
-	"Email Provider",
 	"Press Job Type",
 	"Frappe Version",
 	"MariaDB Variable",
