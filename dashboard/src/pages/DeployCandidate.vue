f8302d4b37670cf9e212f3f937c6d1cca8075762
Aradhya-Tripathi <aradhya@frappe.io>
2025-11-13 15:14:04 +0530

    chore(ui): Remove redundant redeploy button on failure status
    (cherry picked from commit 6b45c36239974523c8cf846ae9bf487683e8982d)

--- dashboard/src/pages/DeployCandidate.vue
+++ dashboard/src/pages/DeployCandidate.vue
@@ -35,13 +35,6 @@
 						Stop Deploy
 					</Button>
 					<Button
-						@click="deploy"
-						v-if="deploy && deploy.status == 'Failure'"
-						theme="green"
-					>
-						Redeploy
-					</Button>
-					<Button
 						@click="$resources.deploy.reload()"
 						:loading="$resources.deploy.get.loading"
 					>
