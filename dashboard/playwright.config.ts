89dbda680a1e92c90a66806f7040c1de086c2862
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2025-11-13 16:50:14 +0530

    feat(playwright): Organise tests, add session storage
    Added a primary auth test that logs in and stores the session state temporarily for other tests to use. Also organised the tests directory: will keep adding tests to increase coverage over the next few weeks.
    
    (cherry picked from commit 5b2be71be80a2ac55e72eb27a995f3523d99ec59)

--- dashboard/playwright.config.ts
+++ dashboard/playwright.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./tests-e2e/.env", quiet: true });
 
 export default defineConfig({
   testDir: './tests-e2e',
@@ -6,13 +9,31 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 4 : undefined,
   use: {
-  baseURL: process.env.BASE_URL || 'http://localhost:8010',
+    baseURL: process.env.BASE_URL || 'http://localhost:8010',
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure'
   },
   reporter: [['list'], ['html', { open: 'never' }]],
   projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    {
+      name: 'cron',
+      testMatch: /.*\.cron\.spec\.ts/,
+      dependencies: undefined
+    },
+    {
+      name: 'setup',
+      testMatch: /.*\.setup\.ts/
+    },
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: 'tests-e2e/.auth/session.json' 
+      },
+      // must match all *.test.ts files only
+      testMatch: /^(?!.*(\.cron|\.setup)\.spec\.ts$).*\.test\.ts$/,
+      dependencies: ['setup']
+    }
   ]
 });
