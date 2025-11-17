c29c8f273dc3ebe96f6185e4ac9ecead11c6a5d0
Sabu Siyad <hello@ssiyad.com>
2025-11-17 13:29:30 +0530

    fix(guards/mfa): Get rid of `api/account` dependency
    - Use own functions for checks
    - Use `user_key` from form if session user is `Guest`
    
    (cherry picked from commit 9df4af775e29fe51ee7fe8dd651f2b153d3e0b5f)

--- press/decorators/mfa.py
+++ press/decorators/mfa.py
@@ -1,19 +1,28 @@
 import functools
 
 import frappe
+import pyotp
+from frappe import auth
 
-from press.api import account
 
+def enabled(user_key: str = "user", raise_error: bool = False):
+	"""
+	Guard to check if two-factor authentication is enabled for a user.
 
-def enabled(raise_error: bool = False):
-	def get_user():
+	:param user_key: The key to retrieve the user from the request form if the session user is "Guest". Defaults to "user".
+	:param raise_error: Whether to raise a PermissionError if 2FA is not enabled. Defaults to False.
+	:raises frappe.PermissionError: If 2FA is not enabled and `raise_error` is True.
+	"""
+
+	def get_user() -> str:
+		if frappe.session.user == "Guest":
+			return frappe.request.form.get(user_key)
 		return frappe.session.user
 
 	def wrapper(fn):
 		@functools.wraps(fn)
 		def inner(*args, **kwargs):
-			user = get_user()
-			if user and account.is_2fa_enabled(user):
+			if frappe.get_value("User 2FA", get_user(), "enabled"):
 				return fn(*args, **kwargs)
 			if raise_error:
 				msg = "Two-factor authentication is not enabled."
@@ -25,21 +34,36 @@ def enabled(raise_error: bool = False):
 	return wrapper
 
 
-def verify(code_key: str = "totp_code", raise_error: bool = False):
-	def get_user():
+def verify(user_key: str = "user", code_key: str = "totp_code", raise_error: bool = False):
+	"""
+	Guard to verify two-factor authentication for a user.
+
+	:param user_key: The key to retrieve the user from the request form if the session user is "Guest". Defaults to "user".
+	:param code_key: The key to retrieve the TOTP code from the request form. Defaults to "totp_code".
+	:param raise_error: Whether to raise a PermissionError if 2FA verification fails. Defaults to False.
+	:raises frappe.PermissionError: If 2FA verification fails and `raise_error` is True.
+	"""
+
+	def get_user() -> str:
+		if frappe.session.user == "Guest":
+			return frappe.request.form.get(user_key)
 		return frappe.session.user
 
 	def get_code():
 		return frappe.request.form.get(code_key)
+
+	def verify_code(user: str, code: str):
+		secret = auth.get_decrypted_password("User 2FA", user, "totp_secret")
+		return secret and pyotp.TOTP(secret).verify(code)
 
 	def wrapper(fn):
 		@functools.wraps(fn)
 		def inner(*args, **kwargs):
 			user = get_user()
 			code = get_code()
-			if user and not account.is_2fa_enabled(user):
+			if user and not frappe.get_value("User 2FA", user, "enabled"):
 				return fn(*args, **kwargs)
-			if user and code and account.verify_2fa(user, code):
+			if user and code and verify_code(user, code):
 				return fn(*args, **kwargs)
 			if raise_error:
 				msg = "Two-factor authentication verification failed."
