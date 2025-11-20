6d368c73088d450e0851a945fdd76a31c76d14c0
Aysha <ayshapp468@gmail.com>
2025-11-20 20:02:25 +0530

    fix(marketplace): Update marketplace payout calculation logic, track commission per Payout Order

--- press/press/doctype/payout_order/payout_order.js
+++ press/press/doctype/payout_order/payout_order.js
@@ -2,6 +2,31 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Payout Order', {
-	// refresh: function(frm) {
-	// }
+	refresh: function (frm) {
+		if (frm.doc.docstatus === 1 && frm.doc.status === 'Unpaid') {
+			frm.add_custom_button(
+				__('Mark as Paid'),
+				function () {
+					frappe.confirm(
+						__('Are you sure you want to mark this payout order as paid?'),
+						function () {
+							frappe.call({
+								method:
+									'press.press.doctype.payout_order.payout_order.mark_as_paid',
+								args: {
+									payout_order: frm.doc.name,
+								},
+								callback: function (r) {
+									if (!r.exc) {
+										frm.reload_doc();
+									}
+								},
+							});
+						},
+					);
+				},
+				__('Actions'),
+			);
+		}
+	},
 });
