e2b72a179da1554e72429c92ae7caaac164a6f3c
phot0n <ritwikpuri5678@gmail.com>
2025-11-25 13:22:14 +0530

    fix: stupid hiccups

--- press/press/doctype/proxy_failover/proxy_failover.py
+++ press/press/doctype/proxy_failover/proxy_failover.py
@@ -42,32 +42,41 @@ class ProxyFailover(Document):
 
 		# take both cases - primary up and primary down
 		# assume secondary to be up
+
+		# TODO: we'll also need to make sure secondary has the latest conf
+		# one easy way is to validate the last X add site to upstream entries in agent job (which succeeded in primary)
+		# and cross check it with secondary
 		try:
-			send_failover_email = False
-			primary_has_static_ip = frappe.db.get_value("Proxy Server", self.primary, "is_static_ip")
-
-			if not primary_has_static_ip:
+			primary_proxy = frappe.get_doc("Proxy Server", self.primary)
+			if not primary_proxy.is_static_ip:
 				self.update_dns_records_for_all_sites()
-				send_failover_email = True
 
-			# TODO: if primary is up and reachable and we dont have a static ip, we should add an nginx config to route all traffic to secondary
+				# TODO: attach the secondary public ip to domain name of primary
 
-			self.stop_primary_agent_and_sync()
+				try:
+					primary_proxy.ping_agent()
+				except Exception:
+					# TODO: trigger an agent job to add an nginx config to route all traffic to secondary
+					pass
+
+
+			self.stop_primary_agent_and_replication(primary_proxy)
 			self.update_app_servers()
 			self.move_wildcard_domains_from_primary()
 			self.remove_primary_access_and_start_nginx_in_secondary()
 			self.forward_jobs_to_secondary()
 
 			# transfer the primary's static ip to secondary
-			if primary_has_static_ip:
+			if primary_proxy.is_static_ip:
 				self.attach_static_ip_to_secondary()
 
 			self.switch_primary()
-			self.add_ssh_users_for_existing_benches()
 
-			if send_failover_email:
+			if not primary_proxy.is_static_ip:
+				# send a mail to people to update their A records or switch to CNAME records
 				self.send_failover_email()
 
+			self.add_ssh_users_for_existing_benches()
 		except Exception:
 			# TODO: we need to mark the proxies broken as well?
 			self.status = "Failed"
@@ -82,7 +91,7 @@ class ProxyFailover(Document):
 			"Virtual Machine", frappe.db.get_value("Proxy Server", self.secondary, "virtual_machine")
 		)
 
-		static_ip = primary_vm.public_ip_address
+		static_ip = primary_vm.public_ip_address  # assuming this is the static ip
 		primary_vm.detach_static_ip()
 		secondary_vm.attach_static_ip(static_ip)
 
@@ -100,6 +109,9 @@ class ProxyFailover(Document):
 			domain.update_dns_records_for_sites([site.name for site in sites], self.secondary)
 
 	def add_ssh_users_for_existing_benches(self):
+		if not frappe.db.get_value("Proxy Server", self.secondary, "is_ssh_proxy_setup"):
+			return
+
 		benches = frappe.qb.DocType("Bench")
 		servers = frappe.qb.DocType("Server")
 		active_benches = (
@@ -119,11 +131,11 @@ class ProxyFailover(Document):
 		frappe.db.set_value("Server", {"proxy_server": self.primary}, "proxy_server", self.secondary)
 
 	def switch_primary(self):
-		frappe.db.set_value("Proxy Server", self.primary, "is_primary", False)
-		self.is_primary = True
-		self.is_replication_setup = False
-		self.primary = None
-		self.status = "Active"
+		frappe.db.set_value(
+			"Proxy Server",
+			self.secondary,
+			{"is_primary": True, "is_replication_setup": False, "primary": None},
+		)
 
 	def forward_jobs_to_secondary(self):
 		frappe.db.set_value(
@@ -141,8 +153,8 @@ class ProxyFailover(Document):
 			self.secondary,
 		)
 
-	def stop_primary_agent_and_sync(self):
-		primary = frappe.get_doc("Proxy Server", self.primary)
+	def stop_primary_agent_and_replication(self, primary_proxy=None):
+		primary = primary_proxy or frappe.get_doc("Proxy Server", self.primary)
 		try:
 			ansible = Ansible(
 				playbook="failover_prepare_primary_proxy.yml",
