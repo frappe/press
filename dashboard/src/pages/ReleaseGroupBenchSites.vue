frappe/press

commit 307f3a11b1d7ad596f98bb3fdb8ce4c6b457af07

feat: bench ssh access

dashboard/src/pages/ReleaseGroupBenchSites.vue
@@ -48,7 +48,7 @@ import DismissableBanner from '../components/DismissableBanner.vue';
 
 export default {
 	name: 'ReleaseGroupBenchSites',
-	props: ['releaseGroup'],
+	props: ['releaseGroup', 'actionsAccess'],
 	components: { ObjectList, DismissableBanner },
 	data() {
 		return {
@@ -116,7 +116,7 @@ export default {
 						<div class="flex items-center">
 							<Tooltip text="View bench details">
 								<a
-									class="cursor-pointer text-base font-medium leading-6 text-gray-900"
+									class="text-base font-medium leading-6 text-gray-900 cursor-pointer"
 									href={`/dashboard/benches/${bench.name}`}
 								>
 									{bench.group}
@@ -128,7 +128,7 @@ export default {
 							{bench.has_app_patch_applied && (
 								<Tooltip text="Apps in this bench may have been patched">
 									<a
-										class="ml-2 rounded bg-gray-100 p-1 text-gray-700"
+										class="p-1 ml-2 text-gray-700 bg-gray-100 rounded"
 										href="https://docs.frappe.io/cloud/benches/app-patches"
 										target="_blank"
 									>
@@ -139,7 +139,7 @@ export default {
 							{bench.has_updated_inplace && (
 								<Tooltip text="This bench has been updated in place">
 									<a
-										class="ml-2 rounded bg-gray-100 p-1 text-gray-700"
+										class="p-1 ml-2 text-gray-700 bg-gray-100 rounded"
 										href="https://docs.frappe.io/cloud/in-place-updates"
 										target="_blank"
 									>
@@ -429,7 +429,12 @@ export default {
 						);
 					},
 				},
-			];
+			].filter((option) => {
+				const hasAccess = this.actionsAccess[option.label] ?? true;
+				if (!hasAccess) return false;
+				if (!option.condition?.()) return false;
+				return true;
+			});
 		},
 		runBenchMethod(name, methodName) {
 			const method = createResource({

dashboard/src/objects/group.js
@@ -163,7 +163,10 @@ export default {
 					() => import('../pages/ReleaseGroupBenchSites.vue'),
 				),
 				props: (releaseGroup) => {
-					return { releaseGroup: releaseGroup.doc.name };
+					return {
+						releaseGroup: releaseGroup.doc.name,
+						actionsAccess: releaseGroup.doc.actions_access,
+					};
 				},
 			},
 			{

press/access/__init__.py
@@ -32,7 +32,7 @@ def dashboard_access_rules(data: frappe._dict):
 	actions = ACTIONS_RULES.get(doctype, {})
 	actions_maps = ACTION_DF_MAP.get(doctype, {})
 	for rule, value in actions.items():
-		data.actions_access[str(rule)] = value
+		data.actions_access[rule] = value
 		if rule in actions_maps:
 			data.actions_access[rule] = has_support_access(doctype, docname, rule)
 

press/access/actions.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class ReleaseGroupActions(str, Enum):
-	GenerateSSHCertificate = "Generate SSH Certificate"
+	SSHAccess = "SSH Access"
 
 
 class SiteActions(str, Enum):
@@ -13,11 +13,11 @@ class SiteActions(str, Enum):
 
 ACTIONS_RULES = {
 	"Release Group": {
-		ReleaseGroupActions.GenerateSSHCertificate: False,
+		ReleaseGroupActions.SSHAccess.value: False,
 	},
 	"Site": {
-		SiteActions.VisitSite: True,
-		SiteActions.ViewInDesk: False,
-		SiteActions.LoginAsAdmin: False,
+		SiteActions.VisitSite.value: True,
+		SiteActions.ViewInDesk.value: False,
+		SiteActions.LoginAsAdmin.value: False,
 	},
 }

press/access/support_access.py
@@ -1,7 +1,7 @@
 import frappe
 import frappe.utils
 
-from press.access.actions import SiteActions
+from press.access.actions import ReleaseGroupActions, SiteActions
 from press.utils import get_current_team
 
 TAB_DF_MAP = {
@@ -11,9 +11,12 @@ TAB_DF_MAP = {
 }
 
 ACTION_DF_MAP = {
+	"Release Group": {
+		ReleaseGroupActions.SSHAccess.value: "bench_ssh",
+	},
 	"Site": {
 		SiteActions.LoginAsAdmin.value: "login_as_administrator",
-	}
+	},
 }
 
 

press/press/doctype/release_group/release_group.py
@@ -982,7 +982,7 @@ class ReleaseGroup(Document, TagHelpers):
 		)
 
 	@dashboard_whitelist()
-	@action_guard(ReleaseGroupActions.GenerateSSHCertificate)
+	@action_guard(ReleaseGroupActions.SSHAccess)
 	def generate_certificate(self):
 		ssh_key = frappe.get_all(
 			"User SSH Key",

commit dc448a99f5568065858ad67e3a2a71057579c108

fix(bench): Always show archive bench
The option was lost completely due to regression in 307f3a11b1

dashboard/src/pages/ReleaseGroupBenchSites.vue
@@ -393,6 +393,7 @@ export default {
 				},
 				{
 					label: 'Archive Bench',
+					condition: () => true,
 					onClick: () => {
 						confirmDialog({
 							title: 'Archive Bench',