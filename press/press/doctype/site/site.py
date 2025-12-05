e3bcb9952000eebf5409806785f9081cacf29219
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2025-12-05 12:09:08 +0530

    fix(server): Don't check for unpaid invs for change plan

--- press/press/doctype/site/site.py
+++ press/press/doctype/site/site.py
@@ -2422,9 +2422,6 @@ class Site(Document, TagHelpers):
 		if team.payment_mode == "Paid By Partner" and team.billing_team:
 			team = frappe.get_doc("Team", team.billing_team)
 
-		if team.is_defaulter():
-			frappe.throw("Cannot change plan because you have unpaid invoices", CannotChangePlan)
-
 		if not (team.default_payment_method or team.get_balance()):
 			frappe.throw(
 				"Cannot change plan because you haven't added a card and not have enough balance",
