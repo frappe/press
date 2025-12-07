905c3ea954278d313d364ac46887a9397d41dacb
Aradhya-Tripathi <aradhya@frappe.io>
2025-12-07 12:18:03 +0530

    feat(pricing): Calculate usage record quantity based on minutes scaled
    (cherry picked from commit f0b02495ae5b03020cf38f1a353218536eb0524d)

--- press/press/doctype/auto_scale_record/auto_scale_record.py
+++ press/press/doctype/auto_scale_record/auto_scale_record.py
@@ -353,6 +353,7 @@ class AutoScaleRecord(Document, StepHandler):
 		step.status = Status.Running
 		step.save()
 
+		# Since only created when scaling down this value will always be present!
 		last_auto_scale_at = frappe.db.get_value(
 			"Auto Scale Record",
 			{
@@ -366,8 +367,9 @@ class AutoScaleRecord(Document, StepHandler):
 
 		auto_scaled_for = frappe.utils.now_datetime() - last_auto_scale_at
 		auto_scaled_for = auto_scaled_for.total_seconds() / 3600
-		secondary_server_plan = frappe.db.get_value("Server", self.secondary_server, "plan")
-		secondary_server_team = frappe.db.get_value("Server", self.secondary_server, "team")
+		secondary_server_plan, secondary_server_team = frappe.db.get_value(
+			"Server", self.secondary_server, ["plan", "team"]
+		)
 
 		calculated_amount = calculate_amount_from_usage(
 			auto_scaled_for=auto_scaled_for,
