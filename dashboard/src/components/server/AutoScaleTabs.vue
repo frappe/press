36e618a8def9da0d69d188e5258f07123dc9064a
Aradhya-Tripathi <aradhya@frappe.io>
2025-12-09 23:57:49 +0530

    fix(auto-scale): Stop fetching multiple times due to redirect

--- dashboard/src/components/server/AutoScaleTabs.vue
+++ dashboard/src/components/server/AutoScaleTabs.vue
@@ -1,46 +1,35 @@
 <template>
-	<div
-		class="-m-5 flex divide-x"
-		:class="{
-			'flex-col': $isMobile,
-		}"
-	>
-		<div
-			:class="{
-				'ml-5 mt-5 w-60 divide-y rounded-sm border': $isMobile,
-				'w-60': !$isMobile,
-			}"
-		>
-			<template v-for="tab in tabs" :key="tab.value">
-				<router-link
-					:to="{
-						name: tab.value,
-						query: {
-							secondaryServer: secondaryServer,
-						},
-					}"
-					class="flex cursor-pointer text-base hover:bg-gray-100"
-					:class="{
-						'bg-gray-50 text-gray-800': isActiveTab(tab),
-						'text-gray-600': !isActiveTab(tab),
-						'border-b': !$isMobile,
-					}"
-				>
-					<div
-						class="px-4"
+	<div v-if="ready">
+		<div class="-m-5 flex divide-x" :class="{ 'flex-col': $isMobile }">
+			<div
+				:class="{
+					'ml-5 mt-5 w-60 divide-y rounded-sm border': $isMobile,
+					'w-60': !$isMobile,
+				}"
+			>
+				<template v-for="tab in tabs" :key="tab.value">
+					<router-link
+						:to="{ name: tab.value, query: { server, secondaryServer } }"
+						class="flex cursor-pointer text-base hover:bg-gray-100"
 						:class="{
-							'py-2': $isMobile,
-							'py-2.5': !$isMobile,
+							'bg-gray-50 text-gray-800': isActiveTab(tab),
+							'text-gray-600': !isActiveTab(tab),
+							'border-b': !$isMobile,
 						}"
 					>
-						{{ tab.label }}
-					</div>
-				</router-link>
-			</template>
-		</div>
+						<div
+							class="px-4"
+							:class="{ 'py-2': $isMobile, 'py-2.5': !$isMobile }"
+						>
+							{{ tab.label }}
+						</div>
+					</router-link>
+				</template>
+			</div>
 
-		<div class="w-full overflow-auto sm:h-[88vh]">
-			<router-view />
+			<div class="w-full overflow-auto sm:h-[88vh]">
+				<router-view />
+			</div>
 		</div>
 	</div>
 </template>
@@ -55,16 +44,26 @@ export default {
 				{ label: 'Triggered', value: 'Triggered' },
 				{ label: 'Scheduled', value: 'Scheduled' },
 			],
+			ready: false,
 		};
 	},
 	mounted() {
-		// If query params are missing, add them
-		if (!this.$route.query.secondaryServer) {
-			this.$router.replace({
-				query: {
-					secondaryServer: this.secondaryServer,
-				},
-			});
+		const query = this.$route.query;
+
+		if (!query.secondaryServer) {
+			this.$router
+				.replace({
+					name: this.$route.name,
+					query: {
+						server: this.server,
+						secondaryServer: this.secondaryServer,
+					},
+				})
+				.then(() => {
+					this.ready = true;
+				});
+		} else {
+			this.ready = true;
 		}
 	},
 	methods: {
