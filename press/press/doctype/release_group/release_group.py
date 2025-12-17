ef9cb7fffaf794c237a09141f76178b1c330880b
Sabu Siyad <hello@ssiyad.com>
2025-12-17 20:40:22 +0530

    chore(cleanup): Remove unwanted role related functions

--- press/press/doctype/release_group/release_group.py
+++ press/press/doctype/release_group/release_group.py
@@ -282,13 +282,6 @@ class ReleaseGroup(Document, TagHelpers):
 		self.set_default_delta_builds_flags()
 		self.setup_default_feature_flags()
 
-	def after_insert(self):
-		from press.press.doctype.press_role.press_role import (
-			add_permission_for_newly_created_doc,
-		)
-
-		add_permission_for_newly_created_doc(self)
-
 	def on_update(self):
 		old_doc = self.get_doc_before_save()
 		if self.flags.in_insert or self.is_new() or not old_doc:
