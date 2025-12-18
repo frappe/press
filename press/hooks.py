f14b91dcde2f88411ebfadc66df59b84d95d04a5
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2025-12-18 10:06:29 +0530

    refactor(site): In case of usage exceed, give window of 14 days
    (cherry picked from commit b68ecac43e679110a52cf8b7f93dedf9f6c6e0e3)

--- press/hooks.py
+++ press/hooks.py
@@ -207,7 +207,7 @@ scheduler_events = {
 		"press.press.doctype.press_webhook_log.press_webhook_log.clean_logs_older_than_24_hours",
 		"press.press.doctype.payment_due_extension.payment_due_extension.remove_payment_due_extension",
 		"press.press.doctype.tls_certificate.tls_certificate.notify_custom_tls_renewal",
-		"press.press.doctype.site.site.suspend_sites_exceeding_disk_usage_for_last_7_days",
+		"press.press.doctype.site.site.suspend_sites_exceeding_disk_usage_for_last_14_days",
 		"press.press.doctype.user_2fa.user_2fa.yearly_2fa_recovery_code_reminder",
 		"press.press.doctype.registry_server.registry_server.delete_old_images_from_registry",
 		"press.saas.doctype.product_trial_request.product_trial_request.gather_daily_stats",
