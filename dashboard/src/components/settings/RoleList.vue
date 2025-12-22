5be68101ac438a775598dc357109201e35bc92d6
Sabu Siyad <hello@ssiyad.com>
2025-12-22 15:04:00 +0530

    fix(dashboard): Roles list members count style
    (cherry picked from commit b416a6038b3dec32c27cfde101a5d911d0dba043)
    
    # Conflicts:
    #	dashboard/src/components/settings/RoleList.vue

--- dashboard/src/components/settings/RoleList.vue
+++ dashboard/src/components/settings/RoleList.vue
@@ -1,5 +1,91 @@
 <template>
+<<<<<<< HEAD
 	<ObjectList :options="listOptions" />
+=======
+	<div class="space-y-5">
+		<div class="ml-auto mr-0 w-max space-x-2">
+			<Button
+				icon="refresh-cw"
+				variant="subtle"
+				@click="() => roles.refresh()"
+			/>
+			<Button icon-left="plus" variant="solid" label="Create Role" />
+		</div>
+		<div class="grid grid-cols-3 gap-4 text-base">
+			<RouterLink
+				v-for="role in roles.data"
+				:to="{
+					name: 'SettingsPermissionRolePermissions',
+					params: {
+						id: role.name,
+					},
+				}"
+			>
+				<div
+					class="px-5 py-4 space-y-3 rounded shadow cursor-pointer hover:shadow-lg transition"
+				>
+					<div class="font-medium h-6">{{ role.title }}</div>
+					<div class="h-6 flex flex-wrap gap-1">
+						<div v-for="permission in permissions(role)">
+							<Badge
+								variant="subtle"
+								:theme="permission.color"
+								:label="permission.label"
+							/>
+						</div>
+					</div>
+					<div class="h-6">
+						<div class="flex items-center -space-x-2">
+							<Tooltip
+								v-for="user in role.users.slice(0, 3)"
+								:text="user.full_name"
+								:hover-delay="1"
+								:placement="'top'"
+							>
+								<Avatar
+									:shape="'circle'"
+									:ref_for="true"
+									:image="user.user_image"
+									:label="user.full_name"
+									size="lg"
+									class="border border-white"
+								/>
+							</Tooltip>
+							<div
+								v-if="role.users.length > 4"
+								class="flex items-center justify-center relative shrink-0 w-7 h-7 rounded-full bg-surface-gray-2 border border-white"
+							>
+								<div class="text-xs font-medium text-ink-gray-5">
+									+{{ role.users.length - 3 }}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</RouterLink>
+		</div>
+		<RoleCreateDialog
+			v-model="showCreateDialog"
+			@create="
+				(title, users, resources) => {
+					insert.submit({
+						doc: {
+							doctype: 'Press Role',
+							title,
+							users: users.map((u) => ({
+								user: u,
+							})),
+							resources: resources.map((resource) => ({
+								document_type: resource.document_type,
+								document_name: resource.document_name,
+							})),
+						},
+					});
+				}
+			"
+		/>
+	</div>
+>>>>>>> b416a6038 (fix(dashboard): Roles list members count style)
 </template>
 
 <script setup lang="jsx">
