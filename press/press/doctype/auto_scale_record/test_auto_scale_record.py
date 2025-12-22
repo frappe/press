0e1126a5f15d4417ead08f29e1fa4ff2477fcf90
Aradhya-Tripathi <aradhya@frappe.io>
2025-12-22 10:58:34 +0530

    feat(test): Add test for scheduled autoscales
    (cherry picked from commit 64f6cb5f4a46ca32ea9223391392174fffedf32f)

--- press/press/doctype/auto_scale_record/test_auto_scale_record.py
+++ press/press/doctype/auto_scale_record/test_auto_scale_record.py
@@ -1,29 +1,109 @@
 # Copyright (c) 2025, Frappe and Contributors
 # See license.txt
+from __future__ import annotations
 
-# import frappe
-from frappe.tests import IntegrationTestCase, UnitTestCase
+import typing
+from datetime import datetime, timedelta
+from typing import Literal
+from unittest import TestCase
+from unittest.mock import Mock, patch
 
-# On IntegrationTestCase, the doctype test records and all
-# link-field test record dependencies are recursively loaded
-# Use these module variables to add/remove to/from that list
-EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
-IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+import frappe
+
+from press.api.server import schedule_auto_scale
+from press.press.doctype.auto_scale_record.auto_scale_record import AutoScaleRecord
+from press.press.doctype.server.test_server import create_test_server
+
+if typing.TYPE_CHECKING:
+	from press.press.doctype.server.server import Server
 
 
-class UnitTestAutoScaleRecord(UnitTestCase):
+def mimic_memory_usage(is_high: bool = False):
+	return {"memory": 0.4} if not is_high else {"memory": 0.9}
+
+
+def mimic_cpu_usage(is_high: bool = False):
+	return {"vcpu": 0.4} if not is_high else {"vcpu": 0.9}
+
+
+def mimic_get_cpu_and_memory_usage(is_high: bool = False):
+	cpu_usage = mimic_get_cpu_and_memory_usage(is_high)
+	cpu_usage.update(mimic_memory_usage(is_high))
+	return cpu_usage
+
+
+def create_test_auto_scale_record(
+	action: Literal["Scale Up", "Scale Down"],
+	is_automatically_triggered: bool = False,
+	server: Server | None = None,
+) -> AutoScaleRecord:
+	server = server or create_test_server()
+	server.secondary_server = (
+		create_test_server().name if not server.secondary_server else server.secondary_server
+	)
+	return frappe.get_doc(
+		{
+			"doctype": "Auto Scale Record",
+			"server": server.name,
+			"action": action,
+			"is_automatically_triggered": is_automatically_triggered,
+		}
+	).insert()
+
+
+@patch.object(AutoScaleRecord, "after_insert", new=Mock())
+class UnitTestAutoScaleRecord(TestCase):
 	"""
 	Unit tests for AutoScaleRecord.
 	Use this class for testing individual functions and methods.
 	"""
 
-	pass
+	def test_scheduled_auto_scale(self):
+		primary_server = create_test_server()
+		secondary_server = create_test_server()
+		primary_server.secondary_server = secondary_server.name
+		primary_server.save()
 
+		first_auto_scale_window = [
+			datetime.now() + timedelta(minutes=1),
+			datetime.now() + timedelta(minutes=6),
+		]
 
-class IntegrationTestAutoScaleRecord(IntegrationTestCase):
-	"""
-	Integration tests for AutoScaleRecord.
-	Use this class for testing interactions between multiple components.
-	"""
+		with self.assertRaises(frappe.ValidationError):
+			# Scales must be atleast 60 minutes apart
+			schedule_auto_scale(
+				primary_server.name,
+				scheduled_scale_up_time=first_auto_scale_window[0],
+				scheduled_scale_down_time=first_auto_scale_window[1],
+			)
 
-	pass
+		first_auto_scale_window[1] = datetime.now() + timedelta(minutes=61)
+		schedule_auto_scale(
+			primary_server.name,
+			scheduled_scale_up_time=first_auto_scale_window[0],
+			scheduled_scale_down_time=first_auto_scale_window[1],
+		)
+
+		# A scheduled auto scale cannot be created if there is a conflicting auto scale window
+		# Trying to scale up when already scaled up according to the previous schedule
+		conflicting_auto_scale_window = [
+			datetime.now() + timedelta(minutes=5),
+			datetime.now() + timedelta(minutes=70),
+		]
+
+		with self.assertRaises(frappe.ValidationError):
+			# Scales must be atleast 60 minutes apart
+			schedule_auto_scale(
+				primary_server.name,
+				scheduled_scale_up_time=conflicting_auto_scale_window[0],
+				scheduled_scale_down_time=conflicting_auto_scale_window[1],
+			)
+
+		# Trying to scale up in the past
+		conflicting_auto_scale_window[0] = datetime.now() + timedelta(minutes=-10)
+		with self.assertRaises(frappe.ValidationError):
+			schedule_auto_scale(
+				primary_server.name,
+				scheduled_scale_up_time=conflicting_auto_scale_window[0],
+				scheduled_scale_down_time=conflicting_auto_scale_window[1],
+			)
