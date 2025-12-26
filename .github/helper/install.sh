7bd87ffdde9bcb9135e6816ee5f0de191782abf6
Balamurali M <balamurali@frappe.io>
2025-12-26 18:05:50 +0530

    ci: Use cache instead of artifact
    Artifact seems slow
    
    (cherry picked from commit fa85a1ca4167856659537e508ebe549c9c6747d6)

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -23,7 +23,7 @@ bench setup requirements --dev
 
 bench start &> bench_start_logs.txt &
 CI=Yes bench build --app frappe &
-bench new-site --db-root-password root --admin-password admin test_site
+bench new-site --no-mariadb-socket --db-root-password root --admin-password admin test_site
 bench --site test_site install-app press
 bench set-config -g server_script_enabled 1
 bench set-config -g http_port 8000
