440c0345354a6d3168de07dfa3066d960ab588fc
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-01-07 13:23:10 +0530

    chore: Ruff

--- press/press/doctype/proxy_server/proxy_server.py
+++ press/press/doctype/proxy_server/proxy_server.py
@@ -16,10 +16,9 @@ class ProxyServer(BaseServer):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
-
 	if TYPE_CHECKING:
 		from frappe.types import DF
+
 		from press.press.doctype.proxy_server_domain.proxy_server_domain import ProxyServerDomain
 
 		agent_password: DF.Password | None
