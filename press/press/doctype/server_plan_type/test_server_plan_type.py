f03e30139bef6d6970ba969723f06e11254a6396
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2026-01-08 00:37:15 +0530

    chore(ci): Lint fix

--- press/press/doctype/server_plan_type/test_server_plan_type.py
+++ press/press/doctype/server_plan_type/test_server_plan_type.py
@@ -1,14 +1,15 @@
+from __future__ import annotations
+
 # Copyright (c) 2026, Frappe and Contributors
 # See license.txt
-
 # import frappe
 from frappe.tests import IntegrationTestCase
 
 # On IntegrationTestCase, the doctype test records and all
 # link-field test record dependencies are recursively loaded
 # Use these module variables to add/remove to/from that list
-EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
-IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+EXTRA_TEST_RECORD_DEPENDENCIES: list[str] = []  # eg. ["User"]
+IGNORE_TEST_RECORD_DEPENDENCIES: list[str] = []  # eg. ["User"]
 
 
 class IntegrationTestServerPlanType(IntegrationTestCase):
