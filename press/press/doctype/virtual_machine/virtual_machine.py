24fa164ac64cedbebb84ba62c10b686cc56a2a14
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-01-07 11:34:41 +0530

    feat(compute): stop vm method
    (cherry picked from commit 7a059ad850dd2a62c204530d5f538d07368f6166)

--- press/press/doctype/virtual_machine/virtual_machine.py
+++ press/press/doctype/virtual_machine/virtual_machine.py
@@ -1414,6 +1414,8 @@ class VirtualMachine(Document):
 			self.client().instance_action(instance_id=self.instance_id, action="STOP")
 		elif self.cloud_provider == "Hetzner":
 			self.client().servers.shutdown(self.get_hetzner_server_instance(fetch_data=False))
+		elif self.cloud_provider == "Frappe Compute":
+			self.client().stop_vm(self.name)
 		self.sync()
 
 	@frappe.whitelist()
