98acb6e2720ef5193d1f4162e91b20bff77f58b4
Sabu Siyad <hello@ssiyad.com>
2026-01-08 20:57:13 +0530

    feat(server): Ignore IPs for Fail2Ban
    (cherry picked from commit e465cfe9e39dd609a06508f0a993becb5b84d697)

--- press/press/doctype/proxy_server/proxy_server.py
+++ press/press/doctype/proxy_server/proxy_server.py
@@ -9,6 +9,7 @@ from frappe.utils import unique
 
 from press.press.doctype.server.server import BaseServer
 from press.runner import Ansible
+from press.security import fail2ban
 from press.utils import log_error
 
 if TYPE_CHECKING:
@@ -195,9 +196,15 @@ class ProxyServer(BaseServer):
 
 	@frappe.whitelist()
 	def setup_fail2ban(self):
+		frappe.enqueue_doc(
+			self.doctype,
+			self.name,
+			"_setup_fail2ban",
+			queue="long",
+			timeout=1200,
+		)
 		self.status = "Installing"
 		self.save()
-		frappe.enqueue_doc(self.doctype, self.name, "_setup_fail2ban", queue="long", timeout=1200)
 
 	def _setup_fail2ban(self):
 		try:
@@ -206,6 +213,9 @@ class ProxyServer(BaseServer):
 				server=self,
 				user=self._ssh_user(),
 				port=self._ssh_port(),
+				variables={
+					"ignore_ips": fail2ban.ignore_ips(),
+				},
 			)
 			play = ansible.run()
 			self.reload()
