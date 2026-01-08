cba2a67fafea07398111c15fc6c6b8e2024aa332
Aradhya-Tripathi <aradhya@frappe.io>
2026-01-09 00:33:17 +0530

    fix(test): Autoscale test for secondary drop
    (cherry picked from commit 45aa75c3e1343467e8692978a60cfcf450e6c006)

--- press/press/doctype/auto_scale_record/test_auto_scale_record.py
+++ press/press/doctype/auto_scale_record/test_auto_scale_record.py
@@ -171,7 +171,7 @@ class UnitTestAutoScaleRecord(TestCase):
 
 		with self.assertRaises(frappe.ValidationError) as context:
 			nfs_volume_detachment: "NFSVolumeDetachment" = frappe.get_doc(
-				{"doctype": "NFS Volume Detachment", "primary_server": self.primary_server}
+				{"doctype": "NFS Volume Detachment", "primary_server": self.primary_server.name}
 			)
 			nfs_volume_detachment.insert(ignore_permissions=True)
 
