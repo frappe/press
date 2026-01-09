eb9de1bf62990c8cb18aef889b93d7bac6140516
Aradhya-Tripathi <aradhya@frappe.io>
2026-01-09 12:25:56 +0530

    fix(unified-server): Update docstring

--- press/press/doctype/virtual_machine/virtual_machine.py
+++ press/press/doctype/virtual_machine/virtual_machine.py
@@ -1563,7 +1563,7 @@ class VirtualMachine(Document):
 
 	@frappe.whitelist()
 	def create_unified_server(self) -> tuple[Server, DatabaseServer]:
-		"""Virtual machines of series U will create a f series app server and m series database server"""
+		"""Virtual machines of series U will create a u series app server and u series database server"""
 
 		if self.series != "u":
 			frappe.throw("Only virtual machines of series 'u' can create unified servers.")
