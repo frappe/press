0ca98a36289c19fbcf30e32753acd6a58a2c3860
Aradhya-Tripathi <aradhya@frappe.io>
2026-01-16 15:39:21 +0530

    feat(auto-scale): Add dedicated workers for autoscale related tasks
    (cherry picked from commit a840abba540ff47df12d6cdbcaa470704c39b566)

--- press/press/doctype/nfs_volume_attachment/nfs_volume_attachment.py
+++ press/press/doctype/nfs_volume_attachment/nfs_volume_attachment.py
@@ -451,7 +451,7 @@ class NFSVolumeAttachment(Document, AutoScaleStepFailureHandler, StepHandler):
 			steps=self.nfs_volume_attachment_steps,
 			timeout=18000,
 			at_front=True,
-			queue="long",
+			queue="auto-scale",
 			enqueue_after_commit=True,
 		)
 
