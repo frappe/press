b2dc0cb0c150af69853c678ded4ae0a48b405554
phot0n <ritwikpuri5678@gmail.com>
2026-01-19 13:29:39 +0530

    feat: add nat server doctype and setup

---
+++ press/press/doctype/nat_server/nat_server.js
@@ -0,0 +1,42 @@
+// Copyright (c) 2026, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('NAT Server', {
+	refresh(frm) {
+		[
+			[__('Prepare Server'), 'prepare_server', true, !frm.doc.is_server_setup],
+			[__('Setup Server'), 'setup_server', true, !frm.doc.is_server_setup],
+			[__('Archive Server'), 'archive', true, frm.doc.is_server_setup],
+		].forEach(([label, method, confirm, condition]) => {
+			if (typeof condition === 'undefined' || condition) {
+				frm.add_custom_button(
+					label,
+					() => {
+						if (confirm) {
+							frappe.confirm(
+								`Are you sure you want to ${label.toLowerCase()}?`,
+								() =>
+									frm.call(method).then((r) => {
+										if (r.message) {
+											frappe.msgprint(r.message);
+										} else {
+											frm.refresh();
+										}
+									}),
+							);
+						} else {
+							frm.call(method).then((r) => {
+								if (r.message) {
+									frappe.msgprint(r.message);
+								} else {
+									frm.refresh();
+								}
+							});
+						}
+					},
+					__('Actions'),
+				);
+			}
+		});
+	},
+});
