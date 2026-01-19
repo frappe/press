e841b18e5162e902f053472aa7ef25beb8549818
Aradhya-Tripathi <aradhya@frappe.io>
2026-01-19 15:03:40 +0530

    feat(do): Figure out the private IP in the cloud init
    (cherry picked from commit 0cae30ef575c7e1a846fa3b4372daa0e3dfcdc39)

--- press/press/doctype/virtual_machine/virtual_machine.py
+++ press/press/doctype/virtual_machine/virtual_machine.py
@@ -706,7 +706,7 @@ class VirtualMachine(Document):
 		if server.doctype == "Database Server":
 			return {
 				"server_id": server.server_id,
-				"private_ip": self.private_ip_address,
+				"private_ip": self.private_ip_address or "__PRIVATE_IP__",
 				"ansible_memtotal_mb": memory,
 				"mariadb_root_password": server.get_password("mariadb_root_password"),
 				"db_port": server.db_port or 3306,
@@ -715,7 +715,7 @@ class VirtualMachine(Document):
 			database_server: DatabaseServer = frappe.get_doc("Database Server", server.database_server)
 			return {
 				"server_id": database_server.server_id,
-				"private_ip": self.private_ip_address,
+				"private_ip": self.private_ip_address or "__PRIVATE_IP__",
 				"ansible_memtotal_mb": memory,
 				"mariadb_root_password": database_server.get_password("mariadb_root_password"),
 				"db_port": database_server.db_port or 3306,
@@ -737,7 +737,9 @@ class VirtualMachine(Document):
 			"monitoring_password": server.get_monitoring_password(),
 			"statsd_exporter_service": frappe.render_template(
 				"press/playbooks/roles/statsd_exporter/templates/statsd_exporter.service",
-				{"private_ip": self.private_ip_address},
+				{
+					"private_ip": self.private_ip_address or "__PRIVATE_IP__"
+				},  # Replace inside cloudinit in case of DigitalOcean
 				is_path=True,
 			),
 			"filebeat_config": frappe.render_template(
