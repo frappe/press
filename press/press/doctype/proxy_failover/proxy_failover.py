8c6acaa11b4ca517de9ee9ff6d99613d1f375849
phot0n <ritwikpuri5678@gmail.com>
2026-01-25 21:13:29 +0530

    fix(proxy-failover): set use_as_proxy_for_agent_and_metrics on secondary if set on primary
    * also fix security group port opening logic only if the provider is AWS
    
    (cherry picked from commit bb83c82188f20259a51b595671ff2e47f2b4f0ce)
    
    # Conflicts:
    #	press/press/doctype/proxy_failover/proxy_failover.py

--- press/press/doctype/proxy_failover/proxy_failover.py
+++ press/press/doctype/proxy_failover/proxy_failover.py
@@ -70,6 +70,7 @@ class ProxyFailover(Document, StepHandler):
 				self.wait_for_pending_agent_jobs_to_complete,
 				self.stop_replication,
 				self.replicate_once_manually,
+				self.use_secondary_as_proxy_for_agent_and_metrics,
 				self.move_wildcard_domains_from_primary,
 				self.wait_for_wildcard_domains_setup,
 				self.attach_static_ip_to_secondary,
@@ -120,7 +121,21 @@ class ProxyFailover(Document, StepHandler):
 		step.status = Status.Running
 		step.save()
 
+<<<<<<< HEAD
 		cluster = frappe.get_doc("Cluster", frappe.db.get_value("Proxy Server", self.primary, "cluster"))
+=======
+		primary_proxy = frappe.get_doc("Proxy Server", self.primary)
+
+		Ansible(
+			playbook="nginx_conf_changes_for_tcp_streaming.yml",
+			server=primary_proxy,
+			user=primary_proxy._ssh_user(),
+			port=primary_proxy._ssh_port(),
+			variables={"secondary_proxy": self.secondary},
+		).run()
+
+		cluster = frappe.get_doc("Cluster", primary_proxy.cluster)
+>>>>>>> bb83c8218 (fix(proxy-failover): set use_as_proxy_for_agent_and_metrics on secondary if set on primary)
 		if cluster.cloud_provider == "AWS EC2":
 			client = cluster.get_aws_client()
 			client.authorize_security_group_ingress(
@@ -139,6 +154,7 @@ class ProxyFailover(Document, StepHandler):
 					},
 				],
 			)
+<<<<<<< HEAD
 		elif cluster.cloud_provider == "OCI":
 			vcn_client = VirtualNetworkClient(cluster.get_oci_config())
 			vcn_client.add_network_security_group_security_rules(
@@ -155,6 +171,8 @@ class ProxyFailover(Document, StepHandler):
 					]
 				),
 			)
+=======
+>>>>>>> bb83c8218 (fix(proxy-failover): set use_as_proxy_for_agent_and_metrics on secondary if set on primary)
 
 		if self.primary not in (alt_port_servers := servers_using_alternative_port_for_communication()):
 			alt_port_servers.append(self.primary)
@@ -262,6 +280,23 @@ class ProxyFailover(Document, StepHandler):
 
 	def update_app_servers(self, step):
 		frappe.db.set_value("Server", {"proxy_server": self.primary}, "proxy_server", self.secondary)
+
+		step.status = Status.Success
+		step.save()
+
+	def use_secondary_as_proxy_for_agent_and_metrics(self, step):
+		if frappe.db.get_value("Proxy Server", self.primary, "use_as_proxy_for_agent_and_metrics"):
+			frappe.db.set_value(
+				"Proxy Server",
+				self.secondary,
+				{"use_as_proxy_for_agent_and_metrics": 1},
+			)
+
+			frappe.db.set_value(
+				"Proxy Server",
+				self.primary,
+				{"use_as_proxy_for_agent_and_metrics": 0},
+			)
 
 		step.status = Status.Success
 		step.save()
