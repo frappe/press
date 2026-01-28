6884256d882f85dfb1cdae1b6aff40a7e586a3d6
Aradhya-Tripathi <aradhya@frappe.io>
2026-01-28 16:57:33 +0530

    feat(app): Parse frappe version picked from toml

--- press/press/doctype/app/app.py
+++ press/press/doctype/app/app.py
@@ -6,6 +6,7 @@ import typing
 
 import frappe
 import rq
+import semantic_version as sv
 from frappe.model.document import Document
 
 from press.utils.jobs import has_job_timeout_exceeded
@@ -42,15 +43,16 @@ class App(Document):
 
 	def add_source(
 		self,
-		version,
 		repository_url,
 		branch,
+		frappe_version: str,
 		team=None,
 		github_installation_id=None,
 		public=False,
 		repository_owner=None,
 	) -> "AppSource":
 		# Ensure no .git suffix when looking for existing sources
+		supported_frappe_versions = parse_frappe_version(frappe_version)
 		repository_url = repository_url.removesuffix(".git")
 		existing_source = frappe.get_all(
 			"App Source",
@@ -60,15 +62,16 @@ class App(Document):
 		if existing_source:
 			source = frappe.get_doc("App Source", existing_source[0].name)
 			versions = set(version.version for version in source.versions)
-			if version not in versions:
-				source.add_version(version)
+			new_versions = supported_frappe_versions - versions
+			for new_version in new_versions:
+				source.add_version(new_version)
 		else:
 			# Add new App Source
 			source = frappe.get_doc(
 				{
 					"doctype": "App Source",
 					"app": self.name,
-					"versions": [{"version": version}],
+					"versions": [{"version": v} for v in supported_frappe_versions],
 					"repository_url": repository_url,
 					"branch": branch,
 					"team": team,
@@ -106,3 +109,40 @@ def poll_new_releases():
 			return
 		except Exception:
 			frappe.db.rollback()
+
+
+def map_frappe_version(version_string: str, frappe_versions: list[dict[str, int | str]]) -> list[str]:
+	"""Map a version spec to supported Frappe versions."""
+	matched = []
+	version_string = version_string.replace(",", " ")
+	spec = sv.NpmSpec(version_string)
+	highest_supported_stable_version = sv.Version(
+		f"{max(version['number'] for version in frappe_versions if version['status'] == 'Stable')}.0.0",
+	)
+
+	for version in frappe_versions:
+		supported_version = sv.Version(
+			f"{version['number']}.0.0"
+		)  # Converting frappe version number (16 -> 16.0.0)
+		if spec.match(supported_version):
+			matched.append(str(version["name"]))
+
+	# Check if the spec can support more than the highest stable version
+	if spec.match(highest_supported_stable_version.next_patch()):
+		matched.append("Nightly")
+
+	return matched
+
+
+def parse_frappe_version(version_string: str) -> set[str]:
+	"""Parse the Frappe version from a version string."""
+	frappe_versions = frappe.get_all(
+		"Frappe Version",
+		{"public": True},
+		["name", "number", "status"],
+	)
+	# This is already supported return quick
+	if version_string in [frappe_version["name"] for frappe_version in frappe_versions]:
+		return set(version_string)
+
+	return set(map_frappe_version(version_string, frappe_versions))
