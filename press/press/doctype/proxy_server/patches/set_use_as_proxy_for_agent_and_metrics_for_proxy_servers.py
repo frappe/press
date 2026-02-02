4177875afc5077dc39c5df6459a0fc6d74dba6d3
phot0n <ritwikpuri5678@gmail.com>
2026-02-02 12:46:39 +0530

    fix(patch): mark use_as_proxy_for_agent_and_metrics on active proxy server

--- press/press/doctype/proxy_server/patches/set_use_as_proxy_for_agent_and_metrics_for_proxy_servers.py
+++ press/press/doctype/proxy_server/patches/set_use_as_proxy_for_agent_and_metrics_for_proxy_servers.py
@@ -5,11 +5,11 @@ def execute():
 	for cluster in frappe.get_all("Cluster"):
 		if not frappe.db.get_value(
 			"Proxy Server",
-			{"status": ["!=", "Archived"], "cluster": cluster.name, "use_as_proxy_for_agent_and_metrics": 1},
+			{"status": "Active", "cluster": cluster.name, "use_as_proxy_for_agent_and_metrics": 1},
 		):
 			frappe.db.set_value(
 				"Proxy Server",
-				frappe.db.get_value("Proxy Server", {"status": ["!=", "Archived"], "cluster": cluster.name}),
+				frappe.db.get_value("Proxy Server", {"status": "Active", "cluster": cluster.name}),
 				"use_as_proxy_for_agent_and_metrics",
 				1,
 			)
