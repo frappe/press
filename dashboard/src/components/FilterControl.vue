05dd82832f416a4607f8eab4dbb82278ffc1adf7
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2026-02-05 12:15:42 +0530

    fix(object-list-filters): modify gap and checkbox colours
    (cherry picked from commit 8d992e328cb0c3948aa3a72c3f90281fcb150e4b)

--- dashboard/src/components/FilterControl.vue
+++ dashboard/src/components/FilterControl.vue
@@ -16,11 +16,19 @@
 		v-else-if="$attrs.type === 'datetime'"
 		v-bind="{ ...$attrs, type: undefined }"
 	/>
+	<div
+		v-else-if="$attrs.type === 'checkbox'"
+		class="[&_input+label]:text-ink-gray-5 [&_input:checked+label]:text-ink-gray-8 [&_input+label]:font-normal"
+	>
+		<FormControl v-bind="$attrs" />
+	</div>
 	<FormControl v-else v-bind="$attrs" />
 	<div v-if="$attrs.type === 'date' || $attrs.type === 'datetime'"></div>
 	<!-- idk what magic is it but if I remove the div datetime components cease to work -->
 </template>
+
 <script setup>
 import { DatePicker, TabButtons, DateTimePicker } from 'frappe-ui';
 import LinkControl from './LinkControl.vue';
+import FormControl from 'frappe-ui/src/components/FormControl/FormControl.vue';
 </script>
