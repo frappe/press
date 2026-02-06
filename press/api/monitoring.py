c327a2ba6131c683b7ebfc9d8f17825ebdcbdbe3
phamducbinhh <phamducbinh1712000@gmail.com>
2026-02-06 04:12:18 +0000

    Fix lỗi space monitor

--- press/api/monitoring.py
+++ press/api/monitoring.py
@@ -11,88 +11,88 @@ from press.utils import log_error, servers_using_alternative_port_for_communicat
 
 
 def get_benches():
-	self_hosted_stand_alone_servers = frappe.get_all(
-		"Server",
-		{"is_standalone": True, "is_self_hosted": True, "status": "Active"},
-		pluck="name",
-	)
-	sites = frappe.get_all(
-		"Site",
-		["name", "bench"],
-		{"status": "Active", "server": ("not in", self_hosted_stand_alone_servers)},
-		ignore_ifnull=True,
-	)
-	sites.sort(key=lambda x: (x.bench, x.name))
+    self_hosted_stand_alone_servers = frappe.get_all(
+        "Server",
+        {"is_standalone": True, "is_self_hosted": True, "status": "Active"},
+        pluck="name",
+    )
+    sites = frappe.get_all(
+        "Site",
+        ["name", "bench"],
+        {"status": "Active", "server": ("not in", self_hosted_stand_alone_servers)},
+        ignore_ifnull=True,
+    )
+    sites.sort(key=lambda x: (x.bench, x.name))
 
-	bench_map = {
-		bench.name: bench
-		for bench in frappe.get_all(
-			"Bench",
-			{"name": ("in", set(site.bench for site in sites))},
-			["name", "cluster", "server", "group"],
-			ignore_ifnull=True,
-		)
-	}
-	benches = []
-	for bench_name, _sites in groupby(sites, lambda x: x.bench):
-		bench = bench_map[bench_name]
-		bench.update({"sites": [site.name for site in _sites]})
-		benches.append(bench)
+    bench_map = {
+        bench.name: bench
+        for bench in frappe.get_all(
+            "Bench",
+            {"name": ("in", set(site.bench for site in sites))},
+            ["name", "cluster", "server", "group"],
+            ignore_ifnull=True,
+        )
+    }
+    benches = []
+    for bench_name, _sites in groupby(sites, lambda x: x.bench):
+        bench = bench_map[bench_name]
+        bench.update({"sites": [site.name for site in _sites]})
+        benches.append(bench)
 
-	return benches
+    return benches
 
 
 def get_clusters():
-	servers = {}
-	servers["proxy"] = frappe.get_all("Proxy Server", {"status": ("!=", "Archived")}, ["name", "cluster"])
-	servers["app"] = frappe.get_all("Server", {"status": ("!=", "Archived")}, ["name", "cluster"])
-	servers["database"] = frappe.get_all(
-		"Database Server", {"status": ("!=", "Archived")}, ["name", "cluster"]
-	)
-	clusters = frappe.get_all("Cluster")
-	job_map = {
-		"proxy": ["node", "nginx", "proxysql", "mariadb_proxy"],
-		"app": ["node", "nginx", "docker", "cadvisor", "gunicorn", "rq"],
-		"database": ["node", "mariadb"],
-	}
-	servers_using_alternative_port = servers_using_alternative_port_for_communication()
-	for cluster in clusters:
-		cluster["jobs"] = {}
+    servers = {}
+    servers["proxy"] = frappe.get_all("Proxy Server", {"status": ("!=", "Archived")}, ["name", "cluster"])
+    servers["app"] = frappe.get_all("Server", {"status": ("!=", "Archived")}, ["name", "cluster"])
+    servers["database"] = frappe.get_all(
+        "Database Server", {"status": ("!=", "Archived")}, ["name", "cluster"]
+    )
+    clusters = frappe.get_all("Cluster")
+    job_map = {
+        "proxy": ["node", "nginx", "proxysql", "mariadb_proxy"],
+        "app": ["node", "nginx", "docker", "cadvisor", "gunicorn", "rq"],
+        "database": ["node", "mariadb"],
+    }
+    servers_using_alternative_port = servers_using_alternative_port_for_communication()
+    for cluster in clusters:
+        cluster["jobs"] = {}
 
-		for server_type, server_type_servers in servers.items():
-			for server in server_type_servers:
-				if server.cluster == cluster.name:
-					for job in job_map[server_type]:
-						if server.name in servers_using_alternative_port:
-							cluster["jobs"].setdefault(job, []).append(f"{server.name}:8443")
-						else:
-							cluster["jobs"].setdefault(job, []).append(server.name)
+        for server_type, server_type_servers in servers.items():
+            for server in server_type_servers:
+                if server.cluster == cluster.name:
+                    for job in job_map[server_type]:
+                        if server.name in servers_using_alternative_port:
+                            cluster["jobs"].setdefault(job, []).append(f"{server.name}:8443")
+                        else:
+                            cluster["jobs"].setdefault(job, []).append(server.name)
 
-	return clusters
+    return clusters
 
 
 def get_domains():
-	return frappe.get_all(
-		"Site Domain", ["name", "site"], {"tls_certificate": ("is", "set")}, order_by="name"
-	)
+    return frappe.get_all(
+        "Site Domain", ["name", "site"], {"tls_certificate": ("is", "set")}, order_by="name"
+    )
 
 
 def get_tls():
-	tls = []
-	server_types = [
-		"Server",
-		"Proxy Server",
-		"Database Server",
-		"Registry Server",
-		"Log Server",
-		"Monitor Server",
-		"Analytics Server",
-		"Trace Server",
-	]
-	for server_type in server_types:
-		tls += frappe.get_all(server_type, {"status": ("!=", "Archived")}, ["name"])
+    tls = []
+    server_types = [
+        "Server",
+        "Proxy Server",
+        "Database Server",
+        "Registry Server",
+        "Log Server",
+        "Monitor Server",
+        "Analytics Server",
+        "Trace Server",
+    ]
+    for server_type in server_types:
+        tls += frappe.get_all(server_type, {"status": ("!=", "Archived")}, ["name"])
 
-	return tls
+    return tls
 
 
 @frappe.whitelist(allow_guest=True)
@@ -111,11 +111,11 @@ def targets(): # BỎ tham số 'token' khỏi định nghĩa hàm
     # Kiểm tra xem 'token' có tồn tại không
     if not token:
         frappe.throw("Error: 'token' parameter is missing.", frappe.ValidationError)
-    
-	monitor_token = frappe.db.get_single_value("Press Settings", "monitor_token")
+
+    monitor_token = frappe.db.get_single_value("Press Settings", "monitor_token")
     if token != monitor_token:
-        
-        return None 
+
+        return None
 
     # Nếu token hợp lệ, trả về dữ liệu
     return {"benches": get_benches(), "clusters": get_clusters(), "domains": get_domains(), "tls": get_tls()}
@@ -123,21 +123,21 @@ def targets(): # BỎ tham số 'token' khỏi định nghĩa hàm
 
 @frappe.whitelist(allow_guest=True, xss_safe=True)
 def alert(*args, **kwargs):
-	try:
-		user = str(frappe.session.user)
-		frappe.set_user("Administrator")
+    try:
+        user = str(frappe.session.user)
+        frappe.set_user("Administrator")
 
-		doc = frappe.get_doc(
-			{
-				"doctype": "Alertmanager Webhook Log",
-				"payload": frappe.request.get_data().decode(),
-			}
-		)
-		doc.insert()
-	except AlertRuleNotEnabled:
-		pass
-	except Exception:
-		log_error("Alertmanager Webhook Error", args=args, kwargs=kwargs)
-		raise
-	finally:
-		frappe.set_user(user)
+        doc = frappe.get_doc(
+            {
+                "doctype": "Alertmanager Webhook Log",
+                "payload": frappe.request.get_data().decode(),
+            }
+        )
+        doc.insert()
+    except AlertRuleNotEnabled:
+        pass
+    except Exception:
+        log_error("Alertmanager Webhook Error", args=args, kwargs=kwargs)
+        raise
+    finally:
+        frappe.set_user(user)
