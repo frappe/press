79c41529e88671093f0c8264eb8a8f6397ced6ee
Jayanta <55305804+regdocs@users.noreply.github.com>
2026-02-06 19:27:00 +0530

    Revert "fix(dashboard-ui): February 2026 Sprint week-1 (2) (manual-backport #4959)"

--- dashboard/src/objects/marketplace.js
+++ dashboard/src/objects/marketplace.js
@@ -32,7 +32,7 @@ export default {
 				label: 'App',
 				fieldname: 'title',
 				class: 'font-medium',
-				width: 0.6,
+				width: 0.3,
 				prefix(row) {
 					return row.image
 						? h('img', {
