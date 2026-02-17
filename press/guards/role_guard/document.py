560a0a2f9a7ee0e766fdb9a998ca26fc891d143c
Sabu Siyad <hello@ssiyad.com>
2026-02-18 01:11:05 +0530

    feat(roles): All resources checkbox
    Add checkboxes in dashboard to easily select all resources.
    Example: sites, servers and release groups.
    These will not show up resources page of the role.

--- press/guards/role_guard/document.py
+++ press/guards/role_guard/document.py
@@ -3,10 +3,14 @@ from frappe.query_builder import Not
 from frappe.query_builder.functions import Count
 from frappe.query_builder.terms import QueryBuilder
 
+from press.utils import get_current_team
+
 
 def check(base_query: QueryBuilder, document_type: str, document_name: str) -> bool | list[str]:
 	if document_name:
-		return document(base_query, document_type, document_name)
+		return has_user_permission(document_type) or document(base_query, document_type, document_name)
+	if has_user_permission(document_type):
+		return []
 	return documents(base_query, document_type)
 
 
@@ -41,5 +45,26 @@ def document(base_query: QueryBuilder, document_type: str, document_name: str) -
 		.run(as_dict=True)
 		.pop()
 		.get("document_count")
+		> 0
+	)
+
+
+def has_user_permission(doctype: str) -> bool:
+	PressRole = frappe.qb.DocType("Press Role")
+	PressRoleUser = frappe.qb.DocType("Press Role User")
+	# HACK: This is probably not a good idea. At this point, only possible
+	# doctypes are servers, sites and release groups.
+	key = f"all_{doctype.replace(' ', '_').lower()}s"
+	return (
+		frappe.qb.from_(PressRole)
+		.inner_join(PressRoleUser)
+		.on(PressRoleUser.parent == PressRole.name)
+		.select(Count(PressRole.name).as_("count"))
+		.where(PressRole.team == get_current_team())
+		.where(PressRole[key] == 1)
+		.where(PressRoleUser.user == frappe.session.user)
+		.run(as_dict=True)
+		.pop()
+		.get("count")
 		> 0
 	)
