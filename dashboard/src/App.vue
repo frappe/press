dd7d3ece689e74f47923440473f1d760f5346bf7
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2026-02-17 05:54:53 +0530

    fix(sidebar): show only if team is valid

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -6,7 +6,7 @@
 					v-if="!isSignupFlow && !$isMobile && !isHideSidebar"
 					class="relative block min-h-0 flex-shrink-0 overflow-hidden hover:overflow-auto"
 				>
-					<AppSidebar v-if="$session.user" />
+					<AppSidebar v-if="$session.user && $team?.doc" />
 				</div>
 				<div class="w-full overflow-auto z-0" id="scrollContainer">
 					<MobileNav
