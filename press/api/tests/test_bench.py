6a2c96bb470d1becde81e22f83f5c44afead7d09
Aradhya-Tripathi <aradhya@frappe.io>
2026-02-18 14:06:13 +0530

    fix(test): Fix incident tests & Server Test 1 (attempt 1)
    (cherry picked from commit e9fec553f2332df4b01b5b1fdbff44e6e5abb874)

--- press/api/tests/test_bench.py
+++ press/api/tests/test_bench.py
@@ -1,6 +1,7 @@
 import json
 import os
 import time
+import typing
 from unittest import skip
 from unittest.mock import MagicMock, Mock, patch
 
@@ -37,6 +38,9 @@ from press.press.doctype.server.test_server import create_test_server
 from press.press.doctype.team.test_team import create_test_press_admin_team
 from press.utils import get_current_team
 from press.utils.test import foreground_enqueue_doc
+
+if typing.TYPE_CHECKING:
+	from press.press.doctype.release_group.release_group import ReleaseGroup
 
 
 @patch.object(AgentJob, "enqueue_http_request", new=Mock())
@@ -139,7 +143,7 @@ class TestAPIBench(FrappeTestCase):
 		dc_count_before = frappe.db.count("Deploy Candidate", filters={"group": group})
 
 		release = create_test_app_release(frappe.get_doc("App Source", self.app_source.name))
-		deploy_and_update(group, [{"release": release.name}], [])
+		deploy_and_update(group, [{"release": release.name, "hash": frappe.generate_hash(length=8)}], [])
 
 		bu_count_after = frappe.db.count("Bench Update", filters={"group": group})
 		dc_count_after = frappe.db.count("Deploy Candidate", filters={"group": group})
@@ -190,7 +194,8 @@ class TestAPIBench(FrappeTestCase):
 	@patch("press.press.doctype.deploy_candidate.deploy_candidate.frappe.db.commit", new=Mock())
 	def test_deploy_fn_fails_with_empty_apps(self):
 		frappe.set_user(self.team.user)
-		group = new(
+		server = create_test_server()
+		group: ReleaseGroup = new(
 			{
 				"title": "Test Bench",
 				"apps": [{"name": self.app.name, "source": self.app_source.name}],
@@ -200,6 +205,8 @@ class TestAPIBench(FrappeTestCase):
 				"server": None,
 			}
 		)
+		group.build_server = server.name
+		group.save()
 		deploy(group, [])
 
 	@timeout(20)
