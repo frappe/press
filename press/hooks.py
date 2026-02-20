1b382034cce465639bb71dab8904bfc2fa47b8ea
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2026-02-20 16:53:09 +0530

    feat(database): Add iops limit in db size calculation via parser (#5115)

--- press/hooks.py
+++ press/hooks.py
@@ -241,6 +241,7 @@ scheduler_events = {
 		"press.press.doctype.server_snapshot.server_snapshot.expire_snapshots",
 		"press.saas.doctype.product_trial.product_trial.sync_product_site_users",
 		"press.press.doctype.database_server.database_server.sync_binlogs_info",
+		"press.press.doctype.database_server.database_server.update_database_schema_sizes",
 	],
 	"hourly_long": [
 		"press.press.doctype.release_group.release_group.prune_servers_without_sites",
@@ -278,9 +279,6 @@ scheduler_events = {
 		],
 		"*/2 * * * *": [
 			"press.press.doctype.incident.incident.resolve_incidents",
-		],
-		"45 * * * *": [
-			"press.press.doctype.database_server.database_server.update_database_schema_sizes",
 		],
 		"0 4 * * *": [
 			"press.press.doctype.site.backups.cleanup_offsite",
