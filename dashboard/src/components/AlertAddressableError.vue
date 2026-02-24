f70234ed911a42c1d1bbcd22a93774d73a8966da
Balamurali M <balamurali@frappe.io>
2026-02-24 13:12:00 +0530

    fix(dashboard): Make user addressable error primary button
    To incentivize clicking
    
    (cherry picked from commit f13c989e5fc054547865d0ffdb7d3a1737885b62)

--- dashboard/src/components/AlertAddressableError.vue
+++ dashboard/src/components/AlertAddressableError.vue
@@ -1,6 +1,6 @@
 <template>
 	<AlertBanner :title="title" :type="type">
-		<Button class="ml-auto" variant="outline" @click="show">View</Button>
+		<Button class="ml-auto" variant="solid" @click="show">Fix</Button>
 	</AlertBanner>
 </template>
 
