d208f3ae51a12bd7716cf80d2af5835fc70f0409
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2026-02-26 15:00:43 +0530

    feat(prm): Patch for merging status with engagement stage
    (cherry picked from commit 718c6f4fb28c2b4b429113191db40c3a8cffef62)

--- press/partner/doctype/partner_lead/partner_lead.py
+++ press/partner/doctype/partner_lead/partner_lead.py
@@ -159,8 +159,6 @@ class PartnerLead(Document):
 				query = query.where(PartnerLead.status == filters.get("status"))
 			if filters.get("origin"):
 				query = query.where(PartnerLead.origin == filters.get("origin"))
-			if filters.get("engagement_stage"):
-				query = query.where(PartnerLead.engagement_stage == filters.get("engagement_stage"))
 			if filters.get("search-text"):
 				search_text = f"%{filters.get('search-text')}%"
 				query = query.where(
