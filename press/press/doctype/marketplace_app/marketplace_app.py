6e7fec09cb522ea52e2d42430614ee06537210e8
Aradhya-Tripathi <aradhya@frappe.io>
2026-03-06 13:38:22 +0530

    refactor(marketplace): Better missing versioning errors

--- press/press/doctype/marketplace_app/marketplace_app.py
+++ press/press/doctype/marketplace_app/marketplace_app.py
@@ -188,14 +188,21 @@ class MarketplaceApp(WebsiteGenerator):
 			app_doc = new_app_doc(self.name, self.title)
 
 		if not self.sources:
-			source = app_doc.add_source(
-				repository_url=self.repository_url,
-				branch=self.branch,
-				team=self.team,
-				github_installation_id=self.github_installation_id,
-				frappe_version=self.frappe_version,
-				public=True,
-			)
+			try:
+				source = app_doc.add_source(
+					repository_url=self.repository_url,
+					branch=self.branch,
+					team=self.team,
+					github_installation_id=self.github_installation_id,
+					frappe_version=self.frappe_version,
+					public=True,
+				)
+			except frappe.MandatoryError:
+				frappe.throw(
+					"Failed to create app source since no version was found matching "
+					f"{self.frappe_version} try relaxing the version constraints",
+					VersioningError,
+				)
 			self.app = source.app
 			for version in source.versions:
 				self.append("sources", {"version": version.version, "source": source.name})
