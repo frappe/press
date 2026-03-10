cfa0dcd14a630687f4f499d3158c726d4b972e55
Aysha <ayshapp468@gmail.com>
2026-03-10 22:43:45 +0530

    fix: Bug in hooks.py

--- press/hooks.py
+++ press/hooks.py
@@ -364,9 +364,9 @@ scheduler_events = {
 		"*/30 * * * *": [
 			"press.press.doctype.site_update.scheduled_auto_updates.trigger",
 			"press.press.doctype.team.suspend_sites.execute",
-			"press.press.doctype.site_backup.site_backup.delete_successful_unavailable_backups_for_archived_sites"
-			"press.press.doctype.site_backup.site_backup.delete_failed_unavailable_backups_for_archived_sites"
-			"press.press.doctype.site_backup.site_backup.delete_agent_job_records_for_archived_sites"
+			"press.press.doctype.site_backup.site_backup.delete_successful_unavailable_backups_for_archived_sites",
+			"press.press.doctype.site_backup.site_backup.delete_failed_unavailable_backups_for_archived_sites",
+			"press.press.doctype.site_backup.site_backup.delete_agent_job_records_for_archived_sites",
 			"press.press.doctype.site_backup.site_backup.delete_site_activity_records_for_archived_sites",
 		],
 		"15,45 * * * *": [
