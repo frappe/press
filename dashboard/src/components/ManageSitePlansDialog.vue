9fdef733dfe0f1090093d6ec81ad6e6fc681713d
Bowrna <mailbowrna@gmail.com>
2026-03-12 14:42:40 +0530

    fix(billing): Fix for missing button to upgrade plan
    (cherry picked from commit cd30f3f867e48596510b191c1999866e07081dd2)

--- dashboard/src/components/ManageSitePlansDialog.vue
+++ dashboard/src/components/ManageSitePlansDialog.vue
@@ -150,7 +150,6 @@
 import { getCachedDocumentResource, createResource, Progress } from 'frappe-ui';
 import SitePlansCards from './SitePlansCards.vue';
 import { getPlans, getPlan } from '../data/plans';
-import { planDisplay } from '../utils/format';
 import CardForm from './billing/CardForm.vue';
 import BillingDetails from './billing/BillingDetails.vue';
 import PrepaidCreditsForm from './billing/PrepaidCreditsForm.vue';
@@ -226,10 +225,6 @@ export default {
 				},
 			);
 		},
-		planDisplayTitle(plan) {
-			const display = planDisplay(plan, false);
-			return `${display.title}${display.unit}`;
-		},
 		paymentModeAdded() {
 			this.$team.reload();
 			const mode = this.isAutomatedBilling ? 'Card' : 'Prepaid Credits';
@@ -252,11 +247,7 @@ export default {
 		},
 		nextButtonLabel() {
 			if (this.showSetupSubscription) {
-				if (this.plan) {
-					const display = planDisplay(this.plan, false);
-					return `Select Plan: ${display.title}${display.unit}`;
-				}
-				return 'Next';
+				return this.plan ? 'Select Plan' : 'Next';
 			}
 			return this.$site.doc?.current_plan?.is_trial_plan
 				? 'Upgrade Plan'
