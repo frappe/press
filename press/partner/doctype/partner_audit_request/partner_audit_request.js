a69957c41b53e362404f0c83fc5635ca7c068915
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2026-03-16 17:14:50 +0530

    feat(prm): Improve the partner audit doctype
    (cherry picked from commit 4ab5bd31dd3eb43a1719fdd1edf3d91769849363)

--- press/partner/doctype/partner_audit_request/partner_audit_request.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Frappe and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Partner Audit Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
