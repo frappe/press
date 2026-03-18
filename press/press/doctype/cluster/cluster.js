972f3de7388c24b062fbfd46605009ce3ff3c597
phot0n <ritwikpuri5678@gmail.com>
2026-03-18 11:20:52 +0530

    chore(cluster): minor formatting
    (cherry picked from commit e226b3a1d913ab0a99c0f2ff4a1680110607e98c)

--- press/press/doctype/cluster/cluster.js
+++ press/press/doctype/cluster/cluster.js
@@ -30,13 +30,11 @@ frappe.ui.form.on('Cluster', {
 			[__('Create Proxy'), 'create_proxy', frm.doc.status === 'Active'],
 			[__('Add Images'), 'add_images', frm.doc.status === 'Active'],
 			[
-				__(
-					'Create NAT Security Group',
-					'create_nat_security_group',
-					frm.doc.cloud_provider === 'AWS EC2' &&
-						frm.doc.status === 'Active' &&
-						!frm.doc.nat_security_group_id,
-				),
+				__('Create NAT Security Group'),
+				'create_nat_security_group',
+				frm.doc.cloud_provider === 'AWS EC2' &&
+					frm.doc.status === 'Active' &&
+					!frm.doc.nat_security_group_id,
 			],
 		].forEach(([label, method, condition]) => {
 			if (typeof condition === 'undefined' || condition) {
