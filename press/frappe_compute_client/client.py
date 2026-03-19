9b172adcdf177b67ed87f4833ccc5b7fcf2ecce2
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-03-19 20:20:28 +0530

    fix(compute): Make Frappe Compute use POST data params

--- press/frappe_compute_client/client.py
+++ press/frappe_compute_client/client.py
@@ -1,10 +1,11 @@
 from frappe import _dict
-from frappe.frappeclient import FrappeClient
+
+from press.frappe_compute_client.frappe_press_client import FrappePressClient
 
 
 class Client:
 	def __init__(self, url, api_key: str, api_secret: str):
-		self.client = FrappeClient(url, api_key=api_key, api_secret=api_secret)
+		self.client = FrappePressClient(url, api_key=api_key, api_secret=api_secret)
 
 	def validate(self):
 		return self.client.get_api("frappe.auth.get_logged_user") == "Administrator"
