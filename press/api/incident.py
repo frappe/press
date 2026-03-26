87e693260ed5e3621ef664cfd8a7025d395f23ea
Aradhya-Tripathi <aradhya@frappe.io>
2026-03-26 15:09:39 +0530

    feat(pagination): Add route to fetch incident count

--- press/api/incident.py
+++ press/api/incident.py
@@ -1,5 +1,5 @@
 import frappe
-from frappe.query_builder.functions import DistinctOptionFunction
+from frappe.query_builder.functions import Count, DistinctOptionFunction
 from pypika.enums import Order
 from pypika.terms import Field
 
@@ -20,6 +20,39 @@ class GroupConcat(DistinctOptionFunction):
 			else ""
 		)
 		return f"{order_sql}"
+
+
+@frappe.whitelist()
+def get_incident_count(resolved: bool = False) -> int:
+	team = get_current_team()
+	if not team:
+		return 0
+
+	site_servers = frappe.get_all(
+		"Site",
+		{"team": team, "status": ("in", ["Active", "Broken", "Pending"])},
+		pluck="server",
+	)
+
+	all_user_servers = set(
+		site_servers + frappe.get_all("Server", {"team": team}, pluck="name")
+	)  # Include shared + dedicated server customers
+
+	if not all_user_servers:
+		return 0
+
+	Incident = frappe.qb.DocType("Incident")
+	query = (
+		frappe.qb.from_(Incident).select(Count(Incident.name)).where(Incident.server.isin(all_user_servers))
+	)
+	query = (
+		query.where((Incident.status == "Resolved") | (Incident.status == "Auto-Resolved"))
+		if resolved
+		else query.where(Incident.status.isin(["Confirmed", "Validating", "Investigating"]))
+	)
+
+	result = query.run(pluck=True)
+	return result[0] if result else 0
 
 
 @frappe.whitelist()
