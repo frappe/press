30d57924876565d0f504321ef9635a003259720a
siduck <siduck@tutanota.com>
2026-03-30 15:49:19 +0530

    refactor(notification-panel): update UI & integrate api

--- dashboard/src/components/navigation/sidebar/Notifications.vue
+++ dashboard/src/components/navigation/sidebar/Notifications.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { Popover } from 'frappe-ui';
+import { Popover, Badge, Button, createResource } from 'frappe-ui';
 import LucideInbox from '~icons/lucide/inbox';
+import { dayjsLocal } from '@/utils/dayjs';
+import LucideKey from '~icons/lucide/key';
+
 import { unreadNotificationsCount } from '@/data/notifications';
 
 let props = defineProps({
@@ -9,10 +12,61 @@ let props = defineProps({
 		required: true,
 	},
 });
+
+const icons = {
+	'Site Update': LucideRefreshCw,
+	'Site Migrate': LucideArrowUpCircle,
+	'Version Upgrade': LucideArrowUpCircle,
+	'Bench Deploy': LucideRocket,
+	'Site Recovery': LucideShieldCheck,
+	'Agent Job Failure': LucideAlertTriangle,
+	'Downtime/Performance': LucideActivity,
+	'Support Access': LucideKey,
+	'Auto Scale': LucideTrendingUp,
+};
+
+const iconColors = {
+	'Site Update': 'text-ink-green-2',
+	'Site Migrate': 'text-ink-green-2',
+	'Version Upgrade': 'text-ink-green-2',
+	'Bench Deploy': 'text-ink-green-2',
+	'Site Recovery': 'text-ink-green-2',
+	'Agent Job Failure': 'text-ink-red-4',
+	'Downtime/Performance': 'text-ink-red-4',
+	'Support Access': 'text-ink-amber-3',
+	'Auto Scale': 'text-ink-red-4',
+};
+
+const iconBgColors = {
+	'Site Update': 'bg-surface-green-1',
+	'Site Migrate': 'bg-surface-green-1',
+	'Version Upgrade': 'bg-surface-green-1',
+	'Bench Deploy': 'bg-surface-green-1',
+	'Site Recovery': 'bg-surface-green-1',
+	'Agent Job Failure': 'bg-surface-red-1',
+	'Downtime/Performance': 'bg-surface-red-1',
+	'Support Access': 'bg-surface-amber-1',
+	'Auto Scale': 'bg-surface-red-1',
+};
+
+const data = createResource({
+	doctype: 'Press Notification',
+	url: 'press.api.notifications.get_notifications',
+	auto: true,
+	filters: {
+		read: 'Unread',
+	},
+	cache: ['Notifications'],
+});
+
+const formatHtml = (str: string) => {
+	return str.replace(/<(?!\/?b\b)[^>]*>/g, '').split('\n')[0];
+};
 </script>
 
 <template>
 	<Popover placement="right-start">
+		<!-- sidebar item -->
 		<template #target="{ togglePopover }">
 			<button
 				@click="togglePopover"
@@ -38,9 +92,56 @@ let props = defineProps({
 			</button>
 		</template>
 
-		<template #body>
-			<div class="text-ink-gray-9 bg-white p-4 h-screen ml-2 shadow-sm">
-				Popover content
+		<!-- floating drawer  -->
+		<template #body="{ togglePopover }">
+			<div
+				class="text-ink-gray-9 bg-white h-screen ml-2 shadow-sm w-[400px] overflow-auto"
+			>
+				<div class="text-base flex items-center py-2 px-4 border-b">
+					<span class="font-medium mr-auto"> Notifications</span>
+
+					<Button variant="ghost">
+						<template #icon>
+							<LucideCheckCheck class="size-4" />
+						</template>
+					</Button>
+
+					<Button variant="ghost" @click="togglePopover">
+						<template #icon>
+							<LucideX class="size-4" />
+						</template>
+					</Button>
+				</div>
+
+				<div
+					v-for="x in data.data"
+					class="[&_b]:font-semibold border-b px-4 py-3 flex gap-4 items-center"
+				>
+					<div
+						class="size-8 flex-shrink-0 flex items-center p-2 rounded"
+						:class="iconBgColors[x.type] || 'bg-surface-gray-1'"
+					>
+						<component
+							:is="icons[x.type]"
+							class="size-4"
+							:class="iconColors[x.type]"
+						/>
+					</div>
+
+					<div
+						class="text-sm leading-relaxed flex flex-wrap gap-2 w-full min-w-0"
+					>
+						<p v-html="formatHtml(x.message)" class="w-full truncate" />
+
+						<Badge class="text-xs mr-auto">
+							{{ x.title }}
+						</Badge>
+
+						<span class="text-ink-gray-5 text-xs">
+							{{ dayjsLocal(x.creation).fromNow() }}
+						</span>
+					</div>
+				</div>
 			</div>
 		</template>
 	</Popover>
