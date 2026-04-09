263d5cd485ad76d58ff21c901fd9ef335374c9aa
Aysha <ayshapp468@gmail.com>
2026-04-09 12:51:12 +0530

    fix: Use is_standby flag to filter out standby sites for setting bench

--- press/press/doctype/site/site.py
+++ press/press/doctype/site/site.py
@@ -454,7 +454,7 @@ class Site(Document, TagHelpers):
 
 	def before_insert(self):
 		if not self.bench and self.group:
-			if self.server and self.team != "Administrator":  # Check to avoid standby sites
+			if self.server and not self.is_standby:
 				self.set_bench_for_server()
 			else:
 				self.set_latest_bench()
