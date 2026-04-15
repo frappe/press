a436858aa777bb3333a3065033dc93dae840c89d
siduck <siduck@tutanota.com>
2026-04-15 18:53:08 +0530

    fix(sidebar): improve mobile view

--- dashboard/src/components/navigation/sidebar/SearchItem.vue
+++ dashboard/src/components/navigation/sidebar/SearchItem.vue
@@ -6,12 +6,10 @@ import { searchModalOpen } from '@/data/ui';
 
 <template>
 	<button
-		class="flex items-center rounded px-2 py-1 text-ink-gray-6 transition gap-1 text-sm w-full mt-1 hover:bg-surface-gray-3"
+		class="flex items-center rounded px-3 py-2 text-ink-gray-6 transition gap-2 text-sm w-full mt-1 hover:bg-surface-gray-3"
 		@click="() => (searchModalOpen = true)"
 	>
-		<span class="grid h-5 w-6 place-items-center">
-			<LucideSearch class="size-4 text-ink-gray-6" />
-		</span>
+		<LucideSearch class="size-4 text-ink-gray-6" />
 		<span class="text-left mr-auto"> Search</span>
 
 		<LucideCommand class="size-3.5 text-ink-gray-6" />
