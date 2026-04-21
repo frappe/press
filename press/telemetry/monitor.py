d2b95eaff92d7794d8794d821bd9a74cfeff9634
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2026-04-21 13:09:07 +0530

    feat(telemetry): Add user context to sentry
    (cherry picked from commit 5c476b5978f277f50ffe6a5f0517b190164c2484)

--- press/telemetry/monitor.py
+++ press/telemetry/monitor.py
@@ -8,9 +8,11 @@ from press.utils import get_current_team
 
 def add_user_context(**kwargs):
 	with suppress(Exception):
-		add_data_to_monitor(
-			user_details={
-				"user": frappe.session.user,
-				"team": get_current_team(get_doc=False) if frappe.session.user != "Guest" else None,
-			}
-		)
+		add_data_to_monitor(user_details=get_user_details())
+
+
+def get_user_details() -> dict:
+	return {
+		"user": frappe.session.user,
+		"team": get_current_team(get_doc=False) if frappe.session.user != "Guest" else None,
+	}
