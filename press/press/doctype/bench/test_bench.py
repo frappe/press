79d7b8f8b90126607fa8a47b3a0006e1d61716da
kehwar <erickkwr@gmail.com>
2026-04-23 14:27:19 +0000

    fix: test
    scale_workers still uses release group settings, max_* keys are ignored in bench config

--- press/press/doctype/bench/test_bench.py
+++ press/press/doctype/bench/test_bench.py
@@ -291,11 +291,14 @@ class TestBench(FrappeTestCase):
 		bench = self._create_bench_with_n_sites_with_cpu_time(3, 5)
 		self.assertEqual(bench.gunicorn_workers, 2)
 		self.assertEqual(bench.background_workers, 1)
-		bench.bench_config = json.dumps({"max_gunicorn_workers": 10, "max_background_workers": 5})
+		group = frappe.get_doc("Release Group", bench.group)
+		group.db_set("max_gunicorn_workers", 10)
+		group.db_set("max_background_workers", 5)
+		bench.bench_config = json.dumps({"max_gunicorn_workers": 20, "max_background_workers": 10})  # bench config should not override max workers
 		scale_workers()
 		bench.reload()
-		self.assertEqual(bench.gunicorn_workers, 2) # max_gunicorn_workers setting is not used
-		self.assertEqual(bench.background_workers, 1) # max_background_workers setting is not used
+		self.assertEqual(bench.gunicorn_workers, 10)
+		self.assertEqual(bench.background_workers, 5)
 		frappe.db.set_value("Server", bench.server, "ram", 1600)
 		scale_workers()
 		bench.reload()
