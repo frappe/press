9e6eef0adf54c01aabf0d5dfeb501368a2ac7cc8
Aradhya-Tripathi <aradhya@frappe.io>
2026-04-26 23:05:02 +0530

    refactor(bench): We need to account for number of sites as well

--- press/telemetry/worker_allocator.py
+++ press/telemetry/worker_allocator.py
@@ -22,7 +22,16 @@ class BenchType(TypedDict):
 	site_info: list[SiteInfo]
 
 
-class WorkerAllocation(TypedDict):
+class BenchAllocation(TypedDict):
+	bench: str
+	weight: float
+	max_web: int
+	max_bg: int
+	web_workers: float
+	bg_workers: float
+
+
+class AllocationResult(TypedDict):
 	web_workers: int
 	bg_workers: int
 
@@ -45,10 +54,10 @@ class WorkerScheduler:
 	total_bg_worker_slots: float
 
 	def get_guaranteed_web_and_bg_workers(self, site_info: SiteInfo) -> tuple[float, float]:
-		"""Returns the guaranteed web + background workers for a bench based on its site plan."""
+		"""Returns the guaranteed web & background workers for a bench based on its site plan."""
 		plan = PLAN_CONFIG.get(site_info["plan"])
-
 		if not plan:
+			print("Plan not found for site:", site_info["name"], "with plan:", site_info["plan"])
 			return 1, 1  # Default to min workers plan might be None/Trial etc
 
 		return plan["web"]["min"], plan["bg"]["min"]
@@ -62,104 +71,88 @@ class WorkerScheduler:
 			and self.total_bg_worker_slots >= total_guaranteed_bg_workers
 		)
 
-	def get_site_with_highest_weight(self, site_info_list: list[SiteInfo]) -> SiteInfo:
-		"""Returns the site with the highest budget from a list of site info."""
-		return max(
-			site_info_list, key=lambda site_info: PLAN_CONFIG.get(site_info["plan"], {}).get("weight", 0)
-		)
-
-	def get_site_with_highest_max_workers(self, site_info_list: list[SiteInfo], worker_type: str) -> SiteInfo:
-		"""Returns the site with the highest max workers for a given worker type from a list of site info."""
-		return max(
-			site_info_list,
-			key=lambda site_info: PLAN_CONFIG.get(site_info["plan"], {}).get(worker_type, {}).get("max", 0),
-		)
-
 	def get_total_guaranteed_workers(self) -> tuple[float, float]:
-		"""Returns the total guaranteed web + background workers for all benches."""
+		"""
+		Returns the total guaranteed web + background workers for all benches.
+		This calculation is done per site, assuming cases like
+		Bench A [10 x 5USD + 1 x 50USD]; Bench B [1 x 50USD]
+		Bench A should get more workers compared to Bench B
+		"""
 		total_guaranteed_web_workers = 0.0
 		total_guaranteed_bg_workers = 0.0
 
 		for bench in self.benches:
-			site_with_highest_web_workers = self.get_site_with_highest_max_workers(bench["site_info"], "web")
-			site_with_highest_bg_workers = self.get_site_with_highest_max_workers(bench["site_info"], "bg")
-
-			total_guaranteed_web_workers += (
-				PLAN_CONFIG.get(site_with_highest_web_workers["plan"], {}).get("web", {}).get("min", 0)
-			)
-			total_guaranteed_bg_workers += (
-				PLAN_CONFIG.get(site_with_highest_bg_workers["plan"], {}).get("bg", {}).get("min", 0)
-			)
+			for site in bench["site_info"]:
+				guaranteed_web, guaranteed_bg = self.get_guaranteed_web_and_bg_workers(site)
+				total_guaranteed_web_workers += guaranteed_web
+				total_guaranteed_bg_workers += guaranteed_bg
 
 		return total_guaranteed_web_workers, total_guaranteed_bg_workers
 
-	def allocate_workers(self) -> list[dict[str, WorkerAllocation]] | None:
+	def allocate_workers(self) -> list[dict[str, AllocationResult]]:
 		"""
-		Check if we can meet guaranteed worker limits for all benches.
-			- If yes, check if there are surplus workers available
-				- If yes, allocate surplus workers based on the priority weight of the plans.
-				- If no, allocate only the guaranteed workers for each bench.
-
-		- If no, Don't know what to do for now....
+		Allocate workers to benches based on guaranteed limits and surplus distribution.
 		"""
 		total_guaranteed_web_workers, total_guaranteed_bg_workers = self.get_total_guaranteed_workers()
 
 		if not self.can_server_accommodate_guaranteed_workers(
 			total_guaranteed_web_workers, total_guaranteed_bg_workers
 		):
-			# Don't know what to do for now.
-			return None
+			raise ResourceWarning(
+				f"Server cannot accommodate the guaranteed workers for all benches. Required web workers: {total_guaranteed_web_workers}, "
+				f"Required bg workers: {total_guaranteed_bg_workers}, Available web worker slots: {self.total_web_worker_slots}, "
+				f"Available bg worker slots: {self.total_bg_worker_slots}"
+			)
 
 		bench_allocations = []
-		total_server_weight = 0
+		total_server_weight = 0.0
+
+		# Just in case we have no surplus left
+		surplus_web_workers = max(self.total_web_worker_slots - total_guaranteed_web_workers, 0)
+		surplus_bg_workers = max(self.total_bg_worker_slots - total_guaranteed_bg_workers, 0)
 
 		for bench in self.benches:
-			# Assuming bench has multiple sites, we take the highest plan site
-			top_site_weight = self.get_site_with_highest_weight(bench["site_info"])
+			bench_weight = 0.0
+			bench_web_workers = 0.0
+			bench_bg_workers = 0.0
+			max_web = 0
+			max_bg = 0
 
-			# guaranteed worker & weight calculation for largest site in the bench
-			top_site_web = self.get_site_with_highest_max_workers(bench["site_info"], "web")
-			top_site_bg = self.get_site_with_highest_max_workers(bench["site_info"], "bg")
-			bench_weight = PLAN_CONFIG.get(top_site_weight["plan"], {}).get("weight", 0)
-			max_web = PLAN_CONFIG.get(top_site_web["plan"], {}).get("web", {}).get("max", 0)
-			max_bg = PLAN_CONFIG.get(top_site_bg["plan"], {}).get("bg", {}).get("max", 0)
-			min_web = PLAN_CONFIG.get(top_site_web["plan"], {}).get("web", {}).get("min", 0)
-			min_bg = PLAN_CONFIG.get(top_site_bg["plan"], {}).get("bg", {}).get("min", 0)
-
-			bench_web_workers = min(min_web, max_web)
-			bench_bg_workers = min(min_bg, max_bg)
+			for site in bench["site_info"]:
+				guaranteed_web, guaranteed_bg = self.get_guaranteed_web_and_bg_workers(site)
+				bench_web_workers += guaranteed_web
+				bench_bg_workers += guaranteed_bg
+				bench_weight += PLAN_CONFIG.get(site["plan"], {}).get("weight", 1)
+				max_web += PLAN_CONFIG.get(site["plan"], {}).get("web", {}).get("max", 1)
+				max_bg += PLAN_CONFIG.get(site["plan"], {}).get("bg", {}).get("max", 1)
 
 			bench_allocations.append(
-				{
-					"bench": bench["name"],
-					"weight": bench_weight,
-					"max_web": max_web,
-					"max_bg": max_bg,
-					"web_workers": bench_web_workers,
-					"bg_workers": bench_bg_workers,
-				}
+				BenchAllocation(
+					bench=bench["name"],
+					weight=bench_weight,
+					max_web=max_web,
+					max_bg=max_bg,
+					web_workers=bench_web_workers,
+					bg_workers=bench_bg_workers,
+				)
 			)
 			total_server_weight += bench_weight
 
-		# Allocate surplus workers based on weight if there is any surplus
-		surplus_web_workers = self.total_web_worker_slots - total_guaranteed_web_workers
-		surplus_bg_workers = self.total_bg_worker_slots - total_guaranteed_bg_workers
-
 		for b in bench_allocations:
 			if total_server_weight > 0:
-				# Some weighted bonus calculation
+				# Raw calculations to get the bonus workers before applying the max limits
 				web_bonus = (b["weight"] / total_server_weight) * surplus_web_workers
 				bg_bonus = (b["weight"] / total_server_weight) * surplus_bg_workers
 
 				raw_web = b["web_workers"] + web_bonus
 				raw_bg = b["bg_workers"] + bg_bonus
 
-				# Ensure we are not allocating more than the max limit for any bench
+				# Ensure we didn't exceed the max limits for the bench based on individual site plan maximums
 				b["web_workers"] = min(b["max_web"], math.floor(raw_web))
 				b["bg_workers"] = min(b["max_bg"], math.floor(raw_bg))
 
 		return [
-			{b["bench"]: {"web_workers": b["web_workers"], "bg_workers": b["bg_workers"]}}
+			{b["bench"]: {"web_workers": int(b["web_workers"]), "bg_workers": int(b["bg_workers"])}}
 			for b in bench_allocations
 		]
 
@@ -171,8 +164,8 @@ if __name__ == "__main__":
 				{
 					"name": "Bench 1",
 					"site_info": [
-						SiteInfo(name="Site A", plan="USD 5"),
-						SiteInfo(name="Site B", plan="USD 10"),
+						# SiteInfo(name="Site A", plan="USD 5"),
+						SiteInfo(name="Site B", plan="random"),
 					],
 				}
 			),
