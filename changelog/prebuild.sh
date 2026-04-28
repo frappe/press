7f90b9e0e396ef4b73369b068811466aaea25689
siduck <siduck@tutanota.com>
2026-04-28 11:10:21 +0530

    fix(changelog): use @ src alias in imports

--- changelog/prebuild.sh
+++
@@ -1,6 +0,0 @@
-cd ./src/content/
-
-grep -rl '^https://github.com/user-attachments/assets/' . \
-  | xargs sed -i '' -E 's|^(https://github.com/user-attachments/assets/.*)|<video src="\1" width="600" controls />|'
-
-cd ../../
