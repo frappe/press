a7b0919c1ab1e530b2a672c5e7d98c8ea9505761
Sabu Siyad <hello@ssiyad.com>
2026-05-06 14:33:46 +0530

    fix(roles): Patch: disable roles instead of deleting

--- press/patches/v0_8_0/merge_ff_roles_to_one.py
+++ press/patches/v0_8_0/merge_ff_roles_to_one.py
@@ -4,6 +4,7 @@ import frappe
 from frappe.utils import update_progress_bar
 
 if TYPE_CHECKING:
+	from frappe.core.doctype.role.role import Role
 	from frappe.core.doctype.user.user import User
 
 
@@ -17,11 +18,13 @@ def execute():
 		chunk = users[chunk_start : chunk_start + chunk_size]
 		for i, user in enumerate(chunk, start=chunk_start):
 			user_doc: User = frappe.get_doc("User", user)
-			user_doc.remove_roles("Press Admin", "Press Member")
+			user_doc.remove_roles(*old_roles)
 			user_doc.add_roles("Press User")
 			update_progress_bar("Merging roles", i, total)
 		frappe.db.commit()
 	for old_role in old_roles:
 		if frappe.db.exists("Role", old_role):
-			frappe.delete_doc("Role", old_role)
+			role_doc: Role = frappe.get_doc("Role", old_role)
+			role_doc.disabled = True
+			role_doc.save()
 	frappe.db.commit()
