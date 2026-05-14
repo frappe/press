e0c7a6f8cb58128512a9dc9a0392a4c46060ab2a
Jayanta Pandit <55305804+regdocs@users.noreply.github.com>
2026-05-14 08:26:59 +0530

    feat(migrate-plans): move methods from server plan doctype

--- press/plans/__init__.py
+++ press/plans/__init__.py
@@ -1,28 +1,51 @@
 from collections.abc import Callable
-from typing import Literal, TypeAlias, cast
+from typing import TypeAlias
 
-from press.plans.server import _server_plans
+import frappe
+from frappe.utils.data import rounded
+
 from press.plans.server._types import ServerPlan
-from press.plans.site import _site_plans
 from press.plans.site._types import SitePlan
-
-PlanType: TypeAlias = Literal["Site", "Server"]
 
 AnyPlan: TypeAlias = SitePlan | ServerPlan
 FilterFn: TypeAlias = Callable[[AnyPlan], bool]
 
-plan_type_select: dict[PlanType, list[AnyPlan]] = {
-	"Site": cast("list[AnyPlan]", _site_plans),
-	"Server": cast("list[AnyPlan]", _server_plans),
-}
+
+def _get_price_per_day(plan: AnyPlan, currency):
+	price = plan["price_inr"] if currency == "INR" else plan["price_usd"]
+	period = frappe.utils.get_last_day(None).day
+	return rounded(price / period, 2)
 
 
-def get_plans(
-	plan_type: PlanType,
-	filter_fn: FilterFn | None = None,
-) -> list[AnyPlan]:
-	plans = plan_type_select[plan_type]
-	if filter_fn is None:
-		return plans
+def _is_server_plan_unavailable(plan: str) -> bool:
+	result = frappe.db.get_value("Server Plan Unavailability", {"server_plan": plan}, "unavailable")
+	return bool(result)
 
-	return list(filter(filter_fn, plans))
+
+def _get_unavailable_server_plans() -> dict[str, bool]:
+	docs = frappe.get_all("Server Plan Unavailability", fields=["server_plan", "unavailable"])
+	return {doc["server_plan"]: bool(doc["unavailable"]) for doc in docs}
+
+
+def _mark_server_plan_as_unavailable(plan: str):
+	name = frappe.db.get_value("Server Plan Unavailability", {"server_plan": plan}, "name")
+	if name:
+		frappe.db.set_value("Server Plan Unavailability", name, "unavailable", 1)
+	else:
+		frappe.get_doc(
+			{
+				"doctype": "Server Plan Unavailability",
+				"server_plan": plan,
+				"unavailable": 1,
+			}
+		).insert()
+
+
+def _filter_by_roles(plans):
+	out = []
+	for plan in plans:
+		if frappe.utils.has_common(plan["roles"], frappe.get_roles()):
+			plan.pop("roles", "")
+			out.append(plan)
+
+	return out
--- press/plans/server/__init__.py
+++ press/plans/server/__init__.py
@@ -1,11 +1,28 @@
-from typing import Final
+from collections.abc import Callable
+from copy import deepcopy
+from typing import TYPE_CHECKING, Final, TypeAlias
 
+import frappe
+
+from press.plans import (
+	_filter_by_roles,
+	_get_price_per_day,
+	_get_unavailable_server_plans,
+	_is_server_plan_unavailable,
+	_mark_server_plan_as_unavailable,
+)
 from press.plans.server._types import ServerPlan
 from press.plans.server.aws import _aws_server_plans
 from press.plans.server.do import _do_server_plans
 from press.plans.server.frappe import _frappe_compute_server_plans
 from press.plans.server.hetzner import _hetzner_server_plans
 from press.plans.server.oci import _oci_server_plans
+from press.utils.jobs import has_job_timeout_exceeded
+
+if TYPE_CHECKING:
+	from press.press.doctype.cluster.cluster import Cluster
+
+FilterFn: TypeAlias = Callable[[ServerPlan], bool]
 
 _server_plans: Final[list[ServerPlan]] = [
 	*_aws_server_plans,
@@ -14,3 +31,115 @@ _server_plans: Final[list[ServerPlan]] = [
 	*_oci_server_plans,
 	*_hetzner_server_plans,
 ]
+
+
+def get_server_plan_list(
+	filter_fn: FilterFn | None = None,
+	fields: list[str] | None = None,
+) -> list[ServerPlan]:
+	plans = deepcopy(_server_plans) if filter_fn is None else deepcopy(list(filter(filter_fn, _server_plans)))
+
+	if fields and "machine_unavailable" in fields:
+		unavailable_plans = _get_unavailable_server_plans()
+		for plan in plans:
+			plan["machine_unavailable"] = unavailable_plans.get(plan["name"], False)
+
+	role_filtered_plans = _filter_by_roles(plans)
+
+	if fields is None:
+		return role_filtered_plans
+
+	return [{k: v for k, v in plan.items() if k in fields} for plan in plans]
+
+
+def get_server_plan_doc(
+	filter_fn: FilterFn | None = None,
+	fields: list[str] | None = None,
+) -> ServerPlan:
+	plans = _server_plans if filter_fn is None else list(filter(filter_fn, _server_plans))
+	doc = deepcopy(plans[0])
+
+	doc["price_per_day_inr"] = _get_price_per_day(doc, "INR")
+	doc["price_per_day_usd"] = _get_price_per_day(doc, "USD")
+
+	if fields and "machine_unavailable" in fields:
+		doc["machine_unavailable"] = _is_server_plan_unavailable(doc["name"])
+
+	if fields is None:
+		return doc
+
+	return {k: v for k, v in doc.items() if k in fields}
+
+
+def sync_machine_availability_status_of_plans():
+	frappe.enqueue(
+		_sync_machine_availability_status_of_plans,
+		timeout=3600,
+		deduplicate=True,
+		job_id="sync_machine_availability_status_of_plans",
+	)
+
+
+def _sync_machine_availability_status_of_plans():  # noqa
+	plans = get_server_plan_list(
+		filter_fn=lambda plan: plan["ignore_machine_availability_sync"] == 0 and plan["enabled"],
+		fields=["name", "cluster", "machine_unavailable", "instance_type"],
+	)
+
+	# Sync also disabled but legacy_plan
+	legacy_plans = get_server_plan_list(
+		filter_fn=lambda plan: plan["legacy_plan"]
+		and not plan["ignore_machine_availability_sync"]
+		and not plan["enabled"],
+		fields=["name", "cluster", "machine_unavailable", "instance_type"],
+	)
+	plans.extend(legacy_plans)
+
+	cluster_doc_map: dict[str, Cluster] = {}
+	cluster_plans: dict[str, list[dict]] = {}
+
+	for plan in plans:
+		if has_job_timeout_exceeded():
+			return
+
+		if not plan["instance_type"]:
+			continue
+
+		if not plan["cluster"]:
+			# Some plan types has no cluster assigned to them.
+			# Skip those as they are not relevant for machine availability status sync.
+			continue
+
+		if plan["cluster"] not in cluster_doc_map:
+			cluster_doc_map[plan["cluster"]] = frappe.get_doc("Cluster", plan["cluster"])
+
+		if plan["cluster"] not in cluster_plans:
+			cluster_plans[plan["cluster"]] = []
+		cluster_plans[plan["cluster"]].append(plan)
+
+	for c in cluster_plans:
+		if has_job_timeout_exceeded():
+			return
+
+		try:
+			cluster: Cluster = cluster_doc_map[c]
+			instance_types = [p["instance_type"] for p in cluster_plans[c]]
+			plan_availability_results = cluster.check_machine_availability(instance_types)
+
+			# Some Cluster implementations may return a boolean instead of a dict.
+			# Normalize to a dict so that the code below can safely use .get(...).
+			if isinstance(plan_availability_results, bool):
+				plan_availability_results = {
+					instance_type: plan_availability_results for instance_type in instance_types
+				}
+			for plan in cluster_plans[c]:
+				is_unavailable = not plan_availability_results.get(plan["instance_type"], False)
+				if is_unavailable != plan["machine_unavailable"]:
+					_mark_server_plan_as_unavailable(plan["name"])
+
+			frappe.db.commit()
+		except Exception:
+			frappe.log_error(
+				f"Failed to sync machine availability status for Server Plan {plan['name']}",
+			)
+			frappe.db.rollback()
