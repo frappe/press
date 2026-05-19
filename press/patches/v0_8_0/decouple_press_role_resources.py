001f519b92c63a3a026969a785eb186ff0704634
Sabu Siyad <hello@ssiyad.com>
2026-05-19 12:56:09 +0530

    feat(patch): Sync existing press role resources

---
+++ press/patches/v0_8_0/decouple_press_role_resources.py
@@ -0,0 +1,14 @@
+import frappe
+from frappe.utils import update_progress_bar
+
+from press.press.doctype.team_member_resource.team_member_resource import sync_press_role
+
+
+def execute():
+	press_roles = frappe.get_all("Press Role", pluck="name")
+	total = len(press_roles)
+	for index, press_role in enumerate(press_roles, start=1):
+		press_role_doc = frappe.get_doc("Press Role", press_role)
+		sync_press_role(press_role_doc)
+		update_progress_bar("Syncing Press Role Resources", index, total, absolute=True)
+		frappe.db.commit()
