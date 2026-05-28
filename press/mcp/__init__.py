332df3cc925a4769cac0bb32ea188bc67a4e34b4
Tanmoy Sarkar <ts741127@gmail.com>
2026-05-28 14:42:05 +0530

    fix(mcp): Allow options endpoint for mcp server
    Dont add the url in whitelist
    Instead allow the options endpoint without auth info
    Also, added guard in mcp handler , just for sanity

--- press/mcp/__init__.py
+++ press/mcp/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2026, Frappe and contributors
 # For license information, please see license.txt
 
+import frappe
 import frappe_mcp
 
 mcp = frappe_mcp.MCP("press-mcp")
@@ -8,6 +9,14 @@ mcp = frappe_mcp.MCP("press-mcp")
 
 @mcp.register()
 def handler():
+	frappe.only_for("System Manager")
+
+	user_type = frappe.get_cached_value("User", frappe.session.user, "user_type")
+
+	# Allow unchecked access to System Users
+	if user_type != "System User":
+		frappe.throw("Access not allowed for this URL", frappe.AuthenticationError)
+
 	import press.mcp.tools.documents
 	import press.mcp.tools.telemetry
 
