9dbe376f2d5d6701089516f1c81c261b1c7e80a6
siduck <siduck@tutanota.com>
2026-05-31 22:14:59 +0530

    refactor(server-list): organize into structured components
    (cherry picked from commit 41de58e38a18c3ed3e068aaf08b22ab2861fa78d)

--- dashboard/src/pages/servers/list/Page.vue
+++ dashboard/src/pages/servers/list/Page.vue
@@ -6,11 +6,15 @@ import {
 	Select,
 	Tooltip,
 	Spinner,
+	Dropdown,
 } from 'frappe-ui'
 import Header from '@/components/Header.vue'
 import { ref, reactive, watch } from 'vue'
 import { dayjsLocal } from '@/utils/dayjs'
 import Collapsable from '@/components/common/Collapsable.vue'
+
+import Loader from './Loader.vue'
+import AddBenchDialog from './AddBenchDialog.vue'
 
 import HetnzerLogo from '@/logo/Hetzner.vue'
 import FrappeLogo from '@/logo/Frappe.vue'
@@ -61,7 +65,7 @@ const servers = createListResource({
 	doctype: 'Server',
 	auto: true,
 	pageLength: 10,
-  cache:'servers list',
+	cache: 'servers list',
 	fields: [
 		'name',
 		'title',
@@ -127,6 +131,39 @@ const providerIcons = {
 	Oracle: OracleLogo,
 	DigitalOcean: DigitalOceanLogo,
 }
+
+const addBenchDialog = ref(true)
+const serverActions = [
+	{
+		label: 'Add bench',
+		icon: LucideCirclePlus,
+		onClick: () => (addBenchDialog.value = true),
+	},
+	{
+		label: 'Visit server',
+		icon: 'external-link',
+	},
+	{
+		label: 'View backups',
+		icon: 'archive',
+	},
+
+	{
+		label: 'Change App server plan',
+		icon: LucideArrowLeftRight,
+	},
+
+	{
+		label: 'Change DB server plan',
+		icon: LucideArrowLeftRight,
+	},
+
+	{
+		label: 'Drop server',
+		theme: 'red',
+		icon: 'trash-2',
+	},
+]
 </script>
 
 <template>
@@ -135,6 +172,7 @@ const providerIcons = {
 		<Button variant="solid" class="ml-auto">New Server</Button>
 	</Header>
 
+  <!-- filters -->
 	<div class="flex gap-3 items-center px-5">
 		<TextInput
 			placeholder="Search"
@@ -163,6 +201,7 @@ const providerIcons = {
 			class="!w-fit"
 			:options="['' , 'Active', 'Pending', 'Archived']"
 			@update:modelValue="v => applyFilters('status', v)"
+			w
 		>
 			<template #prefix>
 				<span class="rounded size-2 bg-gray-500" />
@@ -184,50 +223,7 @@ const providerIcons = {
 	</div>
 
 	<div class="p-5 text-ink-gray-8 flex flex-col gap-4">
-		<!-- loader -->
-		<section
-			class="sk-fade border rounded shadow-sm overflow-hidden  bg-surface-cards"
-			v-for="_ in 3"
-			v-if="servers?.list?.loading"
-		>
-			<div class="flex gap-3 items-center p-4">
-				<div class="sk size-8 !rounded" />
-				<div class="flex flex-col gap-1.5">
-					<div class="flex gap-2 items-center">
-						<div class="sk h-4 w-48" />
-						<div class="sk h-3 w-3 !rounded-full" />
-						<div class="sk h-3 w-10" />
-					</div>
-					<div class="sk h-3 w-36" />
-				</div>
-				<div class="flex items-center gap-2 ml-auto">
-					<div class="sk h-4 w-4" />
-					<div class="sk h-4 w-20" />
-					<div class="sk h-7 w-7" />
-				</div>
-			</div>
-
-			<div
-				v-for="(bench, i) in 3"
-				:key="bench"
-				class="grid grid-cols-[1.5rem_1fr_0.5fr_0.7fr_2rem] gap-3 px-4 py-2.5 items-center border-t"
-			>
-				<div class="sk h-4 w-4" />
-				<div class="flex gap-2 items-center">
-					<div class="sk h-4 w-4" />
-					<div
-						class="sk h-4"
-						:style="{ width: ['9rem', '7rem', '11rem'][i] }"
-					/>
-				</div>
-				<div class="flex gap-2 items-center">
-					<div class="sk h-2.5 w-2.5 !rounded-full" />
-					<div class="sk h-3.5 w-20" />
-				</div>
-				<div class="sk h-3.5 w-16" />
-				<div class="sk h-7 w-7" />
-			</div>
-		</section>
+    <Loader v-if="servers?.list?.loading" />
 
 		<template v-else>
 			<section
@@ -251,7 +247,10 @@ const providerIcons = {
 					<div class="flex items-center gap-1 text-ink-gray-6 ml-auto">
 						<LucideMapPin class="size-4" />
 						<span>{{ server?.cluster_title }}</span>
-						<Button variant="ghost"><LucideEllipsis class="size-4" /></Button>
+
+						<Dropdown :options="serverActions">
+							<Button variant="ghost"><LucideEllipsis class="size-4" /></Button>
+						</Dropdown>
 					</div>
 				</div>
 
@@ -372,27 +371,6 @@ const providerIcons = {
 			Load more
 		</Button>
 	</div>
+
+	<AddBenchDialog v-model="addBenchDialog" />
 </template>
-
-<style scoped>
-.bordered {
-	@apply border-b dark:border-outline-gray-2
-}
-
-.sk {
-	@apply rounded animate-pulse bg-surface-gray-3 dark:bg-surface-gray-2;
-}
-.sk-fade {
-	animation: sk-fade-in 0.35s cubic-bezier(0.4, 0, 0.2, 1) both;
-}
-@keyframes sk-fade-in {
-	from {
-		opacity: 0;
-		transform: translateY(5px);
-	}
-	to {
-		opacity: 1;
-		transform: translateY(0);
-	}
-}
-</style>
