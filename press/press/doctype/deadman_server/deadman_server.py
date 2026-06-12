fb10d1abd3e69cb892cf4983dedf5744775fba04
20vikash <thisisforspotifyvikash@gmail.com>
2026-06-12 15:44:31 +0000

    feat(deadman): Add deadman essentials ansible role

--- press/press/doctype/deadman_server/deadman_server.py
+++ press/press/doctype/deadman_server/deadman_server.py
@@ -72,6 +72,11 @@ class DeadmanServer(BaseServer):
 		deadman_password = self.get_password("deadman_password")
 		mariadb_root_password = self.get_password("mariadb_root_password")
 
+		certificate_name = frappe.db.get_value(
+			"TLS Certificate", {"wildcard": True, "domain": self.domain}, "name"
+		)
+		certificate = frappe.get_doc("TLS Certificate", certificate_name)
+
 		try:
 			ansible = Ansible(
 				playbook="deadman.yml",
@@ -85,6 +90,11 @@ class DeadmanServer(BaseServer):
 					"deadman_password": deadman_password,
 					"mariadb_root_password": mariadb_root_password,
 					"private_ip": self.private_ip,
+					"server_id": 1,
+					"db_port": 3306,
+					"certificate_private_key": certificate.private_key,
+					"certificate_full_chain": certificate.full_chain,
+					"certificate_intermediate_chain": certificate.intermediate_chain,
 				},
 			)
 
