b8b7a63b0fe0d53ce78610bd150badc39120b393
20vikash <thisisforspotifyvikash@gmail.com>
2026-06-25 10:30:17 +0000

    fix(nat): Use nat_gateway_ip in playbook to check
    (cherry picked from commit 2fd62f5ec4c28a2e46e85be1236bb1734e66f18e)

--- press/press/doctype/server/server.py
+++ press/press/doctype/server/server.py
@@ -2072,7 +2072,6 @@ class BaseServer(Document, TagHelpers):
 						if vm.cloud_provider == "Hetzner" and cluster.cidr_block
 						else ""
 					),
-					"should_setup_nat_gateway": True if not vm.public_ip_address else None,
 				},
 			)
 			ansible.run()
@@ -3300,7 +3299,6 @@ class Server(BaseServer):
 		)
 
 		cluster: Cluster = frappe.get_doc("Cluster", self.cluster)
-		vm: VirtualMachine = frappe.get_doc("Virtual Machine", self.virtual_machine)
 
 		try:
 			ansible = Ansible(
@@ -3334,7 +3332,6 @@ class Server(BaseServer):
 						if self.provider == "Hetzner" and cluster.cidr_block
 						else ""
 					),
-					"should_setup_nat_gateway": True if not vm.public_ip_address else None,
 					**self.get_mount_variables(),
 				},
 			)
