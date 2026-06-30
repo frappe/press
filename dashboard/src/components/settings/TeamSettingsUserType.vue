53e15ed41137adfb38c05a9ad2aa6d8b8a5f160b
Sabu Siyad <hello@ssiyad.com>
2026-06-30 12:32:32 +0530

    fix(dashboard): Team page UI/UX

---
+++ dashboard/src/components/settings/TeamSettingsUserType.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="flex items-center gap-2">
+		<AdminIcon v-if="hasAdminAccess" class="w-4 h-4 text-red-500" />
+		<div>{{ hasAdminAccess ? 'Admin' : 'Member' }}</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import AdminIcon from '~icons/lucide/shield-user'
+
+defineProps<{
+	hasAdminAccess: boolean
+}>()
+</script>
