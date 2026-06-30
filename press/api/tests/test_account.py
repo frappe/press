56ea7968f2af16a6f3c429aa79169229d6159f7e
Matt Van Horn <455140+mvanhorn@users.noreply.github.com>
2026-06-30 02:39:39 -0700

    style: apply ruff-format to new test block

--- press/api/tests/test_account.py
+++ press/api/tests/test_account.py
@@ -98,8 +98,9 @@ class TestAccountApi(TestCase):
 		other_sid = self._create_session(user)
 		another_sid = self._create_session(user)
 
-		with user_context(user), patch(
-			"frappe.sessions.delete_session", side_effect=self._delete_session_without_commit
+		with (
+			user_context(user),
+			patch("frappe.sessions.delete_session", side_effect=self._delete_session_without_commit),
 		):
 			frappe.session.sid = current_sid
 
@@ -115,8 +116,9 @@ class TestAccountApi(TestCase):
 		create_test_user(user)
 		current_sid = self._create_session(user)
 
-		with user_context(user), patch(
-			"frappe.sessions.delete_session", side_effect=self._delete_session_without_commit
+		with (
+			user_context(user),
+			patch("frappe.sessions.delete_session", side_effect=self._delete_session_without_commit),
 		):
 			frappe.session.sid = current_sid
 
