fabef8c2df2a1be4f52f83982330d1df3e054bd1
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2026-08-15 21:29:13 +0530

    feat(aws): VM cost report

---
+++ press/press/report/aws_vm_cost_utilization/aws_vm_cost_utilization.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2026, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.query_reports['AWS VM Cost Utilization'] = {
+	onload: function (report) {
+		report.page.add_inner_message(
+			__(
+				'Instances are read live from AWS (not from cached Press status). "Tracked In Press" is unchecked when AWS is billing for an instance with no matching Virtual Machine record in Press. Cost is an on-demand list-price estimate for running instances (compute only); it excludes EBS storage and any Reserved Instance / Savings Plan discounts.',
+			),
+		)
+	},
+}
