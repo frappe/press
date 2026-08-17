e1704df8a550e4366e6922a2f06f4237eb814a76
Tanmoy Sarkar <ts741127@gmail.com>
2026-08-17 10:51:37 +0530

    feat: Add 'Untracked Server' report
    (cherry picked from commit ebe896904b89170bef68be04fb9543c18ebbdfaf)

---
+++ press/press/report/untracked_servers/untracked_servers.py
@@ -0,0 +1,158 @@
+# Copyright (c) 2026, Frappe and contributors
+# For license information, please see license.txt
+
+import boto3
+import frappe
+from hcloud import Client as HetznerClient
+
+AWS_STATES = ["pending", "running", "stopping", "stopped", "shutting-down"]
+DIGITAL_OCEAN_PAGE_SIZE = 200
+
+
+def execute(filters=None):
+	frappe.only_for("System Manager")
+	filters = filters or {}
+	provider = filters.get("provider")
+	if not provider:
+		frappe.throw("Select a Provider to run this report")
+
+	columns = frappe.get_doc("Report", "Untracked Servers").get_columns()
+	data = get_data(provider, filters.get("cluster"))
+	return columns, data
+
+
+def get_data(provider, cluster):
+	fetch_untracked = {
+		"AWS EC2": get_untracked_aws_instances,
+		"Hetzner": get_untracked_hetzner_servers,
+		"DigitalOcean": get_untracked_digital_ocean_droplets,
+	}[provider]
+
+	rows = []
+	for cluster_name in get_clusters(provider, cluster):
+		rows.extend(fetch_untracked(frappe.get_doc("Cluster", cluster_name)))
+	return rows
+
+
+def get_clusters(provider, cluster):
+	filters = {"cloud_provider": provider}
+	if cluster:
+		filters["name"] = cluster
+	return frappe.get_all("Cluster", filters, pluck="name")
+
+
+def get_known_instance_ids(provider, cluster_name):
+	instance_ids = frappe.get_all(
+		"Virtual Machine",
+		{
+			"cloud_provider": provider,
+			"cluster": cluster_name,
+			"status": ("!=", "Terminated"),
+			"instance_id": ("is", "set"),
+		},
+		pluck="instance_id",
+	)
+	return {str(instance_id) for instance_id in instance_ids}
+
+
+def get_untracked_aws_instances(cluster):
+	secret_key = cluster.get_password("aws_secret_access_key", raise_exception=False)
+	if not cluster.aws_access_key_id or not secret_key:
+		frappe.throw(f"AWS credentials are not configured on Cluster {cluster.name}")
+
+	client = boto3.client(
+		"ec2",
+		region_name=cluster.region,
+		aws_access_key_id=cluster.aws_access_key_id,
+		aws_secret_access_key=secret_key,
+	)
+	known_instance_ids = get_known_instance_ids("AWS EC2", cluster.name)
+
+	paginator = client.get_paginator("describe_instances")
+	pages = paginator.paginate(Filters=[{"Name": "instance-state-name", "Values": AWS_STATES}])
+
+	rows = []
+	for page in pages:
+		for reservation in page["Reservations"]:
+			for instance in reservation["Instances"]:
+				if instance["InstanceId"] in known_instance_ids:
+					continue
+				rows.append(
+					{
+						"provider": "AWS EC2",
+						"cluster": cluster.name,
+						"instance_id": instance["InstanceId"],
+						"name": get_aws_name_tag(instance),
+						"status": instance["State"]["Name"],
+						"instance_type": instance["InstanceType"],
+						"region": cluster.region,
+					}
+				)
+	return rows
+
+
+def get_aws_name_tag(instance):
+	for tag in instance.get("Tags", []):
+		if tag["Key"] == "Name":
+			return tag["Value"]
+	return None
+
+
+def get_untracked_hetzner_servers(cluster):
+	api_token = cluster.get_password("hetzner_api_token", raise_exception=False)
+	if not api_token:
+		frappe.throw(f"Hetzner API token is not configured on Cluster {cluster.name}")
+
+	client = HetznerClient(token=api_token)
+	known_instance_ids = get_known_instance_ids("Hetzner", cluster.name)
+
+	rows = []
+	for server in client.servers.get_all():
+		if str(server.id) in known_instance_ids:
+			continue
+		rows.append(
+			{
+				"provider": "Hetzner",
+				"cluster": cluster.name,
+				"instance_id": str(server.id),
+				"name": server.name,
+				"status": server.status,
+				"instance_type": server.server_type.name if server.server_type else None,
+				"region": cluster.region,
+			}
+		)
+	return rows
+
+
+def get_untracked_digital_ocean_droplets(cluster):
+	import pydo
+
+	api_token = cluster.get_password("digital_ocean_api_token", raise_exception=False)
+	if not api_token:
+		frappe.throw(f"DigitalOcean API token is not configured on Cluster {cluster.name}")
+
+	client = pydo.Client(token=api_token)
+	known_instance_ids = get_known_instance_ids("DigitalOcean", cluster.name)
+
+	rows = []
+	page = 1
+	while True:
+		droplets = client.droplets.list(per_page=DIGITAL_OCEAN_PAGE_SIZE, page=page).get("droplets", [])
+		for droplet in droplets:
+			if str(droplet["id"]) in known_instance_ids:
+				continue
+			rows.append(
+				{
+					"provider": "DigitalOcean",
+					"cluster": cluster.name,
+					"instance_id": str(droplet["id"]),
+					"name": droplet.get("name"),
+					"status": droplet.get("status"),
+					"instance_type": droplet.get("size_slug"),
+					"region": cluster.region,
+				}
+			)
+		if len(droplets) < DIGITAL_OCEAN_PAGE_SIZE:
+			break
+		page += 1
+	return rows
