3276bf16848b9d631d181594530c8332f4c863a5
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2026-08-25 15:41:26 +0530

    fix(test): Revert the amount due set zero

--- press/press/doctype/team/team.py
+++ press/press/doctype/team/team.py
@@ -1808,6 +1808,8 @@ def handle_payment_intent_succeeded(payment_intent):  # noqa: C901
 		status="Paid",
 		due_date=datetime.fromtimestamp(payment_intent["created"]),
 		total=amount,
+		amount_due=amount,
+		amount_due_with_tax=amount_with_tax,
 		gst=gst or 0,
 		amount_paid=amount_with_tax,
 		stripe_payment_intent_id=payment_intent["id"],
