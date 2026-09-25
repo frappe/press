4523416b144692a5852cd9f09bd13529aad85d31
Sabu Siyad <hello@ssiyad.com>
2026-09-25 15:38:03 +0530

    feat(support-access): Auto expire after 7 days
    (cherry picked from commit c801f1f4fe55d785f2ae2b65405f1624c3491644)

--- press/press/doctype/support_access/support_access.py
+++ press/press/doctype/support_access/support_access.py
@@ -11,6 +11,8 @@ from frappe.query_builder.functions import Count
 
 from press.utils import get_current_team
 
+PENDING_REQUEST_EXPIRY_DAYS = 7
+
 
 class SupportAccess(Document):
 	# begin: auto-generated types
@@ -33,7 +35,7 @@ class SupportAccess(Document):
 		resources: DF.Table[SupportAccessResource]
 		site_domains: DF.Check
 		site_release_group: DF.Check
-		status: DF.Literal["Pending", "Accepted", "Rejected", "Forfeited", "Revoked"]
+		status: DF.Literal["Pending", "Accepted", "Rejected", "Forfeited", "Revoked", "Expired"]
 		target_team: DF.Link | None
 	# end: auto-generated types
 
@@ -177,7 +179,7 @@ class SupportAccess(Document):
 		"""
 		current_team = get_current_team()
 		if self.flags.ignore_permissions:
-			return ["Pending", "Accepted", "Rejected", "Forfeited", "Revoked"]
+			return ["Pending", "Accepted", "Rejected", "Forfeited", "Revoked", "Expired"]
 		if self.target_team == current_team:
 			return ["Accepted", "Rejected", "Revoked"]
 		if self.requested_team == current_team:
@@ -189,11 +191,12 @@ class SupportAccess(Document):
 		Checks if status can be changed from `status_from` to `status_to`.
 		"""
 		return status_to in {
-			"Pending": ["Accepted", "Rejected"],
+			"Pending": ["Accepted", "Rejected", "Expired"],
 			"Accepted": ["Revoked", "Forfeited"],
 			"Rejected": [],
 			"Forfeited": [],
 			"Revoked": [],
+			"Expired": [],
 		}.get(status_from, [])
 
 	def validate_status_change(self):
@@ -324,6 +327,21 @@ class SupportAccess(Document):
 				"team": self.target_team,
 			},
 		)
+
+
+def expire_pending_requests():
+	frappe.db.set_value(
+		"Support Access",
+		{
+			"status": "Pending",
+			"creation": (
+				"<",
+				frappe.utils.add_to_date(frappe.utils.now_datetime(), days=-PENDING_REQUEST_EXPIRY_DAYS),
+			),
+		},
+		"status",
+		"Expired",
+	)
 
 
 def has_permission(doc, user=None, permission_type=None) -> bool:
